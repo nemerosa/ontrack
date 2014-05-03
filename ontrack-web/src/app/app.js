b0d397f6f3bbcb9b77c7a8339f498e4b47c19a6c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-05-03 19:58:44 +0200

    Web: minimal app.js

---
+++ ontrack-web/src/app/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var ontrack = angular.module('ontrack', [
+        'ui.bootstrap',
+        'ngSanitize'
+        // TODO Templates as JS
+    ])
+        // TODO HTTP configuration
+        // TODO Runs the initial security service (in case of refresh)
+        // TODO HTTP error interceptor
+        // TODO Routing configuration
+        // Main controller
+        .controller('AppCtrl', function () {
+        })
+    ;
