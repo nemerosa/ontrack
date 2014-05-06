aa6d88ac81555bc222379aadacb099bd7192557c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-05-06 08:34:46 +0200

    Resource as JSON: basic unit test

--- ontrack-boot/src/test/java/net/nemerosa/ontrack/boot/resource/ResourceTest.java
+++ ontrack-boot/src/test/java/net/nemerosa/ontrack/boot/resource/ResourceTest.java
@@ -1,14 +1,24 @@
 package net.nemerosa.ontrack.boot.resource;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import net.nemerosa.ontrack.json.JsonUtils;
 import net.nemerosa.ontrack.model.Branch;
+import net.nemerosa.ontrack.test.TestUtils;
 import org.junit.Test;
 
 public class ResourceTest {
 
     @Test
-    public void to_json_only_data() {
+    public void to_json_only_data() throws JsonProcessingException {
         Branch branch = new Branch("b", "B1", "Branche 1");
-
+        TestUtils.assertJsonWrite(
+                JsonUtils.object()
+                        .with("id", "b")
+                        .with("name", "B1")
+                        .with("description", "Branche 1")
+                        .end(),
+                Resource.of(branch)
+        );
     }
 
 }
---
+++ ontrack-test-utils/src/main/java/net/nemerosa/ontrack/test/TestUtils.java
@@ -0,0 +1,70 @@
+package net.nemerosa.ontrack.test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.nemerosa.ontrack.json.ObjectMapperFactory;
+import org.apache.commons.lang3.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+public final class TestUtils {
+
+    private static final ObjectMapper mapper = ObjectMapperFactory.create();
+
+    private TestUtils() {
+    }
+
+    public static String uid(String prefix) {
+        return prefix + new SimpleDateFormat("mmssSSS").format(new Date());
+    }
+
+    public static String getEnvIfPresent(String systemProperty, String envProperty, String defaulValue) {
+        // Trying with the system property first
+        String value = System.getProperty(systemProperty);
+        if (StringUtils.isNotBlank(value)) {
+            return value;
+        }
+        // Trying with the environment variable
+        value = System.getenv(envProperty);
+        if (StringUtils.isNotBlank(value)) {
+            return value;
+        }
+        // Not found
+        return defaulValue;
+    }
+
+    public static String getEnv(String systemProperty, String envProperty, String name) {
+        String value = getEnvIfPresent(systemProperty, envProperty, null);
+        if (StringUtils.isBlank(value)) {
+            throw new IllegalStateException(
+                    String.format(
+                            "The %s value must be defined with the system property `%s` or the environment variable `%s`.",
+                            name,
+                            systemProperty,
+                            envProperty
+                    )
+            );
+        } else {
+            return value;
+        }
+    }
+
+    public static void assertJsonWrite(JsonNode expectedJson, Object objectToWrite) throws JsonProcessingException {
+        assertEquals(
+                mapper.writeValueAsString(expectedJson),
+                mapper.writeValueAsString(objectToWrite)
+        );
+    }
+
+    public static <T> void assertJsonRead(T expectedResult, JsonNode jsonToRead, Class<T> type) throws JsonProcessingException {
+        assertEquals(
+                expectedResult,
+                mapper.treeToValue(jsonToRead, type)
+        );
+    }
+
+}
