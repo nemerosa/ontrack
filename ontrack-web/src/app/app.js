50067f58a61fdfab3c28b792ac89eddd27b4c07d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-05-13 10:28:23 +0200

    Home view - missing template

--- ontrack-web/src/app/app.js
+++ ontrack-web/src/app/app.js
@@ -1,12 +1,19 @@
 var ontrack = angular.module('ontrack', [
         'ui.bootstrap',
-        'ngSanitize'
+        'ui.router',
+        'ngSanitize',
         // TODO Templates as JS
+        // Views
+        'ot.view.home'
     ])
         // TODO HTTP configuration
         // TODO Runs the initial security service (in case of refresh)
         // TODO HTTP error interceptor
-        // TODO Routing configuration
+        // Routing configuration
+        .config(function ($stateProvider, $urlRouterProvider) {
+            // For any unmatched url, redirect to /state1
+            $urlRouterProvider.otherwise("/home");
+        })
         // Main controller
         .controller('AppCtrl', function () {
         })
