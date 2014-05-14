d64c828005cc1175b656433c1db8fe609c3c74a9
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-05-14 09:17:42 +0200

    Form: field binding

--- ontrack-web/src/app/dialog/dialog.form.js
+++ ontrack-web/src/app/dialog/dialog.form.js
@@ -2,6 +2,11 @@ angular.module('ot.dialog.form', [])
     .controller('otDialogForm', function ($scope, $modalInstance, config) {
         // General configuration
         $scope.config = config;
+        // Form data
+        $scope.data = {};
+        angular.forEach(config.form.fields, function (field) {
+            $scope.data[field.name] = field.value;
+        });
         // Cancelling the dialog
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
