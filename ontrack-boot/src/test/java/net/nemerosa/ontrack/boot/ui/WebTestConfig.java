cffcebac902f877e3e895dabd7fb3ca6fef68187
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-05-16 17:45:21 +0200

    Security: enables AOP for the tests

--- ontrack-boot/src/test/java/net/nemerosa/ontrack/boot/ui/WebTestConfig.java
+++ ontrack-boot/src/test/java/net/nemerosa/ontrack/boot/ui/WebTestConfig.java
@@ -2,10 +2,12 @@ package net.nemerosa.ontrack.boot.ui;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.convert.converter.ConverterRegistry;
 import org.springframework.core.convert.support.DefaultConversionService;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class WebTestConfig {
 
     @Bean
