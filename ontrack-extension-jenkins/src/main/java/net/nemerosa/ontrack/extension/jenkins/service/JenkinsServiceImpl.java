7e277f637c98e2bc6e968e2cf15ef77bee3f9fe1
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-05-21 21:00:37 +0200

    Jenkins: security checks

--- ontrack-extension-jenkins/src/main/java/net/nemerosa/ontrack/extension/jenkins/service/JenkinsServiceImpl.java
+++ ontrack-extension-jenkins/src/main/java/net/nemerosa/ontrack/extension/jenkins/service/JenkinsServiceImpl.java
@@ -4,6 +4,8 @@ import net.nemerosa.ontrack.extension.jenkins.model.JenkinsConfiguration;
 import net.nemerosa.ontrack.extension.jenkins.model.JenkinsConfigurationNotFoundException;
 import net.nemerosa.ontrack.extension.jenkins.model.JenkinsService;
 import net.nemerosa.ontrack.extension.jenkins.model.JenkinsSettings;
+import net.nemerosa.ontrack.model.security.GlobalSettings;
+import net.nemerosa.ontrack.model.security.SecurityService;
 import net.nemerosa.ontrack.model.support.ConfigurationRepository;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
@@ -16,14 +18,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class JenkinsServiceImpl implements JenkinsService {
 
     private final ConfigurationRepository configurationRepository;
+    private final SecurityService securityService;
 
     @Autowired
-    public JenkinsServiceImpl(ConfigurationRepository configurationRepository) {
+    public JenkinsServiceImpl(ConfigurationRepository configurationRepository, SecurityService securityService) {
         this.configurationRepository = configurationRepository;
+        this.securityService = securityService;
     }
 
     @Override
     public JenkinsSettings getSettings() {
+        securityService.checkGlobalFunction(GlobalSettings.class);
         return new JenkinsSettings(
                 configurationRepository.list(JenkinsConfiguration.class)
         );
@@ -31,11 +36,13 @@ public class JenkinsServiceImpl implements JenkinsService {
 
     @Override
     public JenkinsConfiguration newConfiguration(JenkinsConfiguration configuration) {
+        securityService.checkGlobalFunction(GlobalSettings.class);
         return configurationRepository.save(configuration);
     }
 
     @Override
     public JenkinsConfiguration getConfiguration(String name) {
+        securityService.checkGlobalFunction(GlobalSettings.class);
         return configurationRepository
                 .find(JenkinsConfiguration.class, name)
                 .orElseThrow(() -> new JenkinsConfigurationNotFoundException(name));
@@ -43,11 +50,13 @@ public class JenkinsServiceImpl implements JenkinsService {
 
     @Override
     public void deleteConfiguration(String name) {
+        securityService.checkGlobalFunction(GlobalSettings.class);
         configurationRepository.delete(JenkinsConfiguration.class, name);
     }
 
     @Override
     public void updateConfiguration(String name, JenkinsConfiguration configuration) {
+        securityService.checkGlobalFunction(GlobalSettings.class);
         Validate.isTrue(StringUtils.equals(name, configuration.getName()), "Configuration name must match");
         configurationRepository.save(configuration);
     }
