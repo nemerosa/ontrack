5d6cc6f6ea525997d724ab671702c81ab25fac4c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-05-24 21:10:08 +0200

    Form: date/time selection - correct order of sources

--- ontrack-web/Gruntfile.js
+++ ontrack-web/Gruntfile.js
@@ -6,6 +6,7 @@ module.exports = function (grunt) {
     var vendor = {
         js: [
             'vendor/jquery/dist/jquery.js',
+            'vendor/jquery-ui/ui/jquery-ui.js',
             'vendor/angular/angular.js',
             'vendor/angular-ui-router/release/angular-ui-router.js',
             'vendor/angular-sanitize/angular-sanitize.js',
