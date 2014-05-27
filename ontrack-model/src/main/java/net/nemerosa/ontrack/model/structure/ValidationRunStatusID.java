65ecc6c389e24cc26b9578bbc073b8a2579261e7
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-05-27 10:32:16 +0200

    Validation run status tree

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/structure/ValidationRunStatusID.java
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/structure/ValidationRunStatusID.java
@@ -2,9 +2,16 @@ package net.nemerosa.ontrack.model.structure;
 
 import lombok.Data;
 
+import java.util.Collection;
+
 @Data
 public class ValidationRunStatusID {
 
     private final String id;
+    private final Collection<ValidationRunStatusID> followingStatuses;
+
+    public boolean isRoot() {
+        return followingStatuses.isEmpty();
+    }
 
 }
