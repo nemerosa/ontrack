fd12d97545eb936798c6b99f2df8c29eb990de28
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-06-02 15:49:09 +0200

    Settings: URI for the security settings

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/ui/SettingsController.java
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/ui/SettingsController.java
@@ -2,24 +2,30 @@ package net.nemerosa.ontrack.boot.ui;
 
 import net.nemerosa.ontrack.extension.api.ExtensionManager;
 import net.nemerosa.ontrack.extension.api.GlobalSettingsExtension;
+import net.nemerosa.ontrack.model.Ack;
 import net.nemerosa.ontrack.model.form.DescribedForm;
 import net.nemerosa.ontrack.model.form.Form;
 import net.nemerosa.ontrack.model.form.YesNo;
+import net.nemerosa.ontrack.ui.controller.AbstractResourceController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
+
 /**
  * Global settings management.
  */
 @RestController
 @RequestMapping("/settings")
-public class SettingsController {
+public class SettingsController extends AbstractResourceController {
 
     private final ExtensionManager extensionManager;
 
@@ -46,7 +52,19 @@ public class SettingsController {
         return forms;
     }
 
+    /**
+     * Security
+     */
+    @RequestMapping(value = "security", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    // TODO Security settings
+    public Ack updateSecurity() {
+        return Ack.OK;
+    }
+
+
     private DescribedForm getSecuritySettingsForm() {
+        // TODO Gets the security settings
         return DescribedForm.create(
                 "security",
                 Form.create()
@@ -58,8 +76,8 @@ public class SettingsController {
                                                 // TODO Gets the value from the settings
                                         .value(false)
                         )
-        ).title("Security").description("Global settings for the security.");
-        // TODO URI
+        ).title("Security").description("Global settings for the security.")
+                .uri(uri(on(getClass()).updateSecurity()));
     }
 
 }
