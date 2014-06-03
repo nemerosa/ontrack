f2b7c923cac650d1aa217d86c70ff98571507282
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-06-03 23:50:10 +0200

    Resources: missing supported types for the resource http message converter

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/support/ResourceHttpMessageConverter.java
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/support/ResourceHttpMessageConverter.java
@@ -8,23 +8,28 @@ import net.nemerosa.ontrack.ui.controller.URIBuilder;
 import net.nemerosa.ontrack.ui.resource.*;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.AbstractHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
 
 public class ResourceHttpMessageConverter extends AbstractHttpMessageConverter<Object> {
 
+    public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+
     private final ResourceObjectMapper resourceObjectMapper;
-    @SuppressWarnings("UnusedDeclaration")
-    private final String jsonPrefix;
     private final ObjectMapper mapper;
 
     public ResourceHttpMessageConverter(URIBuilder uriBuilder, SecurityService securityService) {
-        jsonPrefix = "{} && ";
+        super(
+                new MediaType("application", "json", DEFAULT_CHARSET),
+                new MediaType("application", "*+json", DEFAULT_CHARSET)
+        );
         // Resource context
         ResourceContext resourceContext = new DefaultResourceContext(uriBuilder, securityService);
         // Registration of modules
