26b7b9aa3bc3d5b2866b9c58be61d154765ce271
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-06-05 14:17:07 +0200

    Properties: loading the editable properties

--- ontrack-web/src/app/directive/directive.properties.js
+++ ontrack-web/src/app/directive/directive.properties.js
@@ -1,13 +1,23 @@
 angular.module('ot.directive.properties', [
-
+    'ot.service.properties',
+    'ot.service.core'
 ])
-    .directive('otEntityProperties', function () {
+    .directive('otEntityProperties', function ($http, ot, otPropertiesService) {
         return {
             restrict: 'E',
             templateUrl: 'app/directive/directive.properties.tpl.html',
             scope: {
-                entity: '@',
-                entityId: '@'
+                entity: '='
+            },
+            link: function (scope) {
+                scope.$watch('entity', function () {
+                    if (scope.entity) {
+                        // Loading the editable properties
+                        ot.call($http.get(scope.entity._editableProperties)).then(function (propertyTypeDescriptors) {
+                            scope.propertyTypeDescriptors = propertyTypeDescriptors;
+                        });
+                    }
+                });
             }
         };
     })
---
+++ ontrack-web/src/app/service/service.properties.js
@@ -0,0 +1,9 @@
+angular.module('ot.service.properties', [
+    'ot.service.core'
+])
+    .service('otPropertiesService', function ($q, $http, ot) {
+        var self = {};
+
+        return self;
+    })
+;
