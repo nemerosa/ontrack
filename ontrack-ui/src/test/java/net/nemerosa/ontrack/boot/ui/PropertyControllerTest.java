8fca66e6dad60a86ba21254936593b28e556b671
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-06-05 12:56:44 +0200

    Properties: unit test for the property filtering

--- ontrack-ui/src/test/java/net/nemerosa/ontrack/boot/ui/PropertyControllerTest.java
+++ ontrack-ui/src/test/java/net/nemerosa/ontrack/boot/ui/PropertyControllerTest.java
@@ -2,13 +2,13 @@ package net.nemerosa.ontrack.boot.ui;
 
 import net.nemerosa.ontrack.model.security.ProjectConfig;
 import net.nemerosa.ontrack.model.security.ProjectCreation;
+import net.nemerosa.ontrack.model.security.ProjectView;
 import net.nemerosa.ontrack.model.structure.*;
 import net.nemerosa.ontrack.ui.resource.Resources;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Integration tests for the property controller.
@@ -38,6 +38,29 @@ public class PropertyControllerTest extends AbstractWebTestSupport {
         assertNotNull("Editable properties should not be null", properties);
         assertTrue(
                 "At least the Jenkins Job property should have been found",
+                properties.getResources().stream()
+                        .filter(p -> "Jenkins Job".equals(p.getName()))
+                        .findFirst().isPresent()
+        );
+    }
+
+    /**
+     * List of editable properties for a project, filter by authorization.
+     */
+    @Test
+    public void project_properties_filtered_by_authorization() throws Exception {
+        Project project = asUser().with(ProjectCreation.class).call(() -> structureService.newProject(
+                Project.of(nameDescription())
+        ));
+        Entity.isEntityDefined(project, "Project is defined");
+        // Gets the editable properties for this project
+        Resources<PropertyTypeDescriptor> properties = asUser().with(project.id(), ProjectView.class).call(() ->
+                        controller.getEditableProperties(ProjectEntityType.PROJECT, project.getId())
+        );
+        // Checks there is at least the Jenkins Job property
+        assertNotNull("Editable properties should not be null", properties);
+        assertFalse(
+                "The Jenkins Job property should not have been found since the user is not authorized",
                 properties.getResources().stream()
                         .filter(p -> "Jenkins Job".equals(p.getName()))
                         .findFirst().isPresent()
