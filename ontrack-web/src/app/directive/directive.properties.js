b4065f409c4fcf38cd267d98507773af0b0ace08
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-06-05 23:08:28 +0200

    Properties: transformed the edition of properties into an addition

--- ontrack-web/src/app/directive/directive.properties.js
+++ ontrack-web/src/app/directive/directive.properties.js
@@ -11,10 +11,16 @@ angular.module('ot.directive.properties', [
             },
             link: function (scope) {
 
-                // Loading the editable properties
+                // Loading the properties
                 function loadProperties() {
-                    ot.call($http.get(scope.entity._editableProperties)).then(function (propertyTypeDescriptors) {
-                        scope.propertyTypeDescriptors = propertyTypeDescriptors;
+                    ot.call($http.get(scope.entity._properties)).then(function (properties) {
+                        scope.properties = properties;
+                        // At least one property editable and not set?
+                        var addition = false;
+                        angular.forEach(properties.resources, function (property) {
+                            addition = addition || (property.editable && property.empty);
+                        });
+                        scope.addition = addition;
                     });
                 }
 
