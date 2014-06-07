87a1e75c775ff933201db68a18475530a162ceaa
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-06-07 20:52:12 +0200

    List of security functions

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/GlobalFunction.java
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/GlobalFunction.java
@@ -1,4 +1,10 @@
 package net.nemerosa.ontrack.model.security;
 
+/**
+ * Sub-interfaces define all functions available at application level.
+ * <p/>
+ * The list of available functions is available in the {@link SecurityFunctionsService} service
+ * and new functions can be contributed through extensions.
+ */
 public interface GlobalFunction {
 }
--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/ProjectFunction.java
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/ProjectFunction.java
@@ -1,4 +1,10 @@
 package net.nemerosa.ontrack.model.security;
 
+/**
+ * Sub-interfaces define all functions available in a project.
+ * <p/>
+ * The list of available functions is available in the {@link SecurityFunctionsService} service
+ * and new functions can be contributed through extensions.
+ */
 public interface ProjectFunction {
 }
