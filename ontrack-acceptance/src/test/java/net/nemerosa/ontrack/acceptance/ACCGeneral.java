2eeb67a2980a7fd6f510aa7eec1ea05969146808
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-06-11 10:00:51 +0200

    Acceptance - test skeleton

---
+++ ontrack-acceptance/src/test/java/net/nemerosa/ontrack/acceptance/ACCGeneral.java
@@ -0,0 +1,12 @@
+package net.nemerosa.ontrack.acceptance;
+
+import org.junit.Test;
+
+public class ACCGeneral extends AcceptanceSupport {
+
+    @Test
+    public void info_api() {
+        // TODO Gets the info API access
+    }
+
+}
---
+++ ontrack-acceptance/src/test/java/net/nemerosa/ontrack/acceptance/AcceptanceSupport.java
@@ -0,0 +1,8 @@
+package net.nemerosa.ontrack.acceptance;
+
+/**
+ * Class to be inherited in order to create an acceptance test with some support
+ * for the application fixtures.
+ */
+public abstract class AcceptanceSupport {
+}
