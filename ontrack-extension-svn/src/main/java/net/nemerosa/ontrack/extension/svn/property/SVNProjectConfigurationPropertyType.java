2ab38ed8e1d50f39df0ad19a90f31d7d29b42a2c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-06-19 10:30:09 +0200

    SVN change log: project SVN configuration property: edition

--- ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/property/SVNProjectConfigurationPropertyType.java
+++ ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/property/SVNProjectConfigurationPropertyType.java
@@ -1,7 +1,9 @@
 package net.nemerosa.ontrack.extension.svn.property;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import net.nemerosa.ontrack.common.MapBuilder;
 import net.nemerosa.ontrack.extension.support.AbstractPropertyType;
+import net.nemerosa.ontrack.extension.svn.SVNConfiguration;
 import net.nemerosa.ontrack.extension.svn.SVNConfigurationService;
 import net.nemerosa.ontrack.model.form.Form;
 import net.nemerosa.ontrack.model.form.Selection;
@@ -61,20 +63,38 @@ public class SVNProjectConfigurationPropertyType extends AbstractPropertyType<SV
                                 .label("Project path")
                                 .help("Path of the main project branch (trunk) in this configuration. The path is relative to the root " +
                                         "of the repository.")
-                                .validation(value != null ? value.getProjectPath() : "/trunk")
+                                .value(value != null ? value.getProjectPath() : "/trunk")
                 );
     }
 
     @Override
+    public JsonNode forStorage(SVNProjectConfigurationProperty value) {
+        return format(
+                MapBuilder.params()
+                        .with("configuration", value.getConfiguration().getName())
+                        .with("projectPath", value.getProjectPath())
+                        .get()
+        );
+    }
+
+    @Override
     public SVNProjectConfigurationProperty fromClient(JsonNode node) {
-        // FIXME Method net.nemerosa.ontrack.extension.svn.property.SVNProjectConfigurationPropertyType.fromClient
-        return null;
+        return fromStorage(node);
     }
 
     @Override
     public SVNProjectConfigurationProperty fromStorage(JsonNode node) {
-        // FIXME Method net.nemerosa.ontrack.extension.svn.property.SVNProjectConfigurationPropertyType.fromStorage
-        return null;
+        String configurationName = node.path("configuration").asText();
+        String projectPath = node.path("projectPath").asText();
+        // Looks the configuration up
+        SVNConfiguration configuration = configurationService.getObfuscatedConfiguration(configurationName);
+        // Validates the project path
+        validateNotBlank(projectPath, "The project path must not be empty");
+        // OK
+        return new SVNProjectConfigurationProperty(
+                configuration,
+                projectPath
+        );
     }
 
     @Override
