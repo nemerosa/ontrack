a76b36d4f3b0ab93499ddcec2651a70d97f54559
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-06-23 14:44:41 +0200

    SVN change log: page description

--- ontrack-web/src/app/extension/svn/svnChangeLog.js
+++ ontrack-web/src/app/extension/svn/svnChangeLog.js
@@ -1,6 +1,7 @@
 angular.module('ot.extension.svn.changelog', [
     'ui.router',
-    'ot.service.core'
+    'ot.service.core',
+    'ot.service.structure'
 ])
     .config(function ($stateProvider) {
         // SVN configurations
@@ -10,7 +11,7 @@ angular.module('ot.extension.svn.changelog', [
             controller: 'SVNChangeLogCtrl'
         });
     })
-    .controller('SVNChangeLogCtrl', function ($log, $location, $stateParams, $scope, $http, ot) {
+    .controller('SVNChangeLogCtrl', function ($q, $log, $interpolate, $location, $stateParams, $scope, $http, ot, otStructureService) {
 
         // The build request
         $scope.buildDiffRequest = {
@@ -25,7 +26,24 @@ angular.module('ot.extension.svn.changelog', [
         view.description = "Loading change log...";
 
         // Loading the branch
-        //view.breadcrumbs = ot.branchBreadcrumbs(build.branch);
+        otStructureService.getBranch($stateParams.branch).then(function (branch) {
+            view.breadcrumbs = ot.branchBreadcrumbs(branch);
+        });
+
+        // Loading the builds
+        $q.all([
+            otStructureService.getBuild($stateParams.from),
+            otStructureService.getBuild($stateParams.to)
+        ]).then(function (result) {
+            $scope.buildFrom = result[0];
+            $scope.buildTo = result[1];
+            // Adjusting the view description
+            view.description = $interpolate("From build {{buildFrom.name}} to {{buildTo.name}}")({
+                buildFrom: $scope.buildFrom,
+                buildTo: $scope.buildTo
+            });
+        });
+        // otStructureService.getBuild($stateParams.from).then(function (build1))
 
         /**
          * The REST end point to contact is contained by the current path, with the leading
