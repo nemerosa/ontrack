418fb6fde930ff0ba5252232ce0cbf34b740e4d6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-06-26 20:10:41 +0200

    Version: correction of the acceptance tests

--- ontrack-acceptance/src/test/java/net/nemerosa/ontrack/acceptance/ACCGeneral.java
+++ ontrack-acceptance/src/test/java/net/nemerosa/ontrack/acceptance/ACCGeneral.java
@@ -1,5 +1,6 @@
 package net.nemerosa.ontrack.acceptance;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
@@ -10,7 +11,12 @@ public class ACCGeneral extends AcceptanceSupport {
     @Test
     public void info_api() {
         anonymous().get("info").with(node -> {
-            assertTrue(StringUtils.isNotBlank(node.path("version").asText()));
+            JsonNode version = node.path("version");
+            assertTrue(StringUtils.isNotBlank(version.path("full").asText()));
+            assertTrue(StringUtils.isNotBlank(version.path("base").asText()));
+            assertTrue(StringUtils.isNotBlank(version.path("build").asText()));
+            assertTrue(StringUtils.isNotBlank(version.path("commit").asText()));
+            assertTrue(StringUtils.isNotBlank(version.path("source").asText()));
         });
     }
 
