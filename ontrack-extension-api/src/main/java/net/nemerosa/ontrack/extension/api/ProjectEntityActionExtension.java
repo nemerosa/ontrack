9b56331f81e7bc1e9ab06c8ced0858ae2d4d5a85
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-07-02 21:11:21 +0200

    #4 SVN sync action extension

---
+++ ontrack-extension-api/src/main/java/net/nemerosa/ontrack/extension/api/ProjectEntityActionExtension.java
@@ -0,0 +1,18 @@
+package net.nemerosa.ontrack.extension.api;
+
+import net.nemerosa.ontrack.model.security.Action;
+import net.nemerosa.ontrack.model.structure.ProjectEntity;
+
+/**
+ * Allows a {@link net.nemerosa.ontrack.model.structure.ProjectEntity} to get extended
+ * by custom actions.
+ */
+public interface ProjectEntityActionExtension {
+
+    /**
+     * Gets the action for this entity. Returns <code>null</code> if not
+     * applicable or not authorised.
+     */
+    Action getAction(ProjectEntity entity);
+
+}
