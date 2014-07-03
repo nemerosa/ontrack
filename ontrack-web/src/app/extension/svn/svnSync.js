2ca1645fd530385f84a686cc905232a59e817a9b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-07-03 10:26:07 +0200

    #4 Sync page: status check loop

--- ontrack-web/src/app/extension/svn/svnSync.js
+++ ontrack-web/src/app/extension/svn/svnSync.js
@@ -9,7 +9,7 @@ angular.module('ot.extension.svn.sync', [
             controller: 'SVNSyncCtrl'
         });
     })
-    .controller('SVNSyncCtrl', function ($stateParams, $scope, $http, $interpolate, ot) {
+    .controller('SVNSyncCtrl', function ($stateParams, $scope, $http, $interpolate, $interval, ot) {
 
         var branchId = $stateParams.branch;
         var view = ot.view();
@@ -18,6 +18,7 @@ angular.module('ot.extension.svn.sync', [
         ];
 
         $scope.synchronising = false;
+        $scope.synchronisingDone = false;
 
         // Loading of the sync information
         function loadSyncInfo() {
@@ -35,16 +36,43 @@ angular.module('ot.extension.svn.sync', [
         // Synchronisation
         $scope.sync = function () {
             $scope.synchronising = true;
+            $scope.synchronisingDone = false;
             // Launches the sync
             ot.pageCall($http.post($scope.syncInfo._self)).then(
-                function success(syncInfoId) {
-                    // TODO Gets the sync status regularly
+                function success(syncInfoStatus) {
+                    // Gets the sync status regularly
+                    checkSyncStatus(syncInfoStatus);
                     // TODO Displays a message at the end of the sync
                     // TODO Put synchronising to false at the end
                 }, function error() {
                     $scope.synchronising = false;
                 });
         };
+
+        // Checking the status
+        function checkSyncStatus(syncInfoStatus) {
+            $scope.syncInfoStatus = syncInfoStatus;
+            if (syncInfoStatus.finished) {
+                $scope.synchronising = false;
+                $scope.synchronisingDone = true;
+            } else {
+                // TODO Displays the already created builds
+                // Go on with checking the status
+                $interval(goCheckSyncStatus, 3000, 1);
+            }
+        }
+
+        // Fetches and checks the last status
+        function goCheckSyncStatus() {
+            ot.pageCall($http.get($scope.syncInfoStatus._self)).then(
+                function success(syncInfoStatus) {
+                    checkSyncStatus(syncInfoStatus);
+                },
+                function error() {
+                    $scope.synchronising = false;
+                }
+            );
+        }
 
     })
 ;
