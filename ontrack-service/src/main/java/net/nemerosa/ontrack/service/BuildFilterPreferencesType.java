7fd719f2ddb52a134f6650403367fca3f537040f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-07-11 11:03:35 +0200

    Build filter: loading the storage

--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/BuildFilterPreferencesType.java
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/BuildFilterPreferencesType.java
@@ -4,7 +4,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.nemerosa.ontrack.json.ObjectMapperFactory;
 import net.nemerosa.ontrack.model.structure.PreferencesType;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 @Component
 public class BuildFilterPreferencesType implements PreferencesType<BuildFilterPreferences> {
@@ -13,8 +18,37 @@ public class BuildFilterPreferencesType implements PreferencesType<BuildFilterPr
 
     @Override
     public BuildFilterPreferences fromStorage(JsonNode node) {
-        // FIXME Method net.nemerosa.ontrack.service.BuildFilterPreferencesType.fromStorage
-        return null;
+        BuildFilterPreferences preferences = BuildFilterPreferences.empty();
+        Iterator<Map.Entry<String, JsonNode>> entries = node.path("entries").fields();
+        while (entries.hasNext()) {
+            Map.Entry<String, JsonNode> entry = entries.next();
+            int branchId = Integer.parseInt(entry.getKey(), 10);
+            Iterator<Map.Entry<String, JsonNode>> branchEntries = entry.getValue().fields();
+            while (branchEntries.hasNext()) {
+                Map.Entry<String, JsonNode> branchEntry = branchEntries.next();
+                String name = branchEntry.getKey();
+                JsonNode branchEntryData = branchEntry.getValue();
+                String type = branchEntryData.path("type").asText();
+                if (StringUtils.isNotBlank(type)) {
+                    Map<String, String> data = new HashMap<>();
+                    Iterator<Map.Entry<String, JsonNode>> dataEntries = branchEntryData.path("data").fields();
+                    while (dataEntries.hasNext()) {
+                        Map.Entry<String, JsonNode> dataEntry = dataEntries.next();
+                        data.put(dataEntry.getKey(), dataEntry.getValue().asText());
+                    }
+                    preferences = preferences.add(
+                            branchId,
+                            new BuildFilterPreferencesEntry(
+                                    name,
+                                    type,
+                                    data
+                            )
+                    );
+                }
+            }
+        }
+        // OK
+        return preferences;
     }
 
     @Override
--- ontrack-service/src/test/java/net/nemerosa/ontrack/service/BuildFilterPreferencesTypeTest.java
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/BuildFilterPreferencesTypeTest.java
@@ -1,17 +1,37 @@
 package net.nemerosa.ontrack.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import net.nemerosa.ontrack.common.MapBuilder;
 import org.junit.Test;
 
 import static net.nemerosa.ontrack.json.JsonUtils.object;
 import static net.nemerosa.ontrack.test.TestUtils.assertJsonEquals;
+import static org.junit.Assert.assertEquals;
 
 public class BuildFilterPreferencesTypeTest {
 
     @Test
     public void forStorage() throws Exception {
         BuildFilterPreferencesType type = new BuildFilterPreferencesType();
+        JsonNode node = type.forStorage(preferences());
+        assertJsonEquals(
+                json(),
+                node
+        );
+    }
+
+    @Test
+    public void fromStorage() throws Exception {
+        BuildFilterPreferencesType type = new BuildFilterPreferencesType();
+        BuildFilterPreferences preferences = type.fromStorage(json());
+        assertEquals(
+                preferences(),
+                preferences
+        );
+    }
+
+    private static BuildFilterPreferences preferences() {
         BuildFilterPreferences preferences = BuildFilterPreferences.empty();
         preferences = preferences.add(1, new BuildFilterPreferencesEntry(
                 "Copper since bronze",
@@ -32,39 +52,39 @@ public class BuildFilterPreferencesTypeTest {
                 MapBuilder.of("withPromotionLevel", "COPPER")
                         .get()
         ));
-        JsonNode node = type.forStorage(preferences);
-        assertJsonEquals(
-                object()
-                        .with("entries", object()
-                                .with("1", object()
-                                        .with("Copper since bronze", object()
-                                                .with("name", "Copper since bronze")
-                                                .with("type", "net.nemerosa.ontrack.service.StandardBuildFilterProvider")
-                                                .with("data", object()
-                                                        .with("withPromotionLevel", "COPPER")
-                                                        .with("sincePromotionLevel", "BRONZE")
-                                                        .end())
-                                                .end())
-                                        .with("Only copper", object()
-                                                .with("name", "Only copper")
-                                                .with("type", "net.nemerosa.ontrack.service.StandardBuildFilterProvider")
-                                                .with("data", object()
-                                                        .with("withPromotionLevel", "COPPER")
-                                                        .end())
+        return preferences;
+    }
+
+    private static ObjectNode json() {
+        return object()
+                .with("entries", object()
+                        .with("1", object()
+                                .with("Copper since bronze", object()
+                                        .with("name", "Copper since bronze")
+                                        .with("type", "net.nemerosa.ontrack.service.StandardBuildFilterProvider")
+                                        .with("data", object()
+                                                .with("withPromotionLevel", "COPPER")
+                                                .with("sincePromotionLevel", "BRONZE")
                                                 .end())
                                         .end())
-                                .with("2", object()
-                                        .with("Only copper", object()
-                                                .with("name", "Only copper")
-                                                .with("type", "net.nemerosa.ontrack.service.StandardBuildFilterProvider")
-                                                .with("data", object()
-                                                        .with("withPromotionLevel", "COPPER")
-                                                        .end())
+                                .with("Only copper", object()
+                                        .with("name", "Only copper")
+                                        .with("type", "net.nemerosa.ontrack.service.StandardBuildFilterProvider")
+                                        .with("data", object()
+                                                .with("withPromotionLevel", "COPPER")
                                                 .end())
                                         .end())
                                 .end())
-                        .end(),
-                node
-        );
+                        .with("2", object()
+                                .with("Only copper", object()
+                                        .with("name", "Only copper")
+                                        .with("type", "net.nemerosa.ontrack.service.StandardBuildFilterProvider")
+                                        .with("data", object()
+                                                .with("withPromotionLevel", "COPPER")
+                                                .end())
+                                        .end())
+                                .end())
+                        .end())
+                .end();
     }
 }
