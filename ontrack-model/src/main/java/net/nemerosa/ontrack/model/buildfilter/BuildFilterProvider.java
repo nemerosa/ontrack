b9dd02eee36695422f74ad93015a886a29e98761
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-07-12 22:02:35 +0200

    Removed deprecated methods

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/buildfilter/BuildFilterProvider.java
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/buildfilter/BuildFilterProvider.java
@@ -2,9 +2,7 @@ package net.nemerosa.ontrack.model.buildfilter;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import net.nemerosa.ontrack.model.structure.ID;
-import org.apache.commons.lang3.StringUtils;
 
-import java.util.Map;
 import java.util.Optional;
 
 public interface BuildFilterProvider<T> {
@@ -32,20 +30,5 @@ public interface BuildFilterProvider<T> {
     BuildFilter filter(ID branchId, T data);
 
     Optional<T> parse(JsonNode data);
-
-    @Deprecated
-    public static String getParameter(Map<String, String> params, String name) {
-        return params.get(name);
-    }
-
-    @Deprecated
-    public static int getIntParameter(Map<String, String> params, String name, int defaultValue) {
-        String value = getParameter(params, name);
-        if (StringUtils.isNotBlank(value) && StringUtils.isNumeric(value)) {
-            return Integer.parseInt(value, 10);
-        } else {
-            return defaultValue;
-        }
-    }
 
 }
