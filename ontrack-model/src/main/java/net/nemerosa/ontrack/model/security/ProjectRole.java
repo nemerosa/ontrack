fe0d6d8b2370b6163fd8b46259597b7924e18b4e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-07-14 09:31:53 +0200

    Project and global role

---
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/ProjectRole.java
@@ -0,0 +1,29 @@
+package net.nemerosa.ontrack.model.security;
+
+import lombok.Data;
+
+import java.util.Set;
+
+/**
+ * A project role is the association between an identifier, a name and a set of
+ * {@linkplain net.nemerosa.ontrack.model.security.ProjectFunction project functions}.
+ */
+@Data
+public class ProjectRole {
+
+    /**
+     * Project role's identifier
+     */
+    private final String id;
+
+    /**
+     * Project role's name
+     */
+    private final String name;
+
+    /**
+     * Associated set of project functions
+     */
+    private final Set<? extends ProjectFunction> functions;
+
+}
