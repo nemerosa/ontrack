c17d33908150383cba4ab0d951a53042b6605d83
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-07-20 12:49:29 +0200

    Entity information extension - added doc

--- ontrack-extension-api/src/main/java/net/nemerosa/ontrack/extension/api/EntityInformationExtension.java
+++ ontrack-extension-api/src/main/java/net/nemerosa/ontrack/extension/api/EntityInformationExtension.java
@@ -7,6 +7,10 @@ import java.util.Optional;
 /**
  * Defines an extension that can provide additional content to an
  * {@linkplain net.nemerosa.ontrack.model.structure.ProjectEntity entity}.
+ * <p>
+ * This extension returns some arbitrary information (any JSON-serialisable object)
+ * and will be rendered at client side using a template based on the extension
+ * qualified class name.
  */
 public interface EntityInformationExtension extends Extension {
 
