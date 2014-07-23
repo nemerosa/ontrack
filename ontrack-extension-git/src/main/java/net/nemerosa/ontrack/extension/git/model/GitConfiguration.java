47137054e55001c02b42d2f2455e86599de1bee0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-07-23 11:01:56 +0200

    Git: controller and forms

--- ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/model/GitConfiguration.java
+++ ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/model/GitConfiguration.java
@@ -1,8 +1,14 @@
 package net.nemerosa.ontrack.extension.git.model;
 
 import lombok.Data;
+import net.nemerosa.ontrack.extension.issues.model.IssueServiceConfigurationRepresentation;
 import net.nemerosa.ontrack.extension.support.configurations.UserPasswordConfiguration;
+import net.nemerosa.ontrack.model.form.*;
 import net.nemerosa.ontrack.model.support.ConfigurationDescriptor;
+
+import java.util.List;
+
+import static net.nemerosa.ontrack.model.form.Form.defaultNameField;
 
 @Data
 public class GitConfiguration implements UserPasswordConfiguration<GitConfiguration> {
@@ -70,5 +76,71 @@ public class GitConfiguration implements UserPasswordConfiguration<GitConfigurat
                 indexationInterval,
                 issueServiceConfigurationIdentifier
         );
+    }
+
+    public static Form form(List<IssueServiceConfigurationRepresentation> availableIssueServiceConfigurations) {
+        return Form.create()
+                .with(defaultNameField())
+                .with(
+                        Text.of("remote")
+                                .label("Remote")
+                                .help("Remote path to the source repository")
+                )
+                .with(
+                        Text.of("user")
+                                .label("User")
+                                .length(16)
+                                .optional()
+                )
+                .with(
+                        Password.of("password")
+                                .label("Password")
+                                .length(40)
+                                .optional()
+                )
+                .with(
+                        Text.of("commitLink")
+                                .label("Commit link")
+                                .length(250)
+                                .optional()
+                                .help("Link to a commit, using {commit} as placeholder")
+                )
+                .with(
+                        Text.of("fileAtCommitLink")
+                                .label("File at commit link")
+                                .length(250)
+                                .optional()
+                                .help("Link to a file at a given commit, using {commit} and {path} as placeholders")
+                )
+                .with(
+                        Int.of("indexationInterval")
+                                .label("Indexation interval")
+                                .min(0)
+                                .max(60 * 24)
+                                .value(0)
+                                .help("Interval (in minutes) between each indexation of the Git repository. A " +
+                                        "zero value indicates that no indexation must take place automatically and they " +
+                                        "have to be triggered manually.")
+                )
+                .with(
+                        Selection.of("issueServiceConfigurationIdentifier")
+                                .label("Issue configuration")
+                                .help("Select an issue service that is sued to associate tickets and issues to the source.")
+                                .optional()
+                                .items(availableIssueServiceConfigurations)
+                );
+    }
+
+    public Form asForm(List<IssueServiceConfigurationRepresentation> availableIssueServiceConfigurations) {
+        return form(availableIssueServiceConfigurations)
+                .with(defaultNameField().readOnly().value(name))
+                .fill("remote", remote)
+                .fill("user", user)
+                .fill("password", "")
+                .fill("commitLink", commitLink)
+                .fill("fileAtCommitLink", fileAtCommitLink)
+                .fill("indexationInterval", indexationInterval)
+                .fill("issueServiceConfigurationIdentifier", issueServiceConfigurationIdentifier)
+                ;
     }
 }
