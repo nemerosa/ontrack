4833ab05c6a7ce7885877c1c4321ae6219f5ddc3
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-07-23 11:02:50 +0200

    Git: user menu extension

---
+++ ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/GitUserMenuExtension.java
@@ -0,0 +1,28 @@
+package net.nemerosa.ontrack.extension.git;
+
+import net.nemerosa.ontrack.extension.api.UserMenuExtension;
+import net.nemerosa.ontrack.extension.support.AbstractExtension;
+import net.nemerosa.ontrack.model.security.Action;
+import net.nemerosa.ontrack.model.security.GlobalFunction;
+import net.nemerosa.ontrack.model.security.GlobalSettings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GitUserMenuExtension extends AbstractExtension implements UserMenuExtension {
+
+    @Autowired
+    public GitUserMenuExtension(GitExtensionFeature feature) {
+        super(feature);
+    }
+
+    @Override
+    public Class<? extends GlobalFunction> getGlobalFunction() {
+        return GlobalSettings.class;
+    }
+
+    @Override
+    public Action getAction() {
+        return Action.of("git-configurations", "Git configurations", "configurations");
+    }
+}
