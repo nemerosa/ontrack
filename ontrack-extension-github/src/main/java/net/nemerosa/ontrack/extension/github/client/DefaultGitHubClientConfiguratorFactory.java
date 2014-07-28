2c0d1a149beafe66d650cc0d8940584d8252a8da
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-07-28 17:02:17 +0200

    GitHub: client configuration with OAuth2token (wip)

--- ontrack-extension-github/src/main/java/net/nemerosa/ontrack/extension/github/client/DefaultGitHubClientConfiguratorFactory.java
+++ ontrack-extension-github/src/main/java/net/nemerosa/ontrack/extension/github/client/DefaultGitHubClientConfiguratorFactory.java
@@ -1,6 +1,9 @@
 package net.nemerosa.ontrack.extension.github.client;
 
+import net.nemerosa.ontrack.extension.github.property.GitHubProjectConfigurationProperty;
+import net.nemerosa.ontrack.extension.github.property.GitHubProjectConfigurationPropertyType;
 import net.nemerosa.ontrack.model.structure.Branch;
+import net.nemerosa.ontrack.model.structure.Property;
 import net.nemerosa.ontrack.model.structure.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,18 +20,18 @@ public class DefaultGitHubClientConfiguratorFactory implements GitHubClientConfi
 
     @Override
     public GitHubClientConfigurator getGitHubConfigurator(Branch branch) {
-        // TODO GitHubProjectProperty
-        return null;
-        // Gets the authentication parameters
-//        final String authentication = propertiesService.getPropertyValue(Entity.PROJECT, projectId, GitHubExtension.EXTENSION, GitHubAuthenticationProperty.NAME);
-        // Configurator
-//        return new GitHubClientConfigurator() {
-//            @Override
-//            public void configure(GitHubClient client) {
-//                if (StringUtils.isNotBlank(authentication)) {
-//                    client.setOAuth2Token(authentication);
+        Property<GitHubProjectConfigurationProperty> property = propertyService.getProperty(branch.getProject(), GitHubProjectConfigurationPropertyType.class);
+        if (property.isEmpty()) {
+            return client -> {
+            };
+        } else {
+            return client -> {
+                // TODO OAuth2Token
+//                String oAuth2Token = property.getValue().getConfiguration().getOAuth2Token();
+//                if (StringUtils.isNotBlank(oAuth2Token)) {
+//                    client.setOAuth2Token(oAuth2Token);
 //                }
-//            }
-//        };
+            };
+        }
     }
 }
