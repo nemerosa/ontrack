32b8baaed60c362acf4f585edd7abeaeb76ad948
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-07-28 19:10:12 +0200

    GitHub: oauth2 token taken into account

--- ontrack-extension-github/src/main/java/net/nemerosa/ontrack/extension/github/service/GitHubConfigurator.java
+++ ontrack-extension-github/src/main/java/net/nemerosa/ontrack/extension/github/service/GitHubConfigurator.java
@@ -8,6 +8,7 @@ import net.nemerosa.ontrack.extension.github.property.GitHubProjectConfiguration
 import net.nemerosa.ontrack.model.structure.Branch;
 import net.nemerosa.ontrack.model.structure.Property;
 import net.nemerosa.ontrack.model.structure.PropertyService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -29,15 +30,26 @@ public class GitHubConfigurator implements GitConfigurator {
             // GitHub configuration
             GitHubConfiguration gitHub = projectConfig.getValue().getConfiguration();
             // Merge the project configuration
-            return configuration
+            GitConfiguration gitHubConfig = configuration
                     .withRemote(gitHub.getRemote())
-                    .withUser(gitHub.getUser())
-                    .withPassword(gitHub.getPassword())
                     .withIndexationInterval(gitHub.getIndexationInterval())
                     .withCommitLink(gitHub.getCommitLink())
                     .withFileAtCommitLink(gitHub.getFileAtCommitLink())
-                    // TODO .withIssueServiceConfigurationIdentifier("")
-                    ;
+                    .withIssueServiceConfigurationIdentifier(gitHub.toIdentifier().format());
+            // User / password
+            String oAuth2Token = gitHub.getOAuth2Token();
+            String user = gitHub.getUser();
+            if (StringUtils.isNotBlank(oAuth2Token)) {
+                gitHubConfig = gitHubConfig
+                        .withUser(oAuth2Token)
+                        .withPassword("x-oauth-basic");
+            } else if (StringUtils.isNotBlank(user)) {
+                gitHubConfig = gitHubConfig
+                        .withUser(gitHub.getUser())
+                        .withPassword(gitHub.getPassword());
+            }
+            // OK
+            return gitHubConfig;
         } else {
             return configuration;
         }
