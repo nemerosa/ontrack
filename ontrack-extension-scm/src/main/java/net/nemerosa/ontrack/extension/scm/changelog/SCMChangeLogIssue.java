a0f52e82d12ddc94dc24908a258a6d1cbfed670e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-07-29 11:52:12 +0200

    #10 SCM change log issue validation list in the issue

--- ontrack-extension-scm/src/main/java/net/nemerosa/ontrack/extension/scm/changelog/SCMChangeLogIssue.java
+++ ontrack-extension-scm/src/main/java/net/nemerosa/ontrack/extension/scm/changelog/SCMChangeLogIssue.java
@@ -3,9 +3,17 @@ package net.nemerosa.ontrack.extension.scm.changelog;
 import lombok.Data;
 import net.nemerosa.ontrack.extension.issues.model.Issue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public abstract class SCMChangeLogIssue {
 
     private final Issue issue;
+    private final List<SCMChangeLogIssueValidation> validations = new ArrayList<>();
+
+    public void addValidations(List<SCMChangeLogIssueValidation> validations) {
+        this.validations.addAll(validations);
+    }
 
 }
