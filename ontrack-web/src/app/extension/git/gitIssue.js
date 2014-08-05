c5fa76d5395d0bb6aa09ebe64146cd8558806560
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-08-05 21:16:19 +0200

    #17 Git issue: call

--- ontrack-web/src/app/extension/git/gitIssue.js
+++ ontrack-web/src/app/extension/git/gitIssue.js
@@ -11,16 +11,13 @@ angular.module('ot.extension.git.issue', [
     })
     .controller('GitIssueCtrl', function ($stateParams, $scope, $http, $interpolate, ot) {
 
-        var configuration = $stateParams.configuration;
-        var issue = $stateParams.issue;
-
         var view = ot.view();
 
-//        ot.call(
-//            $http.get(
-//                $interpolate('extension/svn/configuration/{{configuration}}/issue/{{issue}}')($stateParams)
-//            )).then(function (ontrackSVNIssueInfo) {
-//                $scope.ontrackSVNIssueInfo = ontrackSVNIssueInfo;
-//            });
+        ot.call(
+            $http.get(
+                $interpolate('extension/git/{{branch}}/issue/{{issue}}')($stateParams)
+            )).then(function (ontrackGitIssueInfo) {
+                $scope.ontrackGitIssueInfo = ontrackGitIssueInfo;
+            });
     })
 ;
