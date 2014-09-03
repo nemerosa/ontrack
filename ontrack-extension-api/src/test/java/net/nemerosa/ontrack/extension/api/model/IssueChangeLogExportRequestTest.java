b3a22c1300cbad20ec20e14d6639834afcf570e4
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-09-03 09:02:25 +0200

    Export specification and test

---
+++ ontrack-extension-api/src/test/java/net/nemerosa/ontrack/extension/api/model/IssueChangeLogExportRequestTest.java
@@ -0,0 +1,56 @@
+package net.nemerosa.ontrack.extension.api.model;
+
+import com.google.common.collect.Sets;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class IssueChangeLogExportRequestTest {
+
+    @Test
+    public void empty() {
+        IssueChangeLogExportRequest request = new IssueChangeLogExportRequest();
+        Map<String, Set<String>> spec = request.getGroupingSpecification();
+        assertTrue(spec.isEmpty());
+    }
+
+    @Test
+    public void one_group() {
+        IssueChangeLogExportRequest request = new IssueChangeLogExportRequest();
+        request.setGrouping("Bugs=bug");
+        Map<String, Set<String>> spec = request.getGroupingSpecification();
+        assertEquals(1, spec.size());
+        assertEquals(Sets.newHashSet("bug"), spec.get("Bugs"));
+    }
+
+    @Test
+    public void one_group_with_several_values() {
+        IssueChangeLogExportRequest request = new IssueChangeLogExportRequest();
+        request.setGrouping("Features=feature,enhancement");
+        Map<String, Set<String>> spec = request.getGroupingSpecification();
+        assertEquals(1, spec.size());
+        assertEquals(Sets.newHashSet("feature", "enhancement"), spec.get("Features"));
+    }
+
+    @Test
+    public void two_groups() {
+        IssueChangeLogExportRequest request = new IssueChangeLogExportRequest();
+        request.setGrouping("Bugs=bug|Features=feature,enhancement");
+        Map<String, Set<String>> spec = request.getGroupingSpecification();
+        assertEquals(2, spec.size());
+        assertEquals(Sets.newHashSet("bug"), spec.get("Bugs"));
+        assertEquals(Sets.newHashSet("feature", "enhancement"), spec.get("Features"));
+    }
+
+    @Test(expected = ExportRequestGroupingFormatException.class)
+    public void group_format_exception_equal_sign() {
+        IssueChangeLogExportRequest request = new IssueChangeLogExportRequest();
+        request.setGrouping("Bugs=bug=test");
+        request.getGroupingSpecification();
+    }
+
+}
