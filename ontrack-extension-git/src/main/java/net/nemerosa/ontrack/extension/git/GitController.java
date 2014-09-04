da6c19fcc124c111257a04db763d4cb0fad91ad5
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-09-04 17:23:44 +0200

    #31 List of formats (Git config)

--- ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/GitController.java
+++ ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/GitController.java
@@ -9,8 +9,9 @@ import net.nemerosa.ontrack.extension.git.model.*;
 import net.nemerosa.ontrack.extension.git.service.GitConfigurationService;
 import net.nemerosa.ontrack.extension.git.service.GitService;
 import net.nemerosa.ontrack.extension.issues.IssueServiceRegistry;
-import net.nemerosa.ontrack.extension.issues.model.ConfiguredIssueService;
+import net.nemerosa.ontrack.extension.issues.export.ExportFormat;
 import net.nemerosa.ontrack.extension.issues.export.ExportedIssues;
+import net.nemerosa.ontrack.extension.issues.model.ConfiguredIssueService;
 import net.nemerosa.ontrack.extension.issues.model.Issue;
 import net.nemerosa.ontrack.extension.scm.model.SCMChangeLogIssue;
 import net.nemerosa.ontrack.extension.scm.model.SCMChangeLogUUIDException;
@@ -20,8 +21,10 @@ import net.nemerosa.ontrack.model.buildfilter.BuildDiff;
 import net.nemerosa.ontrack.model.form.Form;
 import net.nemerosa.ontrack.model.security.GlobalSettings;
 import net.nemerosa.ontrack.model.security.SecurityService;
+import net.nemerosa.ontrack.model.structure.Branch;
 import net.nemerosa.ontrack.model.structure.Build;
 import net.nemerosa.ontrack.model.structure.ID;
+import net.nemerosa.ontrack.model.structure.StructureService;
 import net.nemerosa.ontrack.ui.resource.Link;
 import net.nemerosa.ontrack.ui.resource.Resource;
 import net.nemerosa.ontrack.ui.resource.Resources;
@@ -33,6 +36,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -43,6 +47,7 @@ import static org.springframework.web.servlet.mvc.method.annotation.MvcUriCompon
 @RequestMapping("extension/git")
 public class GitController extends AbstractExtensionController<GitExtensionFeature> {
 
+    private final StructureService structureService;
     private final GitService gitService;
     private final GitConfigurationService configurationService;
     private final IssueServiceRegistry issueServiceRegistry;
@@ -52,11 +57,13 @@ public class GitController extends AbstractExtensionController<GitExtensionFeatu
 
     @Autowired
     public GitController(GitExtensionFeature feature,
+                         StructureService structureService,
                          GitService gitService,
                          GitConfigurationService configurationService,
                          IssueServiceRegistry issueServiceRegistry,
                          SecurityService securityService) {
         super(feature);
+        this.structureService = structureService;
         this.gitService = gitService;
         this.configurationService = configurationService;
         this.issueServiceRegistry = issueServiceRegistry;
@@ -161,6 +168,31 @@ public class GitController extends AbstractExtensionController<GitExtensionFeatu
         logCache.put(changeLog.getUuid(), changeLog);
         // OK
         return changeLog;
+    }
+
+    /**
+     * Change log export, list of formats
+     */
+    @RequestMapping(value = "changelog/export/{branchId}/formats", method = RequestMethod.GET)
+    public Resources<ExportFormat> changeLogExportFormats(@PathVariable ID branchId) {
+        // Gets the branch
+        Branch branch = structureService.getBranch(branchId);
+        // Gets the configuration for the branch
+        GitConfiguration configuration = gitService.getBranchConfiguration(branch);
+        if (configuration.isValid()) {
+            ConfiguredIssueService configuredIssueService = issueServiceRegistry.getConfiguredIssueService(configuration.getIssueServiceConfigurationIdentifier());
+            if (configuredIssueService != null) {
+                return Resources.of(
+                        configuredIssueService.getIssueServiceExtension().exportFormats(),
+                        uri(on(GitController.class).changeLogExportFormats(branchId))
+                );
+            }
+        }
+        // No configured issues
+        return Resources.of(
+                Collections.emptyList(),
+                uri(on(GitController.class).changeLogExportFormats(branchId))
+        );
     }
 
     /**
