9b01ccdd146db3d5bc7a611c596d549c666747b1
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-09-16 12:53:29 +0200

    #59 Displays the list of promotion runs for a build

--- ontrack-web/src/app/dialog/dialog.promotionRuns.js
+++ ontrack-web/src/app/dialog/dialog.promotionRuns.js
@@ -1,9 +1,13 @@
 angular.module('ot.dialog.promotionRuns', [
-    'ot.service.structure'
+    'ot.service.core'
 ])
-    .controller('otDialogPromotionRuns', function ($scope, $modalInstance, config, otStructureService) {
+    .controller('otDialogPromotionRuns', function ($scope, $modalInstance, $http, config, ot) {
         // General configuration
         $scope.config = config;
+        // Loading all the promotion runs
+        ot.call($http.get(config.uri)).then(function (promotionRuns) {
+            $scope.promotionRuns = promotionRuns.resources;
+        });
         // Cancelling the dialog
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
