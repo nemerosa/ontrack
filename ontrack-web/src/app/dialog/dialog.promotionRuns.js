34f1c9a64935e732343cf721586b399d8194e34c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-09-16 14:09:48 +0200

    #59 Calling the deletion

--- ontrack-web/src/app/dialog/dialog.promotionRuns.js
+++ ontrack-web/src/app/dialog/dialog.promotionRuns.js
@@ -5,12 +5,21 @@ angular.module('ot.dialog.promotionRuns', [
         // General configuration
         $scope.config = config;
         // Loading all the promotion runs
-        ot.call($http.get(config.uri)).then(function (promotionRuns) {
-            $scope.promotionRuns = promotionRuns.resources;
-        });
+        function loadRuns() {
+            ot.call($http.get(config.uri)).then(function (promotionRuns) {
+                $scope.promotionRuns = promotionRuns.resources;
+            });
+        }
+
+        loadRuns();
         // Cancelling the dialog
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
+        };
+
+        // Deleting a promotion run
+        $scope.deletePromotionRun = function (promotionRun) {
+            ot.call($http.delete(promotionRun._delete)).then(loadRuns);
         };
     })
 ;
