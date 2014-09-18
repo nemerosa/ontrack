3b6bb3b52b64ed0a032c5e9b19eaf823a2711f5b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-09-18 21:01:24 +0200

    #39 Tests for the copy of promotion levels

--- ontrack-service/src/test/java/net/nemerosa/ontrack/service/CopyServiceImplTest.java
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/CopyServiceImplTest.java
@@ -1,14 +1,29 @@
 package net.nemerosa.ontrack.service;
 
-import net.nemerosa.ontrack.model.structure.Replacement;
+import net.nemerosa.ontrack.model.security.SecurityService;
+import net.nemerosa.ontrack.model.structure.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Optional;
 
+import static net.nemerosa.ontrack.model.structure.NameDescription.nd;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class CopyServiceImplTest {
+
+    private CopyServiceImpl service;
+    private StructureService structureService;
+
+    @Before
+    public void before() {
+        structureService = mock(StructureService.class);
+        SecurityService securityService = mock(SecurityService.class);
+        service = new CopyServiceImpl(structureService, securityService);
+    }
 
     @Test
     public void applyReplacements_none() {
@@ -28,5 +43,36 @@ public class CopyServiceImplTest {
                 new Replacement("trunk", "branches/11.7"),
                 new Replacement("Pipeline", "Release pipeline")
         )));
+    }
+
+    @Test
+    public void doCopyPromotionLevels() {
+        Branch sourceBranch = Branch.of(Project.of(nd("P1", "")).withId(ID.of(1)), nd("B1", "")).withId(ID.of(1));
+        Branch targetBranch = Branch.of(Project.of(nd("P2", "")).withId(ID.of(2)), nd("B2", "")).withId(ID.of(2));
+        // Request
+        BranchCopyRequest request = new BranchCopyRequest(
+                ID.of(1),
+                Collections.emptyList(),
+                Arrays.asList(
+                        new Replacement("P1", "P2")
+                ),
+                Collections.emptyList()
+        );
+        // Promotion levels for source
+        when(structureService.getPromotionLevelListForBranch(ID.of(1))).thenReturn(
+                Arrays.asList(
+                        PromotionLevel.of(sourceBranch, nd("copper", "Copper level for P1"))
+                )
+        );
+        when(structureService.findPromotionLevelByName("P2", "B2", "copper")).thenReturn(Optional.empty());
+        // Copy
+        service.doCopyPromotionLevels(sourceBranch, targetBranch, request);
+        // Checks that the promotion level is created for the target branch
+        verify(structureService, times(1)).newPromotionLevel(
+                PromotionLevel.of(
+                        targetBranch,
+                        nd("copper", "Copper level for P2")
+                )
+        );
     }
 }
