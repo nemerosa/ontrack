6da84fa8c85419aab7ca50339693150b0850a476
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-09-22 13:07:39 +0200

    #33 GUI: event rendering (wip)

--- ontrack-web/src/app/service/service.event.js
+++ ontrack-web/src/app/service/service.event.js
@@ -1,13 +1,44 @@
 angular.module('ot.service.event', [
 
 ])
-    .service('otEventService', function () {
+    .service('otEventService', function ($interpolate) {
 
+        var variableRegex = /\$\{([:a-zA-Z_]+)\}/g;
         var self = {};
 
         self.renderEvent = function (event) {
-            return event.template;
+
+            return event.template.replace(
+                variableRegex,
+                replacementFunction(event)
+            );
         };
+
+
+        function replacementFunction(event) {
+            return function (match, expression) {
+                if (expression.charAt(0) == ':') {
+                    // We want a value
+                    var valueKey = expression.substring(1);
+                } else {
+                    // We want an entity reference
+                    var entity = event.entities[expression];
+                    if (!entity) {
+                        return replacementEntityError(event, expression);
+                    }
+                    // Link definition
+                    var link = {
+                        cls: ''
+                    };
+                    if (expression == 'PROJECT') {
+                        link.uri = "#/project/" + entity.id;
+                        link.name = entity.name;
+                    }
+                    // Link rendering
+                    return $interpolate('<a href="{{uri}}" class="{{cls}}">{{name}}</a>')(link);
+                }
+            };
+        }
 
         return self;
 
