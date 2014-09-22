ccc01b26d4b351a2bb98d63472f8cdd6b3829c1b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-09-22 13:11:22 +0200

    #33 GUI: event rendering: values and entities

--- ontrack-web/src/app/service/service.event.js
+++ ontrack-web/src/app/service/service.event.js
@@ -20,6 +20,15 @@ angular.module('ot.service.event', [
                 if (expression.charAt(0) == ':') {
                     // We want a value
                     var valueKey = expression.substring(1);
+                    var value = event.values[valueKey];
+                    if (!value) {
+                        return replacementValueError(event, valueKey);
+                    }
+                    // Rendering
+                    return $interpolate('<span class="ot-event-value">{{value}}</span>')({
+                        valueKey: valueKey,
+                        value: value
+                    });
                 } else {
                     // We want an entity reference
                     var entity = event.entities[expression];
@@ -33,6 +42,26 @@ angular.module('ot.service.event', [
                     if (expression == 'PROJECT') {
                         link.uri = "#/project/" + entity.id;
                         link.name = entity.name;
+                    }
+                    else if (expression == 'BRANCH') {
+                        link.uri = "#/branch/" + entity.id;
+                        link.name = entity.name;
+                    }
+                    else if (expression == 'BUILD') {
+                        link.uri = "#/build/" + entity.id;
+                        link.name = entity.name;
+                    }
+                    else if (expression == 'PROMOTION_LEVEL') {
+                        link.uri = "#/promotionLevel/" + entity.id;
+                        link.name = entity.name;
+                    }
+                    else if (expression == 'VALIDATION_STAMP') {
+                        link.uri = "#/validationStamp/" + entity.id;
+                        link.name = entity.name;
+                    }
+                    else if (expression == 'VALIDATION_RUN') {
+                        link.uri = "#/validationRun/" + entity.id;
+                        link.name = '#' + entity.runOrder;
                     }
                     // Link rendering
                     return $interpolate('<a href="{{uri}}" class="{{cls}}">{{name}}</a>')(link);
