35b08f1edfed0dc5205415d117b462ed99658fc5
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-09-23 18:52:57 +0200

    #33 Property event at client side

--- ontrack-web/src/app/service/service.event.js
+++ ontrack-web/src/app/service/service.event.js
@@ -14,67 +14,74 @@ angular.module('ot.service.event', [
             );
         };
 
-
         function replacementFunction(event) {
             return function (match, expression) {
+                var entity;
                 if (expression.charAt(0) == ':') {
                     // We want a value
                     var valueKey = expression.substring(1);
                     var value = event.values[valueKey];
                     if (!value) {
-                        return replacementValueError(event, valueKey);
+                        return "#ERROR:" + valueKey;
                     }
                     // Rendering
                     return $interpolate('<span class="ot-event-value">{{value.value}}</span>')({
                         valueKey: valueKey,
                         value: value
                     });
+                } else if (expression == 'REF') {
+                    if (event.ref) {
+                        entity = event.entities[event.ref];
+                        if (!entity) {
+                            return "#ERROR:REF:" + event.ref;
+                        } else {
+                            return renderEntity(event.ref, entity);
+                        }
+                    } else {
+                        return "#ERROR:REF";
+                    }
                 } else {
                     // We want an entity reference
-                    var entity = event.entities[expression];
+                    entity = event.entities[expression];
                     if (!entity) {
-                        return replacementEntityError(event, expression);
+                        return "#ERROR:" + expression;
                     }
-                    // Link definition
-                    var link = {
-                        cls: ''
-                    };
-                    if (expression == 'PROJECT') {
-                        link.uri = "#/project/" + entity.id;
-                        link.name = entity.name;
-                    }
-                    else if (expression == 'BRANCH') {
-                        link.uri = "#/branch/" + entity.id;
-                        link.name = entity.name;
-                    }
-                    else if (expression == 'BUILD') {
-                        link.uri = "#/build/" + entity.id;
-                        link.name = entity.name;
-                    }
-                    else if (expression == 'PROMOTION_LEVEL') {
-                        link.uri = "#/promotionLevel/" + entity.id;
-                        link.name = entity.name;
-                    }
-                    else if (expression == 'VALIDATION_STAMP') {
-                        link.uri = "#/validationStamp/" + entity.id;
-                        link.name = entity.name;
-                    }
-                    else if (expression == 'VALIDATION_RUN') {
-                        link.uri = "#/validationRun/" + entity.id;
-                        link.name = '#' + entity.runOrder;
-                    }
-                    // Link rendering
-                    return $interpolate('<a href="{{uri}}" class="{{cls}}">{{name}}</a>')(link);
+                    return renderEntity(expression, entity);
                 }
             };
         }
 
-        function replacementEntityError (event, expression) {
-            return "#ERROR";
-        }
-
-        function replacementValueError (event, key) {
-            return "#ERROR";
+        function renderEntity(expression, entity) {
+            // Link definition
+            var link = {
+                cls: ''
+            };
+            if (expression == 'PROJECT') {
+                link.uri = "#/project/" + entity.id;
+                link.name = entity.name;
+            }
+            else if (expression == 'BRANCH') {
+                link.uri = "#/branch/" + entity.id;
+                link.name = entity.name;
+            }
+            else if (expression == 'BUILD') {
+                link.uri = "#/build/" + entity.id;
+                link.name = entity.name;
+            }
+            else if (expression == 'PROMOTION_LEVEL') {
+                link.uri = "#/promotionLevel/" + entity.id;
+                link.name = entity.name;
+            }
+            else if (expression == 'VALIDATION_STAMP') {
+                link.uri = "#/validationStamp/" + entity.id;
+                link.name = entity.name;
+            }
+            else if (expression == 'VALIDATION_RUN') {
+                link.uri = "#/validationRun/" + entity.id;
+                link.name = '#' + entity.runOrder;
+            }
+            // Link rendering
+            return $interpolate('<a href="{{uri}}" class="{{cls}}">{{name}}</a>')(link);
         }
 
         return self;
