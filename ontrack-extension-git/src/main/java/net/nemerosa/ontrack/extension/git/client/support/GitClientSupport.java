e0257cd5a9676632b11bda4de785873b4cfee119
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-09-23 20:58:51 +0200

    Removed obsolete TODOs

--- ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/client/support/GitClientSupport.java
+++ ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/client/support/GitClientSupport.java
@@ -1,13 +1,11 @@
 package net.nemerosa.ontrack.extension.git.client.support;
 
-import org.apache.commons.io.IOUtils;
+import net.nemerosa.ontrack.common.Utils;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+import static net.nemerosa.ontrack.common.Utils.run;
 
 /**
  * Utility classes that use the Git command line when the JGit client is not enough.
@@ -27,56 +25,7 @@ public class GitClientSupport {
      * @return List of tag names
      */
     public static List<String> tagContains(File wd, String commitId) {
-        return asList(run(wd, "git", "tag", "--contains", commitId));
-    }
-
-    /**
-     * Splits the output in several lines.
-     * <p>
-     * TODO This code should be moved in a utility class in <code>ontrack-common</code>.
-     *
-     * @param output Output received from the command.
-     * @return Lines. This can be empty but not null.
-     */
-    private static List<String> asList(String output) {
-        try {
-            return IOUtils.readLines(new StringReader(output));
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot get lines", e);
-        }
-    }
-
-    /**
-     * Runs a command in the <code>wd</code> directory and returns its output. In case of error (exit
-     * code different than 0), an exception is thrown.
-     * <p>
-     * TODO This code should be moved in a utility class in <code>ontrack-common</code>.
-     *
-     * @param wd   Directory where to execute the command
-     * @param cmd  Command to execute
-     * @param args Command parameters
-     * @return Output of the command
-     */
-    private static String run(File wd, String cmd, String... args) {
-        // Complete list of arguments
-        List<String> list = new ArrayList<>();
-        list.add(cmd);
-        list.addAll(Arrays.asList(args));
-        try {
-            // Builds a process
-            Process process = new ProcessBuilder(list).directory(wd).start();
-            // Running the process and waiting for its completion
-            int exit = process.waitFor();
-            // In case of error
-            if (exit != 0) {
-                String error = IOUtils.toString(process.getErrorStream());
-                throw new RuntimeException(error);
-            } else {
-                return IOUtils.toString(process.getInputStream());
-            }
-        } catch (IOException | InterruptedException ex) {
-            throw new RuntimeException("Error while executing " + cmd + " command", ex);
-        }
+        return Utils.asList(run(wd, "git", "tag", "--contains", commitId));
     }
 
 }
