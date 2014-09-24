85f8f3671e08a3e65ac03a5761a4441dca575164
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-09-24 08:33:02 +0200

    #77 Injection of the test configuration service

--- ontrack-service/src/test/java/net/nemerosa/ontrack/service/support/configuration/ConfigurationServiceIT.java
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/support/configuration/ConfigurationServiceIT.java
@@ -2,8 +2,12 @@ package net.nemerosa.ontrack.service.support.configuration;
 
 import net.nemerosa.ontrack.it.AbstractITTestSupport;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ConfigurationServiceIT extends AbstractITTestSupport {
+
+    @Autowired
+    private TestConfigurationService configurationService;
 
     @Test
     public void encryptedPassword() {
