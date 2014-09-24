1b218ec54d2a87ea10d0320811c129ac83398143
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-09-24 20:21:22 +0200

    #77 More logging

--- ontrack-extension-support/src/main/java/net/nemerosa/ontrack/extension/support/configurations/EncryptedConfigurationMigrationAction.java
+++ ontrack-extension-support/src/main/java/net/nemerosa/ontrack/extension/support/configurations/EncryptedConfigurationMigrationAction.java
@@ -2,6 +2,10 @@ package net.nemerosa.ontrack.extension.support.configurations;
 
 import net.nemerosa.ontrack.model.support.ConfigurationRepository;
 import net.nemerosa.ontrack.model.support.DBMigrationAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
 import java.util.Collection;
@@ -10,14 +14,20 @@ import java.util.List;
 /**
  * Migration for the configurations, so they can be encrypted
  */
+@Component
 public class EncryptedConfigurationMigrationAction implements DBMigrationAction {
+
+    private final Logger logger = LoggerFactory.getLogger(EncryptedConfigurationMigrationAction.class);
 
     private final Collection<ConfigurationService<?>> configurationServices;
     private final ConfigurationRepository configurationRepository;
+    private final EncryptionService encryptionService;
 
-    protected EncryptedConfigurationMigrationAction(Collection<ConfigurationService<?>> configurationServices, ConfigurationRepository configurationRepository) {
+    @Autowired
+    protected EncryptedConfigurationMigrationAction(Collection<ConfigurationService<?>> configurationServices, ConfigurationRepository configurationRepository, EncryptionService encryptionService) {
         this.configurationServices = configurationServices;
         this.configurationRepository = configurationRepository;
+        this.encryptionService = encryptionService;
     }
 
     @Override
@@ -34,13 +44,20 @@ public class EncryptedConfigurationMigrationAction implements DBMigrationAction 
     }
 
     private <T extends UserPasswordConfiguration> void migrate(ConfigurationService<T> configurationService) {
+        logger.info("Encrypting configurations for: {}", configurationService.getClass().getName());
         // Class for the configuration
         Class<T> configurationType = configurationService.getConfigurationType();
         // Gets all configurations for this type
         List<T> configurations = configurationRepository.list(configurationType);
         // Saves them again to force encryption
         for (T configuration : configurations) {
-            configurationService.updateConfiguration(configuration.getName(), configuration);
+            logger.info("Encrypting configuration: {}", configuration.getDescriptor().getName());
+            UserPasswordConfiguration migratedConfig = configuration.withPassword(
+                    encryptionService.encrypt(
+                            configuration.getPassword()
+                    )
+            );
+            configurationRepository.save(migratedConfig);
         }
     }
 
