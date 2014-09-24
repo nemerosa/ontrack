254535fbf2a9d7832c2a3d1eaba49c0e56bea6c3
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-09-24 08:45:58 +0200

    #77 Integration test for the encryption

--- ontrack-service/src/test/java/net/nemerosa/ontrack/service/support/configuration/ConfigurationServiceIT.java
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/support/configuration/ConfigurationServiceIT.java
@@ -1,17 +1,48 @@
 package net.nemerosa.ontrack.service.support.configuration;
 
-import net.nemerosa.ontrack.it.AbstractITTestSupport;
+import net.nemerosa.ontrack.it.AbstractServiceTestSupport;
+import net.nemerosa.ontrack.model.security.GlobalSettings;
+import net.nemerosa.ontrack.model.support.ConfigurationRepository;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class ConfigurationServiceIT extends AbstractITTestSupport {
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+public class ConfigurationServiceIT extends AbstractServiceTestSupport {
+
+    public static final String PLAIN_PASSWORD = "verysecret";
+    public static final String CONFIG_NAME = "test";
 
     @Autowired
     private TestConfigurationService configurationService;
 
-    @Test
-    public void encryptedPassword() {
+    @Autowired
+    private ConfigurationRepository configurationRepository;
 
+    @Test
+    public void encryptedPasswordForNewConfig() throws Exception {
+        // Creates a new configuration, with a password we want to keep secret
+        TestConfiguration configuration = new TestConfiguration(CONFIG_NAME, "user", PLAIN_PASSWORD);
+        asUser().with(GlobalSettings.class).call(() -> {
+            // Saves this configuration in the database
+            TestConfiguration savedConfiguration = configurationService.newConfiguration(configuration);
+            // The returned password is not encrypted (we may need it)
+            assertEquals(PLAIN_PASSWORD, savedConfiguration.getPassword());
+            // Loads the configuration
+            TestConfiguration loadedConfiguration = configurationService.getConfiguration(CONFIG_NAME);
+            assertEquals(PLAIN_PASSWORD, loadedConfiguration.getPassword());
+            // Now, checks the raw result in the repository
+            Optional<TestConfiguration> rawConfiguration = configurationRepository.find(TestConfiguration.class, CONFIG_NAME);
+            assertTrue(rawConfiguration.isPresent());
+            assertNotEquals("Password must be encrypted", PLAIN_PASSWORD, rawConfiguration.get().getPassword());
+            // End of test
+            return true;
+        });
     }
+
+    // TODO Checks the migration process if possible?
+    // TODO Migration of existing configuration through DBInit?
 
 }
