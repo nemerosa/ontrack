2dea6a9a39613b7fecae5611d2dd7699419ea7fb
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-09-30 13:59:26 +0200

    #47 Async call to get the decorations

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/ui/DecorationsController.java
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/ui/DecorationsController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Callable;
+
 import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
 
 @RestController
@@ -24,12 +26,10 @@ public class DecorationsController extends AbstractProjectEntityController {
 
     /**
      * Decorations for an entity.
-     * <p/>
-     * TODO Makes this call asynchronous since some decorations might take a long time to be resolved
      */
     @RequestMapping(value = "{entityType}/{id}", method = RequestMethod.GET)
-    public Resources<Decoration> getDecorations(@PathVariable ProjectEntityType entityType, @PathVariable ID id) {
-        return Resources.of(
+    public Callable<Resources<Decoration>> getDecorations(@PathVariable ProjectEntityType entityType, @PathVariable ID id) {
+        return () -> Resources.of(
                 decorationService.getDecorations(getEntity(entityType, id)),
                 uri(on(getClass()).getDecorations(entityType, id))
         );
