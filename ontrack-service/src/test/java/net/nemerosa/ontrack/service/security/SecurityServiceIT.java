29752e874ba6f19dd0108e72c53879e88ae3edec
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-09-30 13:48:56 +0200

    #47 Integration test for the runner

--- ontrack-service/src/test/java/net/nemerosa/ontrack/service/security/SecurityServiceIT.java
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/security/SecurityServiceIT.java
@@ -2,12 +2,16 @@ package net.nemerosa.ontrack.service.security;
 
 import net.nemerosa.ontrack.it.AbstractServiceTestSupport;
 import net.nemerosa.ontrack.model.security.Account;
+import net.nemerosa.ontrack.model.security.ProjectCreation;
 import net.nemerosa.ontrack.model.security.SecurityService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import java.util.function.Function;
+
+import static org.junit.Assert.*;
 
 public class SecurityServiceIT extends AbstractServiceTestSupport {
 
@@ -24,6 +28,23 @@ public class SecurityServiceIT extends AbstractServiceTestSupport {
     public void getCurrentAccount_none() throws Exception {
         Account account = securityService.getCurrentAccount();
         assertNull(account);
+    }
+
+    @Test
+    public void runner_function() throws Exception {
+        // Function that needs an authentication context
+        Function<String, String> fn = (s) -> s + " -> " + getContextName();
+        // Testing outside a context
+        assertEquals("test -> none", fn.apply("test"));
+        // With a context
+        Function<String, String> securedFn = asUser().with(ProjectCreation.class).call(() -> securityService.runner(fn));
+        // Calls the secured function
+        assertEquals("test -> TestingAuthenticationToken", securedFn.apply("test"));
+    }
+
+    private static String getContextName() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null ? authentication.getClass().getSimpleName() : "none";
     }
 
 }
