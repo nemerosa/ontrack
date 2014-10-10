8b1fc1b69c8d055d5faf03868b5b6eb32a3054d2
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-10-10 20:41:52 +0200

    #40 Adding template definition parameters (button)

--- ontrack-web/src/app/dialog/dialog.template.definition.js
+++ ontrack-web/src/app/dialog/dialog.template.definition.js
@@ -6,6 +6,20 @@ angular.module('ot.dialog.template.definition', [
         // Inject the configuration into the scope
         $scope.templateDefinition = templateDefinition;
         $scope.config = config;
+        // Preparing the data
+        $scope.data = otFormService.prepareForDisplay(templateDefinition);
+
+        // Adding a new parameter
+        $scope.addParameter = function () {
+            if (!$scope.data.parameters) {
+                $scope.data.parameters = [];
+            }
+            $scope.data.parameters.push({
+                name: '',
+                description: ''
+            });
+        };
+
         // Cancelling the dialog
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
