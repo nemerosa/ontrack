23a311517abb7403ead8d09d174e12946a101b08
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-10-10 18:01:22 +0200

    #40 Fixed list synchronisation source

---
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/support/template/FixedListTemplateSynchronisationSource.java
@@ -0,0 +1,54 @@
+package net.nemerosa.ontrack.service.support.template;
+
+import net.nemerosa.ontrack.model.form.Form;
+import net.nemerosa.ontrack.model.form.MultiStrings;
+import net.nemerosa.ontrack.model.structure.Project;
+import net.nemerosa.ontrack.model.support.AbstractTemplateSynchronisationSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class FixedListTemplateSynchronisationSource extends AbstractTemplateSynchronisationSource<FixedListTemplateSynchronisationSourceConfig> {
+
+    protected FixedListTemplateSynchronisationSource() {
+        super(FixedListTemplateSynchronisationSourceConfig.class);
+    }
+
+    @Override
+    public String getId() {
+        return "fixed";
+    }
+
+    @Override
+    public String getName() {
+        return "Fixed list of names";
+    }
+
+    @Override
+    public boolean isApplicable(Project project) {
+        return true;
+    }
+
+    @Override
+    public Form getForm(Project project, FixedListTemplateSynchronisationSourceConfig config) {
+        return Form.create()
+                .with(
+                        MultiStrings.of("names")
+                                .help("Fixed list of names")
+                                .label("Names")
+                );
+    }
+
+    @Override
+    public List<String> getBranchNames(Project project, FixedListTemplateSynchronisationSourceConfig config) {
+        return config.getNames();
+    }
+
+    @Override
+    public FixedListTemplateSynchronisationSourceConfig getDefaultConfig(Project project) {
+        return new FixedListTemplateSynchronisationSourceConfig(Collections.emptyList());
+    }
+
+}
