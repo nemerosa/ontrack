02bc3eef2027cf3be845cc22071f50f636ca0e45
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-10-12 21:22:42 +0200

    #40 Template definition - unit tests

---
+++ ontrack-service/src/test/groovy/net/nemerosa/ontrack/service/support/template/BranchTemplateServiceImplTest.groovy
@@ -0,0 +1,65 @@
+package net.nemerosa.ontrack.service.support.template
+
+import net.nemerosa.ontrack.json.JsonUtils
+import net.nemerosa.ontrack.model.exceptions.BranchTemplateHasBuildException
+import net.nemerosa.ontrack.model.exceptions.BranchTemplateInstanceException
+import net.nemerosa.ontrack.model.security.SecurityService
+import net.nemerosa.ontrack.model.structure.*
+import org.junit.Before
+import org.junit.Test
+
+import static org.mockito.Mockito.mock
+import static org.mockito.Mockito.when
+
+class BranchTemplateServiceImplTest {
+
+    private BranchTemplateService service
+    private StructureService structureService
+    private SecurityService securityService
+
+    @Before
+    void before() {
+        structureService = mock(StructureService)
+        securityService = mock(SecurityService)
+        service = new BranchTemplateServiceImpl(
+                structureService,
+                securityService
+        )
+    }
+
+    @Test(expected = BranchTemplateInstanceException)
+    void 'Cannot make a template instance into a definition'() {
+        Branch branch = Branch.of(
+                Project.of(NameDescription.nd('P', "Project")),
+                NameDescription.nd('B', "Branch")
+        ).withType(BranchType.TEMPLATE_INSTANCE)
+        when(structureService.getBranch(ID.of(1))).thenReturn(branch)
+
+        service.setTemplateDefinition(ID.of(1), new TemplateDefinition(
+                [],
+                '',
+                JsonUtils.object().end(),
+                TemplateSynchronisationAbsencePolicy.DISABLE,
+                0
+        ))
+    }
+
+    @Test(expected = BranchTemplateHasBuildException)
+    void 'Cannot make a template definition when builds are present'() {
+        Branch branch = Branch.of(
+                Project.of(NameDescription.nd('P', "Project")),
+                NameDescription.nd('B', "Branch")
+        )
+        when(structureService.getBranch(ID.of(1))).thenReturn(branch)
+        when(structureService.getBuildCount(branch)).thenReturn(1)
+
+        service.setTemplateDefinition(ID.of(1), new TemplateDefinition(
+                [],
+                '',
+                JsonUtils.object().end(),
+                TemplateSynchronisationAbsencePolicy.DISABLE,
+                0
+        ))
+    }
+
+}
