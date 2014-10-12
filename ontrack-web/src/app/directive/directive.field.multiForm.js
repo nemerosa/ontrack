2c0e07c95dcf2cc2cb98a4986e6f8f21cfd9c0c7
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-10-12 17:51:53 +0200

    #40 MultiForm - nesting forms through an additional technical directive (see http://sporto.github.io/blog/2013/06/24/nested-recursive-directives-in-angular/)

--- ontrack-web/src/app/directive/directive.field.multiForm.js
+++ ontrack-web/src/app/directive/directive.field.multiForm.js
@@ -1,7 +1,7 @@
 angular.module('ot.directive.field.multiForm', [
-
+    'ot.service.form'
 ])
-    .directive('otFieldMultiForm', function () {
+    .directive('otFieldMultiForm', function (otFormService) {
         return {
             restrict: 'E',
             templateUrl: 'app/directive/directive.field.multiForm.tpl.html',
@@ -21,15 +21,35 @@ angular.module('ot.directive.field.multiForm', [
                     var form = $scope.field.form;
                     // Duplicates the form
                     var entryForm = angular.copy(form);
+                    // Data
+                    var entryData = otFormService.prepareForDisplay(entryForm);
                     // Entry
                     var entry = {
                         form: entryForm,
-                        data: {}
+                        data: entryData
                     };
                     // Adds to the entries
                     $scope.formEntries.push(entry);
                 };
 
+            }
+        };
+    })
+    .directive('otFieldMultiFormEntry', function ($compile) {
+        return {
+            restrict: 'E',
+            template: '<div></div>',
+            scope: {
+                form: '=',
+                data: '=',
+                formRoot: '='
+            },
+            link: function (scope, element) {
+                if (angular.isDefined(scope.form)) {
+                    $compile('<ot-form form="form" data="data" form-root="formRoot"></ot-form>')(scope, function (cloned, scope) {
+                        element.append(cloned);
+                    });
+                }
             }
         };
     })
