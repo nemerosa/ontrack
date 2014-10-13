a11a7cf5c8525c0e1be04ea0230e46c1285b0fef
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-10-13 19:56:03 +0200

    #40 Service configurator field - displaying the form

--- ontrack-web/src/app/directive/directive.field.serviceConfigurator.js
+++ ontrack-web/src/app/directive/directive.field.serviceConfigurator.js
@@ -14,9 +14,57 @@ angular.module('ot.directive.field.serviceConfigurator', [
 
                 // Loading of the form on selection
                 $scope.$watch('sourceId', function (sourceId) {
-                    console.log('sourceId=', sourceId);
+                    selectSourceId(sourceId);
                 });
 
+                function selectSourceId(sourceId) {
+                    var source = getSource(sourceId);
+                    if (source) {
+                        // Clones the form
+                        var form = angular.copy(source.form);
+                        // Prepares the data
+                        var data = otFormService.prepareForDisplay(form);
+                        // Sets the entry
+                        $scope.formEntry = {
+                            form: form,
+                            data: data
+                        };
+                    } else {
+                        delete $scope.formEntry;
+                    }
+                }
+
+                function getSource(sourceId) {
+                    // Gets the source definition
+                    var filteredSources = $scope.field.sources.filter(function (s) {
+                        return s.id == sourceId;
+                    });
+                    if (filteredSources.length) {
+                        return filteredSources[0];
+                    }
+                }
+
+            }
+        };
+    })
+    .directive('otFieldServiceConfiguratorForm', function ($compile) {
+        return {
+            restrict: 'E',
+            template: '<div></div>',
+            scope: {
+                form: '=',
+                data: '=',
+                formRoot: '='
+            },
+            link: function (scope, element) {
+                scope.$watch('form', function () {
+                    if (angular.isDefined(scope.form)) {
+                        $compile('<ot-form form="form" data="data" form-root="formRoot"></ot-form>')(scope, function (cloned, scope) {
+                            element.empty();
+                            element.append(cloned);
+                        });
+                    }
+                });
             }
         };
     })
