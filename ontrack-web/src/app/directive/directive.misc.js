8163c239662ad6d1a6a350c996d0f6e1902631e6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-10-22 21:55:25 +0200

    #112 Enter key for OK buttons in confirmation alerts

--- ontrack-web/src/app/directive/directive.misc.js
+++ ontrack-web/src/app/directive/directive.misc.js
@@ -50,4 +50,16 @@ angular.module('ot.directive.misc', [
             }
         };
     })
+    .directive('otEnter', function () {
+        return function (scope, element, attrs) {
+            element.bind('keydown keypress', function (event) {
+                if (event.which === 13) {
+                    scope.$apply(function () {
+                        scope.$eval(attrs.otEnter);
+                    });
+                    event.preventDefault();
+                }
+            });
+        };
+    })
 ;
