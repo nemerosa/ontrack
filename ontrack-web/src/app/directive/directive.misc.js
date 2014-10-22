04e7efec0cb687a3105c3bde8c3d4d6d89982c71
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-10-22 21:56:43 +0200

    #112 No need for a directive

--- ontrack-web/src/app/directive/directive.misc.js
+++ ontrack-web/src/app/directive/directive.misc.js
@@ -50,16 +50,4 @@ angular.module('ot.directive.misc', [
             }
         };
     })
-    .directive('otEnter', function () {
-        return function (scope, element, attrs) {
-            element.bind('keydown keypress', function (event) {
-                if (event.which === 13) {
-                    scope.$apply(function () {
-                        scope.$eval(attrs.otEnter);
-                    });
-                    event.preventDefault();
-                }
-            });
-        };
-    })
 ;
