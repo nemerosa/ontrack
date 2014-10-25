c3165aa964b56dd0a7bb5199e62271c8dfa8f727
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-10-25 14:35:21 +0200

    #107 Downloading ontrack (hard coded for the moment)

--- ontrack-delivery/vagrant/README.md
+++ ontrack-delivery/vagrant/README.md
@@ -27,6 +27,7 @@ To rerun the provisioning:
 * [x] Running puppet apply
 * [x] Installing Java
 * [ ] Installing ontrack
+* [ ] Script to start ontrack on boot
 * [ ] Passing parameters to download ontrack
 * [ ] Open port in Vagrant
 * [ ] Installing nginx as proxy
--- ontrack-delivery/vagrant/manifests/default.pp
+++ ontrack-delivery/vagrant/manifests/default.pp
@@ -13,7 +13,7 @@ apt::ppa { "ppa:webupd8team/java": }
 
 exec { 'apt-get update':
   command => '/usr/bin/apt-get update',
-  before => Apt::Ppa["ppa:webupd8team/java"],
+  before  => Apt::Ppa["ppa:webupd8team/java"],
 }
 
 exec { 'apt-get update 2':
@@ -21,18 +21,27 @@ exec { 'apt-get update 2':
   require => [ Apt::Ppa["ppa:webupd8team/java"] ],
 }
 
+package { [
+  "curl",
+  "git-core",
+  "bash"]:
+  ensure  => present,
+  require => Exec["apt-get update"],
+  before  => Apt::Ppa["ppa:webupd8team/java"],
+}
+
 package { ["oracle-java8-installer"]:
-  ensure => present,
+  ensure  => present,
   require => Exec["apt-get update 2"],
 }
 
 exec {
   "accept_license":
-    command => "echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections && echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections",
-    cwd => "/home/vagrant",
-    user => "vagrant",
-    path    => "/usr/bin/:/bin/",
-    before => Package["oracle-java8-installer"],
+    command   => "echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections && echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections",
+    cwd       => "/home/vagrant",
+    user      => "vagrant",
+    path      => "/usr/bin/:/bin/",
+    before    => Package["oracle-java8-installer"],
     logoutput => true,
 }
 
@@ -40,7 +49,20 @@ exec {
 ## TODO Extract in a module
 
 file { '/opt/ontrack':
-  ensure => 'present',
-  group => 'ontrack',
-  owner => 'ontrack',
+  ensure  => 'directory',
+  group   => 'ontrack',
+  owner   => 'ontrack',
+  require => User['ontrack']
+}
+
+
+exec {
+  "ontrack-install":
+    require   => File['/opt/ontrack'],
+    user      => 'ontrack',
+    cwd       => '/opt/ontrack',
+    path      => "/usr/bin/:/bin/",
+    logoutput => true,
+  # TODO ontrack could be available through the Web, or locally, for acceptance tests
+    command   => 'curl --location https://github.com/nemerosa/ontrack/releases/download/2.0.0-beta-8-74/ontrack.jar --output ontrack.jar',
 }
