af0c6b866dea35142d31b7dc487126994b0c6e5d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-10-25 17:05:36 +0200

    #107 Ontrack tools

--- ontrack-delivery/vagrant/modules/ontrack/manifests/init.pp
+++ ontrack-delivery/vagrant/modules/ontrack/manifests/init.pp
@@ -1,11 +1,27 @@
 class ontrack {
 
-  # Ontrack user
+# Apt for tools
+  include apt
+  exec { 'apt-get-update':
+    command => '/usr/bin/apt-get update',
+  }
+
+# Tools
+  package { [
+    "curl",
+    "git-core",
+    "bash"]:
+    ensure  => present,
+    require => Exec["apt-get-update"],
+  }
+
+
+# Ontrack user
   user { 'ontrack':
     ensure => present,
   }
 
-  # Depends on Java
+# Depends on Java
   include java
 
 }
