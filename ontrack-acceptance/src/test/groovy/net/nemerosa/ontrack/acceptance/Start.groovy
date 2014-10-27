8042529998d22d85cf91f6264d347d9f331eebcf
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-10-27 19:37:20 +0100

    #107 Test JAR

---
+++ ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/Start.groovy
@@ -0,0 +1,10 @@
+package net.nemerosa.ontrack.acceptance
+
+class Start {
+
+    static void main(String... args) {
+        println "Starting acceptance tests."
+        println "Options: ${args}"
+    }
+
+}
