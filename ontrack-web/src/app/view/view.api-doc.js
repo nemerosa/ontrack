b80c16c77a1fec75e08aa23cdff7d8e2531c6181
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-11-01 20:00:47 +0100

    #133 Identifying the root methods

--- ontrack-web/src/app/view/view.api-doc.js
+++ ontrack-web/src/app/view/view.api-doc.js
@@ -22,6 +22,17 @@ angular.module('ot.view.api-doc', [
             $scope.apiLoading = true;
             ot.pageCall($http.get('api/list'))
                 .then(function (list) {
+                    /**
+                     * Preprocessing - collecting root access points
+                     */
+                    angular.forEach(list.resources, function (apiInfo) {
+                        angular.forEach(apiInfo.methods, function (apiMethodInfo) {
+                            apiMethodInfo.root = (apiMethodInfo.path.indexOf('{') < 0);
+                        });
+                        apiInfo.root = apiInfo.methods.some(function (apiMethodInfo) {
+                            return apiMethodInfo.root;
+                        });
+                    });
                     $scope.list = list;
                 })
                 .finally(function () {
