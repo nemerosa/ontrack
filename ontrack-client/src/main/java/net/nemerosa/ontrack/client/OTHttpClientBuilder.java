00d11042f1699bcd1455adc896581ecf28c320dd
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-11-06 22:27:42 +0100

    #141 It is possible to disable SSL checks using Apache HTTPClient 4.3.x

--- ontrack-client/src/main/java/net/nemerosa/ontrack/client/OTHttpClientBuilder.java
+++ ontrack-client/src/main/java/net/nemerosa/ontrack/client/OTHttpClientBuilder.java
@@ -8,12 +8,26 @@ import org.apache.http.client.AuthCache;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.*;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.function.Supplier;
 
 public class OTHttpClientBuilder {
@@ -69,10 +83,45 @@ public class OTHttpClientBuilder {
         CookieStore cookieStore = new BasicCookieStore();
         httpContext.setCookieStore(cookieStore);
 
+        //
+        // SSLConnectionSocketFactory sslSocketFactory = SSLConnectionSocketFactory.getSocketFactory();
+
+        SSLContext ctx;
+        try {
+            X509TrustManager x509TrustManager = new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+                }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            };
+            ctx = SSLContext.getInstance("TLS");
+            ctx.init(new KeyManager[0], new TrustManager[]{x509TrustManager}, new SecureRandom());
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new OTHttpClientSSLSetupException(e);
+        }
+
+        SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(
+                ctx,
+                SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER
+        );
+
+        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                .register("https", sslSocketFactory)
+                .build();
+
         Supplier<CloseableHttpClient> httpClientSupplier = () -> {
             // Defaults
             HttpClientBuilder builder = HttpClientBuilder.create()
-                    .setConnectionManager(new PoolingHttpClientConnectionManager());
+                    .setConnectionManager(new PoolingHttpClientConnectionManager(registry));
 
             // OK
             return builder.build();
