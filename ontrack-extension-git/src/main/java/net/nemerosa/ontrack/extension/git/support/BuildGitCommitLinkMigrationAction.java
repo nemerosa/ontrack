c8232daa50079e9d1060d4cf9a1748567f7ecb33
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-11-18 09:53:01 +0100

    #163 Migration of Git configurations to remove the `tagPattern` flag

--- ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/support/BuildGitCommitLinkMigrationAction.java
+++ ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/support/BuildGitCommitLinkMigrationAction.java
@@ -2,17 +2,24 @@ package net.nemerosa.ontrack.extension.git.support;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import net.nemerosa.ontrack.extension.git.model.GitConfiguration;
 import net.nemerosa.ontrack.extension.git.property.GitBranchConfigurationProperty;
 import net.nemerosa.ontrack.extension.git.property.GitBranchConfigurationPropertyType;
 import net.nemerosa.ontrack.json.JsonUtils;
 import net.nemerosa.ontrack.json.ObjectMapperFactory;
 import net.nemerosa.ontrack.model.structure.ServiceConfiguration;
 import net.nemerosa.ontrack.model.support.DBMigrationAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Migration task for #163, for transforming the Git branch configurations tag patterns into
@@ -21,6 +28,8 @@ import java.sql.ResultSet;
 @Component
 public class BuildGitCommitLinkMigrationAction implements DBMigrationAction {
 
+
+    private final Logger logger = LoggerFactory.getLogger(BuildGitCommitLinkMigrationAction.class);
     private final ObjectMapper objectMapper = ObjectMapperFactory.create();
 
     @Override
@@ -30,6 +39,33 @@ public class BuildGitCommitLinkMigrationAction implements DBMigrationAction {
 
     @Override
     public void migrate(Connection connection) throws Exception {
+        migrateGitConfigurations(connection);
+        migrateBranchConfigurations(connection);
+    }
+
+    private void migrateGitConfigurations(Connection connection) throws SQLException, IOException {
+        // For all Git configurations
+        try (PreparedStatement ps = connection.prepareStatement("SELECT * FROM CONFIGURATIONS WHERE TYPE = ?", ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE)) {
+            ps.setString(1, GitConfiguration.class.getName());
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    String name = rs.getString("NAME");
+                    logger.info("Migrating Git configuration: {}", name);
+                    // Configuration as JSON
+                    String json = rs.getString("CONTENT");
+                    // Parses the configuration
+                    ObjectNode node = (ObjectNode) objectMapper.readTree(json);
+                    // Removes any `tagPattern`
+                    node.remove("tagPattern");
+                    // Saves the data back
+                    rs.updateString("CONTENT", objectMapper.writeValueAsString(node));
+                    rs.updateRow();
+                }
+            }
+        }
+    }
+
+    private void migrateBranchConfigurations(Connection connection) throws SQLException, IOException {
         // For all branches...
         try (PreparedStatement psBranches = connection.prepareStatement("SELECT ID FROM BRANCHES")) {
             try (ResultSet rsBranches = psBranches.executeQuery()) {
@@ -41,6 +77,7 @@ public class BuildGitCommitLinkMigrationAction implements DBMigrationAction {
                         psProperty.setInt(2, branchId);
                         try (ResultSet rsProperty = psProperty.executeQuery()) {
                             while (rsProperty.next()) {
+                                logger.info("Migrating Branch Git configuration: {}", branchId);
                                 // Configuration as JSON
                                 String json = rsProperty.getString("JSON");
                                 // Parses the configuration
