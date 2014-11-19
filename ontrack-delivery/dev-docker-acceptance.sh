a26fdb1575af6efba0ca84b48ef52cd8b065f803
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-11-19 09:07:58 +0100

    Development: script to run Docker acceptance tests locally

---
+++ ontrack-delivery/dev-docker-acceptance.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Runs the acceptance tests in a VM created using Docker
+# The results of the acceptance tests will be in JUnit XML format in local `ontrack-acceptance.xml` file
+# See `docker-acceptance.sh` for the argument details
+
+# Before being run, the application must have been built:
+#
+#    # Development mode
+#    ./gradlew build
+#
+# or
+#
+#    # Release mode
+#    ./gradlew release
+
+# Root of the project
+PROJECT=`dirname $0`/..
+
+# Getting the UI jar
+UI=`find ${PROJECT}/ontrack-ui/build/libs -name "*.jar"`
+if [ "$?" != "0" ]
+then
+	echo "Could not find the UI JAR"
+	exit 1
+fi
+echo "Found UI JAR at $UI"
+
+# Getting the Acceptance jar
+ACCEPTANCE=`find ${PROJECT}/ontrack-acceptance/build/libs -name "*.jar"`
+if [ "$?" != "0" ]
+then
+	echo "Could not find the Acceptance JAR"
+	exit 1
+fi
+echo "Found Acceptance JAR at $ACCEPTANCE"
+
