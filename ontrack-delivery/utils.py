39521305e677104ab5e0cd5c7d3ea38f5cbe989d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-11-20 17:51:58 +0100

    Release: Using `subprocess` to launch commands

--- ontrack-delivery/utils.py
+++ ontrack-delivery/utils.py
@@ -1,7 +1,9 @@
-import os
+import subprocess
 
 
 def run_command(executable, args):
-    co_code = os.spawnv(os.P_WAIT, executable, args)
+    arguments = [executable]
+    arguments = arguments + args
+    co_code = subprocess.call(arguments)
     if co_code != 0:
         raise Exception("Cannot run command %s %s: %d" % (executable, args, co_code))
