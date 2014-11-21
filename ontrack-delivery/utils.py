84ad696d326bb784419c43c58360306a76e42657
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-11-21 10:19:19 +0100

    Release: Make `utils.run` returns the output

--- ontrack-delivery/utils.py
+++ ontrack-delivery/utils.py
@@ -4,6 +4,4 @@ import subprocess
 def run_command(executable, args):
     arguments = [executable]
     arguments = arguments + args
-    co_code = subprocess.call(arguments)
-    if co_code != 0:
-        raise Exception("Cannot run command %s %s: %d" % (executable, args, co_code))
+    return subprocess.check_output(arguments)
