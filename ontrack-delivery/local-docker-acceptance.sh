b50202d126dd69dcbb8a8ee4270f451a97f7267e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-11-24 11:37:15 +0100

    Deployment: Acceptance tests delay

--- ontrack-delivery/local-docker-acceptance.sh
+++ ontrack-delivery/local-docker-acceptance.sh
@@ -14,31 +14,29 @@
 #    # Release mode
 #    ./gradlew release
 
-# Root of the project
-PROJECT=`dirname $0`/..
-
 # Getting the UI jar
-UI=`find ${PROJECT}/ontrack-ui/build/libs -name "*.jar"`
+UI=`find ../ontrack-ui/build/libs -name "*.jar"`
 if [ "$?" != "0" ]
 then
 	echo "Could not find the UI JAR"
 	exit 1
 fi
+UI="$(pwd)/${UI}"
 echo "Found UI JAR at $UI"
 
 # Getting the Acceptance jar
-ACCEPTANCE=`find ${PROJECT}/ontrack-acceptance/build/libs -name "*.jar"`
+ACCEPTANCE=`find ../ontrack-acceptance/build/libs -name "*.jar"`
 if [ "$?" != "0" ]
 then
 	echo "Could not find the Acceptance JAR"
 	exit 1
 fi
+ACCEPTANCE="$(pwd)/${ACCEPTANCE}"
 echo "Found Acceptance JAR at $ACCEPTANCE"
 
 # Running the docker acceptance tests
-cd ${PROJECT}/ontrack-delivery
 ./docker-acceptance.sh \
-	--jar=${PROJECT}/${UI} \
-	--acceptance=${PROJECT}/${ACCEPTANCE} \
+	--jar=${UI} \
+	--acceptance=${ACCEPTANCE} \
 	$*
 
