deff275fe7da54d305612d9a2c430efa7f24c5fc
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-12-05 08:26:18 +0100

    #172 Correction of unit tests

--- ontrack-git/src/test/groovy/net/nemerosa/ontrack/git/GitRepositoryTest.groovy
+++ ontrack-git/src/test/groovy/net/nemerosa/ontrack/git/GitRepositoryTest.groovy
@@ -6,22 +6,27 @@ class GitRepositoryTest {
 
     @Test
     void getRepositoryId_1() {
-        assert new GitRepository("ontrack", "git://github.com/nemerosa/ontrack.git", "", "").id == "ontrack_git___github_com_nemerosa_ontrack_git";
+        assert new GitRepository("basic", "ontrack", "git://github.com/nemerosa/ontrack.git", "", "").id == "basic_ontrack_git___github_com_nemerosa_ontrack_git";
     }
 
     @Test
     void getRepositoryId_2() {
-        assert new GitRepository("ontrack", "git@github.com:nemerosa/ontrack.git", "", "").id == "ontrack_git_github_com_nemerosa_ontrack_git";
+        assert new GitRepository("basic", "ontrack", "git@github.com:nemerosa/ontrack.git", "", "").id == "basic_ontrack_git_github_com_nemerosa_ontrack_git";
     }
 
     @Test
     void getRepositoryId_3() {
-        assert new GitRepository("ontrack", "https://github.com/nemerosa/ontrack.git", "", "").id == "ontrack_https___github_com_nemerosa_ontrack_git";
+        assert new GitRepository("github", "ontrack", "git@github.com:nemerosa/ontrack.git", "", "").id == "github_ontrack_git_github_com_nemerosa_ontrack_git";
     }
 
     @Test
     void getRepositoryId_4() {
-        assert new GitRepository("ontrack2", "https://github.com/nemerosa/ontrack.git", "", "").id == "ontrack2_https___github_com_nemerosa_ontrack_git";
+        assert new GitRepository("basic", "ontrack", "https://github.com/nemerosa/ontrack.git", "", "").id == "basic_ontrack_https___github_com_nemerosa_ontrack_git";
+    }
+
+    @Test
+    void getRepositoryId_5() {
+        assert new GitRepository("basic", "ontrack2", "https://github.com/nemerosa/ontrack.git", "", "").id == "basic_ontrack2_https___github_com_nemerosa_ontrack_git";
     }
 
 }
