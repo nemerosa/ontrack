a77286e728ac388e05343ae4f885635b78828da2
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-12-05 22:31:48 +0100

    #172 Correction of encoding

--- ontrack-ui-support/src/main/java/net/nemerosa/ontrack/ui/controller/MockURIBuilder.java
+++ ontrack-ui-support/src/main/java/net/nemerosa/ontrack/ui/controller/MockURIBuilder.java
@@ -1,7 +1,6 @@
 package net.nemerosa.ontrack.ui.controller;
 
 import net.nemerosa.ontrack.common.RunProfile;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
@@ -9,6 +8,9 @@ import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBui
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 
@@ -18,18 +20,22 @@ public class MockURIBuilder implements URIBuilder {
     @Override
     public URI build(Object invocationInfo) {
         MvcUriComponentsBuilder.MethodInvocationInfo info = (MvcUriComponentsBuilder.MethodInvocationInfo) invocationInfo;
+        return URI.create(
+                format(
+                        "urn:test:%s#%s:%s",
+                        info.getControllerMethod().getDeclaringClass().getName(),
+                        info.getControllerMethod().getName(),
+                        Arrays.asList(info.getArgumentValues()).stream()
+                                .map(o -> Objects.toString(o, ""))
+                                .map(this::encode)
+                                .collect(Collectors.joining(","))
+                )
+        );
+    }
+
+    private String encode(String s) {
         try {
-            return URI.create(
-                    format(
-                            "urn:test:%s#%s:%s",
-                            info.getControllerMethod().getDeclaringClass().getName(),
-                            info.getControllerMethod().getName(),
-                            URLEncoder.encode(
-                                    StringUtils.join(info.getArgumentValues(), ","),
-                                    "UTF-8"
-                            )
-                    )
-            );
+            return URLEncoder.encode(s, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException("Cannot encode URI parameter", e);
         }
