ef2e931d240b8ed9e75851bf2402bdafa83f7d3b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-12-07 16:30:08 +0100

    #170 Folder per branch

--- ontrack-delivery/jenkins/jenkins.groovy
+++ ontrack-delivery/jenkins/jenkins.groovy
@@ -16,34 +16,46 @@
  * Variables
  */
 
-String repository = 'nemerosa/ontrack'
-String project = 'ontrack'
+def REPOSITORY = 'nemerosa/ontrack'
+def PROJECT = 'ontrack'
 
 /**
- * Pipeline for a branch
+ * Folder for the project (making sure)
  */
-def generateBranch(String branchName) {
-    // Normalised branch name
-    def name = branchName.replaceAll(/[^A-Za-z0-9\.\-_]/, '-')
-    // Folder for the branch
-    folder {
-        name "${project}/${project}-${name}"
-    }
-//    job {
-//        name "${project}-${branchName}".replaceAll('/','-')
-//        scm {
-//            git("git://github.com/${project}.git", branchName)
-//        }
-//    }
+
+folder {
+    name PROJECT
 }
 
 /**
  * Generation for all branches
  */
 
-String branchApi = new URL("https://api.github.com/repos/${repository}/branches")
+URL branchApi = new URL("https://api.github.com/repos/${REPOSITORY}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 
 branches.each {
-    generateBranch it.name
+    def BRANCH = it.name
+    // Branch type
+    def branchType
+    int pos = BRANCH.indexOf('/')
+    if (pos > 0) {
+        branchType = BRANCH.substring(0, pos)
+    } else {
+        branchType = BRANCH
+    }
+    println "BRANCH = ${BRANCH}"
+    println "\tBranchType = ${branchType}"
+    // Keeps only some version types
+    if (['master', 'feature', 'release', 'hotfix'].contains(branchType)) {
+        // Normalised branch name
+        def NAME = BRANCH.replaceAll(/[^A-Za-z0-9\.\-_]/, '-')
+        println "\tGenerating ${NAME}..."
+        // Folder for the branch
+        folder {
+            name "${PROJECT}/${PROJECT}-${NAME}"
+        }
+    } else {
+        println "\tSkipping."
+    }
 }
