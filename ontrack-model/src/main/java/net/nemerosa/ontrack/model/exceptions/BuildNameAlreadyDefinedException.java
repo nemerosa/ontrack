50049e042497e2358fd0c4a57dc9fb29ca432b95
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-12-19 12:00:44 +0100

    #186 Correction of error message when build already exists

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/exceptions/BuildNameAlreadyDefinedException.java
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/exceptions/BuildNameAlreadyDefinedException.java
@@ -3,6 +3,6 @@ package net.nemerosa.ontrack.model.exceptions;
 public class BuildNameAlreadyDefinedException extends DuplicationException {
 
     public BuildNameAlreadyDefinedException(String name) {
-        super("Branch name already exists: %s", name);
+        super("Build name already exists: %s", name);
     }
 }
