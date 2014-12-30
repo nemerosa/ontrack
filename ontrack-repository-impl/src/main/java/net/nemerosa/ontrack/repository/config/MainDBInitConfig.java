235f64f52181a1993d803766d364a9ceea354f7d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-12-30 20:07:23 +0100

    #204 Clean-up of Git configurations

--- ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/config/MainDBInitConfig.java
+++ ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/config/MainDBInitConfig.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 @Component
 public class MainDBInitConfig extends AbstractDBInitConfig {
 
-    public static final int VERSION = 10;
+    public static final int VERSION = 11;
 
     private final ApplicationContext applicationContext;
 
---
+++ ontrack-repository-impl/src/main/resources/META-INF/db/update.11.sql
@@ -0,0 +1,5 @@
+-- 11. Cleanup of Git configurations (#204)
+
+-- No update. The version increment is used to trigger a migration service
+
+-- Migration action: net.nemerosa.ontrack.extension.git.support.GitConfigurationMigrationAction11
