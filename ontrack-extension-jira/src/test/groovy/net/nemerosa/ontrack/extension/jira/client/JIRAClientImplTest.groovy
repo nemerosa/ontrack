3b2eb8fb7c828c8fdee44b9b1f95c2e5c6d9fbdd
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-01-07 20:27:03 +0100

    #179 Link inward test

--- ontrack-extension-jira/src/test/groovy/net/nemerosa/ontrack/extension/jira/client/JIRAClientImplTest.groovy
+++ ontrack-extension-jira/src/test/groovy/net/nemerosa/ontrack/extension/jira/client/JIRAClientImplTest.groovy
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import net.nemerosa.ontrack.extension.jira.JIRAConfiguration
 import net.nemerosa.ontrack.extension.jira.model.JIRAIssue
+import net.nemerosa.ontrack.extension.jira.model.JIRALink
 import net.nemerosa.ontrack.extension.jira.model.JIRAStatus
 import net.nemerosa.ontrack.extension.jira.model.JIRAVersion
 import org.junit.Test
@@ -25,7 +26,7 @@ class JIRAClientImplTest {
     }
 
     @Test
-    void toIssue() throws IOException {
+    void toIssue() {
         // Configuration to test with
         JIRAConfiguration config = new JIRAConfiguration("Test", "http://host", "user", "secret");
         // Issue to parse
@@ -41,6 +42,33 @@ class JIRAClientImplTest {
         assert issue.fixVersions == [new JIRAVersion('1.2', false)]
         assert issue.assignee == 'dcoraboeuf'
         assert issue.affectedVersions == [new JIRAVersion('1.0', true)]
+    }
+
+    @Test
+    void 'toIssue with one inward link'() {
+        // Configuration to test with
+        JIRAConfiguration config = new JIRAConfiguration("Test", "http://host", "user", "secret");
+        // Issue to parse
+        JsonNode node = new ObjectMapper().readTree(getClass().getResource("/issue-link-inward.json"));
+        // Parsing the issue
+        JIRAIssue issue = JIRAClientImpl.toIssue(config, node);
+        // Checking the issue
+        assert issue != null
+        assert issue.key == 'PRJ-136'
+        assert issue.summary == 'Issue summary'
+        assert issue.updateTime == LocalDateTime.of(2014, 6, 18, 15, 12, 52, 369000000)
+        assert issue.status == new JIRAStatus('Closed', 'http://jira/images/icons/statuses/closed.png')
+        assert issue.fixVersions == [new JIRAVersion('1.2', false)]
+        assert issue.assignee == 'dcoraboeuf'
+        assert issue.affectedVersions == [new JIRAVersion('1.0', true)]
+        assert issue.links == [
+                new JIRALink(
+                        'PRJ-900',
+                        'https://jira/rest/api/2/issue/10702',
+                        new JIRAStatus('Open', 'http://jira/images/icons/statuses/open.png'),
+                        'is blocked by'
+                )
+        ]
     }
 
 }
