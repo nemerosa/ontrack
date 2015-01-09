046b1b827bf34f74a38c7bb7bbe58ec637bc7569
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-01-09 18:18:27 +0100

    #179 Git issue search - integration test - wip

--- ontrack-extension-git/src/test/groovy/net/nemerosa/ontrack/extension/git/service/GitSearchIT.groovy
+++ ontrack-extension-git/src/test/groovy/net/nemerosa/ontrack/extension/git/service/GitSearchIT.groovy
@@ -9,6 +9,7 @@ import net.nemerosa.ontrack.extension.git.property.GitProjectConfigurationProper
 import net.nemerosa.ontrack.extension.git.support.CommitBuildNameGitCommitLink
 import net.nemerosa.ontrack.extension.git.support.CommitLinkConfig
 import net.nemerosa.ontrack.extension.issues.support.MockIssueServiceConfiguration
+import net.nemerosa.ontrack.git.GitRepositoryClientFactory
 import net.nemerosa.ontrack.git.support.GitRepo
 import net.nemerosa.ontrack.it.AbstractServiceTestSupport
 import net.nemerosa.ontrack.model.security.GlobalSettings
@@ -33,6 +34,9 @@ class GitSearchIT extends AbstractServiceTestSupport {
     @Autowired
     private GitService gitService
 
+    @Autowired
+    private GitRepositoryClientFactory gitRepositoryClientFactory
+
     @Test
     void 'Issue search on one branch'() {
         // Git repository
@@ -46,57 +50,60 @@ class GitSearchIT extends AbstractServiceTestSupport {
                 commit 4, '#1'
                 git 'log', '--oneline', '--decorate'
             }
+
+            // Create a Git configuration
+            String gitConfigurationName = uid('C')
+            BasicGitConfiguration gitConfiguration = asUser().with(GlobalSettings).call {
+                gitConfigurationService.newConfiguration(
+                        BasicGitConfiguration.empty()
+                                .withName(gitConfigurationName)
+                                .withIssueServiceConfigurationIdentifier(MockIssueServiceConfiguration.INSTANCE.toIdentifier().format())
+                                .withRemote("file://${repo.dir.absolutePath}")
+                )
+            }
+
+            // Creates a project and branch
+            Branch branch = doCreateBranch()
+            Project project = branch.project
+
+            // Configures the project
+            asUser().with(project, ProjectEdit).call {
+                propertyService.editProperty(
+                        project,
+                        GitProjectConfigurationPropertyType,
+                        new GitProjectConfigurationProperty(gitConfiguration)
+                )
+                // ...  & the branch with a link based on commits
+                propertyService.editProperty(
+                        branch,
+                        GitBranchConfigurationPropertyType,
+                        new GitBranchConfigurationProperty(
+                                'master',
+                                new ConfiguredBuildGitCommitLink<>(
+                                        new CommitBuildNameGitCommitLink(),
+                                        new CommitLinkConfig(true)
+                                ).toServiceConfiguration(),
+                                false, 0
+                        )
+                )
+            }
+
+            // Need to force the Git sync first
+            gitRepositoryClientFactory.getClient(gitConfiguration.gitRepository).sync { println it }
+
+            // Looks for an issue
+            def info = asUser().with(project, ProjectView).call { gitService.getIssueInfo(branch.id, '2') }
+            assert info != null
+            assert info.issue.key == '2'
+            assert info.issue.displayKey == '#2'
+            assert info.commitInfos.size() == 1
+            def ci = info.commitInfos.first()
+            assert ci.branchInfos.size() == 1
+            assert ci.branchInfos.first().branch.id == branch.id
+
         } finally {
             repo.close()
         }
-        // Issue service
-        def issueServiceIdentifier = MockIssueServiceConfiguration.INSTANCE.toIdentifier().format()
-        // Create a Git configuration
-        String gitConfigurationName = uid('C')
-        BasicGitConfiguration gitConfiguration = asUser().with(GlobalSettings).call {
-            gitConfigurationService.newConfiguration(
-                    BasicGitConfiguration.empty()
-                            .withName(gitConfigurationName)
-                            .withIssueServiceConfigurationIdentifier(issueServiceIdentifier)
-                            .withRemote("file://${repo.dir.absolutePath}")
-            )
-        }
-
-        // Creates a project and branch
-        Branch branch = doCreateBranch()
-        Project project = branch.project
-
-        // Configures the project
-        asUser().with(project, ProjectEdit).call {
-            propertyService.editProperty(
-                    project,
-                    GitProjectConfigurationPropertyType,
-                    new GitProjectConfigurationProperty(gitConfiguration)
-            )
-            // ...  & the branch with a link based on commits
-            propertyService.editProperty(
-                    branch,
-                    GitBranchConfigurationPropertyType,
-                    new GitBranchConfigurationProperty(
-                            'master',
-                            new ConfiguredBuildGitCommitLink<>(
-                                    new CommitBuildNameGitCommitLink(),
-                                    new CommitLinkConfig(true)
-                            ).toServiceConfiguration(),
-                            false, 0
-                    )
-            )
-        }
-
-        // Looks for an issue
-        def info = asUser().with(project, ProjectView).call { gitService.getIssueInfo(branch.id, '2') }
-        assert info != null
-        assert info.issue.key == '2'
-        assert info.issue.displayKey == '#2'
-        assert info.commitInfos.size() == 1
-        def ci = info.commitInfos.first()
-        assert ci.branchInfos.size() == 1
-        assert ci.branchInfos.first().branch.id == branch.id
 
     }
 
