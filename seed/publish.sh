ea6d9564c0b7b1b45d2991a27bf88576dc9dfa84
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-01-10 11:29:43 +0100

    #210 Seed folder

---
+++ seed/publish.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Maven environment
+
+if [ "${MAVEN_3_2_X_HOME}" == "" ]
+then
+    echo "MAVEN_3_2_X_HOME is not set"
+    exit 1
+fi
+export PATH=${MAVEN_3_2_X_HOME}/bin:$PATH
+mvn --version
+
+# Environment
+
+REPOSITORY=/var/lib/jenkins/repository/ontrack/2.0
+
+# Cleanup
+
+rm -rf ${WORKSPACE}/*
+
+# Extract the Docker delivery JAR
+
+unzip ${REPOSITORY}/ontrack-delivery-docker-${VERSION_FULL}.jar -d ${WORKSPACE}
+
+# Publication of the release
+
+./publish.py \
+    --repository=${REPOSITORY} \
+    --version-commit=${VERSION_COMMIT} \
+    --version-full=${VERSION_FULL} \
+    --version-release=${VERSION_DISPLAY} \
+    --ontrack-url=https://ontrack.nemerosa.net \
+    --github-user=dcoraboeuf \
+    --github-token=${GITHUB_TOKEN} \
+    --ossrh-profile=${OSSRH_PROFILE}
