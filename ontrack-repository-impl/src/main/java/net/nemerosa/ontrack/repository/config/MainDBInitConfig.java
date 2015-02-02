d6406aa36ddf1c487bb1c0bf7093c4595ea0d613
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-02-02 19:54:09 +0100

    #234 Converting BLOB columns to BINARY columns

--- ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/config/MainDBInitConfig.java
+++ ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/config/MainDBInitConfig.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 @Component
 public class MainDBInitConfig extends AbstractDBInitConfig {
 
-    public static final int VERSION = 11;
+    public static final int VERSION = 12;
 
     private final ApplicationContext applicationContext;
 
