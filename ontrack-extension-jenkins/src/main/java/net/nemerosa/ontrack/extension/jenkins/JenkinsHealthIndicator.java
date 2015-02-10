a9b9333f516a20b9ebf01b95eeffce679203a1f2
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-02-10 20:27:57 +0100

    #240 GitHub health indicator + factorisation of code for the configurations health indicators

--- ontrack-extension-jenkins/src/main/java/net/nemerosa/ontrack/extension/jenkins/JenkinsHealthIndicator.java
+++ ontrack-extension-jenkins/src/main/java/net/nemerosa/ontrack/extension/jenkins/JenkinsHealthIndicator.java
@@ -1,47 +1,27 @@
 package net.nemerosa.ontrack.extension.jenkins;
 
 import net.nemerosa.ontrack.extension.jenkins.client.JenkinsClientFactory;
+import net.nemerosa.ontrack.extension.support.ConfigurationHealthIndicator;
+import net.nemerosa.ontrack.extension.support.ConfigurationService;
 import net.nemerosa.ontrack.model.security.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthAggregator;
-import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-import java.util.stream.Collectors;
-
 @Component
-public class JenkinsHealthIndicator implements HealthIndicator {
+public class JenkinsHealthIndicator extends ConfigurationHealthIndicator<JenkinsConfiguration> {
 
-    private final JenkinsConfigurationService configurationService;
-    private final SecurityService securityService;
-    private final HealthAggregator healthAggregator;
     private final JenkinsClientFactory jenkinsClientFactory;
 
     @Autowired
-    public JenkinsHealthIndicator(JenkinsConfigurationService configurationService, SecurityService securityService, HealthAggregator healthAggregator, JenkinsClientFactory jenkinsClientFactory) {
-        this.configurationService = configurationService;
-        this.securityService = securityService;
-        this.healthAggregator = healthAggregator;
+    public JenkinsHealthIndicator(ConfigurationService<JenkinsConfiguration> configurationService, SecurityService securityService, HealthAggregator healthAggregator, JenkinsClientFactory jenkinsClientFactory) {
+        super(configurationService, securityService, healthAggregator);
         this.jenkinsClientFactory = jenkinsClientFactory;
     }
 
     @Override
-    public Health health() {
-        return securityService.asAdmin(() -> {
-            Map<String, Health> healths = configurationService.getConfigurations().stream()
-                    .collect(
-                            Collectors.toMap(
-                                    JenkinsConfiguration::getName,
-                                    this::getHealth
-                            )
-                    );
-            return healthAggregator.aggregate(healths);
-        });
-    }
-
-    private Health getHealth(JenkinsConfiguration config) {
+    protected Health getHealth(JenkinsConfiguration config) {
         try {
             jenkinsClientFactory.getClient(config).getInfo();
             return Health.up().build();
--- ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/SVNHealthIndicator.java
+++ ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/SVNHealthIndicator.java
@@ -1,9 +1,10 @@
 package net.nemerosa.ontrack.extension.svn;
 
+import net.nemerosa.ontrack.extension.support.ConfigurationHealthIndicator;
+import net.nemerosa.ontrack.extension.support.ConfigurationService;
 import net.nemerosa.ontrack.extension.svn.client.SVNClient;
 import net.nemerosa.ontrack.extension.svn.db.SVNRepository;
 import net.nemerosa.ontrack.extension.svn.model.SVNConfiguration;
-import net.nemerosa.ontrack.extension.svn.service.SVNConfigurationService;
 import net.nemerosa.ontrack.extension.svn.service.SVNService;
 import net.nemerosa.ontrack.extension.svn.support.SVNUtils;
 import net.nemerosa.ontrack.model.security.SecurityService;
@@ -12,48 +13,26 @@ import net.nemerosa.ontrack.tx.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthAggregator;
-import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 import org.tmatesoft.svn.core.SVNURL;
 
-import java.util.Map;
-import java.util.stream.Collectors;
-
 @Component
-public class SVNHealthIndicator implements HealthIndicator {
+public class SVNHealthIndicator extends ConfigurationHealthIndicator<SVNConfiguration> {
 
-    private final SVNConfigurationService configurationService;
     private final SVNService svnService;
-    private final SVNClient svnClient;
     private final TransactionService transactionService;
-    private final SecurityService securityService;
-    private final HealthAggregator healthAggregator;
+    private final SVNClient svnClient;
 
     @Autowired
-    public SVNHealthIndicator(SVNConfigurationService configurationService, SVNService svnService, SVNClient svnClient, TransactionService transactionService, SecurityService securityService, HealthAggregator healthAggregator) {
-        this.configurationService = configurationService;
+    public SVNHealthIndicator(ConfigurationService<SVNConfiguration> configurationService, SecurityService securityService, HealthAggregator healthAggregator, SVNService svnService, TransactionService transactionService, SVNClient svnClient) {
+        super(configurationService, securityService, healthAggregator);
         this.svnService = svnService;
-        this.svnClient = svnClient;
-        this.securityService = securityService;
-        this.healthAggregator = healthAggregator;
         this.transactionService = transactionService;
+        this.svnClient = svnClient;
     }
 
     @Override
-    public Health health() {
-        return securityService.asAdmin(() -> {
-            // Gets the status for all configurations
-            Map<String, Health> healths = configurationService.getConfigurations().stream()
-                    .collect(Collectors.toMap(
-                            SVNConfiguration::getName,
-                            this::getConfigurationHealth
-                    ));
-            // Aggregates the healths
-            return healthAggregator.aggregate(healths);
-        });
-    }
-
-    private Health getConfigurationHealth(SVNConfiguration config) {
+    protected Health getHealth(SVNConfiguration config) {
         // Just gets the latest revision
         SVNRepository repository = svnService.getRepository(config.getName());
         try (Transaction ignored = transactionService.start()) {
@@ -67,5 +46,4 @@ public class SVNHealthIndicator implements HealthIndicator {
             }
         }
     }
-
 }
