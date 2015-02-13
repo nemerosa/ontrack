e75e887fad05f0f57af514c970c22c5fdcf92a4d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-02-13 08:44:03 +0100

    #146 SVN/Git change log file with a `path` property

--- ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/model/SVNChangeLogFile.java
+++ ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/model/SVNChangeLogFile.java
@@ -1,12 +1,13 @@
 package net.nemerosa.ontrack.extension.svn.model;
 
 import lombok.Data;
+import net.nemerosa.ontrack.extension.scm.model.SCMChangeLogFile;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class SVNChangeLogFile {
+public class SVNChangeLogFile implements SCMChangeLogFile {
 
     private final String path;
     private final String url;
