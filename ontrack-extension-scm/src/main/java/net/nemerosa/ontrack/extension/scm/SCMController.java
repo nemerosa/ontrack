b1e9125c6cd520024975141cb3490dd565163340
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-02-17 19:28:19 +0100

    #146 REST model for the change log file filter API

--- ontrack-extension-scm/src/main/java/net/nemerosa/ontrack/extension/scm/SCMController.java
+++ ontrack-extension-scm/src/main/java/net/nemerosa/ontrack/extension/scm/SCMController.java
@@ -3,6 +3,9 @@ package net.nemerosa.ontrack.extension.scm;
 import net.nemerosa.ontrack.extension.scm.model.SCMFileChangeFilter;
 import net.nemerosa.ontrack.extension.scm.model.SCMFileChangeFilters;
 import net.nemerosa.ontrack.model.Ack;
+import net.nemerosa.ontrack.model.form.Form;
+import net.nemerosa.ontrack.model.form.Memo;
+import net.nemerosa.ontrack.model.form.Text;
 import net.nemerosa.ontrack.model.security.ProjectConfig;
 import net.nemerosa.ontrack.model.security.SecurityService;
 import net.nemerosa.ontrack.model.structure.EntityDataService;
@@ -10,11 +13,14 @@ import net.nemerosa.ontrack.model.structure.ID;
 import net.nemerosa.ontrack.model.structure.Project;
 import net.nemerosa.ontrack.model.structure.StructureService;
 import net.nemerosa.ontrack.ui.controller.AbstractResourceController;
+import net.nemerosa.ontrack.ui.resource.Link;
 import net.nemerosa.ontrack.ui.resource.Resource;
 import net.nemerosa.ontrack.ui.resource.Resources;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Collectors;
 
 import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
 
@@ -34,10 +40,41 @@ public class SCMController extends AbstractResourceController {
     }
 
     /**
-     * Saving a change log file filter
+     * Gets the list of change log file filters
      */
-    @RequestMapping(value = "changeLog/fileFilter/{projectId}", method = RequestMethod.PUT)
-    public Ack saveChangeLogFileFilter(@PathVariable ID projectId, @RequestBody SCMFileChangeFilter filter) {
+    @RequestMapping(value = "changeLog/fileFilter/{projectId}", method = RequestMethod.GET)
+    public Resources<Resource<SCMFileChangeFilter>> getChangeLogFileFilters(@PathVariable ID projectId) {
+        // Gets the store
+        SCMFileChangeFilters config = loadStore(projectId);
+        // Resources
+        return Resources.of(
+                config.getFilters().stream().map(f -> toResource(projectId, f)),
+                uri(on(getClass()).getChangeLogFileFilters(projectId))
+        )
+                .with(Link.CREATE, uri(on(getClass()).createChangeLogFileFilterForm(projectId)))
+                ;
+    }
+
+    /**
+     * Form to create a change log filter
+     */
+    @RequestMapping(value = "changeLog/fileFilter/{projectId}/create", method = RequestMethod.GET)
+    public Form createChangeLogFileFilterForm(@SuppressWarnings("UnusedParameters") @PathVariable ID projectId) {
+        return Form.create()
+                .with(Text.of("name")
+                        .label("Name")
+                        .help("Name to use to save the filter."))
+                .with(Memo.of("patterns")
+                        .label("Filter(s)")
+                        .help("List of ANT-like patterns (one per line)."))
+                ;
+    }
+
+    /**
+     * Adding a change log file filter
+     */
+    @RequestMapping(value = "changeLog/fileFilter/{projectId}/create", method = RequestMethod.POST)
+    public Ack createChangeLogFileFilter(@PathVariable ID projectId, @RequestBody SCMFileChangeFilter filter) {
         securityService.checkProjectFunction(projectId.get(), ProjectConfig.class);
         return securityService.asAdmin(() -> {
             // Loads the project
@@ -58,17 +95,33 @@ public class SCMController extends AbstractResourceController {
     }
 
     /**
-     * Gets the list of change log file filters
+     * Updating form for a change log file filter
      */
-    @RequestMapping(value = "changeLog/fileFilter/{projectId}", method = RequestMethod.GET)
-    public Resources<SCMFileChangeFilter> getChangeLogFileFilters(@PathVariable ID projectId) {
-        // Gets the store
-        SCMFileChangeFilters config = loadStore(projectId);
-        // Resources
-        return Resources.of(
-                config.getFilters(),
-                uri(on(getClass()).getChangeLogFileFilters(projectId))
-        );
+    @RequestMapping(value = "changeLog/fileFilter/{projectId}/{name}/update", method = RequestMethod.GET)
+    public Form saveChangeLogFileFilterForm(@PathVariable ID projectId, @PathVariable String name) {
+        Resource<SCMFileChangeFilter> filter = getChangeLogFileFilter(projectId, name);
+        return Form.create()
+                .with(Text.of("name")
+                        .label("Name")
+                        .help("Name to use to save the filter.")
+                        .readOnly()
+                        .value(filter.getData().getName()))
+                .with(Memo.of("patterns")
+                        .label("Filter(s)")
+                        .help("List of ANT-like patterns (one per line).")
+                        .value(filter.getData().getPatterns().stream().collect(Collectors.joining("\n"))))
+                ;
+    }
+
+    /**
+     * Updating a change log file filter
+     */
+    @RequestMapping(value = "changeLog/fileFilter/{projectId}/{name}/update", method = RequestMethod.PUT)
+    public Ack saveChangeLogFileFilter(@PathVariable ID projectId, @PathVariable String name, @RequestBody SCMFileChangeFilter filter) {
+        if (!StringUtils.equals(name, filter.getName())) {
+            throw new IllegalStateException("The name of the filter in the request body must match the one in the URL");
+        }
+        return createChangeLogFileFilter(projectId, filter);
     }
 
     /**
@@ -81,13 +134,18 @@ public class SCMController extends AbstractResourceController {
         return config.getFilters().stream()
                 .filter(filter -> StringUtils.equals(name, filter.getName()))
                 .findFirst()
-                .map(filter ->
-                                Resource.of(
-                                        filter,
-                                        uri(on(getClass()).getChangeLogFileFilter(projectId, name))
-                                ).with("_delete", uri(on(getClass()).deleteChangeLogFileFilter(projectId, name)))
-                )
+                .map(filter -> toResource(projectId, filter))
                 .orElseThrow(() -> new SCMFileChangeFilterNotFound(name));
+    }
+
+    private Resource<SCMFileChangeFilter> toResource(ID projectId, SCMFileChangeFilter filter) {
+        return Resource.of(
+                filter,
+                uri(on(getClass()).getChangeLogFileFilter(projectId, filter.getName()))
+        )
+                .with(Link.DELETE, uri(on(getClass()).deleteChangeLogFileFilter(projectId, filter.getName())))
+                .with(Link.UPDATE, uri(on(getClass()).saveChangeLogFileFilterForm(projectId, filter.getName())))
+                ;
     }
 
     private SCMFileChangeFilters loadStore(ID projectId) {
@@ -106,7 +164,7 @@ public class SCMController extends AbstractResourceController {
     /**
      * Deletes a change log file filter
      */
-    @RequestMapping(value = "changeLog/fileFilter/{projectId}/{name:.*}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "changeLog/fileFilter/{projectId}/{name}/delete", method = RequestMethod.DELETE)
     public Ack deleteChangeLogFileFilter(@PathVariable ID projectId, @PathVariable String name) {
         securityService.checkProjectFunction(projectId.get(), ProjectConfig.class);
         securityService.runAsAdmin(() -> entityDataService.delete(structureService.getProject(projectId), name));
