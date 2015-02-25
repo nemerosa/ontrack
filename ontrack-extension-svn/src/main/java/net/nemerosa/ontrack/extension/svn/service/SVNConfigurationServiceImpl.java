e979e3ec655b5aad3fc2f3f4e11c86cdbee6bb45
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-02-25 09:12:22 +0100

    #243 SVN: deleting the repository when deleting the configuration

--- ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/service/SVNConfigurationServiceImpl.java
+++ ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/service/SVNConfigurationServiceImpl.java
@@ -1,8 +1,9 @@
 package net.nemerosa.ontrack.extension.svn.service;
 
 import net.nemerosa.ontrack.extension.support.AbstractConfigurationService;
-import net.nemerosa.ontrack.model.security.EncryptionService;
+import net.nemerosa.ontrack.extension.svn.db.SVNRepositoryDao;
 import net.nemerosa.ontrack.extension.svn.model.SVNConfiguration;
+import net.nemerosa.ontrack.model.security.EncryptionService;
 import net.nemerosa.ontrack.model.security.SecurityService;
 import net.nemerosa.ontrack.model.support.ConfigurationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +14,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class SVNConfigurationServiceImpl extends AbstractConfigurationService<SVNConfiguration> implements SVNConfigurationService {
 
+    private final SVNRepositoryDao repositoryDao;
+
     @Autowired
-    public SVNConfigurationServiceImpl(ConfigurationRepository configurationRepository, SecurityService securityService, EncryptionService encryptionService) {
+    public SVNConfigurationServiceImpl(ConfigurationRepository configurationRepository, SecurityService securityService, EncryptionService encryptionService, SVNRepositoryDao repositoryDao) {
         super(SVNConfiguration.class, configurationRepository, securityService, encryptionService);
+        this.repositoryDao = repositoryDao;
     }
 
+    @Override
+    public void deleteConfiguration(String name) {
+        super.deleteConfiguration(name);
+        Integer id = repositoryDao.findByName(name);
+        if (id != null) {
+            repositoryDao.delete(id);
+        }
+    }
 }
