6203517e68729174dd56174210f75b2e3156baed
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-03-05 22:09:17 +0100

    #251 Workaround for SPR-12771

--- ontrack-ui-support/src/main/java/net/nemerosa/ontrack/ui/controller/DefaultURIBuilder.java
+++ ontrack-ui-support/src/main/java/net/nemerosa/ontrack/ui/controller/DefaultURIBuilder.java
@@ -2,9 +2,17 @@ package net.nemerosa.ontrack.ui.controller;
 
 import net.nemerosa.ontrack.common.RunProfile;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 
 @Component
@@ -12,6 +20,21 @@ import java.net.URI;
 public class DefaultURIBuilder implements URIBuilder {
     @Override
     public URI build(Object methodInvocation) {
-        return MvcUriComponentsBuilder.fromMethodCall(methodInvocation).build().toUri();
+
+        // Default builder
+        UriComponentsBuilder builder = MvcUriComponentsBuilder.fromMethodCall(methodInvocation);
+
+        // TODO #251 Workaround for SPR-12771
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        HttpRequest httpRequest = new ServletServerHttpRequest(request);
+        String portHeader = httpRequest.getHeaders().getFirst("X-Forwarded-Port");
+        if (StringUtils.hasText(portHeader)) {
+            int port = Integer.parseInt(portHeader);
+            builder.port(port);
+        }
+
+        // OK
+        return builder.build().toUri();
     }
 }
