8b2914e0aee8b0958237df4d2ef23c554e428304
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-05-31 22:54:34 +0200

    #254 Other approach with direct representation of the validation stamp

--- ontrack-extension-general/src/main/java/net/nemerosa/ontrack/extension/general/AutoPromotionProperty.java
+++ ontrack-extension-general/src/main/java/net/nemerosa/ontrack/extension/general/AutoPromotionProperty.java
@@ -10,6 +10,11 @@ public class AutoPromotionProperty {
 
     /**
      * List of needed validation stamps
+     *
+     * FIXME Use direct reference to validation stamps
+     *
+     * Use `fromStorage` and co to store only the IDs, but use the complete validation stamps
+     * for back to the client, in order to get a full representation of the validation stamp.
      */
     private final List<String> validationStamps;
 
