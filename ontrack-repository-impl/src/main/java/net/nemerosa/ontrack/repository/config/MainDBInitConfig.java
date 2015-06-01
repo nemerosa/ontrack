618e94cf39af93784b8ab6eaa5410e3cee17324d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-06-01 09:49:58 +0200

    #269 Changes the branch name maximum size to 120 in the database

--- ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/config/MainDBInitConfig.java
+++ ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/config/MainDBInitConfig.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 @Component
 public class MainDBInitConfig extends AbstractDBInitConfig {
 
-    public static final int VERSION = 15;
+    public static final int VERSION = 16;
 
     private final ApplicationContext applicationContext;
 
