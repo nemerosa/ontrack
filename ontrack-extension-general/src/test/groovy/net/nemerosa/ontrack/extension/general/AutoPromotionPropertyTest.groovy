27d44be90e04df85d35fb259c4b7e1ae74b29e55
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-06-01 09:46:46 +0200

    #254 Unit test for the `AutoPromotionProperty` - wip

---
+++ ontrack-extension-general/src/test/groovy/net/nemerosa/ontrack/extension/general/AutoPromotionPropertyTest.groovy
@@ -0,0 +1,16 @@
+package net.nemerosa.ontrack.extension.general
+
+import org.junit.Test
+
+class AutoPromotionPropertyTest {
+
+    @Test
+    void 'Checks that a validation stamp is contained'() {
+        AutoPromotionProperty property = new AutoPromotionProperty(
+                [
+
+                ]
+        );
+    }
+
+}
