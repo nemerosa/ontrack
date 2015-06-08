c6432ce9e7e26dd16afc7dddd23ed1cc641cd22f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-06-08 16:46:51 +0200

    #274 Combined issue service - user menu

---
+++ ontrack-extension-issues/src/main/java/net/nemerosa/ontrack/extension/issues/combined/CombinedIssueServiceUserMenuExtension.java
@@ -0,0 +1,28 @@
+package net.nemerosa.ontrack.extension.issues.combined;
+
+import net.nemerosa.ontrack.extension.api.UserMenuExtension;
+import net.nemerosa.ontrack.extension.support.AbstractExtension;
+import net.nemerosa.ontrack.model.security.GlobalFunction;
+import net.nemerosa.ontrack.model.security.GlobalSettings;
+import net.nemerosa.ontrack.model.support.Action;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CombinedIssueServiceUserMenuExtension extends AbstractExtension implements UserMenuExtension {
+
+    @Autowired
+    public CombinedIssueServiceUserMenuExtension(CombinedIssueServiceExtensionFeature feature) {
+        super(feature);
+    }
+
+    @Override
+    public Class<? extends GlobalFunction> getGlobalFunction() {
+        return GlobalSettings.class;
+    }
+
+    @Override
+    public Action getAction() {
+        return Action.of("combined-issue-service-configurations", "Combined issue services configurations", "configurations");
+    }
+}
