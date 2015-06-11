21c20b508f652175bb79c5c53ec180848c763a0e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-06-11 14:05:58 +0200

    #274 DSL issues

--- ontrack-dsl/src/main/groovy/net/nemerosa/ontrack/dsl/ChangeLog.groovy
+++ ontrack-dsl/src/main/groovy/net/nemerosa/ontrack/dsl/ChangeLog.groovy
@@ -12,12 +12,23 @@ class ChangeLog extends AbstractResource {
 
     List<ChangeLogCommit> getCommits() {
         // The commit link is available as _commits or as _revisions
-        String url = optionalLink('_commits')
+        String url = optionalLink('commits')
         if (!url) {
-            url = link('_revisions')
+            url = link('revisions')
         }
         ontrack.get(url)['commits'].collect {
             new ChangeLogCommit(ontrack, it)
+        }
+    }
+
+    List<ChangeLogIssue> getIssues() {
+        String url = optionalLink('issues')
+        if (url) {
+            return ontrack.get(url).list.collect {
+                new ChangeLogIssue(ontrack, it['issue'])
+            }
+        } else {
+            return []
         }
     }
 
