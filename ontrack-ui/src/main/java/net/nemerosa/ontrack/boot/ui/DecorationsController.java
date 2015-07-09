291c46e687701b9fe3b236d5131f6f2b7f00dac6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-07-09 14:18:55 +0200

    #288 Decorators might need to access the current request for generating URIs - storing the current context in the callable

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/ui/DecorationsController.java
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/ui/DecorationsController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import java.util.concurrent.Callable;
 
 import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
 
@@ -26,11 +30,20 @@ public class DecorationsController extends AbstractProjectEntityController {
      * Decorations for an entity.
      */
     @RequestMapping(value = "{entityType}/{id}", method = RequestMethod.GET)
-    public Resources<Decoration> getDecorations(@PathVariable ProjectEntityType entityType, @PathVariable ID id) {
-        return Resources.of(
-                decorationService.getDecorations(getEntity(entityType, id)),
-                uri(on(getClass()).getDecorations(entityType, id))
-        );
+    public Callable<Resources<Decoration>> getDecorations(@PathVariable ProjectEntityType entityType, @PathVariable ID id) {
+        // Gets the current request attributes
+        RequestAttributes attributes = RequestContextHolder.currentRequestAttributes();
+        return () -> {
+            RequestContextHolder.setRequestAttributes(attributes);
+            try {
+                return Resources.of(
+                        decorationService.getDecorations(getEntity(entityType, id)),
+                        uri(on(getClass()).getDecorations(entityType, id))
+                );
+            } finally {
+                RequestContextHolder.resetRequestAttributes();
+            }
+        };
     }
 
 }
