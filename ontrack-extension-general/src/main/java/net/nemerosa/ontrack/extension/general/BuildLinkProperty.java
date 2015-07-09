ec2e6e78bf3ac3203ed688d14558959a98ec3cc0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-07-09 11:04:34 +0200

    #288 Build link property

---
+++ ontrack-extension-general/src/main/java/net/nemerosa/ontrack/extension/general/BuildLinkProperty.java
@@ -0,0 +1,12 @@
+package net.nemerosa.ontrack.extension.general;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class BuildLinkProperty {
+
+    private final List<BuildLinkPropertyItem> links;
+
+}
---
+++ ontrack-extension-general/src/main/java/net/nemerosa/ontrack/extension/general/BuildLinkPropertyItem.java
@@ -0,0 +1,11 @@
+package net.nemerosa.ontrack.extension.general;
+
+import lombok.Data;
+
+@Data
+public class BuildLinkPropertyItem {
+
+    private final String project;
+    private final String build;
+
+}
