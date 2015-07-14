631597054e488239f8d4249ace133f60b9221af3
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-07-14 19:48:34 +0200

    #288 Build link search - acceptance test

--- ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/ACCDSLBuildLinks.groovy
+++ ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/ACCDSLBuildLinks.groovy
@@ -2,6 +2,7 @@ package net.nemerosa.ontrack.acceptance
 
 import net.nemerosa.ontrack.acceptance.support.AcceptanceTest
 import net.nemerosa.ontrack.acceptance.support.AcceptanceTestSuite
+import net.nemerosa.ontrack.dsl.SearchResult
 import org.junit.Test
 
 import static net.nemerosa.ontrack.test.TestUtils.uid
@@ -12,6 +13,38 @@ import static net.nemerosa.ontrack.test.TestUtils.uid
 @AcceptanceTestSuite
 @AcceptanceTest(excludes = 'production')
 class ACCDSLBuildLinks extends AbstractACCDSL {
+
+    @Test
+    void 'Build link search'() {
+        // Creating two builds with some build links
+        def p1 = uid('P1')
+        def p2 = uid('P2')
+        ontrack.project(p1) {
+            branch('B1') {
+                build('1.1') {
+                    config {
+                        buildLink p2, '2.0'
+                    }
+                }
+                build('1.2') {
+                    config {
+                        buildLink p2, '3.0'
+                    }
+                }
+            }
+        }
+        // Build id
+        def id = ontrack.build(p1, 'B1', '1.1').id
+        // Performs a search
+        def results = ontrack.search("${p2}:2*")
+        // Gets the results
+        assert results.size() == 1
+        SearchResult result = results[0]
+        assert result.title == "Build ${p1}/B1/1.1"
+        assert result.description == "${p2} -> 2.0"
+        assert result.uri == "${baseURL}/structure/builds/${id}"
+        assert result.page == "${baseURL}/#/build/${id}"
+    }
 
     @Test
     void 'Build links'() {
@@ -55,11 +88,11 @@ class ACCDSLBuildLinks extends AbstractACCDSL {
 
         // Gets the links
         def buildLinks = ontrack.build(p1, 'B1', '1.0').config.buildLinks
-        assert buildLinks.collect { [ it.project, it.build, it.page ] } == [
-                [ p1, '1.1', "${baseURL}/#/build/${b111}" ],
-                [ p2, '2.0', "${baseURL}/#/build/${b220}" ],
-                [ p2, '2.2', null ],
-                [ p3, '3.0', null ],
+        assert buildLinks.collect { [it.project, it.build, it.page] } == [
+                [p1, '1.1', "${baseURL}/#/build/${b111}"],
+                [p2, '2.0', "${baseURL}/#/build/${b220}"],
+                [p2, '2.2', null],
+                [p3, '3.0', null],
         ]
 
     }
