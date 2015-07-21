cbd494f207060d7e430d63ec94db8b62562d8159
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-07-21 16:07:21 +0200

    #289 Cleanup

--- ontrack-delivery/utils.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-def run_command(executable, args):
-    arguments = [executable]
-    arguments = arguments + args
-    output = subprocess.check_output(arguments)
-    print output
-    return output
