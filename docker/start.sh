b1aab17377e2f09df6cdfa630ebef3f558a5639a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-07-21 12:02:49 +0200

    #289 Using the `/var/ontrack/data` volume for the database location

--- docker/start.sh
+++ docker/start.sh
@@ -8,6 +8,7 @@ rm -rf /opt/ontrack/config \
 
 # Launching the application
 java -jar /opt/ontrack/ontrack.jar \
-    --spring.profiles.active=${PROFILE} \
-    --ontrack.config.applicationWorkingDir=/var/ontrack/data \
-    --logging.path=/var/ontrack/data/log/
+    "--spring.profiles.active=${PROFILE}" \
+    "--spring.datasource.url=jdbc:h2:/var/ontrack/data/database/data;MODE=MYSQL;DB_CLOSE_ON_EXIT=FALSE;DEFRAG_ALWAYS=TRUE" \
+    "--ontrack.config.applicationWorkingDir=/var/ontrack/data" \
+    "--logging.path=/var/ontrack/data/log/"
