bc996891d3251aa9432c55c15ce0f2907f710375
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-07-29 13:10:56 +0200

    #300 Management of Stash configurations

---
+++ ontrack-extension-stash/src/main/java/net/nemerosa/ontrack/extension/stash/service/StashConfigurationService.java
@@ -0,0 +1,7 @@
+package net.nemerosa.ontrack.extension.stash.service;
+
+import net.nemerosa.ontrack.extension.stash.model.StashConfiguration;
+import net.nemerosa.ontrack.extension.support.ConfigurationService;
+
+public interface StashConfigurationService extends ConfigurationService<StashConfiguration> {
+}
