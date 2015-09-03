d510293fb89acfab8a92e86ad4e3e2359dc34e29
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-09-03 09:17:37 +0200

    #301 Predefined promotion level model

--- ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/config/MainDBInitConfig.java
+++ ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/config/MainDBInitConfig.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 @Component
 public class MainDBInitConfig extends AbstractDBInitConfig {
 
-    public static final int VERSION = 18;
+    public static final int VERSION = 19;
 
     private final ApplicationContext applicationContext;
 
