4ad538529033cdd77e6143910f73eb55fea4e55f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-09-09 11:50:34 +0200

    #86 `ACCOUNT_GROUP_MAPPING` generic mapping table for the groups

--- ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/config/MainDBInitConfig.java
+++ ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/config/MainDBInitConfig.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 @Component
 public class MainDBInitConfig extends AbstractDBInitConfig {
 
-    public static final int VERSION = 19;
+    public static final int VERSION = 20;
 
     private final ApplicationContext applicationContext;
 
