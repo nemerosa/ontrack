bb88e26daccc4d4d8a56883ed3b7af5b992cc093
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-09-10 19:37:35 +0200

    #320 Migration of LDAP settings

--- ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/config/MainDBInitConfig.java
+++ ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/config/MainDBInitConfig.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 @Component
 public class MainDBInitConfig extends AbstractDBInitConfig {
 
-    public static final int VERSION = 20;
+    public static final int VERSION = 21;
 
     private final ApplicationContext applicationContext;
 
