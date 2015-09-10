fbbbb4ad2a67a0ede69c871a26ec845cc8fc1dab
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-09-10 11:18:58 +0200

    #320 LDAP settings manager in its own module

---
+++ ontrack-extension-ldap/src/main/java/net/nemerosa/ontrack/extension/ldap/LDAPSettingsProvider.java
@@ -0,0 +1,41 @@
+package net.nemerosa.ontrack.extension.ldap;
+
+import net.nemerosa.ontrack.model.security.EncryptionService;
+import net.nemerosa.ontrack.model.settings.SettingsProvider;
+import net.nemerosa.ontrack.model.support.SettingsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LDAPSettingsProvider implements SettingsProvider<LDAPSettings> {
+
+    private final SettingsRepository settingsRepository;
+    private final EncryptionService encryptionService;
+
+    @Autowired
+    public LDAPSettingsProvider(SettingsRepository settingsRepository, EncryptionService encryptionService) {
+        this.settingsRepository = settingsRepository;
+        this.encryptionService = encryptionService;
+    }
+
+    @Override
+    public LDAPSettings getSettings() {
+        return new LDAPSettings(
+                settingsRepository.getBoolean(LDAPSettings.class, "enabled", false),
+                settingsRepository.getString(LDAPSettings.class, "url", ""),
+                settingsRepository.getString(LDAPSettings.class, "searchBase", ""),
+                settingsRepository.getString(LDAPSettings.class, "searchFilter", ""),
+                settingsRepository.getString(LDAPSettings.class, "user", ""),
+                settingsRepository.getPassword(LDAPSettings.class, "password", "", encryptionService::decrypt),
+                settingsRepository.getString(LDAPSettings.class, "fullNameAttribute", ""),
+                settingsRepository.getString(LDAPSettings.class, "emailAttribute", ""),
+                settingsRepository.getString(LDAPSettings.class, "groupAttribute", ""),
+                settingsRepository.getString(LDAPSettings.class, "groupFilter", "")
+        );
+    }
+
+    @Override
+    public Class<LDAPSettings> getSettingsClass() {
+        return LDAPSettings.class;
+    }
+}
