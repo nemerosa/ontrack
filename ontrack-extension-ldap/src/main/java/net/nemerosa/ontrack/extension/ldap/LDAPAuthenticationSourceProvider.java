e2b250d7b2909988efc2761aab8ce35a2af4608d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-09-10 14:32:35 +0200

    #320 Moved the `LDAPAuthenticationSourceProvider` to the `ldap` module

--- ontrack-extension-ldap/src/main/java/net/nemerosa/ontrack/extension/ldap/LDAPAuthenticationSourceProvider.java
+++ ontrack-extension-ldap/src/main/java/net/nemerosa/ontrack/extension/ldap/LDAPAuthenticationSourceProvider.java
@@ -1,11 +1,10 @@
-package net.nemerosa.ontrack.service.security.ldap;
+package net.nemerosa.ontrack.extension.ldap;
 
 import net.nemerosa.ontrack.model.security.AuthenticationSource;
 import net.nemerosa.ontrack.model.security.AuthenticationSourceProvider;
 import org.springframework.stereotype.Component;
 
 @Component
-@Deprecated
 public class LDAPAuthenticationSourceProvider implements AuthenticationSourceProvider {
 
     private final AuthenticationSource source = AuthenticationSource.of(
