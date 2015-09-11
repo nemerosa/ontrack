e4cb8cdfb99429ef99528f586b39a33e701b9bd6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-09-11 14:51:47 +0200

    #86 LDAP mapping page - creating a mapping

--- ontrack-web/src/app/extension/ldap/ldap.js
+++ ontrack-web/src/app/extension/ldap/ldap.js
@@ -1,5 +1,4 @@
-angular.module('ontrack.extension.ldap', [
-])
+angular.module('ontrack.extension.ldap', [])
     .config(function ($stateProvider) {
         // Artifactory configurations
         $stateProvider.state('ldap-mapping', {
@@ -12,5 +11,27 @@ angular.module('ontrack.extension.ldap', [
         var view = ot.view();
         view.title = 'LDAP Mappings';
         view.description = 'Mapping from LDAP groups to Ontrack groups.';
+
+        // Loading the mappings
+        function loadMappings() {
+            ot.pageCall($http.get('extension/ldap/ldap-mapping')).then(function (mappingResources) {
+                $scope.mappingResources = mappingResources;
+                // Commands
+                view.commands = [
+                    {
+                        id: 'ldap-mapping-create',
+                        name: "Create mapping",
+                        cls: 'ot-command-new',
+                        action: function () {
+                            otFormService.create($scope.mappingResources._create, "Mapping creation").then(loadMappings);
+                        }
+                    },
+                    ot.viewApiCommand(mappingResources._self),
+                    ot.viewCloseCommand('/admin-accounts')
+                ];
+            });
+        }
+
+        loadMappings();
     })
 ;
