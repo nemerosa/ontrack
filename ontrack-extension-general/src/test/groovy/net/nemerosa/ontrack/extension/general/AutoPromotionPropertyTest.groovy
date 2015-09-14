bbaeda1b88892b3b809c5e2b06fe99fce37eefd8
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-09-14 09:15:16 +0200

    #316 Unit test for the `contains` method

--- ontrack-extension-general/src/test/groovy/net/nemerosa/ontrack/extension/general/AutoPromotionPropertyTest.groovy
+++ ontrack-extension-general/src/test/groovy/net/nemerosa/ontrack/extension/general/AutoPromotionPropertyTest.groovy
@@ -64,4 +64,100 @@ class AutoPromotionPropertyTest {
         assert !property.contains(vs1)
     }
 
+    @Test
+    void 'Inclusion based on pattern'() {
+        def vs1 = ValidationStamp.of(
+                branch,
+                nd('CI.1', '')
+        ).withId(ID.of(1))
+        def vs2 = ValidationStamp.of(
+                branch,
+                nd('CI.2', '')
+        ).withId(ID.of(2))
+        def qa = ValidationStamp.of(
+                branch,
+                nd('QA', '')
+        ).withId(ID.of(3))
+
+        AutoPromotionProperty property = new AutoPromotionProperty(
+                [], 'CI.*', ''
+        )
+
+        assert property.contains(vs1)
+        assert property.contains(vs2)
+        assert !property.contains(qa)
+    }
+
+    @Test
+    void 'Inclusion based on pattern and list'() {
+        def vs1 = ValidationStamp.of(
+                branch,
+                nd('CI.1', '')
+        ).withId(ID.of(1))
+        def vs2 = ValidationStamp.of(
+                branch,
+                nd('CI.2', '')
+        ).withId(ID.of(2))
+        def qa = ValidationStamp.of(
+                branch,
+                nd('QA', '')
+        ).withId(ID.of(3))
+
+        AutoPromotionProperty property = new AutoPromotionProperty(
+                [qa], 'CI.*', ''
+        )
+
+        assert property.contains(vs1)
+        assert property.contains(vs2)
+        assert property.contains(qa)
+    }
+
+    @Test
+    void 'Inclusion and exclusion based on pattern'() {
+        def vs1 = ValidationStamp.of(
+                branch,
+                nd('CI.1', '')
+        ).withId(ID.of(1))
+        def vs2 = ValidationStamp.of(
+                branch,
+                nd('CI.NIGHT', '')
+        ).withId(ID.of(2))
+        def qa = ValidationStamp.of(
+                branch,
+                nd('QA', '')
+        ).withId(ID.of(3))
+
+        AutoPromotionProperty property = new AutoPromotionProperty(
+                [], 'CI.*', '.*NIGHT.*'
+        )
+
+        assert property.contains(vs1)
+        assert !property.contains(vs2)
+        assert !property.contains(qa)
+    }
+
+    @Test
+    void 'Inclusion and exclusion based on pattern and list'() {
+        def vs1 = ValidationStamp.of(
+                branch,
+                nd('CI.1', '')
+        ).withId(ID.of(1))
+        def vs2 = ValidationStamp.of(
+                branch,
+                nd('CI.NIGHT', '')
+        ).withId(ID.of(2))
+        def qa = ValidationStamp.of(
+                branch,
+                nd('QA', '')
+        ).withId(ID.of(3))
+
+        AutoPromotionProperty property = new AutoPromotionProperty(
+                [qa], 'CI.*', '.*NIGHT.*'
+        )
+
+        assert property.contains(vs1)
+        assert !property.contains(vs2)
+        assert property.contains(qa)
+    }
+
 }
