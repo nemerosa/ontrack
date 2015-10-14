8d14922ccb2210d7b5584cadf433bf20334dba50
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-10-14 20:12:54 +0200

    #181 Debian package - post install including the /opt/ontrack directory

--- gradle/os-package/postInstall.sh
+++ gradle/os-package/postInstall.sh
@@ -3,7 +3,8 @@
 useradd ontrack
 
 mkdir -p /usr/lib/ontrack
-chown ontrack:ontrack /usr/lib/ontrack
-
 mkdir -p /var/log/ontrack
+
+chown ontrack:ontrack /opt/ontrack
+chown ontrack:ontrack /usr/lib/ontrack
 chown ontrack:ontrack /var/log/ontrack
