953f6aa215424801a776a089c6b09ebdb4e5eb95
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-10-18 21:05:54 +0200

    #181 RPM acceptance tests for CentOS 7

--- gradle/os-package/postInstall.sh
+++ gradle/os-package/postInstall.sh
@@ -1,10 +1,3 @@
-#!/usr/bin/env bash
-
-useradd ontrack
-
-mkdir -p /usr/lib/ontrack
-mkdir -p /var/log/ontrack
-
-chown ontrack:ontrack /opt/ontrack
-chown ontrack:ontrack /usr/lib/ontrack
-chown ontrack:ontrack /var/log/ontrack
+/usr/bin/chown ontrack:ontrack /opt/ontrack
+/usr/bin/chown ontrack:ontrack /usr/lib/ontrack
+/usr/bin/chown ontrack:ontrack /var/log/ontrack
---
+++ gradle/os-package/preInstall.sh
@@ -0,0 +1,4 @@
+/usr/sbin/useradd ontrack
+
+mkdir -p /usr/lib/ontrack
+mkdir -p /var/log/ontrack
