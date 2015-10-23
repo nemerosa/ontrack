2e8c0e030d4190bc8673914ca809171c70b52eba
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-10-23 09:46:10 +0200

    #333 SVN branch configuration migration - logging & correction

--- ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/support/BuildSvnRevisionLinkMigrationAction.java
+++ ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/support/BuildSvnRevisionLinkMigrationAction.java
@@ -2,6 +2,7 @@ package net.nemerosa.ontrack.extension.svn.support;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import net.nemerosa.ontrack.client.JsonClientMappingException;
 import net.nemerosa.ontrack.extension.scm.support.TagPattern;
 import net.nemerosa.ontrack.extension.svn.property.SVNBranchConfigurationPropertyType;
 import net.nemerosa.ontrack.json.ObjectMapperFactory;
@@ -9,8 +10,12 @@ import net.nemerosa.ontrack.model.structure.ServiceConfiguration;
 import net.nemerosa.ontrack.model.support.DBMigrationAction;
 import net.nemerosa.ontrack.model.support.NoConfig;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -24,11 +29,13 @@ import java.util.regex.Pattern;
 @Component
 public class BuildSvnRevisionLinkMigrationAction implements DBMigrationAction {
 
+    private final Logger logger = LoggerFactory.getLogger(BuildSvnRevisionLinkMigrationAction.class);
     private final ObjectMapper objectMapper = ObjectMapperFactory.create();
 
     private final RevisionSvnRevisionLink revisionLink;
     private final TagNamePatternSvnRevisionLink tagPatternLink;
 
+    @Autowired
     public BuildSvnRevisionLinkMigrationAction(RevisionSvnRevisionLink revisionLink, TagNamePatternSvnRevisionLink tagPatternLink) {
         this.revisionLink = revisionLink;
         this.tagPatternLink = tagPatternLink;
@@ -42,18 +49,18 @@ public class BuildSvnRevisionLinkMigrationAction implements DBMigrationAction {
     @Override
     public void migrate(Connection connection) throws Exception {
         // For all Svn branch configurations
-        try (PreparedStatement ps = connection.prepareStatement("SELECT * FROM CONFIGURATIONS WHERE TYPE = ?", ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE)) {
+        try (PreparedStatement ps = connection.prepareStatement("SELECT * FROM PROPERTIES WHERE TYPE = ?", ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE)) {
             ps.setString(1, SVNBranchConfigurationPropertyType.class.getName());
             try (ResultSet rs = ps.executeQuery()) {
                 while (rs.next()) {
                     // Configuration as JSON
-                    String json = rs.getString("CONTENT");
+                    String json = rs.getString("JSON");
                     // Parses the configuration as JSON
                     ObjectNode node = (ObjectNode) objectMapper.readTree(json);
                     // Migrates the node
                     migrate(node);
                     // Updating
-                    rs.updateString("CONTENT", objectMapper.writeValueAsString(node));
+                    rs.updateString("JSON", objectMapper.writeValueAsString(node));
                     rs.updateRow();
                 }
             }
@@ -62,6 +69,7 @@ public class BuildSvnRevisionLinkMigrationAction implements DBMigrationAction {
 
     protected void migrate(ObjectNode node) {
         // Gets the build path & branch path
+        String branchPath = node.get("branchPath").asText();
         String buildPath = node.get("buildPath").asText();
         // Removes the build path property
         node.remove("buildPath");
@@ -73,6 +81,17 @@ public class BuildSvnRevisionLinkMigrationAction implements DBMigrationAction {
         ServiceConfiguration serviceConfiguration = configuredBuildSvnRevisionLink.toServiceConfiguration();
         // As json...
         node.put("buildRevisionLink", (ObjectNode) objectMapper.valueToTree(serviceConfiguration));
+        // Logging
+        try {
+            logger.info(
+                    "SVN branch config for {} with build expression {} has been converted to {}",
+                    branchPath,
+                    buildPath,
+                    objectMapper.writeValueAsString(serviceConfiguration)
+            );
+        } catch (IOException ex) {
+            throw new JsonClientMappingException(ex);
+        }
     }
 
     protected ConfiguredBuildSvnRevisionLink<?> toBuildSvnRevisionLinkConfiguration(String buildPath) {
