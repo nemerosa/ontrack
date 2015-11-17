dd55daa4934341dc2092a5f0f8719192c92e4a63
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-11-17 10:50:45 +0100

    #294 Direct injection

--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/ApplicationInfoServiceImpl.java
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/ApplicationInfoServiceImpl.java
@@ -3,22 +3,27 @@ package net.nemerosa.ontrack.service;
 import net.nemerosa.ontrack.model.support.ApplicationInfo;
 import net.nemerosa.ontrack.model.support.ApplicationInfoProvider;
 import net.nemerosa.ontrack.model.support.ApplicationInfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class ApplicationInfoServiceImpl implements ApplicationInfoService {
 
+    private final Logger logger = LoggerFactory.getLogger(ApplicationInfoService.class);
+
     private final List<ApplicationInfoProvider> providers;
 
     @Autowired
-    public ApplicationInfoServiceImpl(ApplicationContext applicationContext) {
-        this.providers = new ArrayList<>(applicationContext.getBeansOfType(ApplicationInfoProvider.class).values());
+    public ApplicationInfoServiceImpl(List<ApplicationInfoProvider> providers) {
+        this.providers = providers;
+        providers.forEach(provider ->
+                        logger.info("[info] Info provided: {}", provider.getClass().getName())
+        );
     }
 
     @Override
