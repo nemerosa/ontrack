53b0ad45ccf1b78b6aea072fc9de0da4f08e2b4f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-11-23 13:15:07 +0100

    #344 Removed deprecated class

--- ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/service/SVNConfigurationServiceImpl.java
+++ ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/service/SVNConfigurationServiceImpl.java
@@ -17,8 +17,6 @@ import net.nemerosa.ontrack.model.support.OntrackConfigProperties;
 import net.nemerosa.ontrack.tx.Transaction;
 import net.nemerosa.ontrack.tx.TransactionService;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,10 +30,7 @@ import java.util.Objects;
 @Transactional
 public class SVNConfigurationServiceImpl extends AbstractConfigurationService<SVNConfiguration> implements SVNConfigurationService {
 
-    private final Logger logger = LoggerFactory.getLogger(SVNConfigurationService.class);
-
     private final SVNRepositoryDao repositoryDao;
-    private final OntrackConfigProperties ontrackConfigProperties;
     private final TransactionService transactionService;
     private final SVNClient svnClient;
 
@@ -43,7 +38,6 @@ public class SVNConfigurationServiceImpl extends AbstractConfigurationService<SV
     public SVNConfigurationServiceImpl(ConfigurationRepository configurationRepository, SecurityService securityService, EncryptionService encryptionService, SVNRepositoryDao repositoryDao, EventPostService eventPostService, EventFactory eventFactory, OntrackConfigProperties ontrackConfigProperties, TransactionService transactionService, SVNClient svnClient) {
         super(SVNConfiguration.class, configurationRepository, securityService, encryptionService, eventPostService, eventFactory, ontrackConfigProperties);
         this.repositoryDao = repositoryDao;
-        this.ontrackConfigProperties = ontrackConfigProperties;
         this.transactionService = transactionService;
         this.svnClient = svnClient;
     }
@@ -67,42 +61,36 @@ public class SVNConfigurationServiceImpl extends AbstractConfigurationService<SV
                     url
             );
         }
-        if (ontrackConfigProperties.isConfigurationTest()) {
-            //noinspection unused
-            try (Transaction tx = transactionService.start()) {
-                // Creates a repository
-                SVNRepository repository = SVNRepository.of(
-                        0,
-                        configuration,
-                        null
-                );
-                // Configuration URL
-                SVNURL svnurl = SVNUtils.toURL(configuration.getUrl());
-                // Connection to the root
-                if (!svnClient.exists(
-                        repository,
-                        svnurl,
-                        SVNRevision.HEAD
-                )) {
-                    return ConnectionResult.error(configuration.getUrl() + " does not exist.");
-                }
-                // Gets base info
-                SVNInfo info = svnClient.getInfo(repository, svnurl, SVNRevision.HEAD);
-                // Checks the repository root
-                if (!Objects.equals(
-                        info.getRepositoryRootURL(),
-                        svnurl
-                )) {
-                    return ConnectionResult.error(configuration.getUrl() + " must be the root of the repository.");
-                }
-                // OK
-                return ConnectionResult.ok();
-            } catch (Exception ex) {
-                return ConnectionResult.error(ex.getMessage());
+        try (Transaction tx = transactionService.start()) {
+            // Creates a repository
+            SVNRepository repository = SVNRepository.of(
+                    0,
+                    configuration,
+                    null
+            );
+            // Configuration URL
+            SVNURL svnurl = SVNUtils.toURL(configuration.getUrl());
+            // Connection to the root
+            if (!svnClient.exists(
+                    repository,
+                    svnurl,
+                    SVNRevision.HEAD
+            )) {
+                return ConnectionResult.error(configuration.getUrl() + " does not exist.");
             }
-        } else {
-            logger.warn("[svn] SVN configuration URL checks have been disabled.");
+            // Gets base info
+            SVNInfo info = svnClient.getInfo(repository, svnurl, SVNRevision.HEAD);
+            // Checks the repository root
+            if (!Objects.equals(
+                    info.getRepositoryRootURL(),
+                    svnurl
+            )) {
+                return ConnectionResult.error(configuration.getUrl() + " must be the root of the repository.");
+            }
+            // OK
             return ConnectionResult.ok();
+        } catch (Exception ex) {
+            return ConnectionResult.error(ex.getMessage());
         }
     }
 }
