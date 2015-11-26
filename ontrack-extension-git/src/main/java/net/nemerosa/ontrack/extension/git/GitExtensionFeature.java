6920d3147fe865feacaee91265184108a69754de
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-11-26 21:09:58 +0100

    #357 `git` extension

--- ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/GitExtensionFeature.java
+++ ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/GitExtensionFeature.java
@@ -1,12 +1,13 @@
 package net.nemerosa.ontrack.extension.git;
 
 import net.nemerosa.ontrack.extension.support.AbstractExtensionFeature;
+import net.nemerosa.ontrack.model.extension.ExtensionFeatureOptions;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GitExtensionFeature extends AbstractExtensionFeature {
 
     public GitExtensionFeature() {
-        super("git", "Git", "Support for Git");
+        super("git", "Git", "Support for Git", ExtensionFeatureOptions.DEFAULT.withGui(true));
     }
 }
