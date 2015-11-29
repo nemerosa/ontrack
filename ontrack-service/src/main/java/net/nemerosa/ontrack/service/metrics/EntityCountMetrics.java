196a3ba567206a2deabd2771121e4b45087fceee
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-11-29 20:16:14 +0100

    #362 Entity counts are actually gauges

--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/metrics/EntityCountMetrics.java
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/metrics/EntityCountMetrics.java
@@ -24,16 +24,16 @@ public class EntityCountMetrics implements OntrackMetrics {
     @Transactional(readOnly = true)
     public Collection<Metric<?>> metrics() {
         return Arrays.asList(
-                new Metric<>("counter.entity.project", repository.getProjectCount()),
-                new Metric<>("counter.entity.branch", repository.getBranchCount()),
-                new Metric<>("counter.entity.build", repository.getBuildCount()),
-                new Metric<>("counter.entity.promotionLevel", repository.getPromotionLevelCount()),
-                new Metric<>("counter.entity.promotionRun", repository.getPromotionRunCount()),
-                new Metric<>("counter.entity.validationStamp", repository.getValidationStampCount()),
-                new Metric<>("counter.entity.validationRun", repository.getValidationRunCount()),
-                new Metric<>("counter.entity.validationRunStatus", repository.getValidationRunStatusCount()),
-                new Metric<>("counter.entity.property", repository.getPropertyCount()),
-                new Metric<>("counter.entity.event", repository.getEventCount())
+                new Metric<>("gauge.entity.project", repository.getProjectCount()),
+                new Metric<>("gauge.entity.branch", repository.getBranchCount()),
+                new Metric<>("gauge.entity.build", repository.getBuildCount()),
+                new Metric<>("gauge.entity.promotionLevel", repository.getPromotionLevelCount()),
+                new Metric<>("gauge.entity.promotionRun", repository.getPromotionRunCount()),
+                new Metric<>("gauge.entity.validationStamp", repository.getValidationStampCount()),
+                new Metric<>("gauge.entity.validationRun", repository.getValidationRunCount()),
+                new Metric<>("gauge.entity.validationRunStatus", repository.getValidationRunStatusCount()),
+                new Metric<>("gauge.entity.property", repository.getPropertyCount()),
+                new Metric<>("gauge.entity.event", repository.getEventCount())
         );
     }
 
