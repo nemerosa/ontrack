f6fec822d60585740375f57cf578eecfecaff834
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-11-30 09:24:35 +0100

    #357 `combined` extension

--- ontrack-extension-combined/src/main/java/net/nemerosa/ontrack/extension/issues/combined/CombinedIssueServiceExtensionFeature.java
+++ ontrack-extension-combined/src/main/java/net/nemerosa/ontrack/extension/issues/combined/CombinedIssueServiceExtensionFeature.java
@@ -1,12 +1,14 @@
 package net.nemerosa.ontrack.extension.issues.combined;
 
 import net.nemerosa.ontrack.extension.support.AbstractExtensionFeature;
+import net.nemerosa.ontrack.model.extension.ExtensionFeatureOptions;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CombinedIssueServiceExtensionFeature extends AbstractExtensionFeature {
 
     public CombinedIssueServiceExtensionFeature() {
-        super("combined", "Combined issue service", "This issue service allows to combine several issue services together");
+        super("combined", "Combined issue service", "This issue service allows to combine several issue services together",
+                ExtensionFeatureOptions.DEFAULT.withGui(true));
     }
 }
