3afd9953e4b0d782e48609fc4cc3dac83ea26aef
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-11-30 09:21:53 +0100

    #357 `svn` extension

--- ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/SVNExtensionFeature.java
+++ ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/SVNExtensionFeature.java
@@ -1,11 +1,12 @@
 package net.nemerosa.ontrack.extension.svn;
 
 import net.nemerosa.ontrack.extension.support.AbstractExtensionFeature;
+import net.nemerosa.ontrack.model.extension.ExtensionFeatureOptions;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SVNExtensionFeature extends AbstractExtensionFeature {
     public SVNExtensionFeature() {
-        super("svn", "Subversion", "Support for Subversion");
+        super("svn", "Subversion", "Support for Subversion", ExtensionFeatureOptions.DEFAULT.withGui(true));
     }
 }
