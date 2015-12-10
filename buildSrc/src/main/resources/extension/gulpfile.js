318a65394d94ca0eea4468020f27e7620746d304
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-12-10 19:40:05 +0100

    #363 Angular concatenation

--- buildSrc/src/main/resources/extension/gulpfile.js
+++ buildSrc/src/main/resources/extension/gulpfile.js
@@ -9,6 +9,7 @@ var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var templateCache = require('gulp-angular-templatecache');
 var ngAnnotate = require('gulp-ng-annotate');
+var ngFilesort = require('gulp-angular-filesort');
 var debug = require('gulp-debug');
 var minimist = require('minimist');
 
@@ -26,6 +27,7 @@ var options = minimist(process.argv.slice(2), knownOptions);
 var src = options.src;
 
 var templateSources = src + '/**/*.html';
+var jsSources = src + '/**/*.js';
 
 // Targets
 
@@ -33,6 +35,7 @@ var build = '.'; // Current directory
 
 var buildPath = build + '/dev';
 var buildTemplates = buildPath + '/templates';
+var buildAngular = buildPath + '/angular';
 
 // NG templates
 
@@ -45,6 +48,29 @@ gulp.task('templates', function () {
         ;
 });
 
+// JS Linting
+
+gulp.task('js:lint', function () {
+    return gulp.src(jsSources)
+        .pipe(debug({title: 'lint:'}))
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        ;
+});
+
+// Sorted and annotated Angular files
+
+gulp.task('js:angular', ['js:lint', 'templates'], function () {
+    return gulp.src([buildTemplates + '/*.js', jsSources])
+        .pipe(debug({title: 'js:angular:input'}))
+        .pipe(ngAnnotate())
+        .pipe(ngFilesort())
+        .pipe(concat('ci-angular.js'))
+        .pipe(gulp.dest(buildAngular))
+        .pipe(debug({title: 'js:angular:output'}));
+});
+
 // Default build
 
-gulp.task('default', ['templates']);
+//gulp.task('default', ['templates']);
+gulp.task('default', ['js:angular']);
