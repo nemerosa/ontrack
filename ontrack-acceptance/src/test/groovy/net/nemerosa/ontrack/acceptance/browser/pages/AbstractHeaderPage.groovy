907d11b22bf40be8fffea64131d78eb620fd8ae8
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-12-12 15:41:29 +0100

    #363 Acceptance test for GitHub configurations

--- ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/browser/pages/AbstractHeaderPage.groovy
+++ ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/browser/pages/AbstractHeaderPage.groovy
@@ -1,6 +1,7 @@
 package net.nemerosa.ontrack.acceptance.browser.pages
 
 import net.nemerosa.ontrack.acceptance.browser.Browser
+import net.nemerosa.ontrack.acceptance.browser.Page
 import net.nemerosa.ontrack.acceptance.browser.modules.HeaderModule
 import org.openqa.selenium.By
 import org.openqa.selenium.WebElement
@@ -10,6 +11,9 @@ public abstract class AbstractHeaderPage extends AbstractPage {
 
     @FindBy(className = 'ot-view-title')
     protected WebElement pageTitle;
+
+    @FindBy(id = 'header-user-menu')
+    protected WebElement userMenu
 
     private final HeaderModule header;
 
@@ -29,6 +33,26 @@ public abstract class AbstractHeaderPage extends AbstractPage {
 
     public void login(String user, String password) {
         header.login(user, password);
+    }
+
+    /**
+     * Navigates to a user menu item using its ID and returns the target page
+     */
+    def <P extends Page> P selectUserMenu(Class<P> pageClass, String id) {
+        // Clicks on the user menu
+        userMenu.click()
+        // Item to select
+        WebElement item = browser.findElement(By.id(id))
+        // Waits until the item is selected
+        browser.waitUntil(id) { item.displayed }
+        // Clicks on the item
+        item.click()
+        // Selects the page
+        P page = browser.page(pageClass)
+        // Waits for the page
+        page.waitFor()
+        // OK
+        return page
     }
 
     APIPage goToAPI() {
---
+++ ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/browser/pages/GitHubConfigurationPage.groovy
@@ -0,0 +1,41 @@
+package net.nemerosa.ontrack.acceptance.browser.pages
+
+import net.nemerosa.ontrack.acceptance.browser.Browser
+import net.nemerosa.ontrack.acceptance.browser.dialogs.GitHubConfigurationDialog
+import org.openqa.selenium.By
+
+/**
+ * GitHub configuration page.
+ */
+class GitHubConfigurationPage extends AbstractHeaderPage {
+
+    GitHubConfigurationPage(Browser browser) {
+        super(browser)
+    }
+
+    @Override
+    String getPath(Map<String, Object> parameters) {
+        "index.html#/extension/github/configurations"
+    }
+
+    public GitHubConfigurationDialog createConfiguration(Closure closure) {
+        def command = $('.ot-command-new')
+        command.click()
+        GitHubConfigurationDialog dialog = new GitHubConfigurationDialog(browser).waitFor()
+        closure.delegate = dialog
+        closure(dialog)
+        dialog.ok()
+        return dialog
+    }
+
+    /**
+     * Get the configuration for a given name
+     */
+    Map<String, String> getConfiguration(String name) {
+        def line = browser.findElement(By.cssSelector("tr[name='${name}']"))
+        return [
+                name: line.findElement(By.cssSelector("td:first-child > code")).text,
+                url: line.findElement(By.cssSelector("td:nth-child(2) > a")).text,
+        ]
+    }
+}
