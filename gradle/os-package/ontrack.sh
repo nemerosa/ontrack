7071a7aba82939784760e4c98fc838d4c8262e39
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-12-17 08:55:09 +0100

    #359 OS package having an extension directory

--- gradle/os-package/ontrack.sh
+++ gradle/os-package/ontrack.sh
@@ -32,6 +32,12 @@ LOG_DIR="/var/log/$PROJECT_NAME"
 # data directory
 DATA_DIR="/usr/lib/$PROJECT_NAME"
 
+# extension directory
+EXT_DIR="$DATA_DIR/extensions"
+
+# java options
+SPRINGBOOTAPP_JAVA_OPTIONS="-Dloader.path=$EXT_DIR"
+
 # spring boot options
 SPRINGBOOTAPP_OPTIONS="--logging.file=$LOG_DIR/$PROJECT_NAME.log --ontrack.config.applicationWorkingDir=$DATA_DIR/files \"--spring.datasource.url=jdbc:h2:$DATA_DIR/database/data;MODE=MYSQL;DB_CLOSE_ON_EXIT=FALSE;DEFRAG_ALWAYS=TRUE\""
 
@@ -45,7 +51,7 @@ start() {
     echo -n $"Starting $PROJECT_NAME: "
 
     cd "$DATA_DIR"
-    su $SERVICE_USER -c "nohup $SPRINGBOOTAPP_JAVA -jar \"$SPRINGBOOTAPP_JAR\" $SPRINGBOOTAPP_OPTIONS  >> /dev/null 2>&1 &"
+    su $SERVICE_USER -c "nohup $SPRINGBOOTAPP_JAVA $SPRINGBOOTAPP_JAVA_OPTIONS -jar \"$SPRINGBOOTAPP_JAR\" $SPRINGBOOTAPP_OPTIONS  >> /dev/null 2>&1 &"
 
     cnt=10
     while ! { pid_of_spring_boot > /dev/null ; } && [ $cnt -gt 0 ] ; do
