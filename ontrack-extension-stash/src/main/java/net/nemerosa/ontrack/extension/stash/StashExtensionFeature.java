21a655b97a300108e67886251778965278a61c00
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-01-11 21:17:16 +0100

    #375 Different behaviour between BitBucket Cloud & Server

--- ontrack-extension-stash/src/main/java/net/nemerosa/ontrack/extension/stash/StashExtensionFeature.java
+++ ontrack-extension-stash/src/main/java/net/nemerosa/ontrack/extension/stash/StashExtensionFeature.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class StashExtensionFeature extends AbstractExtensionFeature {
     public StashExtensionFeature() {
-        super("stash", "Stash", "Support for Atlassian Stash");
+        super("stash", "BitBucket", "Support for Atlassian BitBucket (Cloud & Server)");
     }
 }
--- ontrack-web/src/app/extension/stash/stashConfigurations.js
+++ ontrack-web/src/app/extension/stash/stashConfigurations.js
@@ -14,8 +14,8 @@ angular.module('ot.extension.stash.configuration', [
     })
     .controller('StashConfigurationsCtrl', function ($scope, $http, ot, otFormService, otAlertService, otConfigurationService) {
         var view = ot.view();
-        view.title = 'Stash configurations';
-        view.description = 'Management of the Stash configurations.';
+        view.title = 'BitBucket configurations';
+        view.description = 'Management of the BitBucket configurations.';
 
         // Loading the configurations
         function load() {
@@ -40,7 +40,7 @@ angular.module('ot.extension.stash.configuration', [
         $scope.createConfiguration = function () {
             otFormService.display({
                 uri: $scope.configurations._create,
-                title: "Stash configuration",
+                title: "BitBucket configuration",
                 buttons: [ otConfigurationService.testButton($scope.configurations._test) ],
                 submit: function (data) {
                     return ot.call($http.post($scope.configurations._create, data));
@@ -52,7 +52,7 @@ angular.module('ot.extension.stash.configuration', [
         $scope.deleteConfiguration = function (configuration) {
             otAlertService.confirm({
                 title: 'Stash configuration',
-                message: "Do you really want to delete this Stash configuration? Some projects may still refer to it."
+                message: "Do you really want to delete this BitBucket configuration? Some projects may still refer to it."
             }).then(
                 function success() {
                     ot.call($http.delete(configuration._delete)).then(load);
@@ -64,7 +64,7 @@ angular.module('ot.extension.stash.configuration', [
         $scope.updateConfiguration = function (configuration) {
             otFormService.display({
                 uri: configuration._update,
-                title: "Stash configuration",
+                title: "BitBucket configuration",
                 buttons: [ otConfigurationService.testButton($scope.configurations._test) ],
                 submit: function (data) {
                     return ot.call($http.put(configuration._update, data));
