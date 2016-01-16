7286f24fafd3ebf24b3fa9944e026175b304848d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-01-16 11:30:14 +0100

    #377 Git project sync page skeleton

--- ontrack-web/src/extension/git/git.js
+++ ontrack-web/src/extension/git/git.js
@@ -1,10 +1,37 @@
 angular.module('ontrack.extension.git', [
-    'ot.extension.git.configuration',
-    'ot.extension.git.sync',
-    'ot.extension.git.changelog',
-    'ot.extension.git.issue',
-    'ot.extension.git.commit'
-])
+        'ot.extension.git.configuration',
+        'ot.extension.git.sync',
+        'ot.extension.git.changelog',
+        'ot.extension.git.issue',
+        'ot.extension.git.commit',
+        'ot.service.core',
+        'ot.service.structure'
+    ])
+    .config(function ($stateProvider) {
+        // Git project sync
+        $stateProvider.state('git-project-sync', {
+            url: '/extension/git/project-sync/{projectId}',
+            templateUrl: 'extension/git/git.project-sync.tpl.html',
+            controller: 'GitProjectSyncCtrl'
+        });
+    })
+    .controller('GitProjectSyncCtrl', function ($http, $scope, $stateParams, ot, otStructureService) {
+        // Gets the project ID from the parameters
+        var projectId = $stateParams.projectId;
+        // View definition
+        var view = ot.view();
+        view.title = 'Git project synchronisation';
+        // Loading the project
+        otStructureService.getProject(projectId).then(function (project) {
+            $scope.project = project;
+            // Sub page of the project
+            view.breadcrumbs = ot.projectBreadcrumbs(project);
+            // Commands
+            view.commands = [
+                ot.viewCloseCommand('/project/' + project.id)
+            ];
+        });
+    })
     .directive('otExtensionGitCommitSummary', function () {
         return {
             restrict: 'E',
