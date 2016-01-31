cb4b09db780104ac18aef02c4ed5a02d7737b7e2
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-01-31 14:17:01 +0100

    Merge branch 'master' into feature/359-plugin
    # Conflicts:
    #	ontrack-extension-git/src/test/groovy/net/nemerosa/ontrack/extension/git/service/GitBuildSyncIT.groovy
    #	ontrack-extension-stash/src/main/java/net/nemerosa/ontrack/extension/stash/StashExtensionFeature.java
    #	ontrack-web/gulpfile.js
    #	ontrack-web/src/extension/git/git.js

--- ontrack-extension-git/src/main/resources/static/extension/git/module.js
+++ ontrack-extension-git/src/main/resources/static/extension/git/module.js
@@ -317,4 +317,53 @@ angular.module('ontrack.extension.git', [
             }
         };
     })
+
+    // Git project sync
+
+    .config(function ($stateProvider) {
+        // Git project sync
+        $stateProvider.state('git-project-sync', {
+            url: '/extension/git/project-sync/{projectId}',
+            templateUrl: 'extension/git/git.project-sync.tpl.html',
+            controller: 'GitProjectSyncCtrl'
+        });
+    })
+    .controller('GitProjectSyncCtrl', function ($http, $scope, $stateParams, ot, otStructureService, otNotificationService) {
+        // Gets the project ID from the parameters
+        var projectId = $stateParams.projectId;
+        // View definition
+        var view = ot.view();
+        view.title = 'Git project synchronisation';
+        // Loading the project
+        otStructureService.getProject(projectId).then(function (project) {
+            $scope.project = project;
+            // Sub page of the project
+            view.breadcrumbs = ot.projectBreadcrumbs(project);
+            // Commands
+            view.commands = [
+                ot.viewCloseCommand('/project/' + project.id)
+            ];
+            // Loads the Git synchronisation information
+            return ot.pageCall($http.get(project._gitSync));
+        }).then(function (gitSyncInfo) {
+            $scope.gitSyncInfo = gitSyncInfo;
+        });
+        // Project synchronisation
+        $scope.projectSync = function (reset) {
+            $scope.synchronising = true;
+            ot.pageCall($http.post($scope.project._gitSync, {reset: reset})).then(function (ack) {
+                if (!ack.success) {
+                    otNotificationService.error("The Git synchronisation could be launched.");
+                } else {
+                    otNotificationService.success("The Git synchronisation has been launched in the background.");
+                }
+                // Loads the Git synchronisation information
+                return ot.pageCall($http.get($scope.project._gitSync));
+            }).then(function (gitSyncInfo){
+                $scope.gitSyncInfo = gitSyncInfo;
+            }).finally(function () {
+                $scope.synchronising = false;
+            });
+        };
+    })
 ;
