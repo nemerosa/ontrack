c865475520ae8d5992c86a4a96122c9effb6c4c7
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-02-01 08:30:43 +0100

    #383 Metrics

--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/job/DefaultJobListener.java
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/job/DefaultJobListener.java
@@ -1,5 +1,6 @@
 package net.nemerosa.ontrack.service.job;
 
+import com.codahale.metrics.MetricRegistry;
 import net.nemerosa.ontrack.job.JobKey;
 import net.nemerosa.ontrack.job.JobListener;
 import net.nemerosa.ontrack.job.JobRunProgress;
@@ -8,43 +9,42 @@ import net.nemerosa.ontrack.model.structure.NameDescription;
 import net.nemerosa.ontrack.model.support.ApplicationLogEntry;
 import net.nemerosa.ontrack.model.support.ApplicationLogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class DefaultJobListener implements JobListener {
 
     private final ApplicationLogService logService;
-
-    // TODO Metrics
-
-    /**
-     * String jobCategoryMetric = "job-category." + registeredJob.getJobCategory();
-     * String jobMetric = "job";
-     * Timer.Context jobTime = metricRegistry.timer(jobMetric).time();
-     * Timer.Context jobCategoryTime = metricRegistry.timer(jobCategoryMetric).time();
-     * try {
-     * counterService.increment(jobMetric);
-     * counterService.increment(jobCategoryMetric);
-     * wrappedTask.run();
-     * } finally {
-     * counterService.decrement(jobMetric);
-     * counterService.decrement(jobCategoryMetric);
-     * jobTime.stop();
-     * jobCategoryTime.stop();
-     * }
-     */
+    private final MetricRegistry metricRegistry;
+    private final CounterService counterService;
 
     @Autowired
-    public DefaultJobListener(ApplicationLogService logService) {
+    public DefaultJobListener(ApplicationLogService logService, MetricRegistry metricRegistry, CounterService counterService) {
         this.logService = logService;
+        this.metricRegistry = metricRegistry;
+        this.counterService = counterService;
+    }
+
+    protected String getJobTypeMetric(JobKey key) {
+        return "job-type." + key.getType().getKey();
     }
 
     @Override
     public void onJobStart(JobKey key) {
+        counterService.increment("job");
+        counterService.increment(getJobTypeMetric(key));
     }
 
     @Override
     public void onJobEnd(JobKey key, long milliseconds) {
+        counterService.decrement("job");
+        counterService.decrement(getJobTypeMetric(key));
+        // Durations
+        metricRegistry.timer("job").update(milliseconds, TimeUnit.MILLISECONDS);
+        metricRegistry.timer(getJobTypeMetric(key)).update(milliseconds, TimeUnit.MILLISECONDS);
     }
 
     @Override
