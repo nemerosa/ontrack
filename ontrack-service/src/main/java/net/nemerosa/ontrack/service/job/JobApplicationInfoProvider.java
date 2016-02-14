eae7c2e997d952fb84bd5bdcabf72784e860e705
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-02-14 12:07:52 +0100

    #390 Collection of job statuses must run as admin

--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/job/JobApplicationInfoProvider.java
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/job/JobApplicationInfoProvider.java
@@ -3,6 +3,7 @@ package net.nemerosa.ontrack.service.job;
 import net.nemerosa.ontrack.job.JobRunProgress;
 import net.nemerosa.ontrack.job.JobScheduler;
 import net.nemerosa.ontrack.job.JobStatus;
+import net.nemerosa.ontrack.model.security.SecurityService;
 import net.nemerosa.ontrack.model.support.ApplicationInfo;
 import net.nemerosa.ontrack.model.support.ApplicationInfoProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,18 +16,21 @@ import java.util.stream.Collectors;
 public class JobApplicationInfoProvider implements ApplicationInfoProvider {
 
     private final JobScheduler jobScheduler;
+    private final SecurityService securityService;
 
     @Autowired
-    public JobApplicationInfoProvider(JobScheduler jobScheduler) {
+    public JobApplicationInfoProvider(JobScheduler jobScheduler, SecurityService securityService) {
         this.jobScheduler = jobScheduler;
+        this.securityService = securityService;
     }
 
     @Override
     public List<ApplicationInfo> getApplicationInfoList() {
-        return jobScheduler.getJobStatuses().stream()
+        return securityService.asAdmin(() -> jobScheduler.getJobStatuses().stream()
                 .map(this::getApplicationInfo)
                 .filter(info -> info != null)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList())
+        );
     }
 
     private ApplicationInfo getApplicationInfo(JobStatus status) {
