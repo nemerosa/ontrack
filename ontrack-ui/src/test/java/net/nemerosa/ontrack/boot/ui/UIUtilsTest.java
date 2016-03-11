389dbc2975d472b55d9d16ff8b1fc1c6912af383
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-03-11 08:30:04 +0100

    #397 No cache for default image - unit tests

---
+++ ontrack-ui/src/test/java/net/nemerosa/ontrack/boot/ui/UIUtilsTest.java
@@ -0,0 +1,36 @@
+package net.nemerosa.ontrack.boot.ui;
+
+import net.nemerosa.ontrack.common.Document;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class UIUtilsTest {
+
+    @Test
+    public void imageCacheControl() {
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        UIUtils.setupDefaultImageCache(response, new Document("image/png", new byte[10])); // Content does not matter
+        String header = response.getHeader("Cache-Control");
+        assertEquals("max-age=86400, public", header);
+    }
+
+    @Test
+    public void imageConfigurableCacheControl() {
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        UIUtils.setupImageCache(response, new Document("image/png", new byte[10]), 2); // Content does not matter
+        String header = response.getHeader("Cache-Control");
+        assertEquals("max-age=172800, public", header);
+    }
+
+    @Test
+    public void empyImageNoCacheControl() {
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        UIUtils.setupDefaultImageCache(response, Document.EMPTY);
+        String header = response.getHeader("Cache-Control");
+        assertNull(header);
+    }
+
+}
