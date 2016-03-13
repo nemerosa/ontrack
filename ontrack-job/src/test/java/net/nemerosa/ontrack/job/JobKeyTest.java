ab08e3352cfe7b406e3ea50756581cf823a10503
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-03-13 16:20:20 +0100

    #403 Key as string (test)

--- ontrack-job/src/test/java/net/nemerosa/ontrack/job/JobKeyTest.java
+++ ontrack-job/src/test/java/net/nemerosa/ontrack/job/JobKeyTest.java
@@ -4,6 +4,7 @@ import net.nemerosa.ontrack.job.support.CountJob;
 import net.nemerosa.ontrack.job.support.LongCountJob;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class JobKeyTest {
@@ -14,6 +15,12 @@ public class JobKeyTest {
         assertTrue(key.sameCategory(Fixtures.TEST_CATEGORY));
         assertTrue(new CountJob().getKey().sameCategory(Fixtures.TEST_CATEGORY));
         assertTrue(new LongCountJob().getKey().sameCategory(Fixtures.TEST_CATEGORY));
+    }
+
+    @Test
+    public void key_string() {
+        JobKey key = Fixtures.TEST_CATEGORY.getType("my-type").getKey("my-key");
+        assertEquals("[test][my-type][my-key]", key.toString());
     }
 
 }
