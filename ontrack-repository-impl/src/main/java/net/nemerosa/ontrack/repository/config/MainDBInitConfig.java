5b08729775d5c7231ebfc23dc6f247ddb8fe8b49
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-03-16 15:15:44 +0100

    #372 Added missing index on `ENTITY_DATA.NAME`

--- ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/config/MainDBInitConfig.java
+++ ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/config/MainDBInitConfig.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 @Component
 public class MainDBInitConfig extends AbstractDBInitConfig {
 
-    public static final int VERSION = 26;
+    public static final int VERSION = 27;
 
     private final ApplicationContext applicationContext;
 
