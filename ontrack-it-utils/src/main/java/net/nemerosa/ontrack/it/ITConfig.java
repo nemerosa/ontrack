0bae3f76ddac11c6fd60ce953fdd801811f61fa8
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-03-18 21:25:27 +0100

    POSTGRESQL - integration tests

--- ontrack-it-utils/src/main/java/net/nemerosa/ontrack/it/ITConfig.java
+++ ontrack-it-utils/src/main/java/net/nemerosa/ontrack/it/ITConfig.java
@@ -1,8 +1,6 @@
 package net.nemerosa.ontrack.it;
 
 import net.nemerosa.ontrack.common.RunProfile;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.autoconfigure.MetricRepositoryAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.MetricsDropwizardAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -11,12 +9,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.convert.converter.ConverterRegistry;
 import org.springframework.core.convert.support.DefaultConversionService;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-import java.io.IOException;
 
 @Configuration
 @Profile(RunProfile.UNIT_TEST)
@@ -26,28 +19,6 @@ import java.io.IOException;
         MetricsDropwizardAutoConfiguration.class
 })
 public class ITConfig {
-
-    private final Logger logger = LoggerFactory.getLogger(ITConfig.class);
-
-    @Bean
-    public DataSource dataSource() throws IOException {
-        String dbURL = "jdbc:h2:mem:ontrack;MODE=MYSQL;DB_CLOSE_ON_EXIT=FALSE;DEFRAG_ALWAYS=TRUE";
-        logger.info("Using database at {}", dbURL);
-        org.apache.tomcat.jdbc.pool.DataSource pool = new org.apache.tomcat.jdbc.pool.DataSource();
-        pool.setDriverClassName("org.h2.Driver");
-        pool.setUrl(dbURL);
-        pool.setUsername("sa");
-        pool.setPassword("");
-        pool.setDefaultAutoCommit(false);
-        pool.setInitialSize(1);
-        pool.setMaxActive(2);
-        return pool;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager() throws IOException {
-        return new DataSourceTransactionManager(dataSource());
-    }
 
     @Bean
     public ConverterRegistry converterRegistry() {
