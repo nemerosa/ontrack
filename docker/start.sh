10b6a916649e9d518ef24fb5d87a19d57413c20e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-04-05 09:07:41 +0200

    POSTGRESQL - Docker image and default database connection

--- docker/start.sh
+++ docker/start.sh
@@ -12,6 +12,5 @@ java \
     -jar /opt/ontrack/ontrack.jar \
     ${JAVA_OPTIONS} \
     "--spring.profiles.active=${PROFILE}" \
-    "--spring.datasource.url=jdbc:h2:/var/ontrack/data/database/data;MODE=MYSQL;DB_CLOSE_ON_EXIT=FALSE;DEFRAG_ALWAYS=TRUE" \
     "--ontrack.config.applicationWorkingDir=/var/ontrack/data" \
     "--logging.file=/var/ontrack/data/log/ontrack.log"
