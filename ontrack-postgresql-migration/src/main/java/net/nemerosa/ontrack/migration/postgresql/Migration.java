af761a1e157aba22567356b293b79aef7d7fc910
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-04-07 09:53:44 +0200

    POSTGRESQL - Migration - projects (wip)

--- ontrack-postgresql-migration/src/main/java/net/nemerosa/ontrack/migration/postgresql/Migration.java
+++ ontrack-postgresql-migration/src/main/java/net/nemerosa/ontrack/migration/postgresql/Migration.java
@@ -1,14 +1,22 @@
 package net.nemerosa.ontrack.migration.postgresql;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.util.Collections;
+import java.util.Map;
 
 @Component
 public class Migration {
+
+    private final Logger logger = LoggerFactory.getLogger(Migration.class);
+
+    // TODO Transaction templates
 
     private final NamedParameterJdbcTemplate h2;
     private final NamedParameterJdbcTemplate postgresql;
@@ -20,8 +28,28 @@ public class Migration {
     }
 
     public void run() {
+        // Projects
+        migrateProjects();
         // FIXME Method net.nemerosa.ontrack.migration.postgresql.Migration.run
+    }
 
+    private void migrateProjects() {
+        logger.info("Migrating projects...");
+        simpleMigration(
+                "Projects",
+                "SELECT * FROM PROJECTS",
+                Collections.emptyMap(),
+                "INSERT INTO PROJECTS (ID, NAME, DESCRIPTION, DISABLED) VALUES (:id, :name, :description, :disabled)"
+        );
+    }
+
+    private void simpleMigration(String name, String h2Query, Map<String, Object> h2Params, String postgresqlUpdate) {
+        logger.info("Migrating {}...", name);
+        long count = h2.queryForList(h2Query, h2Params)
+                .stream()
+                .map(it -> postgresql.update(postgresqlUpdate, it))
+                .count();
+        logger.info("{} count = {}...", name, count);
     }
 
 }
