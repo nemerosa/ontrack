7c0620340529b7636357ee7eb094a2154825c215
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-04-07 09:46:47 +0200

    POSTGRESQL - Migration - datasources

--- ontrack-postgresql-migration/src/main/java/net/nemerosa/ontrack/migration/postgresql/Migration.java
+++ ontrack-postgresql-migration/src/main/java/net/nemerosa/ontrack/migration/postgresql/Migration.java
@@ -1,9 +1,23 @@
 package net.nemerosa.ontrack.migration.postgresql;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
 
 @Component
 public class Migration {
+
+    private final NamedParameterJdbcTemplate h2;
+    private final NamedParameterJdbcTemplate postgresql;
+
+    @Autowired
+    public Migration(@Qualifier("h2") DataSource h2Datasource, @Qualifier("postgresql") DataSource postgresqlDatasource) {
+        h2 = new NamedParameterJdbcTemplate(h2Datasource);
+        postgresql = new NamedParameterJdbcTemplate(postgresqlDatasource);
+    }
 
     public void run() {
         // FIXME Method net.nemerosa.ontrack.migration.postgresql.Migration.run
