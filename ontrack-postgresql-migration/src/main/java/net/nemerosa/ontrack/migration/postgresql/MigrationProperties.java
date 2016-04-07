8455534e19f4795606a8ddaf37b776530cd0e90f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-04-07 09:44:23 +0200

    POSTGRESQL - Migration - configuration

--- ontrack-postgresql-migration/src/main/java/net/nemerosa/ontrack/migration/postgresql/MigrationProperties.java
+++ ontrack-postgresql-migration/src/main/java/net/nemerosa/ontrack/migration/postgresql/MigrationProperties.java
@@ -1,6 +1,5 @@
 package net.nemerosa.ontrack.migration.postgresql;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
@@ -10,26 +9,14 @@ import org.springframework.stereotype.Component;
 @Data
 public class MigrationProperties {
 
-    private DatabaseProperties h2 = new DatabaseProperties(
-            "jdbc:h2:./work/ontrack/db/data;MODE=MYSQL;DB_CLOSE_ON_EXIT=FALSE;DEFRAG_ALWAYS=TRUE",
-            "ontrack",
-            "ontrack"
-    );
-    private DatabaseProperties postgresql = new DatabaseProperties(
-            "jdbc:postgresql://postgresql/ontrack",
-            "ontrack",
-            "ontrack"
-    );
+    private DatabaseProperties h2 = new DatabaseProperties();
+    private DatabaseProperties postgresql = new DatabaseProperties();
 
     @Data
-    @AllArgsConstructor
     public static class DatabaseProperties {
         private String url;
         private String username;
         private String password;
-
-        public DatabaseProperties() {
-        }
     }
 
 }
