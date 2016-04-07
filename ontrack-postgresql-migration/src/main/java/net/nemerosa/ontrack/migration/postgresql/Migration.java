a5cc52b4e2e1b777ce46f3c9ae30deaca48093ae
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-04-07 09:39:06 +0200

    POSTGRESQL - Migration - tool skeleton

---
+++ ontrack-postgresql-migration/src/main/java/net/nemerosa/ontrack/migration/postgresql/Migration.java
@@ -0,0 +1,13 @@
+package net.nemerosa.ontrack.migration.postgresql;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Migration {
+
+    public void run() {
+        // FIXME Method net.nemerosa.ontrack.migration.postgresql.Migration.run
+
+    }
+
+}
