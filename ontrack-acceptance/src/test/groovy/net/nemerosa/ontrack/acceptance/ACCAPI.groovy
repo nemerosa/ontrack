f930a956f9456dbc6a5ade6be4f1b5676daffa53
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-04-29 17:07:35 +0200

    #420 API controller - refine the test to be independent of ordering

--- ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/ACCAPI.groovy
+++ ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/ACCAPI.groovy
@@ -23,9 +23,9 @@ class ACCAPI extends AcceptanceTestClient {
         def vs = ontrack.validationStamp(project, 'master', 'VS')
         // Gets its API
         def api = ontrack.get("api/describe?path=/structure/validationStamps/${vs.id}")
-        def getMethod = api.methods.find { it.path == 'structure/validationStamps/{validationStampId}' }
+        def getMethod = api.methods.find { it.path == 'structure/validationStamps/{validationStampId}' && it.methods == ['GET'] }
+        assert getMethod != null
         assert getMethod.name == 'Get validation stamp'
-        assert getMethod.methods == ['GET']
     }
 
 }
