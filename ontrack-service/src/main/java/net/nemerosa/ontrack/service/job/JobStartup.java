d6bd69ed6824d2e11675494fcc5a79d9b3aa2f81
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-06-03 10:03:44 +0200

    #425 Make sure to start job registration in admin mode

--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/job/JobStartup.java
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/job/JobStartup.java
@@ -1,6 +1,7 @@
 package net.nemerosa.ontrack.service.job;
 
 import net.nemerosa.ontrack.job.JobScheduler;
+import net.nemerosa.ontrack.model.security.SecurityService;
 import net.nemerosa.ontrack.model.support.JobProvider;
 import net.nemerosa.ontrack.model.support.StartupService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +16,14 @@ import java.util.Collection;
 @Component
 public class JobStartup implements StartupService {
 
+    private final SecurityService securityService;
     private final JobScheduler jobScheduler;
     private final Collection<JobProvider> jobProviders;
 
     @Autowired
-    public JobStartup(JobScheduler jobScheduler, ApplicationContext applicationContext) {
+    public JobStartup(JobScheduler jobScheduler, ApplicationContext applicationContext, SecurityService securityService) {
         this.jobScheduler = jobScheduler;
+        this.securityService = securityService;
         this.jobProviders = applicationContext.getBeansOfType(JobProvider.class).values();
     }
 
@@ -36,11 +39,13 @@ public class JobStartup implements StartupService {
 
     @Override
     public void start() {
-        jobProviders.stream()
-                .flatMap(jobProvider -> jobProvider.getStartingJobs().stream())
-                .forEach(jobRegistration -> jobScheduler.schedule(
-                        jobRegistration.getJob(),
-                        jobRegistration.getSchedule()
-                ));
+        securityService.asAdmin(() ->
+                jobProviders.stream()
+                        .flatMap(jobProvider -> jobProvider.getStartingJobs().stream())
+                        .forEach(jobRegistration -> jobScheduler.schedule(
+                                jobRegistration.getJob(),
+                                jobRegistration.getSchedule()
+                        ))
+        );
     }
 }
