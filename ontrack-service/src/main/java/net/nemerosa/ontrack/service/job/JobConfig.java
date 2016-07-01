71bfbb0f4f539aaa5a289fe15125d9a175153a13
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-07-01 12:24:58 +0200

    #434 Job orchestration - configuration

--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/job/JobConfig.java
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/job/JobConfig.java
@@ -3,6 +3,9 @@ package net.nemerosa.ontrack.service.job;
 import com.codahale.metrics.MetricRegistry;
 import net.nemerosa.ontrack.job.JobListener;
 import net.nemerosa.ontrack.job.JobScheduler;
+import net.nemerosa.ontrack.job.Schedule;
+import net.nemerosa.ontrack.job.orchestrator.JobOrchestrator;
+import net.nemerosa.ontrack.job.orchestrator.JobOrchestratorSupplier;
 import net.nemerosa.ontrack.job.support.DefaultJobScheduler;
 import net.nemerosa.ontrack.model.support.ApplicationLogService;
 import net.nemerosa.ontrack.model.support.OntrackConfigProperties;
@@ -13,6 +16,7 @@ import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collection;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -36,6 +40,19 @@ public class JobConfig {
 
     @Autowired
     private SettingsRepository settingsRepository;
+
+    @Autowired(required = false)
+    private Collection<JobOrchestratorSupplier> jobOrchestratorSuppliers;
+
+    @Bean
+    public JobOrchestrator jobOrchestrator() {
+        return new JobOrchestrator(
+                jobScheduler(),
+                Schedule.EVERY_MINUTE, // TODO Makes this configurable in OntrackConfigProperties
+                "Job orchestration",
+                jobOrchestratorSuppliers
+        );
+    }
 
     @Bean
     public JobListener jobListener() {
