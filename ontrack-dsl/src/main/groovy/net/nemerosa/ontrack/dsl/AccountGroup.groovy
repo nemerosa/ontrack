bb1b1a4c7e0912a906b7567a4018997d994a4009
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-07-19 18:03:46 +0200

    #435 Account group

--- ontrack-dsl/src/main/groovy/net/nemerosa/ontrack/dsl/AccountGroup.groovy
+++ ontrack-dsl/src/main/groovy/net/nemerosa/ontrack/dsl/AccountGroup.groovy
@@ -1,19 +1,25 @@
 package net.nemerosa.ontrack.dsl
 
+import net.nemerosa.ontrack.dsl.doc.DSL
+
+@DSL("Account group. Just a name and a description.")
 class AccountGroup extends AbstractResource {
 
     AccountGroup(Ontrack ontrack, Object node) {
         super(ontrack, node)
     }
 
+    @DSL("Unique ID for the group.")
     int getId() {
         node['id'] as int
     }
 
+    @DSL("Name of the group. Unique.")
     String getName() {
         node['name']
     }
 
+    @DSL("Description of the group.")
     String getDescription() {
         node['description']
     }
