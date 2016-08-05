3d076ec8183ccfb13dcfbe9ba0b1cfd174cb6091
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-08-05 10:22:56 +0200

    #446 Option to disable the SVN build sync

--- ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/service/SVNSyncService.java
+++ ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/service/SVNSyncService.java
@@ -1,14 +1,10 @@
 package net.nemerosa.ontrack.extension.svn.service;
 
 import net.nemerosa.ontrack.extension.svn.model.SVNSyncInfoStatus;
-import net.nemerosa.ontrack.model.structure.Branch;
 import net.nemerosa.ontrack.model.structure.ID;
 
 public interface SVNSyncService {
 
     SVNSyncInfoStatus launchSync(ID branchId);
 
-    void scheduleSVNBuildSync(Branch branch);
-
-    void unscheduleSVNBuildSync(Branch branch);
 }
