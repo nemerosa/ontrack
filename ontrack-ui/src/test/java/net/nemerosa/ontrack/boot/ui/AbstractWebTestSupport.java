1fa0db1756256fdd7f7e586768207e414bd12620
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-08-08 16:20:02 +0200

    #436 Misc upgrades

--- ontrack-ui/src/test/java/net/nemerosa/ontrack/boot/ui/AbstractWebTestSupport.java
+++ ontrack-ui/src/test/java/net/nemerosa/ontrack/boot/ui/AbstractWebTestSupport.java
@@ -2,9 +2,9 @@ package net.nemerosa.ontrack.boot.ui;
 
 import net.nemerosa.ontrack.boot.Application;
 import net.nemerosa.ontrack.it.AbstractServiceTestSupport;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringBootTest(classes = Application.class)
 public abstract class AbstractWebTestSupport extends AbstractServiceTestSupport {
 
 }
