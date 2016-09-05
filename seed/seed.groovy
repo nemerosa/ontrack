ca642d98d25739f95a1c5fea05d42fdf8f30ed2a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-09-05 10:06:42 +0200

    Corrections of pipeline (cherry picked from commit 1fb85de) (cherry picked from commit cc4074e)

--- seed/seed.groovy
+++ seed/seed.groovy
@@ -325,6 +325,7 @@ job("${SEED_PROJECT}-${SEED_BRANCH}-acceptance-local") {
     debAcceptanceTest \\
     -PacceptanceJar=ontrack-acceptance-\${VERSION}.jar \\
     -PacceptanceDebianDistributionDir=. \\
+    -PciHost=dockerhost \\
     --info \\
     --profile \\
     --console plain \\
@@ -355,6 +356,7 @@ job("${SEED_PROJECT}-${SEED_BRANCH}-acceptance-local") {
     rpmAcceptanceTest${centOsVersion} \\
     -PacceptanceJar=ontrack-acceptance-\${VERSION}.jar \\
     -PacceptanceRpmDistributionDir=. \\
+    -PciHost=dockerhost \\
     --info \\
     --profile \\
     --console plain \\
@@ -473,8 +475,8 @@ job("${SEED_PROJECT}-${SEED_BRANCH}-publish") {
 -Psigning.keyId=\${GPG_KEY_ID}
 -Psigning.password=\${GPG_KEY_PASSWORD}
 -Psigning.secretKeyRingFile=\${GPG_KEY_FILE}
--PossrhUser=\\${OSSRH_USER}
--PossrhPassword=\\${OSSRH_PASSWORD}
+-PossrhUser=\${OSSRH_USER}
+-PossrhPassword=\${OSSRH_PASSWORD}
 publicationRelease
 """
         } else {
