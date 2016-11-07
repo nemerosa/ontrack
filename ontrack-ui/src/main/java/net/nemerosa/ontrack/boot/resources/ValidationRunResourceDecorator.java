d8104eb79caa97b138d404a86384787b01a5c36c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-11-07 19:36:31 +0100

    #458 Links - refactoring - validation run

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/resources/ValidationRunResourceDecorator.java
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/resources/ValidationRunResourceDecorator.java
@@ -6,51 +6,59 @@ import net.nemerosa.ontrack.boot.ui.ValidationRunController;
 import net.nemerosa.ontrack.boot.ui.ValidationStampController;
 import net.nemerosa.ontrack.model.security.ValidationRunStatusChange;
 import net.nemerosa.ontrack.model.structure.ValidationRun;
-import net.nemerosa.ontrack.ui.resource.AbstractResourceDecorator;
+import net.nemerosa.ontrack.ui.resource.AbstractLinkResourceDecorator;
 import net.nemerosa.ontrack.ui.resource.Link;
+import net.nemerosa.ontrack.ui.resource.LinkDefinition;
 import net.nemerosa.ontrack.ui.resource.ResourceContext;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.Arrays;
 
+import static net.nemerosa.ontrack.ui.resource.LinkDefinitions.*;
 import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
 
 @Component
-public class ValidationRunResourceDecorator extends AbstractResourceDecorator<ValidationRun> {
+public class ValidationRunResourceDecorator extends AbstractLinkResourceDecorator<ValidationRun> {
 
     public ValidationRunResourceDecorator() {
         super(ValidationRun.class);
     }
 
     @Override
-    public List<Link> links(ValidationRun validationRun, ResourceContext resourceContext) {
-        return resourceContext.links()
-                .self(on(ValidationRunController.class).getValidationRun(validationRun.getId()))
-                .link(
+    protected Iterable<LinkDefinition<ValidationRun>> getLinkDefinitions() {
+        return Arrays.asList(
+                self(validationRun -> on(ValidationRunController.class).getValidationRun(validationRun.getId())),
+                link(
                         Link.IMAGE_LINK,
-                        on(ValidationStampController.class).getValidationStampImage_(null, validationRun.getValidationStamp().getId())
-                )
-                .link(
+                        validationRun -> on(ValidationStampController.class).getValidationStampImage_(null, validationRun.getValidationStamp().getId())
+                ),
+                link(
                         "_validationStampLink",
-                        on(ValidationStampController.class).getValidationStamp(validationRun.getValidationStamp().getId())
-                )
-                .link(
+                        validationRun -> on(ValidationStampController.class).getValidationStamp(validationRun.getValidationStamp().getId())
+                ),
+                link(
                         "_validationRunStatusChange",
-                        on(ValidationRunController.class).getValidationRunStatusChangeForm(validationRun.getId()),
+                        validationRun -> on(ValidationRunController.class).getValidationRunStatusChangeForm(validationRun.getId()),
                         // Only if transition possible
-                        resourceContext.isProjectFunctionGranted(
-                                validationRun.getBuild().getBranch().getProject().id(),
-                                ValidationRunStatusChange.class
-                        ) && !validationRun.getLastStatus().getStatusID().getFollowingStatuses().isEmpty()
-                )
+                        (ValidationRun validationRun, ResourceContext resourceContext) ->
+                                resourceContext.isProjectFunctionGranted(
+                                        validationRun.getBuild().getBranch().getProject().id(),
+                                        ValidationRunStatusChange.class
+                                ) && !validationRun.getLastStatus().getStatusID().getFollowingStatuses().isEmpty()
+                ),
                 // Actual properties for this entity
-                .link("_properties", on(PropertyController.class).getProperties(validationRun.getProjectEntityType(), validationRun.getId()))
+                link(
+                        "_properties",
+                        validationRun -> on(PropertyController.class).getProperties(validationRun.getProjectEntityType(), validationRun.getId())
+                ),
                 // Decorations
-                .link("_decorations", on(DecorationsController.class).getDecorations(validationRun.getProjectEntityType(), validationRun.getId()))
+                link(
+                        "_decorations",
+                        validationRun -> on(DecorationsController.class).getDecorations(validationRun.getProjectEntityType(), validationRun.getId())
+                ),
                 // Page
-                .page(validationRun)
-                // OK
-                .build();
+                page()
+        );
     }
 
 }
