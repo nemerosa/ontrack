1a29ead05aa345a2bac859ee8a96e7cd823f6b92
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-11-28 11:15:18 +0100

    #465 Removed redundant calls

--- ontrack-dsl/src/main/groovy/net/nemerosa/ontrack/dsl/Ontrack.groovy
+++ ontrack-dsl/src/main/groovy/net/nemerosa/ontrack/dsl/Ontrack.groovy
@@ -32,9 +32,7 @@ class Ontrack {
      */
     @DSLMethod("Gets the list of projects")
     List<Project> getProjects() {
-        return get('structure/projects').resources.collect {
-            new Project(this, get(it._self))
-        }
+        return get('structure/projects').resources
     }
 
     /**
