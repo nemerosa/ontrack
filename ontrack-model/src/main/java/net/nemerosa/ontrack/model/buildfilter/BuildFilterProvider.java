3790d86be049db74522f6fb11070008131742744
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-12-20 19:53:17 +0100

    #472 Filter provider new method

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/buildfilter/BuildFilterProvider.java
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/buildfilter/BuildFilterProvider.java
@@ -1,8 +1,10 @@
 package net.nemerosa.ontrack.model.buildfilter;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import net.nemerosa.ontrack.model.structure.Build;
 import net.nemerosa.ontrack.model.structure.ID;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BuildFilterProvider<T> {
@@ -22,13 +24,34 @@ public interface BuildFilterProvider<T> {
      */
     BuildFilterForm newFilterForm(ID branchId);
 
+    /**
+     * Gets the form for a pre filled filter
+     *
+     * @param branchId Branch to filter
+     * @param data     Filter data
+     * @return Form
+     */
     BuildFilterForm getFilterForm(ID branchId, T data);
+
+    /**
+     * Performs the filtering
+     */
+    default List<Build> filterBranchBuilds(ID branchId, T data) {
+        throw new UnsupportedOperationException("Filter branch builds must be implemented for " + this.getClass());
+    }
 
     /**
      * Builds an actual filter using the given set of parameters
      */
+    @Deprecated
     BuildFilter filter(ID branchId, T data);
 
+    /**
+     * Parses the filter data, provided as JSON, into an actual filter data object, when possible.
+     *
+     * @param data Filter data, as JSON
+     * @return Filter data object, or empty when not possible to parse
+     */
     Optional<T> parse(JsonNode data);
 
 }
