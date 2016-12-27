82b1a5e96a451f727ddf2011f4bb13db7525c05a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-12-27 20:27:31 +0100

    #476 Refactoring of some tests

--- ontrack-service/src/test/java/net/nemerosa/ontrack/service/CopyServiceImplIT.java
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/CopyServiceImplIT.java
@@ -1,16 +1,13 @@
 package net.nemerosa.ontrack.service;
 
-import net.nemerosa.ontrack.extension.api.support.*;
+import net.nemerosa.ontrack.extension.api.support.TestSimpleProperty;
+import net.nemerosa.ontrack.extension.api.support.TestSimplePropertyType;
 import net.nemerosa.ontrack.it.AbstractServiceTestSupport;
-import net.nemerosa.ontrack.model.security.BranchCreate;
 import net.nemerosa.ontrack.model.security.ProjectEdit;
-import net.nemerosa.ontrack.model.security.SecurityService;
 import net.nemerosa.ontrack.model.structure.*;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Optional;
@@ -18,10 +15,7 @@ import java.util.function.Function;
 
 import static net.nemerosa.ontrack.model.structure.NameDescription.nd;
 import static net.nemerosa.ontrack.model.structure.Replacement.replacementFn;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 public class CopyServiceImplIT extends AbstractServiceTestSupport {
 
@@ -31,8 +25,6 @@ public class CopyServiceImplIT extends AbstractServiceTestSupport {
     private StructureService structureService;
     @Autowired
     private PropertyService propertyService;
-    @Autowired
-    private SecurityService securityService;
 
     @Test
     public void cloneProject() throws Exception {
@@ -124,10 +116,8 @@ public class CopyServiceImplIT extends AbstractServiceTestSupport {
     }
 
     @Test
-    @Ignore
-    public void bulkUpdateBranch() {
-        Project project = Project.of(nd("P1", "")).withId(ID.of(1));
-        Branch branch = Branch.of(project, nd("B1", "Branch B1")).withId(ID.of(1));
+    public void bulkUpdateBranch() throws Exception {
+        Branch branch = doCreateBranch();
         // Request
         BranchBulkUpdateRequest request = new BranchBulkUpdateRequest(
                 Collections.singletonList(
@@ -136,84 +126,52 @@ public class CopyServiceImplIT extends AbstractServiceTestSupport {
         );
 
         // Branch properties
-        TestPropertyType testPropertyType = new TestPropertyType(
-                new TestExtensionFeature()
+        setProperty(
+                branch,
+                TestSimplePropertyType.class,
+                new TestSimpleProperty("http://wiki/B1")
         );
-        when(propertyService.getProperties(branch)).thenReturn(
-                Collections.singletonList(
-                        Property.of(
-                                testPropertyType,
-                                TestProperty.of("http://wiki/B1")
-                        )
-                )
-        );
-
-        // Updated branch
-        Branch updatedBranch = branch.withDescription("Branch B2");
-
-        // Edition of the property must be allowed
-        when(securityService.isProjectFunctionGranted(updatedBranch, ProjectEdit.class)).thenReturn(true);
 
         // Updating
-        service.update(branch, request);
-
-        // Checks the branch is updated
-        verify(structureService, times(1)).saveBranch(updatedBranch);
+        Branch updatedBranch = asUser().with(branch, ProjectEdit.class).call(() -> service.update(branch, request));
 
         // Checks the copy of properties for the branch
-        verify(propertyService, times(1)).copyProperty(
-                eq(branch),
-                eq(Property.of(testPropertyType, TestProperty.of("http://wiki/B1"))),
-                eq(updatedBranch),
-                any()
-        );
+        TestSimpleProperty p = getProperty(updatedBranch, TestSimplePropertyType.class);
+        assertNotNull(p);
+        assertEquals("http://wiki/B2", p.getValue());
     }
 
     @Test
-    @Ignore
-    public void doCopyBranchProperties() {
-        Branch sourceBranch = Branch.of(Project.of(nd("P1", "")).withId(ID.of(1)), nd("B1", "")).withId(ID.of(1));
-        Branch targetBranch = Branch.of(Project.of(nd("P2", "")).withId(ID.of(2)), nd("B2", "")).withId(ID.of(2));
+    public void doCopyBranchProperties() throws Exception {
+        Branch sourceBranch = doCreateBranch();
+        Branch targetBranch = doCreateBranch();
         // Request
         Function<String, String> replacementFn = replacementFn(
                 Collections.singletonList(
-                        new Replacement("P1", "P2")
+                        new Replacement("B1", "B2")
                 )
         );
 
         // Properties for the branch
-        TestPropertyType testPropertyType = new TestPropertyType(
-                new TestExtensionFeature()
-        );
-        when(propertyService.getProperties(sourceBranch)).thenReturn(
-                Collections.singletonList(
-                        Property.of(
-                                testPropertyType,
-                                TestProperty.of("http://wiki/P1")
-                        )
-                )
-        );
-
-        // Edition of the property must be allowed
-        when(securityService.isProjectFunctionGranted(targetBranch, ProjectEdit.class)).thenReturn(true);
+        setProperty(sourceBranch, TestSimplePropertyType.class, new TestSimpleProperty("http://wiki/B1"));
 
         // Copy
-        // FIXME service.doCopy(sourceBranch, targetBranch, replacementFn, SyncPolicy.COPY);
+        asUser()
+                .withView(sourceBranch)
+                .with(targetBranch, ProjectEdit.class).execute(() ->
+                service.copy(targetBranch, sourceBranch, replacementFn, SyncPolicy.COPY)
+        );
 
         // Checks the copy of properties for the branch
-        verify(propertyService, times(1)).copyProperty(
-                eq(sourceBranch),
-                eq(Property.of(testPropertyType, TestProperty.of("http://wiki/P1"))),
-                eq(targetBranch),
-                any()
-        );
+        TestSimpleProperty p = getProperty(targetBranch, TestSimplePropertyType.class);
+        assertNotNull(p);
+        assertEquals("http://wiki/B2", p.getValue());
     }
 
     @Test
-    @Ignore
-    public void doCopyPromotionLevels() {
-        Branch sourceBranch = Branch.of(Project.of(nd("P1", "")).withId(ID.of(1)), nd("B1", "")).withId(ID.of(1));
-        Branch targetBranch = Branch.of(Project.of(nd("P2", "")).withId(ID.of(2)), nd("B2", "")).withId(ID.of(2));
+    public void doCopyPromotionLevels() throws Exception {
+        Branch sourceBranch = doCreateBranch();
+        Branch targetBranch = doCreateBranch();
         // Request
         Function<String, String> replacementFn = replacementFn(
                 Collections.singletonList(
@@ -221,57 +179,35 @@ public class CopyServiceImplIT extends AbstractServiceTestSupport {
                 )
         );
         // Promotion levels for source
-        PromotionLevel sourcePromotionLevel = PromotionLevel.of(sourceBranch, nd("copper", "Copper level for P1"));
-        when(structureService.getPromotionLevelListForBranch(ID.of(1))).thenReturn(
-                Collections.singletonList(
-                        sourcePromotionLevel
-                )
-        );
-        when(structureService.findPromotionLevelByName("P2", "B2", "copper")).thenReturn(Optional.empty());
-
-        // Promotion level supposed to be created for the target branch
-        PromotionLevel targetPromotionLevel = PromotionLevel.of(
-                targetBranch,
-                nd("copper", "Copper level for P2")
-        );
-        // Result of the creation
-        when(structureService.newPromotionLevel(targetPromotionLevel)).thenReturn(targetPromotionLevel);
+        PromotionLevel sourcePromotionLevel = doCreatePromotionLevel(sourceBranch, nd("copper", "Copper level for P1"));
 
         // Properties for the promotion level
-        TestPropertyType testPropertyType = new TestPropertyType(
-                new TestExtensionFeature()
-        );
-        when(propertyService.getProperties(sourcePromotionLevel)).thenReturn(
-                Collections.singletonList(
-                        Property.of(
-                                testPropertyType,
-                                TestProperty.of("http://wiki/P1")
-                        )
-                )
-        );
-
-        // Edition of the property must be allowed
-        when(securityService.isProjectFunctionGranted(targetPromotionLevel, ProjectEdit.class)).thenReturn(true);
+        setProperty(sourcePromotionLevel, TestSimplePropertyType.class, new TestSimpleProperty("http://wiki/P1"));
 
         // Copy
-        // FIXME service.doCopyPromotionLevels(sourceBranch, targetBranch, replacementFn, SyncPolicy.COPY);
+        asUser()
+                .withView(sourceBranch)
+                .with(targetBranch, ProjectEdit.class).execute(() ->
+                service.copy(targetBranch, sourceBranch, replacementFn, SyncPolicy.COPY)
+        );
 
         // Checks the promotion level was created
-        verify(structureService, times(1)).newPromotionLevel(targetPromotionLevel);
-        // Checks the copy of properties for the promotion levels
-        verify(propertyService, times(1)).copyProperty(
-                eq(sourcePromotionLevel),
-                eq(Property.of(testPropertyType, TestProperty.of("http://wiki/P1"))),
-                eq(targetPromotionLevel),
-                any()
+        Optional<PromotionLevel> oPL = structureService.findPromotionLevelByName(
+                targetBranch.getProject().getName(),
+                targetBranch.getName(),
+                "copper"
         );
+        assertTrue(oPL.isPresent());
+        // Checks the copy of properties for the promotion levels
+        TestSimpleProperty p = getProperty(oPL.orElse(null), TestSimplePropertyType.class);
+        assertNotNull(p);
+        assertEquals("http://wiki/P2", p.getValue());
     }
 
     @Test
-    @Ignore
-    public void doCopyValidationStamps() {
-        Branch sourceBranch = Branch.of(Project.of(nd("P1", "")).withId(ID.of(1)), nd("B1", "")).withId(ID.of(1));
-        Branch targetBranch = Branch.of(Project.of(nd("P2", "")).withId(ID.of(2)), nd("B2", "")).withId(ID.of(2));
+    public void doCopyValidationStamps() throws Exception {
+        Branch sourceBranch = doCreateBranch();
+        Branch targetBranch = doCreateBranch();
         // Request
         Function<String, String> replacementFn = replacementFn(
                 Collections.singletonList(
@@ -279,51 +215,32 @@ public class CopyServiceImplIT extends AbstractServiceTestSupport {
                 )
         );
         // Validation stamps for source
-        Signature signature = Signature.of(LocalDateTime.of(2016, 12, 27, 19, 24), "test");
-        ValidationStamp sourceValidationStamp = ValidationStamp.of(sourceBranch, nd("smoke", "Smoke test for P1"))
-                .withSignature(signature);
-        when(structureService.getValidationStampListForBranch(ID.of(1))).thenReturn(
-                Collections.singletonList(
-                        sourceValidationStamp
-                )
-        );
-        when(structureService.findValidationStampByName("P2", "B2", "smoke")).thenReturn(Optional.empty());
-
-        // Validation stamp supposed to be created for the target branch
-        ValidationStamp targetValidationStamp = ValidationStamp.of(
-                targetBranch,
-                nd("smoke", "Smoke test for P2")
-        );
-        // Result of the creation
-        when(structureService.newValidationStamp(targetValidationStamp)).thenReturn(targetValidationStamp.withSignature(signature));
+        ValidationStamp sourceValidationStamp = doCreateValidationStamp(sourceBranch, nd("smoke", "Smoke test for P1"));
 
         // Properties for the validation stamp
-        TestPropertyType testPropertyType = new TestPropertyType(
-                new TestExtensionFeature()
+        setProperty(
+                sourceValidationStamp,
+                TestSimplePropertyType.class,
+                new TestSimpleProperty("http://wiki/P1")
         );
-        when(propertyService.getProperties(sourceValidationStamp)).thenReturn(
-                Collections.singletonList(
-                        Property.of(
-                                testPropertyType,
-                                TestProperty.of("http://wiki/P1")
-                        )
-                )
-        );
-
-        // Edition of the property must be allowed
-        when(securityService.isProjectFunctionGranted(targetValidationStamp, ProjectEdit.class)).thenReturn(true);
 
         // Copy
-        // FIXME service.doCopyValidationStamps(sourceBranch, targetBranch, replacementFn, SyncPolicy.COPY);
+        asUser()
+                .withView(sourceBranch)
+                .with(targetBranch, ProjectEdit.class).execute(() ->
+                service.copy(targetBranch, sourceBranch, replacementFn, SyncPolicy.COPY)
+        );
 
         // Checks the validation stamp was created
-        verify(structureService, times(1)).newValidationStamp(targetValidationStamp.withSignature(signature));
-        // Checks the copy of properties for the validation stamps
-        verify(propertyService, times(1)).copyProperty(
-                eq(sourceValidationStamp),
-                eq(Property.of(testPropertyType, TestProperty.of("http://wiki/P1"))),
-                eq(targetValidationStamp),
-                any()
+        Optional<ValidationStamp> oVS = structureService.findValidationStampByName(
+                targetBranch.getProject().getName(),
+                targetBranch.getName(),
+                "smoke"
         );
+        assertTrue(oVS.isPresent());
+        // Checks the copy of properties for the validation stamps
+        TestSimpleProperty p = getProperty(oVS.orElse(null), TestSimplePropertyType.class);
+        assertNotNull(p);
+        assertEquals("http://wiki/P2", p.getValue());
     }
 }
