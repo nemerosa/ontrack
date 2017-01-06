027389f605547a6173c7a716beeeea7fef502a85
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-01-06 19:21:09 +0100

    #480 Output the database URL at startup

--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/EnvServiceImpl.java
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/EnvServiceImpl.java
@@ -26,11 +26,13 @@ public class EnvServiceImpl implements EnvService {
     private final String profiles;
     private final VersionInfo version;
     private final File home;
+    private final ApplicationContext ctx;
 
     @Autowired
     public EnvServiceImpl(VersionInfoConfig version,
                           OntrackConfigProperties configProperties,
                           ApplicationContext ctx) {
+        this.ctx = ctx;
         this.defaultProfiles = StringUtils.join(ctx.getEnvironment().getDefaultProfiles(), ",");
         this.profiles = StringUtils.join(ctx.getEnvironment().getActiveProfiles(), ",");
         // Version information from the configuration
@@ -63,9 +65,11 @@ public class EnvServiceImpl implements EnvService {
 
     @PostConstruct
     public void init() throws FileNotFoundException {
+        String jdbcUrl = ctx.getEnvironment().getProperty("spring.datasource.url", "n/a");
         logger.info("[env] With JDK:              {}", System.getProperty("java.version"));
         logger.info("[env] With default profiles: {}", defaultProfiles);
         logger.info("[env] With active profiles:  {}", profiles);
+        logger.info("[setup] DB JDBC URL:         {}", jdbcUrl);
         logger.info("[version] Display:           {}", version.getDisplay());
         logger.info("[version] Full:              {}", version.getFull());
         logger.info("[version] Branch:            {}", version.getBranch());
