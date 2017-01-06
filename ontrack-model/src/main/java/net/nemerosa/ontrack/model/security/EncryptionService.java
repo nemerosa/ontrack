f0fb28499c6cf8df6ae9662cce02673dfe579a6d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-01-06 19:23:52 +0100

    #481 Javadoc for the encryption service

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/EncryptionService.java
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/EncryptionService.java
@@ -1,7 +1,23 @@
 package net.nemerosa.ontrack.model.security;
 
+/**
+ * Service to decrypt and encrypt secrets.
+ */
 public interface EncryptionService {
+
+    /**
+     * Encrypts a secret, given in its plain form, and returns a string suitable for secure storage.
+     *
+     * @param plain Secret to encode
+     * @return Encrypted secret
+     */
     String encrypt(String plain);
 
+    /**
+     * Decrypts a secret into a plain form from its encrypted version.
+     *
+     * @param crypted Encrypted secret
+     * @return Plain secret
+     */
     String decrypt(String crypted);
 }
