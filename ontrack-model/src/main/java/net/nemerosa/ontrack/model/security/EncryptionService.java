29fb6bbe9c84c4ca7d29e22ee431ca04d9cbff6b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-01-07 19:59:50 +0100

    #481 Import/export service

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/EncryptionService.java
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/EncryptionService.java
@@ -20,4 +20,20 @@ public interface EncryptionService {
      * @return Plain secret
      */
     String decrypt(String crypted);
+
+    /**
+     * Gets the encryption key.
+     *
+     * @return Key encoded as Base64
+     * @throws EncryptionException If the key cannot be exported
+     */
+    String exportKey();
+
+    /**
+     * Imports the encryption key.
+     *
+     * @param key Key encoded as Base64
+     * @throws EncryptionException If the key cannot be imported
+     */
+    void importKey(String key);
 }
