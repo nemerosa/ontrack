0e694368440c94d689fcca8d01dd0759de5133a7
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-02-21 09:05:39 +0100

    [pipeline] Acceptance application accepting remote Selenium web driver

--- ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/browser/Configuration.groovy
+++ ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/browser/Configuration.groovy
@@ -6,6 +6,8 @@ import org.apache.commons.io.FileUtils
 import org.openqa.selenium.*
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxProfile
+import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.remote.RemoteWebDriver
 import org.openqa.selenium.support.ui.FluentWait
 import org.openqa.selenium.support.ui.WebDriverWait
 import org.slf4j.Logger
@@ -15,62 +17,61 @@ import java.text.SimpleDateFormat
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicLong
 import java.util.function.Consumer
+import java.util.logging.Level
 
 import static net.nemerosa.ontrack.acceptance.AcceptanceSupport.env
 
-public class Configuration {
+class Configuration {
 
-    private static final Logger logger = LoggerFactory.getLogger(Configuration.class);
+    static final String SELENIUM_URL = 'ontrack.selenium.url'
 
-    private final WebDriver driver;
+    private static final Logger logger = LoggerFactory.getLogger(Configuration.class)
 
-    private final String baseUrl;
-    private final int implicitWait;
-    private final File screenshotDir;
+    private final WebDriver driver
 
-    private final AtomicLong screenshotIndex = new AtomicLong();
+    private final String baseUrl
+    private final int implicitWait
+    private final File screenshotDir
+
+    private final AtomicLong screenshotIndex = new AtomicLong()
 
     protected Configuration() {
-        try {
-            // Configuration
-            baseUrl = env("ontrack.url", true, "http://localhost:8080", "Base URL");
-            implicitWait = Integer.parseInt(
-                    env("ontrack.implicitWait", false, "5", "Implicit wait time for GUI components (in seconds)"),
-                    10);
-            screenshotDir = new File(env("ontrack.acceptance.screenshots", false, "build/acceptance/screenshots", "Screenshot output directory")).getAbsoluteFile();
-            FileUtils.forceMkdir(screenshotDir);
-            // Logging
-            logger.info("[gui] Base URl      = {}", baseUrl);
-            logger.info("[gui] Implicit wait = {}s", implicitWait);
-            logger.info("[gui] Screenshots   = {}", screenshotDir);
-            // Web driver class
-            driver = initDriver();
-            driver.manage().deleteAllCookies();
-            driver.manage().window().setSize(new Dimension(1024, 768));
-            driver.manage().timeouts().implicitlyWait(implicitWait, TimeUnit.SECONDS);
-        } catch (IOException ex) {
-            throw new ConfigurationException("Cannot initialise browser configuration", ex);
-        }
+        // Configuration
+        baseUrl = env("ontrack.url", true, "http://localhost:8080", "Base URL")
+        implicitWait = Integer.parseInt(
+                env("ontrack.implicitWait", false, "5", "Implicit wait time for GUI components (in seconds)"),
+                10)
+        screenshotDir = new File(env("ontrack.acceptance.screenshots", false, "build/acceptance/screenshots", "Screenshot output directory")).getAbsoluteFile()
+        FileUtils.forceMkdir(screenshotDir)
+        // Logging
+        logger.info("[gui] Base URl      = {}", baseUrl)
+        logger.info("[gui] Implicit wait = {}s", implicitWait)
+        logger.info("[gui] Screenshots   = {}", screenshotDir)
+        // Web driver class
+        driver = initDriver()
+        driver.manage().deleteAllCookies()
+        driver.manage().window().setSize(new Dimension(1024, 768))
+        driver.manage().timeouts().implicitlyWait(implicitWait, TimeUnit.SECONDS)
     }
 
-    public WebDriver getDriver() {
-        return driver;
+    WebDriver getDriver() {
+        return driver
     }
 
     String getBaseUrl() {
         return baseUrl
     }
 
-    public void closeConfiguration() {
-        driver.quit();
+    void closeConfiguration() {
+        driver.quit()
     }
 
-    public void goTo(String path) {
-        logger.info("Go to: ${}", path);
-        driver.get(String.format("%s/%s", baseUrl, path));
+    void goTo(String path) {
+        logger.info("Go to: ${}", path)
+        driver.get(String.format("%s/%s", baseUrl, path))
     }
 
-    public WebElement findElement(By by) {
+    WebElement findElement(By by) {
         new FluentWait<WebDriver>(driver)
                 .withTimeout(implicitWait, TimeUnit.SECONDS)
                 .pollingEvery(1, TimeUnit.SECONDS)
@@ -78,18 +79,14 @@ public class Configuration {
                 .ignoring(StaleElementReferenceException.class)
                 .until(
                 new Function<WebDriver, WebElement>() {
-                    public WebElement apply(WebDriver driver) {
-                        return driver.findElement(by);
+                    WebElement apply(WebDriver driver) {
+                        return driver.findElement(by)
                     }
                 }
         )
     }
 
-    public WebElement findElement(String css) {
-        findElement(By.cssSelector(css))
-    }
-
-    public Collection<WebElement> findElements(By by) {
+    Collection<WebElement> findElements(By by) {
         new FluentWait<WebDriver>(driver)
                 .withTimeout(implicitWait, TimeUnit.SECONDS)
                 .pollingEvery(1, TimeUnit.SECONDS)
@@ -97,67 +94,67 @@ public class Configuration {
                 .ignoring(StaleElementReferenceException.class)
                 .until(
                 new Function<WebDriver, Collection<WebElement>>() {
-                    public Collection<WebElement> apply(WebDriver driver) {
-                        return driver.findElements(by);
+                    Collection<WebElement> apply(WebDriver driver) {
+                        return driver.findElements(by)
                     }
                 }
         )
     }
 
-    public void waitUntil(Closure<Boolean> closure) {
-        waitUntil("element", closure);
+    void waitUntil(Closure<Boolean> closure) {
+        waitUntil("element", closure)
     }
 
-    public void waitUntil(String message, Closure<Boolean> closure) {
-        waitUntil(message, implicitWait, closure);
+    void waitUntil(String message, Closure<Boolean> closure) {
+        waitUntil(message, implicitWait, closure)
     }
 
-    public void waitUntil(String message, int seconds, Closure<Boolean> closure) {
+    void waitUntil(String message, int seconds, Closure<Boolean> closure) {
         try {
             new WebDriverWait(driver, seconds).until(new Predicate<WebDriver>() {
                 @Override
                 boolean apply(WebDriver input) {
                     closure()
                 }
-            });
+            })
         } catch (TimeoutException ex) {
             // Takes a screenshot
-            screenshot("timeout");
+            screenshot("timeout")
             // The error is still there
-            throw new TimeoutException("Could not get ${message} in less than ${seconds} seconds", ex);
+            throw new TimeoutException("Could not get ${message} in less than ${seconds} seconds", ex)
         }
     }
 
-    public void screenshot(String name) {
+    void screenshot(String name) {
         // Screenshot name
         String fullName = String.format(
                 "%s-%d-%s.png",
                 new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()),
                 screenshotIndex.incrementAndGet(),
                 name
-        );
+        )
         // Saves the screenshot in the target directory
-        File targetFile = new File(screenshotDir, fullName);
-        logger.info("[gui] Screenshot at {}", targetFile.getAbsolutePath());
+        File targetFile = new File(screenshotDir, fullName)
+        logger.info("[gui] Screenshot at {}", targetFile.getAbsolutePath())
         // Takes the screenshot
-        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE)
         // Copies the file
         try {
-            FileUtils.copyFile(scrFile, targetFile);
+            FileUtils.copyFile(scrFile, targetFile)
         } catch (IOException e) {
-            throw new CannotTakeScreenshotException(name, e);
+            throw new CannotTakeScreenshotException(name, e)
         }
     }
 
-    public static void driver(Consumer<Configuration> closure) {
+    static void driver(Consumer<Configuration> closure) {
         // Loads the driver environment
-        Configuration configuration = new Configuration();
+        Configuration configuration = new Configuration()
         try {
             // Runs with the driver
-            closure.accept(configuration);
+            closure.accept(configuration)
         } finally {
             // Closes the driver
-            configuration.closeConfiguration();
+            configuration.closeConfiguration()
         }
     }
 
@@ -165,16 +162,29 @@ public class Configuration {
         env('ontrack.admin', false, 'admin', "Admin password")
     }
 
-    public static WebDriver initDriver() throws IOException {
-        File loggingDir = new File(env("ontrack.acceptance.logs", false, "build/acceptance/logs", "Logging output directory")).getAbsoluteFile();
-        FileUtils.forceMkdir(loggingDir);
-        logger.info("[gui] Browser logging directory at {}", loggingDir);
+    static WebDriver initDriver() throws IOException {
+        File loggingDir = new File(env("ontrack.acceptance.logs", false, "build/acceptance/logs", "Logging output directory")).getAbsoluteFile()
+        FileUtils.forceMkdir(loggingDir)
+        logger.info("[gui] Browser logging directory at {}", loggingDir)
 
-        FirefoxProfile profile = new FirefoxProfile();
-        profile.setPreference("webdriver.log.browser.file", new File(loggingDir, "browser.log").getAbsolutePath());
-        profile.setPreference("webdriver.log.browser.level", "all");
-
-        return new FirefoxDriver(profile);
+        String seleniumUrl = env(SELENIUM_URL, false, "", "Selenium URL (defaults to local Firefox)")
+        if (seleniumUrl) {
+            logger.info("[gui] Using remote Selenium driver at ${seleniumUrl}")
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities()
+            desiredCapabilities.setBrowserName('firefox')
+            RemoteWebDriver webDriver = new RemoteWebDriver(
+                    new URL(seleniumUrl),
+                    desiredCapabilities
+            )
+            webDriver.setLogLevel(Level.FINER)
+            return webDriver
+        } else {
+            logger.info("[gui] Using local Firefox Selenium driver")
+            FirefoxProfile profile = new FirefoxProfile()
+            profile.setPreference("webdriver.log.browser.file", new File(loggingDir, "browser.log").getAbsolutePath())
+            profile.setPreference("webdriver.log.browser.level", "all")
+            return new FirefoxDriver(profile)
+        }
     }
 
 }
