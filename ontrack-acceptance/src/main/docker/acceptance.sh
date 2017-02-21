3be064dd35dd9534a6852db184fdbacf31e4844e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-02-21 16:54:24 +0100

    [pipeline] Acceptance Dockerfile

---
+++ ontrack-acceptance/src/main/docker/acceptance.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+java \
+    /ontrack/acceptance/app/acceptance.jar \
+    --ontrack.selenium.url=${ONTRACK_ACCEPTANCE_SELENIUM_URL} \
+    --ontrack.url=${ONTRACK_ACCEPTANCE_TARGET_URL} \
+    --ontrack.admin=${ONTRACK_ACCEPTANCE_TARGET_ADMIN_PASSWORD} \
+    --ontrack.disableSsl=${ONTRACK_ACCEPTANCE_TARGET_SSL_DISABLED} \
+    --ontrack.context=${ONTRACK_ACCEPTANCE_CONTEXT} \
+    --ontrack.timeout=${ONTRACK_ACCEPTANCE_TIMEOUT} \
+    --ontrack.implicitWait=${ONTRACK_ACCEPTANCE_IMPLICIT_WAIT} \
+    --ontrack.resultFile=/ontrack/acceptance/results/acceptance-tests.xml
