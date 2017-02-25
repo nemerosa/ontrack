b68b890b033bfbdda69212882300558bdb599598
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-02-25 13:14:13 +0100

    [pipeline] Acceptance config - specific Selenium Target URL

--- ontrack-acceptance/src/main/compose/docker/acceptance.sh
+++ ontrack-acceptance/src/main/compose/docker/acceptance.sh
@@ -3,7 +3,7 @@ exec java \
     ${JAVA_OPTIONS} \
     -jar /ontrack/acceptance/app/ontrack-acceptance.jar \
     --ontrack.acceptance.url=${ONTRACK_ACCEPTANCE_TARGET_URL} \
-    --ontrack.acceptance.selenium-url=${ONTRACK_ACCEPTANCE_SELENIUM_URL} \
+    --ontrack.acceptance.selenium-grid-url=${ONTRACK_ACCEPTANCE_SELENIUM_GRID_URL} \
     --ontrack.acceptance.disable-ssl=${ONTRACK_ACCEPTANCE_TARGET_SSL_DISABLED} \
     --ontrack.acceptance.admin=${ONTRACK_ACCEPTANCE_TARGET_ADMIN_PASSWORD} \
     --ontrack.acceptance.context=${ONTRACK_ACCEPTANCE_CONTEXT} \
