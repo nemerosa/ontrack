4f8363431b774c5008d3dbd471b226c16929cb62
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-04-10 14:08:19 +0200

    #485 Validation stamp bulk update - tests (more)

--- ontrack-service/src/test/groovy/net/nemerosa/ontrack/service/settings/PredefinedValidationStampServiceIT.groovy
+++ ontrack-service/src/test/groovy/net/nemerosa/ontrack/service/settings/PredefinedValidationStampServiceIT.groovy
@@ -116,4 +116,60 @@ class PredefinedValidationStampServiceIT extends AbstractServiceTestSupport {
         }
     }
 
+    @Test
+    void 'Bulk update of validation stamps updates a predefined validation stamp'() {
+        // Creates three validation stamps, two with the same name, one with a different name
+        def vs1Name = uid('VS')
+        def vs2Name = uid('VS')
+        def branch1 = doCreateBranch()
+        def branch2 = doCreateBranch()
+        def branch3 = doCreateBranch()
+        def vs1 = doCreateValidationStamp(branch1, NameDescription.nd(vs1Name, ''))
+        def vs2 = doCreateValidationStamp(branch2, NameDescription.nd(vs1Name, ''))
+        def vs3 = doCreateValidationStamp(branch3, NameDescription.nd(vs2Name, ''))
+        // Predefined validation stamp
+        def pvs = asAdmin().call {
+            service.newPredefinedValidationStamp(
+                    PredefinedValidationStamp.of(NameDescription.nd(vs1Name, ''))
+            )
+        }
+
+        // Updates the VS1 description and image
+        asUser().with(vs1, ValidationStampEdit).call {
+            structureService.saveValidationStamp(
+                    vs1.withDescription("My new description")
+            )
+            structureService.setValidationStampImage(
+                    vs1.id,
+                    new Document("image/png", TestUtils.resourceBytes("/validationStampImage1.png"))
+            )
+        }
+
+        // Bulk update
+        asUser().with(GlobalSettings).call {
+            structureService.bulkUpdateValidationStamps(vs1.id)
+        }
+
+        // Checks the validation stamp with same name has been updated
+        asAdmin().call {
+            assert structureService.getValidationStamp(vs2.id).description == "My new description"
+            assert structureService.getValidationStamp(vs2.id).image
+        }
+
+        // Checks the validation stamp with NOT same name has NOT been updated
+        asAdmin().call {
+            assert structureService.getValidationStamp(vs3.id).description == ""
+            assert !structureService.getValidationStamp(vs3.id).image
+        }
+
+        // Checks the predefined validation stamp has been created
+        asAdmin().call {
+            def o = service.findPredefinedValidationStampByName(vs1.name)
+            assert o.present
+            assert o.get().id == pvs.id
+            assert o.get().description == "My new description"
+            assert o.get().image
+        }
+    }
+
 }
