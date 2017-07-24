7b9bead2c658d540644338b80e62626c7651571d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-07-24 16:25:24 +0200

    #528 Checks that 3rd party libraries are correctly loaded and useable - actually using the library now...

--- ontrack-extension-test/src/main/kotlin/net/nemerosa/ontrack/extension/test/TestController.kt
+++ ontrack-extension-test/src/main/kotlin/net/nemerosa/ontrack/extension/test/TestController.kt
@@ -1,6 +1,7 @@
 package net.nemerosa.ontrack.extension.test
 
 import net.nemerosa.ontrack.ui.controller.AbstractResourceController
+import org.apache.commons.math3.util.Pair
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -14,7 +15,11 @@ class TestController : AbstractResourceController() {
      * extension distribution (in this case, the Commons Math3 API).
      */
     @GetMapping("/3rdparty")
-    fun thirdParty(@RequestParam value: Double, @RequestParam power: Double) = ThirdPartyResult(value, power)
+    fun thirdParty(@RequestParam value: Double, @RequestParam power: Double): ThirdPartyResult {
+        // Stupid code, but this is just to call some external library
+        val pair = Pair.create(value, power)
+        return ThirdPartyResult(pair.first, pair.second)
+    }
 }
 
 data class ThirdPartyResult(val value: Double, val power: Double) {
