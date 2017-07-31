a22d51b1c65487f5fb78508ba1a736012367faf4
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-07-31 17:01:44 +0200

    #176 Data type model

---
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/structure/ValidationDataType.kt
@@ -0,0 +1,83 @@
+package net.nemerosa.ontrack.model.structure
+
+import com.fasterxml.jackson.databind.JsonNode
+import net.nemerosa.ontrack.model.form.Form
+
+/**
+ * Definition and validation of some arbitrary data associated with [ValidationStamp]s
+ * and [ValidationRun]s.
+ *
+ * @param C Configuration data associated with this type.
+ * @param T Type of data associated with this type.
+ */
+interface ValidationDataType<C, T> {
+
+    /**
+     * Display name for this type
+     */
+    val displayName: String
+
+    /**
+     * Translates the configuration data into JSON
+     */
+    fun configToJson(config: C): JsonNode
+
+    /**
+     * Translates some JSON into configuration data
+     * @return The data or `null` if the JSON cannot be translated. The implementation can also choose
+     * to throw an exception instead.
+     */
+    fun configFromJson(node: JsonNode): C?
+
+    /**
+     * Gets a form to edit / create the configuration.
+     *
+     * @param config Data to edit - `null` if the form is for a creation
+     * @return A prefilled form
+     */
+    fun getConfigForm(config: C?): Form
+
+    /**
+     * Creates the config object from the JSON returned by a form edition
+     *
+     * @param node JSON returned by the form edition
+     * @return Configuration
+     */
+    fun fromConfigForm(node: JsonNode): C
+
+    /**
+     * Translates the data into JSON
+     */
+    fun toJson(data: T): JsonNode
+
+    /**
+     * Translates some JSON into data
+     * @return The data or `null` if the JSON cannot be translated. The implementation can also choose
+     * to throw an exception instead.
+     */
+    fun fromJson(node: JsonNode): T?
+
+    /**
+     * Gets a form to edit / create some data.
+     *
+     * @param data Data to edit - `null` if the form is for a creation
+     * @return A prefilled form
+     */
+    fun getForm(data: T?): Form
+
+    /**
+     * Creates the data object from the JSON returned by a form edition
+     *
+     * @param node JSON returned by the form edition
+     * @return Data
+     */
+    fun fromForm(node: JsonNode): T
+
+    /**
+     * Computes the status from some data
+     *
+     * @param data Data associated with a run
+     * @return Status or `null` if no status can be determined
+     */
+    fun computeStatus(data: T): ValidationRunStatusID?
+}
