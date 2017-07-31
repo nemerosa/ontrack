526b5ec81e2b2c2d254a60cd3b585e519a696d6b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-07-31 16:02:52 +0200

    #518 Correction of schema for Postgres

--- ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/EntityDataStoreJdbcRepository.java
+++ ontrack-repository-impl/src/main/java/net/nemerosa/ontrack/repository/EntityDataStoreJdbcRepository.java
@@ -118,7 +118,7 @@ public class EntityDataStoreJdbcRepository extends AbstractJdbcRepository implem
                 params("recordId", id),
                 (rs, rowNum) -> new EntityDataStoreRecordAudit(
                         EntityDataStoreRecordAuditType.valueOf(rs.getString("AUDIT_TYPE")),
-                        readSignature(rs, "TIMESTAMP", "USER")
+                        readSignature(rs, "TIMESTAMP", "CREATOR")
                 )
         );
     }
@@ -300,7 +300,7 @@ public class EntityDataStoreJdbcRepository extends AbstractJdbcRepository implem
 
     private void audit(EntityDataStoreRecordAuditType type, int recordId, Signature signature) {
         getNamedParameterJdbcTemplate().update(
-                "INSERT INTO ENTITY_DATA_STORE_AUDIT(RECORD_ID, AUDIT_TYPE, TIMESTAMP, USER) " +
+                "INSERT INTO ENTITY_DATA_STORE_AUDIT(RECORD_ID, AUDIT_TYPE, TIMESTAMP, CREATOR) " +
                         "VALUES (:recordId, :auditType, :timestamp, :user)",
                 params("recordId", recordId)
                         .addValue("auditType", type.name())
