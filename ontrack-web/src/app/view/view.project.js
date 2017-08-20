d3e913068a807a50dfe9ef2ec5f35922a89fb83e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-08-20 14:54:28 +0200

    #517 Project page - commands

--- ontrack-web/src/app/view/view.project.js
+++ ontrack-web/src/app/view/view.project.js
@@ -37,6 +37,8 @@ angular.module('ot.view.project', [
                 "      _delete\n" +
                 "      _permissions\n" +
                 "      _clone\n" +
+                "      _enable\n" +
+                "      _disable\n" +
                 "    }\n" +
                 "    branches {\n" +
                 "      id\n" +
@@ -81,137 +83,133 @@ angular.module('ot.view.project', [
                 "  }\n" +
                 "}\n").then(function (data) {
                 $scope.project = data.projects[0];
+                // View commands
+                view.commands = [
+                    {
+                        condition: function () {
+                            return $scope.project.links._createBranch;
+                        },
+                        id: 'createBranch',
+                        name: "Create branch",
+                        cls: 'ot-command-branch-new',
+                        action: function () {
+                            otStructureService.create($scope.project.links._createBranch, "New branch").then(loadBranches);
+                        }
+                    },
+                    {
+                        condition: function () {
+                            return $scope.project.links._update;
+                        },
+                        id: 'updateProject',
+                        name: "Update project",
+                        cls: 'ot-command-project-update',
+                        action: function () {
+                            otStructureService.update(
+                                $scope.project.links._update,
+                                "Update project"
+                            ).then(loadProject);
+                        }
+                    },
+                    {
+                        id: 'searchBuild',
+                        name: "Search builds",
+                        cls: 'ot-command-project-search-builds',
+                        link: '/build-search/' + $scope.project.id
+                    },
+                    {
+                        condition: function () {
+                            return $scope.project.links._disable;
+                        },
+                        id: 'disableProject',
+                        name: "Disable project",
+                        cls: 'ot-command-project-disable',
+                        action: function () {
+                            ot.pageCall($http.put($scope.project.links._disable)).then(loadProject);
+                        }
+                    },
+                    {
+                        condition: function () {
+                            return $scope.project.links._enable;
+                        },
+                        id: 'enableProject',
+                        name: "Enable project",
+                        cls: 'ot-command-project-enable',
+                        action: function () {
+                            ot.pageCall($http.put($scope.project.links._enable)).then(loadProject);
+                        }
+                    }, {
+                        id: 'showDisabled',
+                        name: "Show all branches",
+                        cls: 'ot-command-show-disabled',
+                        condition: function () {
+                            return !$scope.showDisabled;
+                        },
+                        action: function () {
+                            $scope.showDisabled = true;
+                        }
+                    }, {
+                        id: 'hideDisabled',
+                        name: "Hide disabled branches",
+                        cls: 'ot-command-hide-disabled',
+                        condition: function () {
+                            return $scope.showDisabled;
+                        },
+                        action: function () {
+                            $scope.showDisabled = false;
+                        }
+                    },
+                    {
+                        condition: function () {
+                            return $scope.project.links._permissions;
+                        },
+                        id: 'permissionsProject',
+                        name: "Permissions",
+                        cls: 'ot-command-project-permissions',
+                        link: '/admin-project-acl/' + $scope.project.id
+                    },
+                    {
+                        condition: function () {
+                            return $scope.project.links._clone;
+                        },
+                        id: 'cloneProject',
+                        name: "Clone project",
+                        cls: 'ot-command-project-clone',
+                        action: function () {
+                            otStructureService.getProject($scope.project.id).then(function (project) {
+                                otCopyService.cloneProject(project).then(function (newProject) {
+                                    $state.go('project', {
+                                        projectId: newProject.id
+                                    });
+                                });
+                            });
+                        }
+                    },
+                    {
+                        condition: function () {
+                            return $scope.project.links._delete;
+                        },
+                        id: 'deleteProject',
+                        name: "Delete project",
+                        cls: 'ot-command-project-delete',
+                        action: function () {
+                            otAlertService.confirm({
+                                title: "Deleting a project",
+                                message: "Do you really want to delete the project " + $scope.project.name +
+                                " and all its associated data?"
+                            }).then(function () {
+                                return ot.call($http.delete($scope.project.links._delete));
+                            }).then(function () {
+                                $state.go('home');
+                            });
+                        }
+                    },
+                    ot.viewApiCommand($scope.project.links._self),
+                    // FIXME ot.viewActionsCommand($scope.project._actions),
+                    ot.viewCloseCommand('/home')
+                ];
             }).finally(function () {
                 $scope.loadingBranches = false;
             });
-            // ot.call($http.get($scope.project._branchStatusViews)).then(function (branchStatusViewsResources) {
-            //     $scope.branchStatusViewsResources = branchStatusViewsResources;
-            //     $scope.branchStatusViews = branchStatusViewsResources.resources;
-            //     // View commands
-            //     view.commands = [
-            //         {
-            //             condition: function () {
-            //                 return branchStatusViewsResources._create;
-            //             },
-            //             id: 'createBranch',
-            //             name: "Create branch",
-            //             cls: 'ot-command-branch-new',
-            //             action: function () {
-            //                 otStructureService.create(branchStatusViewsResources._create, "New branch").then(loadBranches);
-            //             }
-            //         },
-            //         {
-            //             condition: function () {
-            //                 return $scope.project._update;
-            //             },
-            //             id: 'updateProject',
-            //             name: "Update project",
-            //             cls: 'ot-command-project-update',
-            //             action: function () {
-            //                 otStructureService.update(
-            //                     $scope.project._update,
-            //                     "Update project"
-            //                 ).then(loadProject);
-            //             }
-            //         },
-            //         {
-            //             id: 'searchBuild',
-            //             name: "Search builds",
-            //             cls: 'ot-command-project-search-builds',
-            //             link: '/build-search/' + $scope.project.id
-            //         },
-            //         {
-            //             condition: function () {
-            //                 return $scope.project._disable;
-            //             },
-            //             id: 'disableProject',
-            //             name: "Disable project",
-            //             cls: 'ot-command-project-disable',
-            //             action: function () {
-            //                 ot.pageCall($http.put($scope.project._disable)).then(loadProject);
-            //             }
-            //         },
-            //         {
-            //             condition: function () {
-            //                 return $scope.project._enable;
-            //             },
-            //             id: 'enableProject',
-            //             name: "Enable project",
-            //             cls: 'ot-command-project-enable',
-            //             action: function () {
-            //                 ot.pageCall($http.put($scope.project._enable)).then(loadProject);
-            //             }
-            //         }, {
-            //             id: 'showDisabled',
-            //             name: "Show all branches",
-            //             cls: 'ot-command-show-disabled',
-            //             condition: function () {
-            //                 return !$scope.showDisabled;
-            //             },
-            //             action: function () {
-            //                 $scope.showDisabled = true;
-            //             }
-            //         }, {
-            //             id: 'hideDisabled',
-            //             name: "Hide disabled branches",
-            //             cls: 'ot-command-hide-disabled',
-            //             condition: function () {
-            //                 return $scope.showDisabled;
-            //             },
-            //             action: function () {
-            //                 $scope.showDisabled = false;
-            //             }
-            //         },
-            //         {
-            //             condition: function () {
-            //                 return $scope.project._permissions;
-            //             },
-            //             id: 'permissionsProject',
-            //             name: "Permissions",
-            //             cls: 'ot-command-project-permissions',
-            //             link: '/admin-project-acl/' + $scope.project.id
-            //         },
-            //         {
-            //             condition: function () {
-            //                 return $scope.project._clone;
-            //             },
-            //             id: 'cloneProject',
-            //             name: "Clone project",
-            //             cls: 'ot-command-project-clone',
-            //             action: function () {
-            //                 otCopyService.cloneProject($scope.project).then(function (newProject) {
-            //                     $state.go('project', {
-            //                         projectId: newProject.id
-            //                     });
-            //                 });
-            //             }
-            //         },
-            //         {
-            //             condition: function () {
-            //                 return $scope.project._delete;
-            //             },
-            //             id: 'deleteProject',
-            //             name: "Delete project",
-            //             cls: 'ot-command-project-delete',
-            //             action: function () {
-            //                 otAlertService.confirm({
-            //                     title: "Deleting a project",
-            //                     message: "Do you really want to delete the project " + $scope.project.name +
-            //                         " and all its associated data?"
-            //                 }).then(function () {
-            //                     return ot.call($http.delete($scope.project._delete));
-            //                 }).then(function () {
-            //                     $state.go('home');
-            //                 });
-            //             }
-            //         },
-            //         ot.viewApiCommand($scope.project._self),
-            //         ot.viewActionsCommand($scope.project._actions),
-            //         ot.viewCloseCommand('/home')
-            //     ];
-            // }).finally(function() {
-            //     $scope.loadingBranches = false;
-            // });
         }
 
         // Loading the project
