03e7a9368c50a168e21aaa6e31fad627c587ccc0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-08-31 16:00:49 +0200

    #176 Description can be null

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/structure/ValidationRunRequest.kt
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/structure/ValidationRunRequest.kt
@@ -1,12 +1,8 @@
 package net.nemerosa.ontrack.model.structure
 
 import com.fasterxml.jackson.annotation.JsonIgnore
-import lombok.Data
-import java.beans.ConstructorProperties
 
-@Data
 class ValidationRunRequest
-@ConstructorProperties("validationStampId", "validationStampData", "validationStampName", "validationRunStatusId", "description", "properties")
 @JvmOverloads
 constructor(
         @Deprecated("")
@@ -14,7 +10,7 @@ constructor(
         val validationStampData: ServiceConfiguration?,
         val validationStampName: String?,
         val validationRunStatusId: String,
-        val description: String,
+        val description: String?,
         val properties: List<PropertyCreationRequest> = listOf()) {
 
     val actualValidationStampName: String?
