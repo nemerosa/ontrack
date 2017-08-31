6990c3dea8754e4c2d7b1c833bc433d90e5a8e26
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-08-31 16:36:30 +0200

    #176 Integration test - showing that validation run request data needs to be used...

--- ontrack-ui/src/test/java/net/nemerosa/ontrack/boot/ui/ValidationRunControllerIT.kt
+++ ontrack-ui/src/test/java/net/nemerosa/ontrack/boot/ui/ValidationRunControllerIT.kt
@@ -1,11 +1,15 @@
 package net.nemerosa.ontrack.boot.ui
 
+import com.fasterxml.jackson.databind.node.IntNode
+import net.nemerosa.ontrack.json.JsonUtils
 import net.nemerosa.ontrack.model.security.ValidationRunCreate
-import net.nemerosa.ontrack.model.structure.NameDescription
-import net.nemerosa.ontrack.model.structure.ServiceConfiguration
-import net.nemerosa.ontrack.model.structure.ValidationRunRequest
+import net.nemerosa.ontrack.model.security.ValidationStampCreate
+import net.nemerosa.ontrack.model.structure.*
+import net.nemerosa.ontrack.test.TestUtils
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
 class ValidationRunControllerIT : AbstractWebTestSupport() {
@@ -34,6 +38,54 @@ class ValidationRunControllerIT : AbstractWebTestSupport() {
         }
         // Checks the run has no data
         assertNull(run.data, "Validation run has no data")
+    }
+
+    @Test
+    fun `Validation with required data and status being passed`() {
+        // Creates a validation stamp with a percentage type
+        val branch = doCreateBranch()
+        val vs = asUser().with(branch.getProject().id(), ValidationStampCreate::class.java).call({
+            structureService.newValidationStamp(
+                    ValidationStamp.of(
+                            branch,
+                            NameDescription.nd("VS1", "")
+                    ).withDataType(
+                            ServiceConfiguration(
+                                    ThresholdPercentageValidationDataType::class.java.name,
+                                    JsonUtils.format(
+                                            ThresholdPercentageValidationDataTypeConfig(60)
+                                    )
+                            )
+                    )
+            )
+        }
+        )
+        // Creates a build
+        val build = doCreateBuild(branch, NameDescription.nd("1", ""))
+        // Validates the build with some data, and a fixed status
+        val run = asUser().with(vs, ValidationRunCreate::class.java).call {
+            validationRunController.newValidationRun(
+                    build.id,
+                    ValidationRunRequest(
+                            null,
+                            ServiceConfiguration(
+                                    vs.name,
+                                    JsonUtils.format(70)
+                            ),
+                            null,
+                            "FAILED",
+                            "No description"
+                    )
+            )
+        }
+        // Checks the run has some data
+        val data = run.data
+        assertNotNull(data, "Validation run has some data")
+        assertEquals(ThresholdPercentageValidationDataType::class.qualifiedName, data.id)
+        TestUtils.assertJsonEquals(
+                data.data,
+                IntNode(70)
+        )
     }
 
 }
