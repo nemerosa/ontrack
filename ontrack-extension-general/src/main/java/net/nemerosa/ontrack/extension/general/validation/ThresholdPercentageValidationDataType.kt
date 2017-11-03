a307630bc9f3994d7444c6dc7e90c676e2424654
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-11-03 19:08:03 +0100

    #176 Threshold type - percentage data type

--- ontrack-extension-general/src/main/java/net/nemerosa/ontrack/extension/general/validation/ThresholdPercentageValidationDataType.kt
+++ ontrack-extension-general/src/main/java/net/nemerosa/ontrack/extension/general/validation/ThresholdPercentageValidationDataType.kt
@@ -3,53 +3,28 @@ package net.nemerosa.ontrack.extension.general.validation
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.IntNode
 import net.nemerosa.ontrack.extension.general.GeneralExtensionFeature
-import net.nemerosa.ontrack.json.JsonUtils
+import net.nemerosa.ontrack.json.parse
 import net.nemerosa.ontrack.json.toJson
 import net.nemerosa.ontrack.model.exceptions.ValidationRunDataInputException
 import net.nemerosa.ontrack.model.form.Form
 import net.nemerosa.ontrack.model.structure.AbstractValidationDataType
-import net.nemerosa.ontrack.model.structure.ValidationRunStatusID
 import org.springframework.stereotype.Component
-
-data class ThresholdPercentageValidationDataTypeConfig(val threshold: Int?, val okIfGreater: Boolean = false)
 
 @Component
 class ThresholdPercentageValidationDataType(
         extensionFeature: GeneralExtensionFeature
-) : AbstractValidationDataType<ThresholdPercentageValidationDataTypeConfig, Int>(
+) : AbstractValidationDataType<ThresholdConfig, Int>(
         extensionFeature
 ) {
-    override fun configFromJson(node: JsonNode?): ThresholdPercentageValidationDataTypeConfig =
-            JsonUtils.parse(node, ThresholdPercentageValidationDataTypeConfig::class.java)
+    override fun configFromJson(node: JsonNode?): ThresholdConfig? = node?.parse()
 
-    override fun configToJson(config: ThresholdPercentageValidationDataTypeConfig): JsonNode =
-            JsonUtils.format(config)
+    override fun configToJson(config: ThresholdConfig): JsonNode = config.toJson()!!
 
-    override fun getConfigForm(config: ThresholdPercentageValidationDataTypeConfig?): Form = Form.create()
-            .with(net.nemerosa.ontrack.model.form.Int
-                    .of("threshold")
-                    .label("Threshold")
-                    .value(config?.threshold)
-                    .min(0).max(100)
-                    .optional()
-            )
-            .with(net.nemerosa.ontrack.model.form.YesNo
-                    .of("okIfGreater")
-                    .label("Valid if greater?")
-                    .value(config?.okIfGreater)
-            )
+    override fun getConfigForm(config: ThresholdConfig?): Form = config.toForm()
 
-    override fun configToFormJson(config: ThresholdPercentageValidationDataTypeConfig?): JsonNode? {
-        return config?.let {
-            mapOf(
-                    "threshold" to it.threshold,
-                    "okIfGreater" to it.okIfGreater
-            ).toJson()
-        }
-    }
+    override fun configToFormJson(config: ThresholdConfig?) = config?.toJson()
 
-    override fun fromConfigForm(node: JsonNode?): ThresholdPercentageValidationDataTypeConfig =
-            JsonUtils.parse(node, ThresholdPercentageValidationDataTypeConfig::class.java)
+    override fun fromConfigForm(node: JsonNode?): ThresholdConfig? = node?.parse()
 
     override fun toJson(data: Int): JsonNode =
             IntNode(data)
@@ -81,22 +56,13 @@ class ThresholdPercentageValidationDataType(
         }
     }
 
-    override fun computeStatus(config: ThresholdPercentageValidationDataTypeConfig?, data: Int): ValidationRunStatusID? {
-        if (config?.threshold != null) {
-            return if (data >= config.threshold && config.okIfGreater) {
-                ValidationRunStatusID.STATUS_PASSED
-            } else {
-                ValidationRunStatusID.STATUS_FAILED
-            }
-        } else {
-            return null
-        }
-    }
+    override fun computeStatus(config: ThresholdConfig?, data: Int) =
+            config?.computeStatus(data)
 
-    override fun validateData(config: ThresholdPercentageValidationDataTypeConfig?, data: Int?) =
+    override fun validateData(config: ThresholdConfig?, data: Int?) =
             validateNotNull(data) {
                 validate(this >= 0, "Percentage must be >= 0")
             }
 
-    override val displayName = "Percentage with threshold"
+    override val displayName = "Percentage"
 }
