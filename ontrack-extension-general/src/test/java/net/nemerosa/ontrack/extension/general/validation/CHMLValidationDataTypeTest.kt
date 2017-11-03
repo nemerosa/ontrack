8554ef4de24d1af97d1ed7354a2f655d1aec63eb
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-11-03 16:12:33 +0100

    #176 CHML data display

--- ontrack-extension-general/src/test/java/net/nemerosa/ontrack/extension/general/validation/CHMLValidationDataTypeTest.kt
+++ ontrack-extension-general/src/test/java/net/nemerosa/ontrack/extension/general/validation/CHMLValidationDataTypeTest.kt
@@ -1,9 +1,11 @@
 package net.nemerosa.ontrack.extension.general.validation
 
 import net.nemerosa.ontrack.extension.general.GeneralExtensionFeature
+import net.nemerosa.ontrack.json.toJson
 import net.nemerosa.ontrack.model.structure.ValidationRunStatusID
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class CHMLValidationDataTypeTest {
 
@@ -60,6 +62,22 @@ class CHMLValidationDataTypeTest {
                         )
                 )?.id
         )
+    }
+
+    @Test
+    fun `Parsing data from client`() {
+        val data = dataType.fromForm(mapOf(
+                "CRITICAL" to 1,
+                "HIGH" to 2,
+                "MEDIUM" to 4,
+                "LOW" to 8
+        ).toJson())
+        assertNotNull(data) {
+            assertEquals(1, it.levels[CHML.CRITICAL])
+            assertEquals(2, it.levels[CHML.HIGH])
+            assertEquals(4, it.levels[CHML.MEDIUM])
+            assertEquals(8, it.levels[CHML.LOW])
+        }
     }
 
 }
