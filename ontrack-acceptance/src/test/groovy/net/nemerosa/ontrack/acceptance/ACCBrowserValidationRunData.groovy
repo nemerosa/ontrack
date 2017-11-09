7d3523f2d290784b7e5e453dcd9d5f0c550731f7
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-11-09 18:33:16 +0100

    #176 GUI test

--- ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/ACCBrowserValidationRunData.groovy
+++ ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/ACCBrowserValidationRunData.groovy
@@ -3,7 +3,9 @@ package net.nemerosa.ontrack.acceptance
 import net.nemerosa.ontrack.acceptance.browser.dialogs.ValidationRunDialog
 import net.nemerosa.ontrack.acceptance.browser.pages.BuildPage
 import net.nemerosa.ontrack.acceptance.support.AcceptanceTestSuite
+import net.nemerosa.ontrack.dsl.ValidationRun
 import org.junit.Test
+import org.openqa.selenium.By
 
 import static net.nemerosa.ontrack.acceptance.steps.BasicSteps.loginAsAdmin
 import static net.nemerosa.ontrack.test.TestUtils.uid
@@ -31,7 +33,20 @@ class ACCBrowserValidationRunData extends AcceptanceTestClient {
             BuildPage buildPage = goTo(BuildPage, [id: build1.id])
             // Validation link
             ValidationRunDialog dialog = buildPage.validate()
+            // Selects the "Coverage" timestamp
+            dialog.validationStamp = 'Coverage'
+            // Enters a value
+            dialog.validationStampDataForm.findElement(By.name("value")).sendKeys("25")
+            // Validates
+            dialog.ok()
         }
+
+        // Checks the validation run
+        def run = build1.validationRuns[0]
+        assert run.status == "WARNING"
+        assert run.data != null
+        assert run.data.id == "net.nemerosa.ontrack.extension.general.validation.ThresholdPercentageValidationDataType"
+        assert run.data.data == 25
     }
 
 }
