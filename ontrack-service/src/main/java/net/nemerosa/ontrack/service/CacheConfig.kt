c9256ea167742f6175289056f259fa53c70f09eb
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-12-17 20:14:17 +0100

    #539 Cache configuration extension

--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/CacheConfig.kt
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/CacheConfig.kt
@@ -2,6 +2,7 @@ package net.nemerosa.ontrack.service
 
 import com.github.benmanes.caffeine.cache.Caffeine
 import net.nemerosa.ontrack.common.Caches
+import net.nemerosa.ontrack.extension.api.CacheConfigExtension
 import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cache.caffeine.CaffeineCache
@@ -13,13 +14,15 @@ import java.util.concurrent.TimeUnit
 @Configuration
 @EnableCaching
 class CacheConfig(
-        val cacheConfigProperties: CacheConfigProperties
+        private val cacheConfigProperties: CacheConfigProperties,
+        private val cacheConfigExtensions: List<CacheConfigExtension>
 ) {
     @Bean
     fun cacheManager(): CacheManager {
         val manager = SimpleCacheManager()
 
         manager.setCaches(
+                // Built in caches
                 listOf(
                         // Cache for settings
                         CaffeineCache(
@@ -28,13 +31,10 @@ class CacheConfig(
                                         .maximumSize(1)
                                         .expireAfterWrite(10, TimeUnit.HOURS)
                                         .build()
-                        ),
-                        // Cache for properties
-                        toCache(
-                                "properties",
-                                "maximumSize=1000,expireAfterWrite=1d,recordStats"
                         )
-                )
+                ) + cacheConfigExtensions.flatMap {
+                    it.caches.map { (name, spec) -> toCache(name, spec) }
+                }
         )
 
         return manager
