66e14133922af0f1541fbbd5ef6ad584b4d9d935
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-12-18 19:36:05 +0100

    #586 Removed deprecated code

--- ontrack-ui-graphql/src/main/java/net/nemerosa/ontrack/graphql/service/GraphQLServiceImpl.kt
+++ ontrack-ui-graphql/src/main/java/net/nemerosa/ontrack/graphql/service/GraphQLServiceImpl.kt
@@ -1,5 +1,6 @@
 package net.nemerosa.ontrack.graphql.service
 
+import graphql.ExecutionInput
 import graphql.ExecutionResult
 import graphql.GraphQL
 import graphql.schema.GraphQLSchema
@@ -16,25 +17,24 @@ class GraphQLServiceImpl(
             operationName: String?,
             reportErrors: Boolean
     ): ExecutionResult {
-        val result = GraphQL(schema).execute(
-                query,
-                operationName,
-                null,
-                variables
-        )
-        if (result != null) {
-            if (result.errors != null && !result.errors.isEmpty() && reportErrors) {
-                result.errors.forEach { error ->
-                    graphQLExceptionHandlers.forEach {
-                        it.handle(error)
-                    }
+        val result: ExecutionResult = GraphQL.newGraphQL(schema)
+                .build()
+                .execute(
+                        ExecutionInput.newExecutionInput()
+                                .query(query)
+                                .operationName(operationName)
+                                .variables(variables)
+                                .build()
+                )
+        if (result.errors != null && !result.errors.isEmpty() && reportErrors) {
+            result.errors.forEach { error ->
+                graphQLExceptionHandlers.forEach {
+                    it.handle(error)
                 }
-                throw GraphQLServiceException(result.errors)
-            } else {
-                return result
             }
+            throw GraphQLServiceException(result.errors)
         } else {
-            throw NullPointerException("No execution result returned by the GraphQL query.")
+            return result
         }
     }
 }
