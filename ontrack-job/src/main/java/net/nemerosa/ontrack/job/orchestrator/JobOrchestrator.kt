49e4692dd3c2e5ac7d27e2c9b55133f275f17e0c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2018-03-07 13:39:29 +0100

    #594 Job orchestrator as Kotlin

---
+++ ontrack-job/src/main/java/net/nemerosa/ontrack/job/orchestrator/JobOrchestrator.kt
@@ -0,0 +1,59 @@
+package net.nemerosa.ontrack.job.orchestrator
+
+import net.nemerosa.ontrack.job.*
+import java.util.*
+import kotlin.streams.toList
+
+class JobOrchestrator(
+        private val jobScheduler: JobScheduler,
+        private val name: String,
+        private val jobOrchestratorSuppliers: Collection<JobOrchestratorSupplier>
+) : Job {
+
+    private val cache = HashSet<JobKey>()
+
+    override fun getKey(): JobKey {
+        return JobCategory.CORE.getType("orchestrator").withName("Orchestrator").getKey(name)
+    }
+
+    override fun getTask(): JobRun {
+        return JobRun { this.orchestrate(it) }
+    }
+
+    @Synchronized
+    fun orchestrate(runListener: JobRunListener) {
+        // Complete list of registrations
+        val registrations = jobOrchestratorSuppliers
+                .flatMap { it.collectJobRegistrations().toList() }
+        // List of keys
+        val keys = registrations
+                .map { registration -> registration.job.key }
+        // Jobs to unschedule
+        val toRemove = HashSet(cache)
+        toRemove.removeAll(keys)
+        toRemove.forEach { jobScheduler.unschedule(it) }
+        // Jobs to add
+        val toAdd = HashSet(keys)
+        toAdd.removeAll(cache)
+        registrations.stream()
+                .filter { jobRegistration -> toAdd.contains(jobRegistration.job.key) }
+                .forEach { jobRegistration -> schedule(jobRegistration, runListener) }
+        // Resets the cache
+        cache.clear()
+        cache.addAll(keys)
+    }
+
+    private fun schedule(jobRegistration: JobRegistration, runListener: JobRunListener) {
+        runListener.message("Scheduling: %s", jobRegistration.job.key)
+        jobScheduler.schedule(jobRegistration.job, jobRegistration.schedule)
+    }
+
+    override fun getDescription(): String {
+        return name
+    }
+
+    override fun isDisabled(): Boolean {
+        return false
+    }
+
+}
