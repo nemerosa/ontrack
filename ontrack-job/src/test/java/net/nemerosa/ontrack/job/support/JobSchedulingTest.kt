b87a9ca2b7613365043f4c2ec3ff698e51abb7be
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2018-03-07 19:10:09 +0100

    #594 Migration of tests to the DSL

--- ontrack-job/src/test/java/net/nemerosa/ontrack/job/support/JobSchedulingTest.kt
+++ ontrack-job/src/test/java/net/nemerosa/ontrack/job/support/JobSchedulingTest.kt
@@ -10,8 +10,7 @@ import java.util.*
 import java.util.concurrent.Future
 import java.util.concurrent.TimeUnit
 import java.util.function.BiFunction
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
+import kotlin.test.*
 
 class JobSchedulingTest {
 
@@ -61,6 +60,78 @@ class JobSchedulingTest {
                 tick_seconds(2)
                 // The job has run
                 assertEquals(2, count)
+            }
+        }
+    }
+
+    @Test
+    fun statuses() {
+        scheduler {
+            val longJob = schedule(
+                    ConfigurableJob(name = "long", wait = 5000),
+                    Schedule.EVERY_MINUTE
+            )
+            val shortJob = schedule(
+                    ConfigurableJob(name = "short"),
+                    Schedule.EVERY_SECOND.after(60)
+            )
+            // The long job is already running, not the short one
+            schedulerPool.runNextPendingCommand()
+            val statuses = scheduler.jobStatuses
+                    .associateBy { it.key }
+
+            val longStatus = statuses[longJob.key]
+            assertNotNull(longStatus) { assertTrue(it.isRunning) }
+
+            val shortStatus = statuses[shortJob.key]
+            assertNotNull(shortStatus) { assertFalse(it.isRunning) }
+        }
+
+    }
+
+    @Test
+    fun removing_a_long_running_job() {
+        scheduler {
+            job {
+                // The job is now running
+                schedulerPool.runNextPendingCommand()
+                val status = scheduler.getJobStatus(key)
+                assertPresent(status) {
+                    assertTrue(it.isRunning)
+                }
+                // Now, removes the job
+                unschedule(this)
+                // Waits a bit, and checks the job has stopped running
+                jobPool.runUntilIdle()
+                assertEquals(0, count)
+            }
+        }
+    }
+
+    @Test
+    fun job_failures() {
+        scheduler {
+            val job = schedule(
+                    ConfigurableJob(fail = true),
+                    Schedule.EVERY_SECOND
+            )
+            // After some seconds, the job keeps running and has only failed
+            tick_seconds(3)
+            val status = scheduler.getJobStatus(job.key)
+            assertPresent(status) {
+                val lastErrorCount = it.lastErrorCount
+                assertEquals(4, lastErrorCount)
+                assertEquals("Task failure", it.lastError)
+                // Now, fixes the job
+                job.fail = false
+                // Waits a bit, and checks the job is now OK
+                tick_seconds(2)
+                val newStatus = scheduler.getJobStatus(job.key)
+                assertPresent(newStatus) {
+                    assertEquals(2, job.count)
+                    assertEquals(0, it.lastErrorCount)
+                    assertNull(it.lastError)
+                }
             }
         }
     }
@@ -158,6 +229,205 @@ class JobSchedulingTest {
         }
     }
 
+    @Test
+    fun job_pause() {
+        test_with_pause(
+                { _, job -> job.pause() },
+                { _, job -> job.resume() }
+        )
+    }
+
+    @Test
+    fun job_schedule_pause() {
+        test_with_pause(
+                { jobScheduler, job -> jobScheduler.pause(job.key) },
+                { jobScheduler, job -> jobScheduler.resume(job.key) }
+        )
+    }
+
+    @Test
+    fun scheduler_pause() {
+        test_with_pause(
+                { jobScheduler, _ -> jobScheduler.pause() },
+                { jobScheduler, _ -> jobScheduler.resume() }
+        )
+    }
+
+    @Test
+    fun stop() {
+        scheduler {
+            job {
+                // Kicks the scheduling of the job
+                schedulerPool.runNextPendingCommand()
+
+                // Checks it's running
+                status(key) {
+                    assertTrue(isRunning, "Job is running")
+                }
+
+                // Stops the job
+                println("Stopping the job.")
+                assertTrue(scheduler.stop(key), "Job has been stopped")
+
+                // Checks it has actually been stopped
+                status(key) {
+                    assertFalse(isRunning, "Job is actually stopped")
+                }
+            }
+        }
+    }
+
+    @Test
+    fun keys() {
+        scheduler {
+            val longCountJob = ConfigurableJob("long")
+            val countJob = ConfigurableJob("short")
+            val otherTypeJob = ConfigurableJob("other", category = Fixtures.TEST_OTHER_CATEGORY)
+
+            schedule(longCountJob)
+            schedule(countJob)
+            schedule(otherTypeJob)
+
+            assertEquals(
+                    setOf(longCountJob.key, countJob.key, otherTypeJob.key),
+                    scheduler.allJobKeys
+            )
+
+            assertEquals(
+                    setOf(longCountJob.key, countJob.key),
+                    scheduler.getJobKeysOfCategory(Fixtures.TEST_CATEGORY)
+            )
+
+            assertEquals(
+                    setOf(otherTypeJob.key),
+                    scheduler.getJobKeysOfCategory(Fixtures.TEST_OTHER_CATEGORY)
+            )
+
+        }
+    }
+
+    @Test(expected = JobNotScheduledException::class)
+    fun pause_for_not_schedule_job() {
+        scheduler {
+            scheduler.pause(JobCategory.of("test").getType("test").getKey("x"))
+        }
+    }
+
+    @Test(expected = JobNotScheduledException::class)
+    fun resume_for_not_schedule_job() {
+        scheduler {
+            scheduler.resume(JobCategory.of("test").getType("test").getKey("x"))
+        }
+    }
+
+    @Test(expected = JobNotScheduledException::class)
+    fun fire_immediately_for_not_schedule_job() {
+        scheduler {
+            scheduler.fireImmediately(JobCategory.of("test").getType("test").getKey("x"))
+        }
+    }
+
+    @Test
+    fun job_status_for_not_schedule_job() {
+        scheduler {
+            assertNotPresent(scheduler.getJobStatus(JobCategory.of("test").getType("test").getKey("x")))
+        }
+    }
+
+    @Test
+    fun invalid_job() {
+        scheduler {
+            job {
+                // After some seconds, the job keeps running
+                tick_seconds(3)
+                val currentCount = count
+                assertEquals(4, count, "Job ran four times")
+                // Invalidates the job
+                invalidate()
+                // The status indicates the job is no longer valid, but is still there
+                status(key) {
+                    assertFalse(isValid)
+                    assertNull(nextRunDate)
+                }
+                // After some seconds, the job has not run
+                tick_seconds(1)
+                jobPool.runUntilIdle()
+                assertEquals(currentCount, count)
+                // ... and it's gone
+                assertNotPresent(scheduler.getJobStatus(key))
+            }
+        }
+    }
+
+    @Test
+    fun paused_job_can_be_fired() {
+        scheduler {
+            job {
+                tick_seconds(2)
+                // After a few seconds, the count has moved
+                val currentCount = count
+                assertEquals(3, currentCount)
+                // Pauses the job now
+                scheduler.pause(key)
+                // Not running
+                tick_seconds(2)
+                assertEquals(currentCount, count)
+                // Forcing the run
+                fireImmediatelyRequired(this)
+                jobPool.runUntilIdle()
+                assertTrue(count > currentCount)
+            }
+        }
+    }
+
+    @Test
+    fun not_scheduled_job_can_be_fired() {
+        scheduler {
+            job(Schedule.NONE) {
+                tick_seconds(2)
+                // After a few seconds, the count has NOT moved
+                assertEquals(0, count)
+                // Forcing the run
+                fireImmediatelyRequired(this)
+                jobPool.runUntilIdle()
+                assertEquals(1, count)
+            }
+        }
+    }
+
+    @Test
+    fun not_scheduled_job_cannot_be_paused() {
+        scheduler {
+            job(Schedule.NONE) {
+                tick_seconds(2)
+                // After a few seconds, the count has NOT moved
+                assertEquals(0, count)
+                // Pausing the job
+                scheduler.pause(key)
+                // Not paused
+                status(key) {
+                    assertFalse(isPaused, "Job cannot be paused since it is not scheduled.")
+                }
+            }
+        }
+    }
+
+    @Test
+    fun invalid_job_cannot_be_fired() {
+        scheduler {
+            val job = ConfigurableJob()
+            job.invalidate()
+            // Schedules, but not now
+            scheduler.schedule(job, Schedule.EVERY_MINUTE.after(1))
+            // Forcing the run
+            assertNotPresent(fireImmediately(job))
+            // ... to not avail
+            assertEquals(0, job.count)
+            // ... and it's now gone
+            assertNotPresent(scheduler.getJobStatus(job.key))
+        }
+    }
+
     /**
      * Runs the scheduler for {count} seconds with intervals of 1/2 seconds.
      */
@@ -165,6 +435,40 @@ class JobSchedulingTest {
         repeat(count * 2) {
             schedulerPool.tick(500, TimeUnit.MILLISECONDS)
             jobPool.runUntilIdle()
+        }
+    }
+
+
+    private fun test_with_pause(
+            pause: (JobScheduler, ConfigurableJob) -> Unit,
+            resume: (JobScheduler, ConfigurableJob) -> Unit
+    ) {
+        scheduler {
+            job {
+                // Runs a few times
+                repeat(2) {
+                    schedulerPool.tick(1, TimeUnit.SECONDS)
+                    jobPool.tick(1, TimeUnit.SECONDS)
+                }
+                // Checks the job has run
+                assertEquals(2, count)
+                // Pauses
+                pause(scheduler, this)
+                // After some seconds, the job has not run
+                repeat(2) {
+                    schedulerPool.tick(1, TimeUnit.SECONDS)
+                    jobPool.tick(1, TimeUnit.SECONDS)
+                }
+                assertEquals(2, count)
+                // Resumes the job
+                resume(scheduler, this)
+                // After some seconds, the job has started again
+                repeat(2) {
+                    schedulerPool.tick(1, TimeUnit.SECONDS)
+                    jobPool.tick(1, TimeUnit.SECONDS)
+                }
+                assertEquals(4, count)
+            }
         }
     }
 
@@ -203,7 +507,7 @@ class JobSchedulingTest {
             scheduler.unschedule(job.key)
         }
 
-        fun <T : Job> schedule(job: T, schedule: Schedule): T {
+        fun <T : Job> schedule(job: T, schedule: Schedule = Schedule.EVERY_SECOND): T {
             scheduler.schedule(job, schedule)
             return job
         }
@@ -212,6 +516,13 @@ class JobSchedulingTest {
 
         fun fireImmediatelyRequired(job: Job) {
             fireImmediately(job).orElseThrow { IllegalStateException("No future being returned.") }
+        }
+
+        fun status(key: JobKey, code: JobStatus.() -> Unit) {
+            val jobStatus = scheduler.getJobStatus(key)
+            assertPresent(jobStatus) {
+                it.code()
+            }
         }
     }
 
