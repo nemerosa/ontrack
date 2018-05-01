db78ddd707b546d12956dae891a0880015b0aab2
Jenkins <jenkins@nemerosa.net>
2018-05-01 12:38:54 +0200

    #600 GraphQL validation run type to Kotlin

---
+++ ontrack-ui-graphql/src/main/java/net/nemerosa/ontrack/graphql/schema/GQLTypeValidationRun.kt
@@ -0,0 +1,73 @@
+package net.nemerosa.ontrack.graphql.schema
+
+import graphql.Scalars.GraphQLInt
+import graphql.schema.GraphQLNonNull
+import graphql.schema.GraphQLObjectType
+import graphql.schema.GraphQLObjectType.newObject
+import graphql.schema.GraphQLTypeReference
+import net.nemerosa.ontrack.graphql.support.GraphqlUtils.stdList
+import net.nemerosa.ontrack.model.structure.ProjectEntityType
+import net.nemerosa.ontrack.model.structure.Signature
+import net.nemerosa.ontrack.model.structure.ValidationRun
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class GQLTypeValidationRun(
+        creation: GQLTypeCreation,
+        private val validationRunStatus: GQLTypeValidationRunStatus,
+        projectEntityFieldContributors: List<GQLProjectEntityFieldContributor>,
+        private val projectEntityInterface: GQLProjectEntityInterface
+) : AbstractGQLProjectEntity<ValidationRun>(
+        ValidationRun::class.java,
+        ProjectEntityType.VALIDATION_RUN,
+        projectEntityFieldContributors,
+        creation
+) {
+
+    override fun getTypeName() = VALIDATION_RUN
+
+    override fun createType(cache: GQLTypeCache): GraphQLObjectType =
+            newObject()
+                    .name(VALIDATION_RUN)
+                    .withInterface(projectEntityInterface.typeRef)
+                    .fields(projectEntityInterfaceFields())
+                    // Build
+                    .field {
+                        it
+                                .name("build")
+                                .description("Associated build")
+                                .type(GraphQLNonNull(GraphQLTypeReference(GQLTypeBuild.BUILD)))
+                    }
+                    // Promotion level
+                    .field {
+                        it
+                                .name("validationStamp")
+                                .description("Associated validation stamp")
+                                .type(GraphQLNonNull(GraphQLTypeReference(GQLTypeValidationStamp.VALIDATION_STAMP)))
+                    }
+                    // Run order
+                    .field {
+                        it
+                                .name("runOrder")
+                                .description("Run order")
+                                .type(GraphQLInt)
+                    }
+                    // Validation statuses
+                    .field {
+                        it
+                                .name("validationRunStatuses")
+                                .description("List of validation statuses")
+                                .type(stdList(validationRunStatus.typeRef))
+                    }
+                    // OK
+                    .build()
+
+    override fun getSignature(entity: ValidationRun): Optional<Signature> {
+        return Optional.ofNullable(entity.lastStatus.signature)
+    }
+
+    companion object {
+        const val VALIDATION_RUN = "ValidationRun"
+    }
+}
