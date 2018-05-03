a99618374dce0039afa184ae8c261ecaf6e78e79
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2018-05-03 19:44:32 +0200

    #600 VS page - list of runs

--- ontrack-web/src/app/view/view.validationStamp.js
+++ ontrack-web/src/app/view/view.validationStamp.js
@@ -77,6 +77,7 @@ angular.module('ot.view.validationStamp', [
                         id
                         runOrder
                         build {
+                          id
                           name
                         }
                         runInfo {
