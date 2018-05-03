0ab0e5e47c5f8cbc9748dd0efbcb8a143bd499df
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2018-05-03 14:13:52 +0200

    #600 Pagination list - integration test (wip)

--- ontrack-ui-graphql/src/test/java/net/nemerosa/ontrack/graphql/AbstractQLKTITSupport.kt
+++ ontrack-ui-graphql/src/test/java/net/nemerosa/ontrack/graphql/AbstractQLKTITSupport.kt
@@ -4,18 +4,23 @@ import com.fasterxml.jackson.databind.JsonNode
 import graphql.GraphQL
 import net.nemerosa.ontrack.graphql.schema.GraphqlSchemaService
 import net.nemerosa.ontrack.graphql.support.exception
-import net.nemerosa.ontrack.it.AbstractServiceTestSupport
+import net.nemerosa.ontrack.it.AbstractDSLTestSupport
 import net.nemerosa.ontrack.json.JsonUtils
 import org.springframework.beans.factory.annotation.Autowired
 import kotlin.test.fail
 
-abstract class AbstractQLKTITSupport : AbstractServiceTestSupport() {
+abstract class AbstractQLKTITSupport : AbstractDSLTestSupport() {
 
     @Autowired
     private lateinit var schemaService: GraphqlSchemaService
 
-    fun run(query: String): JsonNode {
-        val result = GraphQL.newGraphQL(schemaService.schema).build().execute(query)
+    fun run(query: String, variables: Map<String, *> = emptyMap<String, Any>()): JsonNode {
+        val result = GraphQL
+                .newGraphQL(schemaService.schema)
+                .build()
+                .execute {
+                    it.query(query).variables(variables)
+                }
         val error = result.exception
         if (error != null) {
             throw error
