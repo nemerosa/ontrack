6708ff984ca1717b9fdcd545beb199114f4c0052
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2018-05-24 21:42:22 +0200

    #614 InfluxDB tests - correction of properties

--- ontrack-extension-influxdb/src/main/kotlin/net/nemerosa/ontrack/extension/influxdb/InfluxDBExtensionProperties.kt
+++ ontrack-extension-influxdb/src/main/kotlin/net/nemerosa/ontrack/extension/influxdb/InfluxDBExtensionProperties.kt
@@ -5,16 +5,13 @@ import org.springframework.stereotype.Component
 
 const val INFLUXDB_EXTENSION_PROPERTIES_PREFIX = "ontrack.influxdb"
 
-/**
- * @property enabled To enable the extension
- */
 @ConfigurationProperties(prefix = INFLUXDB_EXTENSION_PROPERTIES_PREFIX)
 @Component
-class InfluxDBExtensionProperties(
-        var enabled: Boolean = false,
-        var uri: String = "http://localhost:8086",
-        var username: String = "root",
-        var password: String = "root",
-        var db: String = "ontrack",
-        var create: Boolean = true
-)
+class InfluxDBExtensionProperties {
+    var enabled: Boolean = false
+    var uri: String = "http://localhost:8086"
+    var username: String = "root"
+    var password: String = "root"
+    var db: String = "ontrack"
+    var create: Boolean = true
+}
