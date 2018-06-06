a1ab555d79d9dff7b9817195068e71a67fdd3c67
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2018-06-06 22:35:51 +0200

    #592 Added health aggregator auto config

--- ontrack-ui/src/test/java/net/nemerosa/ontrack/boot/ui/AbstractWebTestSupport.java
+++ ontrack-ui/src/test/java/net/nemerosa/ontrack/boot/ui/AbstractWebTestSupport.java
@@ -2,9 +2,14 @@ package net.nemerosa.ontrack.boot.ui;
 
 import net.nemerosa.ontrack.boot.Application;
 import net.nemerosa.ontrack.it.AbstractServiceTestSupport;
+import org.springframework.boot.actuate.autoconfigure.health.HealthIndicatorAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = Application.class)
+@ImportAutoConfiguration({
+        HealthIndicatorAutoConfiguration.class
+})
 public abstract class AbstractWebTestSupport extends AbstractServiceTestSupport {
 
 }
