5f92a62653aa2be5a900c05693d88708494092f4
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2018-10-02 09:12:12 +0200

    #176 Simplifying the `ValidationRunRequest` - correction of tests

--- ontrack-ui-graphql/src/test/groovy/net/nemerosa/ontrack/graphql/ProjectQLIT.groovy
+++ ontrack-ui-graphql/src/test/groovy/net/nemerosa/ontrack/graphql/ProjectQLIT.groovy
@@ -158,9 +158,10 @@ class ProjectQLIT extends AbstractQLITSupport {
                     structureService.newValidationRun(
                             build,
                             new ValidationRunRequest(
-                                    null,
                                     vs.name,
-                                    "PASSED",
+                                    ValidationRunStatusID.STATUS_PASSED,
+                                    null,
+                                    null,
                                     "Validation"
                             )
                     )
@@ -182,62 +183,6 @@ class ProjectQLIT extends AbstractQLITSupport {
             }
         }""")
         assert data.projects.branches.validationStamps.validationRuns.build.name.flatten() == ['4', '2']
-    }
-
-    @Test
-    void 'Validation run statuses for a run for a validation stamp'() {
-        def vs = doCreateValidationStamp()
-        def branch = vs.branch
-        def project = branch.project
-        def build = doCreateBuild(branch, NameDescription.nd("1", "Build 1"))
-        def validationRun = asUser().with(project, ValidationRunCreate).call {
-            structureService.newValidationRun(
-                    build,
-                    new ValidationRunRequest(
-                            null,
-                            vs.name,
-                            "FAILED"
-                    )
-            )
-        }
-        asUser().with(project, ValidationRunStatusChange).call {
-            structureService.newValidationRunStatus(
-                    validationRun,
-                    ValidationRunStatus.of(
-                            Signature.of('test'),
-                            ValidationRunStatusID.STATUS_INVESTIGATING,
-                            "Investigating"
-                    )
-            )
-            structureService.newValidationRunStatus(
-                    validationRun,
-                    ValidationRunStatus.of(
-                            Signature.of('test'),
-                            ValidationRunStatusID.STATUS_EXPLAINED,
-                            "Explained"
-                    )
-            )
-        }
-        def data = run("""{
-            projects (id: ${project.id}) {
-                branches (name: "${branch.name}") {
-                    validationStamps {
-                        name
-                        validationRuns {
-                            validationRunStatuses {
-                                statusID {
-                                    id
-                                }
-                                description
-                            }
-                        }
-                    }
-                }
-            }
-        }""")
-        def validationRunStatuses = data.projects.branches.validationStamps.validationRuns.validationRunStatuses.flatten()
-        assert validationRunStatuses.statusID.id as Set == ['EXPLAINED', 'INVESTIGATING', 'FAILED'] as Set
-        assert validationRunStatuses.description as Set == ['Explained', 'Investigating', 'Validation failed'] as Set
     }
 
     @Test
