daad858eada54e18c3a951b7cb26910e72f4ff4d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2018-12-02 09:36:59 +0100

    #628 Kotlin conversion

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/buildfilter/BuildFilterResource.kt
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/buildfilter/BuildFilterResource.kt
@@ -8,7 +8,7 @@ import net.nemerosa.ontrack.model.structure.Branch
  *
  * @param <T> Type of configuration data for this build filter
  * @property branch Branch
- * @property shared Shared filter?
+ * @property isShared Shared filter?
  * @property name Name for this filter
  * @property type Type for this filter
  * @property data Specific data for this filter
@@ -17,7 +17,7 @@ import net.nemerosa.ontrack.model.structure.Branch
 class BuildFilterResource<T>(
         @JsonIgnore
         val branch: Branch,
-        val shared: Boolean,
+        val isShared: Boolean,
         val name: String,
         val type: String,
         val data: T,
