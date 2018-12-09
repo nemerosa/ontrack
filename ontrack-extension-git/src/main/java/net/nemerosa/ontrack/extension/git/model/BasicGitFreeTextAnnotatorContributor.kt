bf55183c73e701177fdf765d031cde17954b6809
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2018-12-09 14:30:40 +0100

    #631 `FreeTextAnnotationContributor` for Basic git configurator

--- ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/model/BasicGitFreeTextAnnotatorContributor.kt
+++ ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/model/BasicGitFreeTextAnnotatorContributor.kt
@@ -1,23 +1,23 @@
-package net.nemerosa.ontrack.extension.github
+package net.nemerosa.ontrack.extension.git.model
 
-import net.nemerosa.ontrack.extension.github.property.GitHubProjectConfigurationProperty
-import net.nemerosa.ontrack.extension.github.property.GitHubProjectConfigurationPropertyType
+import net.nemerosa.ontrack.extension.git.property.GitProjectConfigurationProperty
+import net.nemerosa.ontrack.extension.git.property.GitProjectConfigurationPropertyType
 import net.nemerosa.ontrack.extension.issues.IssueServiceRegistry
 import net.nemerosa.ontrack.extension.issues.support.AbstractSCMPropertyIssueBasedFreeTextAnnotatorContributor
 import net.nemerosa.ontrack.model.structure.PropertyService
 import org.springframework.stereotype.Component
 
 @Component
-class GitHubFreeTextAnnotatorContributor(
+class BasicGitFreeTextAnnotatorContributor(
         propertyService: PropertyService,
         issueServiceRegistry: IssueServiceRegistry
 ) : AbstractSCMPropertyIssueBasedFreeTextAnnotatorContributor<
-        GitHubProjectConfigurationProperty,
-        GitHubProjectConfigurationPropertyType
+        GitProjectConfigurationProperty,
+        GitProjectConfigurationPropertyType
         >
 (
         propertyService,
         issueServiceRegistry,
-        GitHubProjectConfigurationPropertyType::class,
-        { it.issueServiceConfigurationIdentifier }
+        GitProjectConfigurationPropertyType::class,
+        { it.configuration.issueServiceConfigurationIdentifier }
 )
