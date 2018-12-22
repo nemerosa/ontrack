16b5a4f63be3a322017b70d9c6bef9773ddb71bd
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2018-12-22 20:08:57 +0100

    #625 Branching model service tests

--- ontrack-extension-git/src/test/java/net/nemerosa/ontrack/extension/git/branching/BranchingModelServiceIT.kt
+++ ontrack-extension-git/src/test/java/net/nemerosa/ontrack/extension/git/branching/BranchingModelServiceIT.kt
@@ -1,10 +1,12 @@
 package net.nemerosa.ontrack.extension.git.branching
 
 import net.nemerosa.ontrack.extension.git.AbstractGitTestSupport
+import net.nemerosa.ontrack.model.support.NameValue
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
+import kotlin.test.assertEquals
 
-class BranchingModelServiceIT: AbstractGitTestSupport() {
+class BranchingModelServiceIT : AbstractGitTestSupport() {
 
     @Autowired
     private lateinit var branchingModelService: BranchingModelService
@@ -12,7 +14,34 @@ class BranchingModelServiceIT: AbstractGitTestSupport() {
     @Test
     fun `No property`() {
         project {
-            branchingModelService.getBranchingModel(project)
+            val model = branchingModelService.getBranchingModel(project)
+            assertEquals(
+                    BranchingModel.DEFAULT.patterns,
+                    model.patterns
+            )
+        }
+    }
+
+    @Test
+    fun `Specific property`() {
+        project {
+            setProperty(this, BranchingModelPropertyType::class.java,
+                    BranchingModelProperty(listOf(
+                            NameValue("Development", "master|gatekeeper"),
+                            NameValue("Maintenance", "maintenance/.*"),
+                            NameValue("Release", "release/.*")
+                    )
+                    )
+            )
+            val model = branchingModelService.getBranchingModel(project)
+            assertEquals(
+                    mapOf(
+                            "Development" to "master|gatekeeper",
+                            "Maintenance" to "maintenance/.*",
+                            "Release" to "release/.*"
+                    ),
+                    model.patterns
+            )
         }
     }
 
