d1c271d4d9d515609545d698ad264f206fe86af9
Jenkins <jenkins@nemerosa.net>
2019-01-02 23:46:57 +0000

    GitHub pages for version 3.37.4

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,19 @@
 let html = "";
+const latestRelease = releases[0];
 const prefix = "https://ams3-delivery-space.ams3.cdn.digitaloceanspaces.com/ontrack/release";
+
+html += `
+      <tr>
+        <td>
+            <span class="ontrack-doc-latest">Latest</span> (${latestRelease})
+        </td>
+        <td>
+            <a href="${prefix}/latest/docs/doc/index.html">HTML</a>
+             | <a href="${prefix}/latest/docs/index.pdf">PDF</a>
+             | <a href="${prefix}/latest/docs/javadoc/index.html">Javadoc</a>
+        </td>
+      </tr>`;
+
 $.each(releases, function (index, release) {
    html += `
       <tr>
