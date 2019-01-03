6e1f5cb01a9989729f4796a85afe346088ef5ee6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-01-03 10:48:59 +0100

    #581 List of extensions in the app info

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/structure/Info.kt
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/structure/Info.kt
@@ -1,10 +1,14 @@
 package net.nemerosa.ontrack.model.structure
 
+import net.nemerosa.ontrack.model.extension.ExtensionList
+
 /**
  * Information about the application.
  *
  * @property version Version of the application.
+ * @property extensionList List of extensions
  */
 class Info(
-        val version: VersionInfo
+        val version: VersionInfo,
+        val extensionList: ExtensionList
 )
--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/InfoServiceImpl.kt
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/InfoServiceImpl.kt
@@ -1,5 +1,6 @@
 package net.nemerosa.ontrack.service
 
+import net.nemerosa.ontrack.extension.api.ExtensionManager
 import net.nemerosa.ontrack.model.structure.Info
 import net.nemerosa.ontrack.model.structure.InfoService
 import net.nemerosa.ontrack.model.support.EnvService
@@ -7,11 +8,13 @@ import org.springframework.stereotype.Service
 
 @Service
 class InfoServiceImpl(
-        private val envService: EnvService
+        private val envService: EnvService,
+        private val extensionManager: ExtensionManager
 ) : InfoService {
 
     override val info: Info
         get() = Info(
-                envService.version
+                envService.version,
+                extensionManager.extensionList
         )
 }
