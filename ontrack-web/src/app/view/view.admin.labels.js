4a0240254028ab4ac1b48032da283fb38f48bae6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-01-03 13:29:03 +0100

    #615 Management - GraphQL call

--- ontrack-web/src/app/view/view.admin.labels.js
+++ ontrack-web/src/app/view/view.admin.labels.js
@@ -14,5 +14,28 @@ angular.module('ot.view.admin.labels', [
         const view = ot.view();
         view.title = "Labels";
         view.description = "Management of labels";
+        view.breadcrumbs = ot.homeBreadcrumbs();
+
+        const query = `
+            labels {
+                id
+                category
+                name
+                description
+                color
+                computedBy {
+                    id
+                    name
+                }
+            }
+        `;
+
+        function loadLabels() {
+            otGraphqlService.pageGraphQLCall(query).then(function (data) {
+                $scope.labels = data.labels;
+            });
+        }
+
+        loadLabels();
     })
 ;
