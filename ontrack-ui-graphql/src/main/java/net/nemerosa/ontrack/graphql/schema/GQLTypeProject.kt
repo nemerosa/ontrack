6c3fc4b6ff85924efa8892b26d9df611db00f4e2
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-01-03 21:02:22 +0100

    #615 Assignment - conversion to Kotlin

---
+++ ontrack-ui-graphql/src/main/java/net/nemerosa/ontrack/graphql/schema/GQLTypeProject.kt
@@ -0,0 +1,90 @@
+package net.nemerosa.ontrack.graphql.schema
+
+import graphql.Scalars.GraphQLString
+import graphql.schema.DataFetcher
+import graphql.schema.GraphQLArgument.newArgument
+import graphql.schema.GraphQLFieldDefinition.newFieldDefinition
+import graphql.schema.GraphQLObjectType
+import graphql.schema.GraphQLObjectType.newObject
+import net.nemerosa.ontrack.common.and
+import net.nemerosa.ontrack.graphql.support.GraphqlUtils
+import net.nemerosa.ontrack.graphql.support.GraphqlUtils.stdList
+import net.nemerosa.ontrack.model.structure.*
+import org.springframework.stereotype.Component
+import java.util.*
+import java.util.regex.Pattern
+
+@Component
+class GQLTypeProject(
+        private val structureService: StructureService,
+        creation: GQLTypeCreation,
+        private val branch: GQLTypeBranch,
+        projectEntityFieldContributors: List<GQLProjectEntityFieldContributor>,
+        private val projectEntityInterface: GQLProjectEntityInterface
+) : AbstractGQLProjectEntity<Project>(
+        Project::class.java,
+        ProjectEntityType.PROJECT,
+        projectEntityFieldContributors,
+        creation
+) {
+
+    override fun getTypeName(): String {
+        return PROJECT
+    }
+
+    override fun createType(cache: GQLTypeCache): GraphQLObjectType {
+        return newObject()
+                .name(PROJECT)
+                .withInterface(projectEntityInterface.typeRef)
+                .fields(projectEntityInterfaceFields())
+                .field(GraphqlUtils.disabledField())
+                // Branches
+                .field(
+                        newFieldDefinition()
+                                .name("branches")
+                                .type(stdList(branch.typeRef))
+                                .argument(
+                                        newArgument()
+                                                .name("name")
+                                                .description("Regular expression to match against the branch name")
+                                                .type(GraphQLString)
+                                                .build()
+                                )
+                                .dataFetcher(projectBranchesFetcher())
+                                .build()
+                )
+                // OK
+                .build()
+
+    }
+
+    private fun projectBranchesFetcher(): DataFetcher<*> {
+        return DataFetcher<List<Branch>> { environment ->
+            val source = environment.getSource<Any>()
+            if (source is Project) {
+                val name: String? = environment.getArgument<String>("name")
+                // Combined filter
+                var filter: (Branch) -> Boolean = { true }
+                // Name criteria
+                if (name != null) {
+                    val nameFilter = Pattern.compile(name)
+                    filter = filter.and { branch -> nameFilter.matcher(branch.name).matches() }
+                }
+                structureService
+                        .getBranchesForProject(source.id)
+                        .filter(filter)
+            } else {
+                emptyList()
+            }
+        }
+    }
+
+    override fun getSignature(entity: Project): Optional<Signature> {
+        return Optional.ofNullable(entity.signature)
+    }
+
+    companion object {
+        const val PROJECT = "Project"
+    }
+
+}
