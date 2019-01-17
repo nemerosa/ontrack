962d7f1ee470d5e95d4e600bd987d48ea38d6d34
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-01-17 21:37:05 +0100

    #632 Separate files

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/structure/BranchModelMatcherProvider.kt
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/structure/BranchModelMatcherProvider.kt
@@ -1,14 +1,8 @@
 package net.nemerosa.ontrack.model.structure
 
-import org.springframework.stereotype.Component
-
 interface BranchModelMatcherProvider {
 
     fun getBranchModelMatcher(project: Project): BranchModelMatcher?
 
 }
 
-@Component
-class NOPBranchModelMatcherProvider : BranchModelMatcherProvider {
-    override fun getBranchModelMatcher(project: Project): BranchModelMatcher? = null
-}
