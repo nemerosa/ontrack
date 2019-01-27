135a4642d94c711acb491b8e9192ce208c8dbf12
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-01-27 09:03:07 +0100

    #641 Remove unused code

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/ui/DefaultURIBuilder.java
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/ui/DefaultURIBuilder.java
@@ -3,19 +3,11 @@ package net.nemerosa.ontrack.boot.ui;
 import net.nemerosa.ontrack.common.RunProfile;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 
 @Component
@@ -27,23 +19,6 @@ public class DefaultURIBuilder extends AbstractURIBuilder {
 
         // Default builder
         UriComponentsBuilder builder = MvcUriComponentsBuilder.fromMethodCall(methodInvocation);
-
-        // Default URI
-        UriComponents uriComponents = builder.build();
-
-        // TODO #251 Workaround for SPR-12771
-        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
-        HttpRequest httpRequest = new ServletServerHttpRequest(request);
-        String portHeader = httpRequest.getHeaders().getFirst("X-Forwarded-Port");
-        if (StringUtils.hasText(portHeader)) {
-            int port = Integer.parseInt(portHeader);
-            String scheme = uriComponents.getScheme();
-            if (("https".equals(scheme) && port == 443) || ("http".equals(scheme) && port == 80)) {
-                port = -1;
-            }
-            builder.port(port);
-        }
 
         // OK
         return builder.build().toUri();
