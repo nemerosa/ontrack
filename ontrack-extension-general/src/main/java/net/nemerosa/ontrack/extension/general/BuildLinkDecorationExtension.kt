199b095656e840159280d91382e600d9ea1a5d6c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-04-03 08:20:32 +0200

    #665 Main build links - extra link to the decoration

--- ontrack-extension-general/src/main/java/net/nemerosa/ontrack/extension/general/BuildLinkDecorationExtension.kt
+++ ontrack-extension-general/src/main/java/net/nemerosa/ontrack/extension/general/BuildLinkDecorationExtension.kt
@@ -17,23 +17,41 @@ class BuildLinkDecorationExtension(
         private val propertyService: PropertyService,
         private val mainBuildLinksService: MainBuildLinksService,
         private val mainBuildLinksFilterService: MainBuildLinksFilterService
-) : AbstractExtension(extensionFeature), DecorationExtension<BuildLinkDecoration> {
+) : AbstractExtension(extensionFeature), DecorationExtension<BuildLinkDecorationList> {
 
-    override fun getScope() = EnumSet.of(ProjectEntityType.BUILD)
+    override fun getScope(): EnumSet<ProjectEntityType> = EnumSet.of(ProjectEntityType.BUILD)
 
-    override fun getDecorations(entity: ProjectEntity): List<Decoration<BuildLinkDecoration>> {
+    override fun getDecorations(entity: ProjectEntity): List<Decoration<BuildLinkDecorationList>> {
         // Gets the main build links of the source project
         val labels = mainBuildLinksService.getMainBuildLinksConfig(entity.project).labels
         // Gets the links from
-        return structureService.getBuildLinksFrom(entity as Build)
-                // Filters on the labels
-                .filter { target ->
-                    mainBuildLinksFilterService.isMainBuidLink(target, labels)
-                }
-                .map { getDecoration(it) }
+        val links = structureService.getBuildLinksFrom(entity as Build)
+        // Main links
+        val mainLinks = links.filter { target ->
+            mainBuildLinksFilterService.isMainBuidLink(target, labels)
+        }
+        // Checks if there are extra links (besides the main ones)
+        val extraLinks = links.size > 0 && (links.size > mainLinks.size)
+        val extraLink = if (extraLinks) {
+            uriBuilder.getEntityPage(entity)
+        } else {
+            null
+        }
+        // Decoration items for the main links
+        val decorations = mainLinks.map { getDecoration(it) }
+        // Global decoration
+        return listOf(
+                Decoration.of(
+                        this,
+                        BuildLinkDecorationList(
+                                decorations,
+                                extraLink
+                        )
+                )
+        )
     }
 
-    protected fun getDecoration(build: Build): Decoration<BuildLinkDecoration> {
+    protected fun getDecoration(build: Build): BuildLinkDecoration {
         // Gets the list of promotion runs for this build
         val promotionRuns = structureService.getLastPromotionRunsForBuild(build.id)
         // Gets the label to use for the decoration
@@ -41,7 +59,7 @@ class BuildLinkDecorationExtension(
         val labelProperty: ReleaseProperty? = propertyService.getProperty(build, ReleasePropertyType::class.java).value
         val label = displayProperty.getLabel(build, labelProperty)
         // Decoration
-        return Decoration.of(this, build.asBuildLinkDecoration(uriBuilder, promotionRuns, label))
+        return build.asBuildLinkDecoration(uriBuilder, promotionRuns, label)
     }
 
 }
