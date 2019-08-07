f961e2ef20c2ceb31c4bebc034098350e0c3e147
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-08-07 14:31:06 +0200

    #682 Conversion to Kotlin

--- ontrack-service/src/test/java/net/nemerosa/ontrack/service/IntervalBuildFilterIT.kt
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/IntervalBuildFilterIT.kt
@@ -1,64 +1,56 @@
-package net.nemerosa.ontrack.service;
+package net.nemerosa.ontrack.service
 
-import net.nemerosa.ontrack.model.buildfilter.BuildFilterProviderData;
-import net.nemerosa.ontrack.model.buildfilter.BuildFilterService;
-import net.nemerosa.ontrack.model.exceptions.BuildNotFoundException;
-import net.nemerosa.ontrack.model.structure.Build;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import net.nemerosa.ontrack.model.buildfilter.BuildFilterProviderData
+import net.nemerosa.ontrack.model.exceptions.BuildNotFoundException
+import org.junit.Test
 
-import java.util.List;
+class IntervalBuildFilterIT : AbstractBuildFilterIT() {
 
-public class IntervalBuildFilterIT extends AbstractBuildFilterIT {
-
-    @Autowired
-    private BuildFilterService buildFilterService;
-
-    protected BuildFilterProviderData<BuildIntervalFilterData> filter(BuildIntervalFilterData data) {
+    protected fun filter(data: BuildIntervalFilterData): BuildFilterProviderData<BuildIntervalFilterData> {
         return buildFilterService.getBuildFilterProviderData(
-                BuildIntervalFilterProvider.class.getName(),
+                BuildIntervalFilterProvider::class.java.name,
                 data
-        );
+        )
     }
 
-    @Test(expected = BuildNotFoundException.class)
-    public void from_does_not_exist() {
-        filter(BuildIntervalFilterData.of("xxx", null)).filterBranchBuilds(branch);
+    @Test(expected = BuildNotFoundException::class)
+    fun from_does_not_exist() {
+        filter(BuildIntervalFilterData.of("xxx", null)).filterBranchBuilds(branch)
     }
 
-    @Test(expected = BuildNotFoundException.class)
-    public void to_does_not_exist() throws Exception {
-        build("1.0.0");
-        filter(BuildIntervalFilterData.of("1.0.0", "xxx")).filterBranchBuilds(branch);
-    }
-
-    @Test
-    public void from_only() throws Exception {
-        build("1.0.0");
-        build("1.0.1");
-        build("1.1.0");
-        List<Build> builds = filter(BuildIntervalFilterData.of("1.0.1", null)).filterBranchBuilds(branch);
-        checkList(builds, "1.1.0", "1.0.1");
+    @Test(expected = BuildNotFoundException::class)
+    fun to_does_not_exist() {
+        build("1.0.0")
+        filter(BuildIntervalFilterData.of("1.0.0", "xxx")).filterBranchBuilds(branch)
     }
 
     @Test
-    public void from_and_to() throws Exception {
-        build("1.0.0");
-        build("1.0.1");
-        build("1.0.2");
-        build("1.1.0");
-        List<Build> builds = filter(BuildIntervalFilterData.of("1.0.0", "1.0.2")).filterBranchBuilds(branch);
-        checkList(builds, "1.0.2", "1.0.1", "1.0.0");
+    fun from_only() {
+        build("1.0.0")
+        build("1.0.1")
+        build("1.1.0")
+        val builds = filter(BuildIntervalFilterData.of("1.0.1", null)).filterBranchBuilds(branch)
+        checkList(builds, "1.1.0", "1.0.1")
     }
 
     @Test
-    public void from_and_to_reverse() throws Exception {
-        build("1.0.0");
-        build("1.0.1");
-        build("1.0.2");
-        build("1.1.0");
-        List<Build> builds = filter(BuildIntervalFilterData.of("1.0.2", "1.0.0")).filterBranchBuilds(branch);
-        checkList(builds, "1.0.2", "1.0.1", "1.0.0");
+    fun from_and_to() {
+        build("1.0.0")
+        build("1.0.1")
+        build("1.0.2")
+        build("1.1.0")
+        val builds = filter(BuildIntervalFilterData.of("1.0.0", "1.0.2")).filterBranchBuilds(branch)
+        checkList(builds, "1.0.2", "1.0.1", "1.0.0")
+    }
+
+    @Test
+    fun from_and_to_reverse() {
+        build("1.0.0")
+        build("1.0.1")
+        build("1.0.2")
+        build("1.1.0")
+        val builds = filter(BuildIntervalFilterData.of("1.0.2", "1.0.0")).filterBranchBuilds(branch)
+        checkList(builds, "1.0.2", "1.0.1", "1.0.0")
     }
 
 }
