a0aacf437c28fa6d7a66b386b855504eb83ffa6a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-08-13 14:58:27 +0200

    #661 Promotion run checks - DSL property

--- ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/ACCDSLPreviousPromotionCheck.groovy
+++ ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/ACCDSLPreviousPromotionCheck.groovy
@@ -9,7 +9,7 @@ import static net.nemerosa.ontrack.test.TestUtils.uid
 class ACCDSLPreviousPromotionCheck extends AbstractACCDSL {
 
     @Test
-    void 'Previous promotion required as settings level'() {
+    void 'Previous promotion required at settings level'() {
         String projectName = uid("P")
         def old = ontrack.config.previousPromotionRequired
         try {
@@ -29,6 +29,69 @@ class ACCDSLPreviousPromotionCheck extends AbstractACCDSL {
             }
         } finally {
             ontrack.config.previousPromotionRequired = old
+        }
+    }
+
+    @Test
+    void 'Previous promotion required at project level'() {
+        String projectName = uid("P")
+        ontrack.project(projectName) {
+            assert config.previousPromotionRequired == null
+            config.previousPromotionRequired = true
+            assert config.previousPromotionRequired == true
+            branch("master") {
+                promotionLevel "IRON"
+                promotionLevel "SILVER"
+                build("1") {
+                    assertFailsWithMessage("The \"Previous Promotion Condition\" setup in Project ${projectName} prevents\n" +
+                            "the SILVER to be granted because the IRON promotion\n" +
+                            "has not been granted.") {
+                        promote "SILVER"
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    void 'Previous promotion required at branch level'() {
+        String projectName = uid("P")
+        ontrack.project(projectName) {
+            branch("master") {
+                assert config.previousPromotionRequired == null
+                config.previousPromotionRequired = true
+                assert config.previousPromotionRequired == true
+                promotionLevel "IRON"
+                promotionLevel "SILVER"
+                build("1") {
+                    assertFailsWithMessage("The \"Previous Promotion Condition\" setup in Branch ${projectName}/master prevents\n" +
+                            "the SILVER to be granted because the IRON promotion\n" +
+                            "has not been granted.") {
+                        promote "SILVER"
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    void 'Previous promotion required at promotion level level'() {
+        String projectName = uid("P")
+        ontrack.project(projectName) {
+            branch("master") {
+                promotionLevel "IRON"
+                def pl = promotionLevel("SILVER")
+                assert pl.config.previousPromotionRequired == null
+                pl.config.previousPromotionRequired = true
+                assert pl.config.previousPromotionRequired == true
+                build("1") {
+                    assertFailsWithMessage("The \"Previous Promotion Condition\" setup in Promotion level ${projectName}/master/SILVER prevents\n" +
+                            "the SILVER to be granted because the IRON promotion\n" +
+                            "has not been granted.") {
+                        promote "SILVER"
+                    }
+                }
+            }
         }
     }
 
