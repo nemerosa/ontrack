04aca6c6b4f5558eb9a4340446e172b837bd29c9
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-09-03 20:53:53 +0200

    #662 Version ordering (wip)

--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/ordering/VersionBranchOrdering.kt
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/ordering/VersionBranchOrdering.kt
@@ -1,15 +1,31 @@
 package net.nemerosa.ontrack.service.ordering
 
+import net.nemerosa.ontrack.common.Version
 import net.nemerosa.ontrack.model.ordering.BranchOrdering
 import net.nemerosa.ontrack.model.structure.Branch
 import org.springframework.stereotype.Component
 
+/**
+ * Branch ordering based on a [Version] extracted from the branch name or SCM path. Branches where a version
+ * cannot be extracted are classified alphabetically and put at the end.
+ */
 @Component
 class VersionBranchOrdering : BranchOrdering {
 
     override val id: String = "version"
 
+
     override fun getComparator(param: String?): Comparator<Branch> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return if (param != null && param.isNotBlank()) {
+            compareBy { it.getVersion(param) }
+        } else {
+            throw IllegalArgumentException("`param` argument for the version branch ordering is required.")
+        }
+    }
+
+    private fun Branch.getVersion(param: String): Comparable<*> {
+        // Path to use for the branch
+        // TODO ... SCM path?
+        val path = name
     }
 }
