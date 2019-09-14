32d5bb595ec9d9771fe2c3701a1b7ebfcfffd730
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-09-14 15:17:08 +0200

    #662 Correction of model

--- ontrack-extension-scm/src/main/java/net/nemerosa/ontrack/extension/scm/model/SCMPathInfo.kt
+++ ontrack-extension-scm/src/main/java/net/nemerosa/ontrack/extension/scm/model/SCMPathInfo.kt
@@ -3,6 +3,6 @@ package net.nemerosa.ontrack.extension.scm.model
 data class SCMPathInfo(
         val type: String,
         val url: String,
-        val branch: String,
+        val branch: String?,
         val commit: String?
 )
