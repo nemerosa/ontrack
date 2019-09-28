0dbd72c5ef95d04cbd8a519be085da6d32baed28
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-09-28 21:18:55 +0200

    #592 Removing the async settings (for now)

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/ui/DecorationsController.java
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/ui/DecorationsController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
-import java.util.concurrent.Callable;
-
 import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
 
 @RestController
@@ -30,20 +28,17 @@ public class DecorationsController extends AbstractProjectEntityController {
      * Decorations for an entity.
      */
     @RequestMapping(value = "{entityType}/{id}", method = RequestMethod.GET)
-    public Callable<Resources<Decoration<?>>> getDecorations(@PathVariable ProjectEntityType entityType, @PathVariable ID id) {
-        // Gets the current request attributes
+    public Resources<Decoration<?>> getDecorations(@PathVariable ProjectEntityType entityType, @PathVariable ID id) {
         RequestAttributes attributes = RequestContextHolder.currentRequestAttributes();
-        return () -> {
-            RequestContextHolder.setRequestAttributes(attributes);
-            try {
-                return Resources.of(
-                        decorationService.getDecorations(getEntity(entityType, id)),
-                        uri(on(getClass()).getDecorations(entityType, id))
-                ).forView(Decoration.class);
-            } finally {
-                RequestContextHolder.resetRequestAttributes();
-            }
-        };
+        RequestContextHolder.setRequestAttributes(attributes);
+        try {
+            return Resources.of(
+                    decorationService.getDecorations(getEntity(entityType, id)),
+                    uri(on(getClass()).getDecorations(entityType, id))
+            ).forView(Decoration.class);
+        } finally {
+            RequestContextHolder.resetRequestAttributes();
+        }
     }
 
 }
