91f57ffa1c1075c99e8a1f8a21a26c1fa18fa515
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-09-30 21:02:45 +0200

    #592 eTag support

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/support/WebConfig.kt
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/support/WebConfig.kt
@@ -5,14 +5,18 @@ import net.nemerosa.ontrack.ui.controller.URIBuilder
 import net.nemerosa.ontrack.ui.resource.ResourceModule
 import org.slf4j.LoggerFactory
 import org.springframework.boot.task.TaskExecutorBuilder
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.converter.StringHttpMessageConverter
+import org.springframework.web.filter.ShallowEtagHeaderFilter
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import javax.annotation.PostConstruct
+import javax.servlet.DispatcherType
 
 @Configuration
 class WebConfig(
@@ -47,13 +51,17 @@ class WebConfig(
         configurer.favorPathExtension(false)
     }
 
-//    /**
-//     * ETag support
-//     */
-//    @Bean
-//    fun shallowEtagHeaderFilter(): ShallowEtagHeaderFilter {
-//        return ShallowEtagHeaderFilter()
-//    }
+    /**
+     * ETag support
+     */
+    @Bean
+    fun shallowEtagHeaderFilter(): FilterRegistrationBean<ShallowEtagHeaderFilter> {
+        val registration = FilterRegistrationBean<ShallowEtagHeaderFilter>()
+        registration.filter = ShallowEtagHeaderFilter()
+        registration.setDispatcherTypes(DispatcherType.REQUEST, javax.servlet.DispatcherType.ASYNC)
+        registration.addUrlPatterns("/**")
+        return registration
+    }
 
     override fun configureMessageConverters(converters: MutableList<HttpMessageConverter<*>>) {
         converters.clear()
