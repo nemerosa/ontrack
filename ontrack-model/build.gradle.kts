79d4aaba53f8950f23466329d045c9e576345ded
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-10-06 13:49:50 +0200

    #663 Conversions

--- ontrack-model/build.gradle.kts
+++ ontrack-model/build.gradle.kts
@@ -17,3 +17,22 @@ dependencies {
     testCompile("org.codehaus.groovy:groovy")
     testCompile(project(":ontrack-test-utils"))
 }
+
+val testJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("tests")
+    from(sourceSets["test"].output)
+}
+
+configure<PublishingExtension> {
+    publications {
+        maybeCreate<MavenPublication>("mavenCustom").artifact(tasks["testJar"])
+    }
+}
+
+tasks["assemble"].dependsOn("testJar")
+
+val tests by configurations.creating
+
+artifacts {
+    add("tests", testJar)
+}
