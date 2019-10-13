93d1f815a7ba5635be7067266905a5d0595e26e9
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-10-13 18:17:19 +0200

    #697 Kotlin conversion to remove some Guava dependencies

--- ontrack-extension-api/src/main/java/net/nemerosa/ontrack/extension/api/model/BuildDiffRequest.kt
+++ ontrack-extension-api/src/main/java/net/nemerosa/ontrack/extension/api/model/BuildDiffRequest.kt
@@ -1,25 +1,43 @@
-package net.nemerosa.ontrack.extension.api.model;
+package net.nemerosa.ontrack.extension.api.model
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.Wither;
-import net.nemerosa.ontrack.model.structure.ID;
+import net.nemerosa.ontrack.model.structure.ID
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class BuildDiffRequest {
+open class BuildDiffRequest {
 
     /**
      * Build ID
      */
-    @Wither
-    private ID from;
+    var from: ID? = null
+
     /**
      * Build ID
      */
-    @Wither
-    private ID to;
+    var to: ID? = null
+
+    fun withFrom(value: ID?) = apply { from = value }
+    fun withTo(value: ID?) = apply { to = value }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BuildDiffRequest
+
+        if (from != other.from) return false
+        if (to != other.to) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = from?.hashCode() ?: 0
+        result = 31 * result + (to?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun toString(): String {
+        return "BuildDiffRequest(from=$from, to=$to)"
+    }
+
 
 }
--- ontrack-extension-api/src/main/java/net/nemerosa/ontrack/extension/api/model/IssueChangeLogExportRequest.kt
+++ ontrack-extension-api/src/main/java/net/nemerosa/ontrack/extension/api/model/IssueChangeLogExportRequest.kt
@@ -1,121 +1,111 @@
-package net.nemerosa.ontrack.extension.api.model;
+package net.nemerosa.ontrack.extension.api.model
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.google.common.collect.Sets;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.*;
-import java.util.stream.Collectors;
-
-import static org.apache.commons.lang3.StringUtils.split;
+import com.fasterxml.jackson.annotation.JsonIgnore
+import org.apache.commons.lang3.StringUtils
+import org.apache.commons.lang3.StringUtils.split
+import java.util.*
 
 /**
  * Request for the export of a change log.
- * <p>
- * Additionally to the {@linkplain net.nemerosa.ontrack.extension.api.model.BuildDiffRequest change log request},
- * this request defines how to {@linkplain #grouping group} issues by type and which {@linkplain #format format}
+ *
+ *
+ * Additionally to the [change log request][net.nemerosa.ontrack.extension.api.model.BuildDiffRequest],
+ * this request defines how to [group][.grouping] issues by type and which [format][.format]
  * to use for the export.
- * <p>
- * If no {@link #grouping grouping} is specified, the issues will be returned as a list, without any grouping.
- * <p>
- * The {@link #exclude exclude} field is used to exclude some issues from the export using their type.
- * <p>
+ *
+ *
+ * If no [grouping][.grouping] is specified, the issues will be returned as a list, without any grouping.
+ *
+ *
+ * The [exclude][.exclude] field is used to exclude some issues from the export using their type.
+ *
+ *
  * Note that both the export format and the type of issues are specific notions linked to the issue service
  * (JIRA, GitHub...) that produces the issues.
  */
-@EqualsAndHashCode(callSuper = false)
-@Data
-public class IssueChangeLogExportRequest extends BuildDiffRequest {
+class IssueChangeLogExportRequest : BuildDiffRequest() {
 
     /**
      * Export format, "text" by default
      */
-    private String format = "text";
+    var format = "text"
 
     /**
      * Specification for the grouping, none by default.
-     * <p>
+     *
+     *
      * This specification describes how to group issues using their type. This format is independent from
      * the issue service and this service is responsible for the actual mapping of issue "types" into actual
      * issue fields. For example, for GitHub, an issue type is mapped on a label.
-     * <p>
+     *
+     *
      * The format of the specification is:
-     * <p>
+     *
+     *
      * <pre>
-     *     specification := "" | $group ( "|" $group)*
-     *     group         := $name "=" $type ( "," $type)*
-     * </pre>
-     * <p>
+     * specification := "" | $group ( "|" $group)*
+     * group         := $name "=" $type ( "," $type)*
+    </pre> *
+     *
+     *
      * For example:
-     * <p>
+     *
+     *
      * <pre>
-     *     Bugs=bug|Features=feature,enhancement
-     * </pre>
-     * <p>
+     * Bugs=bug|Features=feature,enhancement
+    </pre> *
+     *
+     *
      * Any issue that would not be mapped in a group will be assigned arbitrarily to the group defined by
-     * {@link #altGroup}.
-     * <p>
+     * [.altGroup].
+     *
+     *
      * If the specification is empty, no grouping will be done.
      */
-    private String grouping = "";
+    var grouping = ""
 
     /**
      * Comma separated list of issue types to exclude from the export.
      */
-    private String exclude = "";
+    var exclude = ""
 
     /**
      * Title of the group to use when an issue does not belong to any group. It defaults to "Other".
      * If left empty, any issue not belonging to a group would be excluded from the export. This field
      * is not used when no grouping is specified.
      */
-    private String altGroup = "Other";
+    var altGroup = "Other"
 
     /**
      * Parses the specification and returns a map of groups x set of types. The map will be empty
      * if no group is defined, but never null.
      */
-    @JsonIgnore
-    public Map<String, Set<String>> getGroupingSpecification() {
-        Map<String, Set<String>> result = new LinkedHashMap<>();
-        if (!StringUtils.isBlank(grouping)) {
-            String[] groups = split(grouping, '|');
-            for (String group : groups) {
-                String[] groupSpec = split(group.trim(), '=');
-                if (groupSpec.length != 2) throw new ExportRequestGroupingFormatException(grouping);
-                String groupName = groupSpec[0].trim();
-                result.put(
-                        groupName,
-                        Sets.newLinkedHashSet(
-                                Arrays.asList(
-                                        split(groupSpec[1].trim(), ',')
-                                ).stream()
-                                        .map(String::trim)
-                                        .collect(Collectors.toList())
-                        )
-                );
+    val groupingSpecification: Map<String, Set<String>>
+        @JsonIgnore
+        get() {
+            val result = LinkedHashMap<String, Set<String>>()
+            if (!StringUtils.isBlank(this.grouping)) {
+                val groups = split(this.grouping, '|')
+                for (group in groups) {
+                    val groupSpec = split(group.trim(), '=')
+                    if (groupSpec.size != 2) throw ExportRequestGroupingFormatException(this.grouping)
+                    val groupName = groupSpec[0].trim()
+                    result[groupName] = split(groupSpec[1].trim(), ',').map {
+                        it.trim()
+                    }.toSet()
+                }
             }
+            return result
         }
-        return result;
-    }
 
     /**
      * Parses the comma-separated list of excluded types.
      */
-    @JsonIgnore
-    public Set<String> getExcludedTypes() {
-        if (StringUtils.isBlank(exclude)) {
-            return Collections.emptySet();
+    val excludedTypes: Set<String>
+        @JsonIgnore
+        get() = if (StringUtils.isBlank(this.exclude)) {
+            emptySet()
         } else {
-            return Sets.newHashSet(
-                    Arrays.asList(
-                            StringUtils.split(exclude, ",")
-                    ).stream()
-                            .map(StringUtils::trim)
-                            .collect(Collectors.toList())
-            );
+            split(this.exclude, ",").map { it.trim() }.toSet()
         }
-    }
 }
