ee1bacb40a7891c396c70cd31e70707ff406db34
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-10-14 22:12:16 +0200

    #697 Correction of dependencies

--- ontrack-it-utils/build.gradle.kts
+++ ontrack-it-utils/build.gradle.kts
@@ -3,15 +3,16 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":ontrack-common"))
-    implementation(project(":ontrack-model"))
-    implementation(project(":ontrack-test-utils"))
+    api("org.springframework:spring-test")
+    api(project(":ontrack-common"))
+    api(project(":ontrack-model"))
+    api(project(":ontrack-test-utils"))
+    api("org.springframework:spring-jdbc")
+
     implementation(project(":ontrack-extension-support"))
     implementation(project(":ontrack-ui-support"))
     implementation("org.jetbrains.kotlin:kotlin-test")
     implementation("org.springframework:spring-context")
-    implementation("org.springframework:spring-jdbc")
-    implementation("org.springframework:spring-test")
     implementation("org.springframework.security:spring-security-core")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.slf4j:slf4j-api")
