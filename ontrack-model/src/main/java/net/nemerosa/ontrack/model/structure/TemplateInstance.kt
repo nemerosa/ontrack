bd29b93ffd9b80f874d61604b51d0db19d7ccd01
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-10-19 18:04:12 +0200

    #700 `TemplateInstance` must be subclasses for proxying

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/structure/TemplateInstance.kt
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/structure/TemplateInstance.kt
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 /**
  * Definition of a branch instance
  */
-data class TemplateInstance(
+open class TemplateInstance(
         /**
          * Template definition (branch ID)
          */
@@ -18,4 +18,23 @@ data class TemplateInstance(
     val parameterMap: Map<String, String>
         @JsonIgnore
         get() = parameterValues.associate { it.name to it.value }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as TemplateInstance
+
+        if (templateDefinitionId != other.templateDefinitionId) return false
+        if (parameterValues != other.parameterValues) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = templateDefinitionId.hashCode()
+        result = 31 * result + parameterValues.hashCode()
+        return result
+    }
+
 }
