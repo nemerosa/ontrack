780d9f873645adf8d5180258a3401a1184ae8493
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-10-25 20:23:32 +0200

    #700 Correction of signature

--- ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/model/SVNChangeLogIssues.kt
+++ ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/model/SVNChangeLogIssues.kt
@@ -3,7 +3,7 @@ package net.nemerosa.ontrack.extension.svn.model
 import net.nemerosa.ontrack.extension.issues.model.IssueServiceConfigurationRepresentation
 
 class SVNChangeLogIssues(
-        val allIssuesLink: String,
+        val allIssuesLink: String?,
         val issueServiceConfiguration: IssueServiceConfigurationRepresentation?,
         val list: List<SVNChangeLogIssue>
 )
