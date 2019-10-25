dc7fa0e6779a6067e5e42243bf7c7ada044c0cd3
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-10-25 20:09:26 +0200

    #700 SVN model to Kotlin

--- ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/model/SVNChangeLog.kt
+++ ontrack-extension-svn/src/main/java/net/nemerosa/ontrack/extension/svn/model/SVNChangeLog.kt
@@ -1,119 +1,96 @@
-package net.nemerosa.ontrack.extension.svn.model;
+package net.nemerosa.ontrack.extension.svn.model
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.google.common.collect.Maps;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import net.nemerosa.ontrack.extension.scm.model.SCMBuildView;
-import net.nemerosa.ontrack.extension.scm.model.SCMChangeLog;
-import net.nemerosa.ontrack.extension.svn.db.SVNRepository;
-import net.nemerosa.ontrack.model.structure.Branch;
-import net.nemerosa.ontrack.model.structure.Project;
+import com.fasterxml.jackson.annotation.JsonIgnore
+import net.nemerosa.ontrack.extension.scm.model.SCMBuildView
+import net.nemerosa.ontrack.extension.scm.model.SCMChangeLog
+import net.nemerosa.ontrack.extension.svn.db.SVNRepository
+import net.nemerosa.ontrack.model.structure.Branch
+import net.nemerosa.ontrack.model.structure.Project
 
-import java.util.*;
-
-@EqualsAndHashCode(callSuper = false)
-@Data
-public class SVNChangeLog extends SCMChangeLog<SVNHistory> {
+class SVNChangeLog(
+        uuid: String,
+        project: Project,
+        @JsonIgnore // Not sent to the client
+        val repository: SVNRepository,
+        scmBuildFrom: SCMBuildView<SVNHistory>,
+        scmBuildTo: SCMBuildView<SVNHistory>) : SCMChangeLog<SVNHistory>(uuid, project, scmBuildFrom, scmBuildTo) {
 
     @JsonIgnore // Not sent to the client
-    private SVNChangeLogRevisions revisions;
+    var revisions: SVNChangeLogRevisions? = null
     @JsonIgnore // Not sent to the client
-    private SVNChangeLogIssues issues;
+    var issues: SVNChangeLogIssues? = null
     @JsonIgnore // Not sent to the client
-    private SVNChangeLogFiles files;
-    @JsonIgnore // Not sent to the client
-    private final SVNRepository repository;
+    var files: SVNChangeLogFiles? = null
 
-    public SVNChangeLog(
-            String uuid,
-            Project project,
-            SVNRepository repository,
-            SCMBuildView<SVNHistory> scmBuildFrom,
-            SCMBuildView<SVNHistory> scmBuildTo) {
-        super(uuid, project, scmBuildFrom, scmBuildTo);
-        this.repository = repository;
-    }
-
-    public SVNChangeLog withRevisions(SVNChangeLogRevisions revisions) {
-        this.revisions = revisions;
-        return this;
-    }
-
-    public SVNChangeLog withIssues(SVNChangeLogIssues issues) {
-        this.issues = issues;
-        return this;
-    }
-
-    public SVNChangeLog withFiles(SVNChangeLogFiles files) {
-        this.files = files;
-        return this;
-    }
-
-    @JsonIgnore
-    public Collection<SVNChangeLogReference> getChangeLogReferences() {
-
-        // Gets the two histories
-        SVNHistory historyFrom = getScmBuildFrom().getScm();
-        SVNHistory historyTo = getScmBuildTo().getScm();
-
-        // Empty references?
-        if (historyFrom.getReferences().isEmpty() || historyTo.getReferences().isEmpty()) {
-            return Collections.emptyList();
-        }
-
-        // Sort them from->to with 'to' having the highest revision
-        {
-            long fromRevision = historyFrom.getReferences().get(0).getRevision();
-            long toRevision = historyTo.getReferences().get(0).getRevision();
-            if (toRevision < fromRevision) {
-                SVNHistory tmp = historyTo;
-                historyTo = historyFrom;
-                historyFrom = tmp;
+    // Gets the two histories
+    // Empty references?
+    // Sort them from->to with 'to' having the highest revision
+    // Indexation of the 'from' history using the paths
+    // List of ranges to collect
+    // For each reference on the 'to' history
+    // Collects a range of revisions
+    // Gets any 'from' reference
+    // Adds this reference
+    // OK
+    val changeLogReferences: Collection<SVNChangeLogReference>
+        @JsonIgnore
+        get() {
+            var historyFrom = scmBuildFrom.scm
+            var historyTo = scmBuildTo.scm
+            if (historyFrom.references.isEmpty() || historyTo.references.isEmpty()) {
+                return emptyList()
             }
-        }
-
-        // Indexation of the 'from' history using the paths
-        Map<String, SVNReference> historyFromIndex = Maps.uniqueIndex(
-                historyFrom.getReferences(),
-                SVNReference::getPath
-        );
-
-        // List of ranges to collect
-        List<SVNChangeLogReference> references = new ArrayList<>();
-
-        // For each reference on the 'to' history
-        for (SVNReference toReference : historyTo.getReferences()) {
-            // Collects a range of revisions
-            long toRevision = toReference.getRevision();
-            long fromRevision = 0;
-            // Gets any 'from' reference
-            SVNReference fromReference = historyFromIndex.get(toReference.getPath());
-            if (fromReference != null) {
-                fromRevision = fromReference.getRevision();
-                if (fromRevision > toRevision) {
-                    long t = toRevision;
-                    toRevision = fromRevision;
-                    fromRevision = t;
+            run {
+                val fromRevision = historyFrom.references[0].revision
+                val toRevision = historyTo.references[0].revision
+                if (toRevision < fromRevision) {
+                    val tmp = historyTo
+                    historyTo = historyFrom
+                    historyFrom = tmp
                 }
             }
-            // Adds this reference
-            references.add(new SVNChangeLogReference(
-                    toReference.getPath(),
-                    fromRevision,
-                    toRevision
-            ));
+            val historyFromIndex = historyFrom.references.associateBy { it.path }
+            val references = mutableListOf<SVNChangeLogReference>()
+            for (toReference in historyTo.references) {
+                var toRevision = toReference.revision
+                var fromRevision: Long = 0
+                val fromReference = historyFromIndex[toReference.path]
+                if (fromReference != null) {
+                    fromRevision = fromReference.revision
+                    if (fromRevision > toRevision) {
+                        val t = toRevision
+                        toRevision = fromRevision
+                        fromRevision = t
+                    }
+                }
+                references.add(SVNChangeLogReference(
+                        toReference.path,
+                        fromRevision,
+                        toRevision
+                ))
+            }
+            return references
         }
-
-        // OK
-        return references;
-    }
 
     /**
      * A SVN change log works always on the same branch.
      */
-    @JsonIgnore
-    public Branch getBranch() {
-        return getFrom().getBuild().getBranch();
+    val branch: Branch
+        @JsonIgnore
+        get() = from.build.branch
+
+    fun withRevisions(revisions: SVNChangeLogRevisions): SVNChangeLog {
+        this.revisions = revisions
+        return this
+    }
+
+    fun withIssues(issues: SVNChangeLogIssues): SVNChangeLog {
+        this.issues = issues
+        return this
+    }
+
+    fun withFiles(files: SVNChangeLogFiles): SVNChangeLog {
+        this.files = files
+        return this
     }
 }
