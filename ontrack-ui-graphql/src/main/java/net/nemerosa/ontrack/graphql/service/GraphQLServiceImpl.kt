5ffc62c0b2837c5f72405190518600e155415de7
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-11-25 21:19:54 +0100

    #709 Transaction for GraphQL service

--- ontrack-ui-graphql/src/main/java/net/nemerosa/ontrack/graphql/service/GraphQLServiceImpl.kt
+++ ontrack-ui-graphql/src/main/java/net/nemerosa/ontrack/graphql/service/GraphQLServiceImpl.kt
@@ -5,16 +5,20 @@ import graphql.ExecutionResult
 import graphql.GraphQL
 import graphql.execution.ExecutionStrategy
 import graphql.schema.GraphQLSchema
+import net.nemerosa.ontrack.tx.TransactionService
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional
 class GraphQLServiceImpl(
         private val graphQLExceptionHandlers: List<GraphQLExceptionHandler>,
         @Qualifier("queryExecutionStrategy")
         private val queryExecutionStrategy: ExecutionStrategy,
         @Qualifier("queryExecutionStrategy")
-        private val mutationExecutionStrategy: ExecutionStrategy
+        private val mutationExecutionStrategy: ExecutionStrategy,
+        private val transactionService: TransactionService
 ) : GraphQLService {
     override fun execute(
             schema: GraphQLSchema,
@@ -23,17 +27,19 @@ class GraphQLServiceImpl(
             operationName: String?,
             reportErrors: Boolean
     ): ExecutionResult {
-        val result: ExecutionResult = GraphQL.newGraphQL(schema)
-                .queryExecutionStrategy(queryExecutionStrategy)
-                .mutationExecutionStrategy(mutationExecutionStrategy)
-                .build()
-                .execute(
-                        ExecutionInput.newExecutionInput()
-                                .query(query)
-                                .operationName(operationName)
-                                .variables(variables)
-                                .build()
-                )
+        val result: ExecutionResult = transactionService.doInTransaction {
+            GraphQL.newGraphQL(schema)
+                    .queryExecutionStrategy(queryExecutionStrategy)
+                    .mutationExecutionStrategy(mutationExecutionStrategy)
+                    .build()
+                    .execute(
+                            ExecutionInput.newExecutionInput()
+                                    .query(query)
+                                    .operationName(operationName)
+                                    .variables(variables)
+                                    .build()
+                    )
+        }
         if (result.errors != null && !result.errors.isEmpty() && reportErrors) {
             result.errors.forEach { error ->
                 graphQLExceptionHandlers.forEach {
