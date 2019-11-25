72662133457ac2696453a0b7ad7daba0ab649586
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-11-25 21:28:38 +0100

    #709 Kotlin conversions

---
+++ ontrack-extension-issues/src/main/java/net/nemerosa/ontrack/extension/issues/export/AbstractTextIssueExportService.kt
@@ -0,0 +1,28 @@
+package net.nemerosa.ontrack.extension.issues.export
+
+import net.nemerosa.ontrack.extension.issues.IssueServiceExtension
+import net.nemerosa.ontrack.extension.issues.model.Issue
+import net.nemerosa.ontrack.extension.issues.model.IssueServiceConfiguration
+
+abstract class AbstractTextIssueExportService(
+        private val exportFormat: ExportFormat
+) : IssueExportService {
+
+    override fun getExportFormat(): ExportFormat = exportFormat
+
+    override fun export(issueServiceExtension: IssueServiceExtension, issueServiceConfiguration: IssueServiceConfiguration, groupedIssues: Map<String, List<Issue>>): ExportedIssues {
+        val s = StringBuilder()
+        exportAsText(issueServiceExtension, issueServiceConfiguration, groupedIssues, s)
+        return ExportedIssues(
+                exportFormat.type,
+                s.toString()
+        )
+    }
+
+    abstract fun exportAsText(
+            issueServiceExtension: IssueServiceExtension,
+            issueServiceConfiguration: IssueServiceConfiguration,
+            groupedIssues: Map<String, List<Issue>>,
+            s: StringBuilder
+    )
+}
