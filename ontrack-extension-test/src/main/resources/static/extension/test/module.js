0e3e2282b89bcf9c60511f375cb656b0158f948f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-12-09 21:05:10 +0100

    #713 ES6 syntax in test extension

--- ontrack-extension-test/src/main/resources/static/extension/test/module.js
+++ ontrack-extension-test/src/main/resources/static/extension/test/module.js
@@ -1,1 +1,31 @@
-// Placeholder
+angular.module('ontrack.extension.test', [
+    'ot.service.core',
+    'ot.service.graphql'
+])
+    .config(function ($stateProvider) {
+        $stateProvider.state('extension-test-projects', {
+            url: '/extension/test/projects',
+            templateUrl: 'extension/test/projects.tpl.html',
+            controller: 'ExtensionTestProjectsCtrl'
+        });
+    })
+    .controller('ExtensionTestProjectsCtrl', function ($stateParams, $scope, $http, $interpolate, ot, otGraphqlService) {
+        const view = ot.view();
+        view.title = "List of projects";
+
+        const query = `
+            query Projects {
+                projects {
+                    name
+                    links {
+                        _page
+                    }
+                }
+            }
+        `;
+
+        otGraphqlService.pageGraphQLCall(query, {}).then(data => {
+            $scope.projects = data.projects;
+        });
+    })
+;
