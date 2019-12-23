6bdeaed06d169ebbe024b87249ea5a0c19ec5eff
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-12-23 18:48:59 +0100

    [catalog] Mgt of errors on catalog info collection

--- ontrack-extension-scm/src/main/java/net/nemerosa/ontrack/extension/scm/catalog/CatalogInfoCollectorImpl.kt
+++ ontrack-extension-scm/src/main/java/net/nemerosa/ontrack/extension/scm/catalog/CatalogInfoCollectorImpl.kt
@@ -2,7 +2,10 @@ package net.nemerosa.ontrack.extension.scm.catalog
 
 import net.nemerosa.ontrack.extension.api.ExtensionManager
 import net.nemerosa.ontrack.model.security.SecurityService
+import net.nemerosa.ontrack.model.structure.NameDescription
 import net.nemerosa.ontrack.model.structure.Project
+import net.nemerosa.ontrack.model.support.ApplicationLogEntry
+import net.nemerosa.ontrack.model.support.ApplicationLogService
 import net.nemerosa.ontrack.repository.support.store.EntityDataStore
 import net.nemerosa.ontrack.repository.support.store.EntityDataStoreFilter
 import net.nemerosa.ontrack.repository.support.store.EntityDataStoreRecord
@@ -15,7 +18,8 @@ class CatalogInfoCollectorImpl(
         private val catalogLinkService: CatalogLinkService,
         private val extensionManager: ExtensionManager,
         private val entityDataStore: EntityDataStore,
-        private val securityService: SecurityService
+        private val securityService: SecurityService,
+        private val applicationLogService: ApplicationLogService
 ) : CatalogInfoCollector {
 
     val contributors: Map<String, CatalogInfoContributor<*>> by lazy {
@@ -68,25 +72,41 @@ class CatalogInfoCollectorImpl(
     ) {
         logger("Catalog info for ${project.name} linked with ${entry.key} by ${contributor.javaClass.name}")
         // Gets the information for this project & this entry
-        val info = contributor.collectInfo(project, entry)
-        // Storing the information as JSON
-        if (info != null) {
-            logger("Storing catalog info for ${project.name} linked with ${entry.key} by ${contributor.javaClass.name}")
-            entityDataStore.add(
-                    project,
-                    STORE_CATEGORY,
-                    contributor.id,
-                    securityService.currentSignature,
-                    null,
-                    contributor.asJson(info)
-            )
-        } else {
-            logger("Deleting catalog info for ${project.name} linked with ${entry.key} by ${contributor.javaClass.name}")
-            entityDataStore.deleteByFilter(
-                    EntityDataStoreFilter(
-                            entity = project,
-                            category = STORE_CATEGORY,
-                            name = contributor.id
+        try {
+            val info = contributor.collectInfo(project, entry)
+            // Storing the information as JSON
+            if (info != null) {
+                logger("Storing catalog info for ${project.name} linked with ${entry.key} by ${contributor.javaClass.name}")
+                entityDataStore.add(
+                        project,
+                        STORE_CATEGORY,
+                        contributor.id,
+                        securityService.currentSignature,
+                        null,
+                        contributor.asJson(info)
+                )
+            } else {
+                logger("Deleting catalog info for ${project.name} linked with ${entry.key} by ${contributor.javaClass.name}")
+                entityDataStore.deleteByFilter(
+                        EntityDataStoreFilter(
+                                entity = project,
+                                category = STORE_CATEGORY,
+                                name = contributor.id
+                        )
+                )
+            }
+        } catch (ex: Exception) {
+            applicationLogService.log(
+                    ApplicationLogEntry.error(
+                            ex,
+                            NameDescription.nd("scm-catalog-info", "Catalog info collection error"),
+                            "Error when collecting catalog info for ${project.name} linked with ${entry.key} by ${contributor.javaClass.name}"
+                    ).withDetail(
+                            "project", project.name
+                    ).withDetail(
+                            "entry", entry.key
+                    ).withDetail(
+                            "contributor", contributor.id
                     )
             )
         }
