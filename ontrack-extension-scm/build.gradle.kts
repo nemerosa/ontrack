9b8b5eed07b0f0eb5bd8c16f2f6c0bbcaf270adf
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-01-06 14:23:51 +0100

    [catalog] Git catalog info (test)

--- ontrack-extension-scm/build.gradle.kts
+++ ontrack-extension-scm/build.gradle.kts
@@ -19,3 +19,22 @@ dependencies {
     testRuntimeOnly(project(":ontrack-service"))
     testRuntimeOnly(project(":ontrack-repository-impl"))
 }
+
+val testJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("tests")
+    from(sourceSets["test"].output)
+}
+
+configure<PublishingExtension> {
+    publications {
+        maybeCreate<MavenPublication>("mavenCustom").artifact(tasks["testJar"])
+    }
+}
+
+tasks["assemble"].dependsOn("testJar")
+
+val tests by configurations.creating
+
+artifacts {
+    add("tests", testJar)
+}
