ff1e01ec6ab0252ef8c014ea7127b1c8310bd317
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-01-23 21:05:24 +0100

    #723 Indexation of branches

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/BranchSearchProvider.kt
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/BranchSearchProvider.kt
@@ -1,10 +1,7 @@
 package net.nemerosa.ontrack.boot
 
 import net.nemerosa.ontrack.common.getOrNull
-import net.nemerosa.ontrack.model.structure.Branch
-import net.nemerosa.ontrack.model.structure.NameDescription
-import net.nemerosa.ontrack.model.structure.SearchResult
-import net.nemerosa.ontrack.model.structure.StructureService
+import net.nemerosa.ontrack.model.structure.*
 import net.nemerosa.ontrack.ui.controller.URIBuilder
 import net.nemerosa.ontrack.ui.support.AbstractSearchProvider
 import org.springframework.stereotype.Component
@@ -14,7 +11,7 @@ import java.util.regex.Pattern
 class BranchSearchProvider(
         uriBuilder: URIBuilder,
         private val structureService: StructureService
-) : AbstractSearchProvider(uriBuilder) {
+) : AbstractSearchProvider(uriBuilder), SearchIndexer<BranchSearchItem> {
 
     override fun isTokenSearchable(token: String): Boolean = Pattern.matches(NameDescription.NAME, token)
 
@@ -37,4 +34,27 @@ class BranchSearchProvider(
                         )
                     }
 
+    override fun getSearchIndexers(): Collection<SearchIndexer<*>> = listOf(this)
+
+    override val indexerName: String = "Branches"
+
+    override val indexName: String = "branches"
+
+    override fun indexation(): Sequence<BranchSearchItem> =
+            structureService.projectList
+                    .asSequence()
+                    .flatMap {
+                        structureService.getBranchesForProject(it.id)
+                                .asSequence()
+                                .map { branch -> BranchSearchItem(branch) }
+                    }
+}
+
+class BranchSearchItem(branch: Branch) : SearchItem {
+    override val id: String = branch.id.toString()
+
+    override val fields: Map<String, Any> = mapOf(
+            "name" to branch.name,
+            "description" to branch.description
+    )
 }
