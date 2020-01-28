30a146c177da4e617d0dd0c0ad001b2901e0af8c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-01-28 09:35:19 +0100

    #723 Testing the search - mixed branches & projects

--- ontrack-ui/src/test/java/net/nemerosa/ontrack/boot/search/BranchSearchIT.kt
+++ ontrack-ui/src/test/java/net/nemerosa/ontrack/boot/search/BranchSearchIT.kt
@@ -4,6 +4,7 @@ import net.nemerosa.ontrack.model.structure.Branch
 import net.nemerosa.ontrack.model.structure.SearchRequest
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 /**
  * Search indexation for branches
@@ -27,6 +28,33 @@ class BranchSearchIT : AbstractSearchTestSupport() {
         result.apply {
             assertEquals(candidate.name, result.title)
             assertEquals(candidate.entityDisplayName, result.description)
+        }
+    }
+
+    @Test
+    fun `Finding projects and branches`() {
+        // Creates a project
+        val project = project()
+        // Creates a branch with the same name than the project above
+        val branch = project<Branch> {
+            branch(name = project.name)
+        }
+        // Creates 3 other projects
+        repeat(3) { project { branch {} } }
+        // Launching indexation for the projects & branches
+        index("projects")
+        index("branches")
+        // Searches for the name
+        val results = searchService.search(SearchRequest(branch.name)).toList()
+        assertEquals(2, results.size)
+        assertTrue(results[0].accuracy > results[1].accuracy, "Project is returned first")
+        results[0].apply {
+            assertEquals(project.name, title)
+            assertEquals(project.entityDisplayName, description)
+        }
+        results[1].apply {
+            assertEquals(branch.name, title)
+            assertEquals(branch.entityDisplayName, description)
         }
     }
 
