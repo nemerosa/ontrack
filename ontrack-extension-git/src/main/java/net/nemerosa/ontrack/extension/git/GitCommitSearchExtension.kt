c9c5a64960804184c7d6d3d04b30289c17da7f81
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-02-01 16:17:48 +0100

    #723 Kotlin conversion

--- ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/GitCommitSearchExtension.kt
+++ ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/GitCommitSearchExtension.kt
@@ -1,89 +1,66 @@
-package net.nemerosa.ontrack.extension.git;
+package net.nemerosa.ontrack.extension.git
 
-import net.nemerosa.ontrack.extension.api.SearchExtension;
-import net.nemerosa.ontrack.extension.git.service.GitService;
-import net.nemerosa.ontrack.extension.support.AbstractExtension;
-import net.nemerosa.ontrack.git.model.GitCommit;
-import net.nemerosa.ontrack.model.structure.SearchProvider;
-import net.nemerosa.ontrack.model.structure.SearchResult;
-import net.nemerosa.ontrack.ui.controller.URIBuilder;
-import net.nemerosa.ontrack.ui.support.AbstractSearchProvider;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.regex.Pattern;
-
-import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
+import net.nemerosa.ontrack.extension.api.SearchExtension
+import net.nemerosa.ontrack.extension.git.model.GitConfiguration
+import net.nemerosa.ontrack.extension.git.service.GitService
+import net.nemerosa.ontrack.extension.support.AbstractExtension
+import net.nemerosa.ontrack.model.structure.Project
+import net.nemerosa.ontrack.model.structure.SearchProvider
+import net.nemerosa.ontrack.model.structure.SearchResult
+import net.nemerosa.ontrack.ui.controller.URIBuilder
+import net.nemerosa.ontrack.ui.support.AbstractSearchProvider
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder
+import java.util.*
+import java.util.function.BiConsumer
+import java.util.regex.Pattern
 
 @Component
-public class GitCommitSearchExtension extends AbstractExtension implements SearchExtension {
+class GitCommitSearchExtension(
+        extensionFeature: GitExtensionFeature?,
+        private val gitService: GitService,
+        private val uriBuilder: URIBuilder
+) : AbstractExtension(extensionFeature), SearchExtension {
 
-    private final Pattern shaPattern = Pattern.compile("[a-f0-9]{40}|[a-f0-9]{7}");
+    private val shaPattern = Pattern.compile("[a-f0-9]{40}|[a-f0-9]{7}")
 
-    private final GitService gitService;
-    private final URIBuilder uriBuilder;
-
-    @Autowired
-    public GitCommitSearchExtension(
-            GitExtensionFeature extensionFeature,
-            GitService gitService,
-            URIBuilder uriBuilder) {
-        super(extensionFeature);
-        this.gitService = gitService;
-        this.uriBuilder = uriBuilder;
+    override fun getSearchProvider(): SearchProvider {
+        return GitCommitSearchProvider(uriBuilder)
     }
 
-    @Override
-    public SearchProvider getSearchProvider() {
-        return new GitCommitSearchProvider(uriBuilder);
-    }
-
-    protected class GitCommitSearchProvider extends AbstractSearchProvider {
-
-        public GitCommitSearchProvider(URIBuilder uriBuilder) {
-            super(uriBuilder);
+    protected inner class GitCommitSearchProvider(uriBuilder: URIBuilder?) : AbstractSearchProvider(uriBuilder) {
+        override fun isTokenSearchable(token: String): Boolean {
+            return shaPattern.matcher(token).matches()
         }
 
-        @Override
-        public boolean isTokenSearchable(String token) {
-            return shaPattern.matcher(token).matches();
-        }
-
-        @Override
-        public Collection<SearchResult> search(String token) {
-            // List of results
-            List<SearchResult> results = new ArrayList<>();
+        override fun search(token: String): Collection<SearchResult> { // List of results
+            val results: MutableList<SearchResult> = ArrayList()
             // For all Git-configured projects
-            gitService.forEachConfiguredProject((project, gitConfiguration) -> {
+            gitService.forEachConfiguredProject(BiConsumer { project: Project, gitConfiguration: GitConfiguration? ->
                 // ... scans for the commit
-                GitCommit commit = gitService.lookupCommit(gitConfiguration, token);
+                val commit = gitService.lookupCommit(gitConfiguration!!, token)
                 // ... and if found
-                if (commit != null) {
-                    // ... creates a result entry
+                if (commit != null) { // ... creates a result entry
                     results.add(
-                            new SearchResult(
-                                    String.format("[%s] %s %s",
-                                            project.getName(),
-                                            commit.getId(),
-                                            commit.getShortMessage()),
-                                    String.format("%s - %s",
-                                            commit.getAuthor().getName(),
-                                            commit.getFullMessage()),
-                                    uri(on(GitController.class)
-                                            .commitProjectInfo(project.getId(), commit.getId())),
+                            SearchResult(String.format("[%s] %s %s",
+                                    project.name,
+                                    commit.id,
+                                    commit.shortMessage), String.format("%s - %s",
+                                    commit.author.name,
+                                    commit.fullMessage),
+                                    uri(MvcUriComponentsBuilder.on(GitController::class.java)
+                                            .commitProjectInfo(project.id, commit.id)),
                                     uriBuilder.page("extension/git/%d/commit/%s",
                                             project.id(),
-                                            commit.getId()),
-                                    100
+                                            commit.id),
+                                    100.0
                             )
-                    );
+                    )
                 }
-            });
+            })
             // OK
-            return results;
+            return results
         }
     }
+
 }
