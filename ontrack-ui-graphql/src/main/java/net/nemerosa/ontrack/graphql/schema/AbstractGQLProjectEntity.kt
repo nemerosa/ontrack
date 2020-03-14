dbdc0831318a20df83907d122524140222e01507
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-03-14 09:26:20 +0100

    #748 `annotatedDescription` field

--- ontrack-ui-graphql/src/main/java/net/nemerosa/ontrack/graphql/schema/AbstractGQLProjectEntity.kt
+++ ontrack-ui-graphql/src/main/java/net/nemerosa/ontrack/graphql/schema/AbstractGQLProjectEntity.kt
@@ -1,17 +1,21 @@
 package net.nemerosa.ontrack.graphql.schema
 
+import graphql.Scalars
 import graphql.schema.GraphQLFieldDefinition
 import net.nemerosa.ontrack.graphql.support.GraphqlUtils
 import net.nemerosa.ontrack.model.structure.ProjectEntity
 import net.nemerosa.ontrack.model.structure.ProjectEntityType
 import net.nemerosa.ontrack.model.structure.Signature
+import net.nemerosa.ontrack.model.support.FreeTextAnnotatorContributor
+import net.nemerosa.ontrack.model.support.MessageAnnotationUtils
 import java.util.*
 
 abstract class AbstractGQLProjectEntity<T : ProjectEntity>(
         private val projectEntityClass: Class<T>,
         private val projectEntityType: ProjectEntityType,
         private val projectEntityFieldContributors: List<GQLProjectEntityFieldContributor>,
-        private val creation: GQLTypeCreation
+        private val creation: GQLTypeCreation,
+        private val freeTextAnnotatorContributors: List<FreeTextAnnotatorContributor>
 ) : GQLType {
 
     protected fun projectEntityInterfaceFields(): List<GraphQLFieldDefinition> {
@@ -40,6 +44,21 @@ abstract class AbstractGQLProjectEntity<T : ProjectEntity>(
                                     val signature = getSignature(entity)
                                     signature?.let {
                                         GQLTypeCreation.getCreationFromSignature(it)
+                                    }
+                                }
+                                .build(),
+                        GraphQLFieldDefinition.newFieldDefinition()
+                                .name("annotatedDescription")
+                                .type(Scalars.GraphQLString)
+                                .description("Description with links.")
+                                .dataFetcher { env ->
+                                    val entity: T = env.getSource<T>()
+                                    val description: String? = entity.description
+                                    if (description.isNullOrBlank()) {
+                                        ""
+                                    } else {
+                                        val annotators = freeTextAnnotatorContributors.flatMap { it.getMessageAnnotators(entity) }
+                                        MessageAnnotationUtils.annotate(description, annotators)
                                     }
                                 }
                                 .build()
