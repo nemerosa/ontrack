80fd01a19e9fbfbe2769c96e2fe2523009178cf1
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-03-14 19:57:30 +0100

    #717 Removed the decoration on the project

--- ontrack-extension-scm/src/main/java/net/nemerosa/ontrack/extension/scm/catalog/ui/SCMCatalogEntryDecorationExtensionData.kt
+++
@@ -1,8 +0,0 @@
-package net.nemerosa.ontrack.extension.scm.catalog.ui
-
-import net.nemerosa.ontrack.extension.scm.catalog.SCMCatalogEntry
-
-data class SCMCatalogEntryDecorationExtensionData(
-        val entry: SCMCatalogEntry,
-        val projectId: Int
-)
