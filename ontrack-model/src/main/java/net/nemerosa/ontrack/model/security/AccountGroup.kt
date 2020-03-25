1386b991d2c3ed3c4a7817efafa80543a9dbf647
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-03-25 09:52:56 +0100

    #763 Core model migration - wip

---
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/AccountGroup.kt
@@ -0,0 +1,98 @@
+package net.nemerosa.ontrack.model.security
+
+import net.nemerosa.ontrack.model.structure.Entity
+import net.nemerosa.ontrack.model.structure.ID
+import net.nemerosa.ontrack.model.structure.NameDescription
+import java.io.Serializable
+
+/**
+ * Group of accounts.
+ */
+class AccountGroup(
+        override val id: ID,
+        val name: String,
+        val description: String?,
+        private var authorisations: Authorisations = Authorisations.none(),
+        private val locked: Boolean
+) : Entity, Serializable {
+
+    fun isGranted(fn: Class<out GlobalFunction>) = authorisations.isGranted(fn)
+
+    fun isGranted(projectId: Int, fn: Class<out ProjectFunction>) = authorisations.isGranted(projectId, fn)
+
+    private fun checkLock() {
+        if (locked) {
+            throw IllegalStateException("Account is locked")
+        }
+    }
+
+    fun withGlobalRole(globalRole: GlobalRole?): AccountGroup {
+        checkLock()
+        authorisations = authorisations.withGlobalRole(globalRole)
+        return this
+    }
+
+    fun withProjectRoles(projectRoleAssociations: Collection<ProjectRoleAssociation>): AccountGroup {
+        checkLock()
+        authorisations = authorisations.withProjectRoles(projectRoleAssociations)
+        return this
+    }
+
+    fun withProjectRole(projectRoleAssociation: ProjectRoleAssociation): AccountGroup {
+        checkLock()
+        authorisations = authorisations.withProjectRole(projectRoleAssociation)
+        return this
+    }
+
+    fun lock(): AccountGroup =
+            AccountGroup(
+                    id,
+                    name,
+                    description,
+                    authorisations,
+                    true
+            )
+
+    companion object {
+        @JvmStatic
+        fun of(name: String, description: String?) =
+                AccountGroup(
+                        ID.NONE,
+                        name,
+                        description,
+                        Authorisations.none(),
+                        false
+                )
+    }
+
+
+    fun withId(id: ID): AccountGroup {
+        checkLock()
+        return AccountGroup(
+                id,
+                name,
+                description,
+                authorisations,
+                locked
+        )
+    }
+
+    fun update(input: NameDescription): AccountGroup {
+        checkLock()
+        return AccountGroup(
+                id,
+                input.name,
+                input.description,
+                authorisations,
+                locked
+        )
+    }
+
+    fun asPermissionTarget() =
+            PermissionTarget(
+                    PermissionTargetType.GROUP,
+                    id(),
+                    name,
+                    description
+            )
+}
--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/PermissionTarget.kt
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/PermissionTarget.kt
@@ -4,5 +4,5 @@ class PermissionTarget(
         val type: PermissionTargetType,
         val id: Int,
         val name: String,
-        val description: String
+        val description: String?
 )
