b86f4089d280bab2067ce4207b29afedfa0dea44
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-03-25 19:22:42 +0100

    #763 Core model migration - done

---
+++ ontrack-extension-ldap/src/main/java/net/nemerosa/ontrack/extension/ldap/LDAPMapping.kt
@@ -0,0 +1,18 @@
+package net.nemerosa.ontrack.extension.ldap
+
+import net.nemerosa.ontrack.model.security.AccountGroup
+import net.nemerosa.ontrack.model.security.AccountGroupMapping
+import net.nemerosa.ontrack.model.structure.ID
+
+class LDAPMapping(id: ID, type: String, name: String, group: AccountGroup) : AccountGroupMapping(id, type, name, group) {
+    companion object {
+        fun of(mapping: AccountGroupMapping): LDAPMapping {
+            return LDAPMapping(
+                    mapping.id,
+                    mapping.type,
+                    mapping.name,
+                    mapping.group
+            )
+        }
+    }
+}
