e14279348453df014edaf52e866f07d4741da39c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-03-25 09:30:00 +0100

    #763 Core model migration - wip

---
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/Authorisations.kt
@@ -0,0 +1,29 @@
+package net.nemerosa.ontrack.model.security;
+
+import java.io.Serializable
+
+class Authorisations(
+        private val globalRole: GlobalRole?,
+        private val projectRoleAssociations: Set<ProjectRoleAssociation> = emptySet()
+) : Serializable {
+
+    companion object {
+        @JvmStatic
+        fun none() = Authorisations(null, emptySet())
+    }
+
+
+    fun isGranted(fn: Class<out GlobalFunction>) = globalRole != null && globalRole.isGlobalFunctionGranted(fn)
+
+    fun isGranted(projectId: Int, fn: Class<out ProjectFunction>) =
+            (globalRole != null && globalRole.isProjectFunctionGranted(fn))
+                    || projectRoleAssociations.any { pa -> pa.projectId == projectId && pa.isGranted(fn) }
+
+    fun withGlobalRole(globalRole: GlobalRole?) = Authorisations(globalRole, projectRoleAssociations)
+
+    fun withProjectRoles(projectRoleAssociations: Collection<ProjectRoleAssociation>) =
+            Authorisations(globalRole, this.projectRoleAssociations + projectRoleAssociations)
+
+    fun withProjectRole(projectRoleAssociation: ProjectRoleAssociation) =
+            Authorisations(globalRole, this.projectRoleAssociations + projectRoleAssociation)
+}
