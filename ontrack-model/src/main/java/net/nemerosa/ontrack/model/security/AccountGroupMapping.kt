012b519c6be114f02ae673b35bd5bee8a3d0adb5
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-03-25 10:34:59 +0100

    #763 Core model migration - wip

---
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/AccountGroupMapping.kt
@@ -0,0 +1,31 @@
+package net.nemerosa.ontrack.model.security
+
+import net.nemerosa.ontrack.model.form.Form
+import net.nemerosa.ontrack.model.form.Form.Companion.create
+import net.nemerosa.ontrack.model.form.Selection
+import net.nemerosa.ontrack.model.structure.Entity
+import net.nemerosa.ontrack.model.structure.ID
+
+open class AccountGroupMapping(
+        override val id: ID,
+        val type: String,
+        val name: String,
+        val group: AccountGroup
+) : Entity {
+
+    fun asForm(groups: List<AccountGroup>): Form = form(groups)
+            .fill("name", name)
+            .fill("group", group.id)
+
+    companion object {
+        @JvmStatic
+        fun form(groups: List<AccountGroup>): Form = create()
+                .name()
+                .with(
+                        Selection.of("group")
+                                .label("Group")
+                                .items(groups)
+                )
+    }
+
+}
