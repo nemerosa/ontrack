93d19cb1c2d902cae1977a646774250569f42039
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-03-29 18:19:05 +0200

    #763 Core model migration - `AccountGroupMapping` not inheritable

--- ontrack-extension-ldap/src/main/java/net/nemerosa/ontrack/extension/ldap/LDAPMapping.kt
+++ ontrack-extension-ldap/src/main/java/net/nemerosa/ontrack/extension/ldap/LDAPMapping.kt
@@ -4,16 +4,19 @@ import net.nemerosa.ontrack.model.security.AccountGroup
 import net.nemerosa.ontrack.model.security.AccountGroupMapping
 import net.nemerosa.ontrack.model.structure.ID
 
-class LDAPMapping(id: ID, type: String, name: String, group: AccountGroup) : AccountGroupMapping(id, type, name, group) {
+class LDAPMapping(
+        val id: ID,
+        val type: String,
+        val name: String,
+        val group: AccountGroup
+) {
     companion object {
         @JvmStatic
-        fun of(mapping: AccountGroupMapping): LDAPMapping {
-            return LDAPMapping(
-                    mapping.id,
-                    mapping.type,
-                    mapping.name,
-                    mapping.group
-            )
-        }
+        fun of(mapping: AccountGroupMapping): LDAPMapping = LDAPMapping(
+                mapping.id,
+                mapping.type,
+                mapping.name,
+                mapping.group
+        )
     }
 }
