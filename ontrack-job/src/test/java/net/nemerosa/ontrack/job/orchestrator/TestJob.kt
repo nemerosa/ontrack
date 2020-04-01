8aabb1e7a7abfc80c8a9506547dc3249424a93c1
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-04-01 19:33:51 +0200

    #761 Removed a deprecation

---
+++ ontrack-job/src/test/java/net/nemerosa/ontrack/job/orchestrator/TestJob.kt
@@ -0,0 +1,26 @@
+package net.nemerosa.ontrack.job.orchestrator
+
+import net.nemerosa.ontrack.job.Job
+import net.nemerosa.ontrack.job.JobCategory.Companion.of
+import net.nemerosa.ontrack.job.JobKey
+import net.nemerosa.ontrack.job.JobRun
+import net.nemerosa.ontrack.job.JobRunListener
+
+/**
+ * Job used for tests
+ */
+class TestJob(private val name: String) : Job {
+
+    override fun getKey(): JobKey = getKey(name)
+
+    override fun getTask(): JobRun = JobRun { runListener: JobRunListener -> runListener.message(name) }
+
+    override fun getDescription(): String = name
+
+    override fun isDisabled(): Boolean = false
+
+    companion object {
+        fun getKey(name: String): JobKey = of("test").getType("orchestrator").getKey(name!!)
+    }
+
+}
