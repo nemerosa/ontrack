f812a8917138f60b4eb9e6c2830093f56187072e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-04-04 20:28:18 +0200

    #761 Removed `SearchProvider`

--- ontrack-service/src/main/kotlin/net/nemerosa/ontrack/service/SearchServiceImpl.kt
+++ ontrack-service/src/main/kotlin/net/nemerosa/ontrack/service/SearchServiceImpl.kt
@@ -1,7 +1,10 @@
 package net.nemerosa.ontrack.service
 
 import net.nemerosa.ontrack.model.Ack
-import net.nemerosa.ontrack.model.structure.*
+import net.nemerosa.ontrack.model.structure.SearchRequest
+import net.nemerosa.ontrack.model.structure.SearchResultType
+import net.nemerosa.ontrack.model.structure.SearchResults
+import net.nemerosa.ontrack.model.structure.SearchService
 import net.nemerosa.ontrack.model.support.OntrackConfigProperties
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
@@ -17,28 +20,23 @@ import org.springframework.transaction.annotation.Transactional
         havingValue = "default",
         matchIfMissing = true
 )
-class SearchServiceImpl(
-        val providers: List<SearchProvider>
-) : SearchService {
+class SearchServiceImpl : SearchService {
 
     /**
      * Not available for this type of service
      */
     override val searchResultTypes: List<SearchResultType> = emptyList()
 
-    override fun paginatedSearch(request: SearchRequest): SearchResults = providers
-            .filter { it.isTokenSearchable(request.token) }
-            .flatMap { it.search(request.token) }
-            .let {
-                SearchResults(it, offset = 0, total = it.size, message = "Search based on direct access to data (non-ElasticSearch) is deprecated and will be removed in version 4.0. Ask your administrator to switch to ElasticSearch-based research.")
-            }
+    override fun paginatedSearch(request: SearchRequest): SearchResults = SearchResults(
+            emptyList(),
+            0, 0,
+            null
+    )
 
     /**
      * Not supported in legacy search.
      */
-    override fun indexReset(reindex: Boolean): Ack {
-        return Ack.NOK;
-    }
+    override fun indexReset(reindex: Boolean): Ack = Ack.NOK
 
     /**
      * NOP
