02e4e3c938b8c1d9f80e930299ca94c5628f11f9
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-04-10 15:34:45 +0200

    #762 Kotlin 1.3.71

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,6 +3,6 @@
  */
 object Versions {
     const val springBootVersion = "2.2.6.RELEASE"
-    const val kotlinVersion = "1.3.50"
+    const val kotlinVersion = "1.3.71"
     const val elasticSearchVersion = "7.5.2"
 }
