844c93314d5c215f3e264520e29e39709ef0e99a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-04-12 21:56:20 +0200

    #756 UI security config

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/support/WebSecurityConfig.kt
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/support/WebSecurityConfig.kt
@@ -11,84 +11,40 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 @EnableWebSecurity
 class WebSecurityConfig : WebSecurityConfigurerAdapter() {
 
-    override fun configure(http: HttpSecurity) {
-        http {
-            // Enabling JS Cookies for CSRF protection (for AngularJS)
-            csrf {
-                csrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse()
+    // FIXME API login
+//    @Configuration
+//    @Order(1)
+//    class ApiWebSecurityConfigurationAdapter: WebSecurityConfigurerAdapter() {
+//        override fun configure(http: HttpSecurity) {
+//            http {
+//                securityMatcher("/api/**")
+//                authorizeRequests {
+//                    authorize(anyRequest, hasRole("ADMIN"))
+//                }
+//                httpBasic { }
+//            }
+//        }
+//    }
+
+    /**
+     * Default UI login
+     */
+    @Configuration
+    class UIWebSecurityConfigurerAdapter : WebSecurityConfigurerAdapter() {
+        override fun configure(http: HttpSecurity) {
+            http {
+                // Enabling JS Cookies for CSRF protection (for AngularJS)
+                csrf {
+                    csrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse()
+                }
+                // Requires authentication always
+                authorizeRequests {
+                    authorize(anyRequest, authenticated)
+                }
+                // Using a form login
+                formLogin { }
             }
         }
     }
 
-    //    private final APIBasicAuthenticationEntryPoint apiBasicAuthenticationEntryPoint;
-    //
-    //    private final AuthenticationManager authenticationManager;
-    //
-    //    private final List<UserSource> userSources;
-    //    public WebSecurityConfig(APIBasicAuthenticationEntryPoint apiBasicAuthenticationEntryPoint, AuthenticationManager authenticationManager, List<UserSource> userSources) {
-    //        this.apiBasicAuthenticationEntryPoint = apiBasicAuthenticationEntryPoint;
-    //        this.authenticationManager = authenticationManager;
-    //        this.userSources = userSources;
-    //    }
-    //    /**
-    //     * By default, all queries are accessible anonymously. Security is enforced at service level.
-    //     */
-    //    @Override
-    //    protected void configure(HttpSecurity http) throws Exception {
-    //        // Gets a secure random key for the remember be token key
-    //        SecureRandom random = new SecureRandom();
-    //        byte[] randomBytes = new byte[64];
-    //        random.nextBytes(randomBytes);
-    //        String rememberBeKey = new String(Hex.encode(randomBytes));
-    //        // @formatter:off
- //        http.antMatcher("/**")
- //            // Only BASIC authentication
- //            .httpBasic()
- //                .authenticationEntryPoint(apiBasicAuthenticationEntryPoint)
- //                .realmName("ontrack")
- //                .and()
- //            // Logout set-up
- //            .logout()
- //                .logoutUrl("/user/logout")
- //                .logoutSuccessUrl("/user/logged-out")
- //                .addLogoutHandler(basicRememberMeUserDetailsService())
- //                .and()
- //            // FIXME CSRF protection for a stateless API?
- //            //.csrf().requireCsrfProtectionMatcher(new CSRFRequestMatcher()).and()
- //            .csrf().disable()
- //            // Allows all at Web level
- //            .authorizeRequests()
- //                .requestMatchers(EndpointRequest.toAnyEndpoint().excluding(
- //                        InfoEndpoint.class,
- //                        HealthEndpoint.class
- //                )).access("hasRole('ADMIN') or @functionBasedSecurity.hasApplicationManagement(authentication)")
- //                .antMatchers("/**").permitAll()
- //            // Remember be authentication token
- //            .and().rememberMe()
- //                .rememberMeServices(rememberMeServices(rememberBeKey))
- //                .key(rememberBeKey)
- //                .tokenValiditySeconds(604800)
- //            // Cache enabled
- //            .and().headers().cacheControl().disable()
- //        ;
- //        // @formatter:on
-    //    }
-    //
-    //    private RememberMeServices rememberMeServices(String rememberBeKey) {
-    //        return new PersistentTokenBasedRememberMeServices(
-    //                rememberBeKey,
-    //                basicRememberMeUserDetailsService(),
-    //                new InMemoryTokenRepositoryImpl()
-    //        );
-    //    }
-    //
-    //    @Bean
-    //    public BasicRememberMeUserDetailsService basicRememberMeUserDetailsService() {
-    //        return new BasicRememberMeUserDetailsService(userSources);
-    //    }
-    //
-    //    @Override
-    //    protected void configure(AuthenticationManagerBuilder auth) {
-    //        auth.parentAuthenticationManager(authenticationManager);
-    //    }
 }
