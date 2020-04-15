b7dab6d1fda3db7c1c334c134ad53da7c50345e9
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-04-15 08:42:58 +0200

    #756 Added basic security policy tests

--- ontrack-service/src/test/java/net/nemerosa/ontrack/service/security/SecuritySetupIT.kt
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/security/SecuritySetupIT.kt
@@ -1,6 +1,7 @@
 package net.nemerosa.ontrack.service.security
 
 import net.nemerosa.ontrack.it.AbstractDSLTestSupport
+import net.nemerosa.ontrack.model.security.ProjectCreation
 import net.nemerosa.ontrack.model.structure.Project
 import org.junit.Test
 import org.springframework.security.access.AccessDeniedException
@@ -27,6 +28,67 @@ class SecuritySetupIT : AbstractDSLTestSupport() {
             structureService.newProject(Project.of(nameDescription()))
         }
         assertTrue(project.id.isSet, "Project has been created")
+    }
+
+    @Test
+    fun `Being authenticated is not enough to create a project`() {
+        assertFailsWith<AccessDeniedException> {
+            asUser().call {
+                structureService.newProject(Project.of(nameDescription()))
+            }
+        }
+    }
+
+    @Test
+    fun `Granting just enough rights`() {
+        asUserWith<ProjectCreation> {
+            structureService.newProject(Project.of(nameDescription()))
+        }
+    }
+
+    @Test
+    fun `Testing DSL to create a project`() {
+        project {
+            assertTrue(id.isSet, "Project has been created")
+        }
+    }
+
+    @Test
+    fun `Viewing projects is granted by default to all authenticated users`() {
+        val project = project {
+            branch()
+        }
+        val branches = asUser().call {
+            structureService.getBranchesForProject(project.id)
+        }
+        assertTrue(branches.isNotEmpty(), "Could access the branches")
+    }
+
+    @Test
+    fun `Viewing projects can be disabled for all authenticated users`() {
+        val project = project {
+            branch()
+        }
+        withNoGrantViewToAll {
+            asUser().call {
+                assertFailsWith<AccessDeniedException> {
+                    structureService.getBranchesForProject(project.id)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `Viewing projects needs to be granted explicitly when disabled globally`() {
+        val project = project {
+            branch()
+        }
+        withNoGrantViewToAll {
+            val branches = asUser().withView(project).call {
+                structureService.getBranchesForProject(project.id)
+            }
+            assertTrue(branches.isNotEmpty(), "Could access the branches")
+        }
     }
 
 }
