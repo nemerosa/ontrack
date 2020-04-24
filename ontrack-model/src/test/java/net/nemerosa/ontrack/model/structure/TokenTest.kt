a6cfa4ee39c19ac0af7fe36b4a815a765f2edb9e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-04-24 19:35:36 +0200

    #756 Some unit tests

---
+++ ontrack-model/src/test/java/net/nemerosa/ontrack/model/structure/TokenTest.kt
@@ -0,0 +1,49 @@
+package net.nemerosa.ontrack.model.structure
+
+import net.nemerosa.ontrack.common.Time
+import org.junit.Test
+import java.time.Duration
+import kotlin.test.*
+
+class TokenTest {
+
+    @Test
+    fun `Always valid when valid until is null`() {
+        val now = Time.now()
+        val token = Token("x", now, null)
+        assertTrue(token.isValid(), "Valid now")
+        assertTrue(token.isValid(now + Duration.ofDays(3650)), "Valid in 10 years")
+    }
+
+    @Test
+    fun `Limited validity`() {
+        val now = Time.now()
+        val token = Token("x", now, now + Duration.ofDays(30))
+        assertTrue(token.isValid(), "Valid now")
+        assertTrue(token.isValid(now + Duration.ofDays(29)), "Valid in 29 days")
+        assertFalse(token.isValid(Time.now() + Duration.ofDays(31)), "Not valid in 31 days")
+    }
+
+    @Test
+    fun `Token valid with negative value`() {
+        val token = Token("x", Time.now(), null).validFor(Duration.ofDays(-1))
+        assertNull(token.validUntil, "Always valid")
+    }
+
+    @Test
+    fun `Token valid with zero value`() {
+        val token = Token("x", Time.now(), null).validFor(Duration.ofDays(0))
+        assertNull(token.validUntil, "Always valid")
+    }
+
+    @Test
+    fun `Token valid with value`() {
+        val now = Time.now()
+        val token = Token("x", now, null).validFor(Duration.ofDays(1))
+        assertNotNull(token.validUntil, "Validity") {
+            val duration = Duration.between(now, it)
+            assertEquals(1, duration.toDays())
+        }
+    }
+
+}
--- ontrack-ui/src/test/java/net/nemerosa/ontrack/boot/support/LocalDateTimeConverterTest.kt
+++ ontrack-ui/src/test/java/net/nemerosa/ontrack/boot/support/LocalDateTimeConverterTest.kt
@@ -1,54 +1,46 @@
-package net.nemerosa.ontrack.boot.support;
+package net.nemerosa.ontrack.boot.support
 
-import org.junit.Test;
+import org.junit.Test
+import java.time.temporal.ChronoField
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoField;
+class LocalDateTimeConverterTest {
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+    private val converter = LocalDateTimeConverter()
 
-public class LocalDateTimeConverterTest {
-
-    private final LocalDateTimeConverter converter = new LocalDateTimeConverter();
 
     @Test
-    public void null_to_null() {
-        assertNull(converter.convert(null));
+    fun empty_to_null() {
+        assertNull(converter.convert(""))
     }
 
     @Test
-    public void empty_to_null() {
-        assertNull(converter.convert(""));
+    fun blank_to_null() {
+        assertNull(converter.convert(" "))
     }
 
     @Test
-    public void blank_to_null() {
-        assertNull(converter.convert(" "));
+    fun utc_with_millis() {
+        val t = converter.convert("2016-12-10T19:49:55.100Z")!!
+        assertEquals(2016, t.year.toLong())
+        assertEquals(12, t.month.value.toLong())
+        assertEquals(10, t.dayOfMonth.toLong())
+        assertEquals(19, t.hour.toLong())
+        assertEquals(49, t.minute.toLong())
+        assertEquals(55, t.second.toLong())
+        assertEquals(100, t[ChronoField.MILLI_OF_SECOND].toLong())
     }
 
     @Test
-    public void utc_with_millis() {
-        LocalDateTime t = converter.convert("2016-12-10T19:49:55.100Z");
-        assertEquals(2016, t.getYear());
-        assertEquals(12, t.getMonth().getValue());
-        assertEquals(10, t.getDayOfMonth());
-        assertEquals(19, t.getHour());
-        assertEquals(49, t.getMinute());
-        assertEquals(55, t.getSecond());
-        assertEquals(100, t.get(ChronoField.MILLI_OF_SECOND));
+    fun utc_with_seconds() {
+        val t = converter.convert("2016-12-10T19:49:55Z")!!
+        assertEquals(2016, t.year.toLong())
+        assertEquals(12, t.month.value.toLong())
+        assertEquals(10, t.dayOfMonth.toLong())
+        assertEquals(19, t.hour.toLong())
+        assertEquals(49, t.minute.toLong())
+        assertEquals(55, t.second.toLong())
+        assertEquals(0, t[ChronoField.MILLI_OF_SECOND].toLong())
     }
-
-    @Test
-    public void utc_with_seconds() {
-        LocalDateTime t = converter.convert("2016-12-10T19:49:55Z");
-        assertEquals(2016, t.getYear());
-        assertEquals(12, t.getMonth().getValue());
-        assertEquals(10, t.getDayOfMonth());
-        assertEquals(19, t.getHour());
-        assertEquals(49, t.getMinute());
-        assertEquals(55, t.getSecond());
-        assertEquals(0, t.get(ChronoField.MILLI_OF_SECOND));
-    }
-
 }
