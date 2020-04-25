1109ee8e0351b38628945cbd3e5073716e35f826
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-04-25 19:50:56 +0200

    #756 Kotlin conversion

--- ontrack-ui-graphql/src/main/java/net/nemerosa/ontrack/graphql/schema/GQLTypeAccount.kt
+++ ontrack-ui-graphql/src/main/java/net/nemerosa/ontrack/graphql/schema/GQLTypeAccount.kt
@@ -1,103 +1,75 @@
-package net.nemerosa.ontrack.graphql.schema;
+package net.nemerosa.ontrack.graphql.schema
 
-import graphql.schema.DataFetcher;
-import graphql.schema.GraphQLObjectType;
-import graphql.schema.GraphQLTypeReference;
-import net.nemerosa.ontrack.graphql.support.GraphqlUtils;
-import net.nemerosa.ontrack.model.security.Account;
-import net.nemerosa.ontrack.model.security.AccountGroup;
-import net.nemerosa.ontrack.model.security.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-import static graphql.Scalars.GraphQLString;
-import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
-import static graphql.schema.GraphQLObjectType.newObject;
-import static net.nemerosa.ontrack.graphql.support.GraphqlUtils.fetcher;
+import graphql.Scalars
+import graphql.schema.DataFetcher
+import graphql.schema.DataFetchingEnvironment
+import graphql.schema.GraphQLObjectType
+import graphql.schema.GraphQLTypeReference
+import net.nemerosa.ontrack.graphql.support.GraphqlUtils
+import net.nemerosa.ontrack.model.security.*
+import org.springframework.stereotype.Component
 
 @Component
-public class GQLTypeAccount implements GQLType {
+class GQLTypeAccount(
+        private val accountService: AccountService,
+        private val globalRole: GQLTypeGlobalRole,
+        private val authorizedProject: GQLTypeAuthorizedProject
+) : GQLType {
 
-    public static final String ACCOUNT = "Account";
+    override fun getTypeName(): String = ACCOUNT
 
-    private final AccountService accountService;
-    private final GQLTypeGlobalRole globalRole;
-    private final GQLTypeAuthorizedProject authorizedProject;
-
-    @Autowired
-    public GQLTypeAccount(AccountService accountService, GQLTypeGlobalRole globalRole, GQLTypeAuthorizedProject authorizedProject) {
-        this.accountService = accountService;
-        this.globalRole = globalRole;
-        this.authorizedProject = authorizedProject;
-    }
-
-    @Override
-    public String getTypeName() {
-        return ACCOUNT;
-    }
-
-    @Override
-    public GraphQLObjectType createType(GQLTypeCache cache) {
-        return newObject()
+    override fun createType(cache: GQLTypeCache): GraphQLObjectType {
+        return GraphQLObjectType.newObject()
                 .name(ACCOUNT)
                 .field(GraphqlUtils.idField())
                 .field(GraphqlUtils.nameField("Unique name for the account"))
                 .field(GraphqlUtils.stringField("fullName", "Full name of the account"))
                 .field(GraphqlUtils.stringField("email", "Email of the account"))
-                .field(
-                        newFieldDefinition()
-                                .name("authenticationSource")
-                                .description("Source of authentication (builtin, ldap, etc.)")
-                                .type(GraphQLString)
-                                .dataFetcher(environment -> ((Account) environment.getSource()).getAuthenticationSource().getId())
-                                .build()
-                )
+                .field {
+                    it.name("authenticationSource")
+                            .description("Source of authentication (builtin, ldap, etc.)")
+                            .type(Scalars.GraphQLString)
+                            .dataFetcher { environment: DataFetchingEnvironment -> (environment.getSource<Any>() as Account).authenticationSource.id }
+                }
                 .field(GraphqlUtils.stringField("role", "Security role (admin or none)"))
-                .field(
-                        newFieldDefinition()
-                                .name("groups")
-                                .description("List of groups the account belongs to")
-                                .type(GraphqlUtils.stdList(new GraphQLTypeReference(GQLTypeAccountGroup.ACCOUNT_GROUP)))
-                                .dataFetcher(accountAccountGroupsFetcher())
-                                .build()
-                )
-                .field(
-                        newFieldDefinition()
-                                .name("globalRole")
-                                .description("Global role for the account")
-                                .type(globalRole.getTypeRef())
-                                .dataFetcher(accountGlobalRoleFetcher())
-                                .build()
-                )
-                .field(
-                        newFieldDefinition()
-                                .name("authorizedProjects")
-                                .description("List of authorized projects")
-                                .type(GraphqlUtils.stdList(authorizedProject.getTypeRef()))
-                                .dataFetcher(accountAuthorizedProjectsFetcher())
-                                .build()
-                )
-                .build();
+                .field {
+                    it.name("groups")
+                            .description("List of groups the account belongs to")
+                            .type(GraphqlUtils.stdList(GraphQLTypeReference(GQLTypeAccountGroup.ACCOUNT_GROUP)))
+                            .dataFetcher(accountAccountGroupsFetcher())
+                }
+                .field {
+                    it.name("globalRole")
+                            .description("Global role for the account")
+                            .type(globalRole.typeRef)
+                            .dataFetcher(accountGlobalRoleFetcher())
+                }
+                .field {
+                    it.name("authorizedProjects")
+                            .description("List of authorized projects")
+                            .type(GraphqlUtils.stdList(authorizedProject.typeRef))
+                            .dataFetcher(accountAuthorizedProjectsFetcher())
+                }
+                .build()
     }
 
-    private DataFetcher accountAuthorizedProjectsFetcher() {
-        return fetcher(Account.class, accountService::getProjectPermissionsForAccount);
+    private fun accountAuthorizedProjectsFetcher() = DataFetcher<Collection<ProjectRoleAssociation>> { env ->
+        val account: Account = env.getSource()
+        accountService.getProjectPermissionsForAccount(account)
     }
 
-    private DataFetcher accountGlobalRoleFetcher() {
-        return fetcher(
-                Account.class,
-                account -> accountService.getGlobalRoleForAccount(account).orElse(null)
-        );
+    private fun accountGlobalRoleFetcher() = DataFetcher { env ->
+        val account: Account = env.getSource()
+        accountService.getGlobalRoleForAccount(account).orElse(null)
     }
 
-    private DataFetcher<List<AccountGroup>> accountAccountGroupsFetcher() {
-        return environment -> {
-            Account account = environment.getSource();
-            return accountService.getGroupsForAccount(account.getId());
-        };
+    private fun accountAccountGroupsFetcher() = DataFetcher<List<AccountGroup>> { env ->
+        val id = env.getSource<Account>().id
+        accountService.getGroupsForAccount(id)
+    }
+
+    companion object {
+        const val ACCOUNT = "Account"
     }
 
 }
