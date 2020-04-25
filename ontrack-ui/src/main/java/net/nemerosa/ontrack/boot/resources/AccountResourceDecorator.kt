208bae436791009a28c1d43814f07e21485696cf
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-04-25 20:09:22 +0200

    #756 Kotlin conversion

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/resources/AccountResourceDecorator.kt
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/resources/AccountResourceDecorator.kt
@@ -1,33 +1,26 @@
-package net.nemerosa.ontrack.boot.resources;
+package net.nemerosa.ontrack.boot.resources
 
-import net.nemerosa.ontrack.boot.ui.AccountController;
-import net.nemerosa.ontrack.model.security.Account;
-import net.nemerosa.ontrack.ui.resource.AbstractResourceDecorator;
-import net.nemerosa.ontrack.ui.resource.Link;
-import net.nemerosa.ontrack.ui.resource.ResourceContext;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
+import net.nemerosa.ontrack.boot.ui.AccountController
+import net.nemerosa.ontrack.model.security.Account
+import net.nemerosa.ontrack.ui.resource.AbstractResourceDecorator
+import net.nemerosa.ontrack.ui.resource.Link
+import net.nemerosa.ontrack.ui.resource.ResourceContext
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on
 
 @Component
-public class AccountResourceDecorator extends AbstractResourceDecorator<Account> {
+class AccountResourceDecorator : AbstractResourceDecorator<Account>(Account::class.java) {
 
-    public AccountResourceDecorator() {
-        super(Account.class);
-    }
-
-    @Override
-    public List<Link> links(Account account, ResourceContext resourceContext) {
+    override fun links(account: Account, resourceContext: ResourceContext): List<Link> {
         return resourceContext.links()
                 // Self
-                .self(on(AccountController.class).getAccount(account.getId()))
-                        // Update
-                .link(Link.UPDATE, on(AccountController.class).getUpdateForm(account.getId()))
-                        // Delete
-                .link(Link.DELETE, on(AccountController.class).deleteAccount(account.getId()), !account.isDefaultAdmin())
-                        // OK
-                .build();
+                .self(on(AccountController::class.java).getAccount(account.id))
+                // Update
+                .link(Link.UPDATE, on(AccountController::class.java).getUpdateForm(account.id))
+                // Delete
+                .link(Link.DELETE, on(AccountController::class.java).deleteAccount(account.id), !account.isDefaultAdmin)
+                // OK
+                .build()
     }
+
 }
