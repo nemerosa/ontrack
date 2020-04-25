0b62ca96361109078a7bbbda3c6160bfc0de198f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-04-25 20:15:32 +0200

    #756 Account decorator links

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/resources/AccountResourceDecorator.kt
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/resources/AccountResourceDecorator.kt
@@ -1,12 +1,15 @@
 package net.nemerosa.ontrack.boot.resources
 
 import net.nemerosa.ontrack.boot.ui.AccountController
+import net.nemerosa.ontrack.boot.ui.TokensController
 import net.nemerosa.ontrack.model.security.Account
+import net.nemerosa.ontrack.model.security.AccountManagement
 import net.nemerosa.ontrack.ui.resource.AbstractResourceDecorator
 import net.nemerosa.ontrack.ui.resource.Link
 import net.nemerosa.ontrack.ui.resource.ResourceContext
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on
+import java.util.concurrent.TimeUnit
 
 @Component
 class AccountResourceDecorator : AbstractResourceDecorator<Account>(Account::class.java) {
@@ -19,6 +22,24 @@ class AccountResourceDecorator : AbstractResourceDecorator<Account>(Account::cla
                 .link(Link.UPDATE, on(AccountController::class.java).getUpdateForm(account.id))
                 // Delete
                 .link(Link.DELETE, on(AccountController::class.java).deleteAccount(account.id), !account.isDefaultAdmin)
+                // Revoke any token
+                .link(
+                        "_revokeToken",
+                        on(TokensController::class.java).revokeForAccount(account.id()),
+                        AccountManagement::class.java
+                )
+                // Generates a token
+                .link(
+                        "_generateToken",
+                        on(TokensController::class.java).generateForAccount(account.id(), TokensController.TokenGenerationRequest(0, TimeUnit.DAYS)),
+                        AccountManagement::class.java
+                )
+                // Gets any token
+                .link(
+                        "_token",
+                        on(TokensController::class.java).getTokenForAccount(account.id()),
+                        AccountManagement::class.java
+                )
                 // OK
                 .build()
     }
---
+++ ontrack-ui/src/test/java/net/nemerosa/ontrack/boot/resources/AccountResourceDecoratorIT.kt
@@ -0,0 +1,22 @@
+package net.nemerosa.ontrack.boot.resources
+
+import org.junit.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+class AccountResourceDecoratorIT : AbstractResourceDecoratorTestSupport() {
+
+    @Autowired
+    private lateinit var accountResourceDecorator: AccountResourceDecorator
+
+    @Test
+    fun `Token links`() {
+        asAdmin {
+            val account = doCreateAccount()
+            account.decorate(accountResourceDecorator) {
+                assertLinkPresent("_revokeToken")
+                assertLinkPresent("_generateToken")
+                assertLinkPresent("_token")
+            }
+        }
+    }
+}
