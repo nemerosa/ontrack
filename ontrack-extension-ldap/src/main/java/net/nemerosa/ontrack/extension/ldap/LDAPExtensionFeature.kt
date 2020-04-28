eeeafab5b3e9e28d6f99467ae72115aa1f239ddd
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-04-28 09:31:41 +0200

    #756 No UI resource for the LDAP

--- ontrack-extension-ldap/src/main/java/net/nemerosa/ontrack/extension/ldap/LDAPExtensionFeature.kt
+++ ontrack-extension-ldap/src/main/java/net/nemerosa/ontrack/extension/ldap/LDAPExtensionFeature.kt
@@ -1,13 +1,11 @@
 package net.nemerosa.ontrack.extension.ldap
 
 import net.nemerosa.ontrack.extension.support.AbstractExtensionFeature
-import net.nemerosa.ontrack.model.extension.ExtensionFeatureOptions
 import org.springframework.stereotype.Component
 
 @Component
 class LDAPExtensionFeature : AbstractExtensionFeature(
         "ldap",
         "LDAP",
-        "LDAP support for authentication and authorisations",
-        ExtensionFeatureOptions.DEFAULT.withGui(true)
+        "LDAP support for authentication and authorisations"
 )
