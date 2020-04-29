42eba7798f792354b84d86155e4489f776d5fa59
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-04-29 16:48:45 +0200

    #756 Programmatic registrations

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/support/WebSecurityConfig.kt
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/support/WebSecurityConfig.kt
@@ -6,12 +6,17 @@ import net.nemerosa.ontrack.model.security.SecurityService
 import net.nemerosa.ontrack.model.structure.TokensService
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientPropertiesRegistrationAdapter
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.web.servlet.invoke
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 
@@ -82,9 +87,8 @@ class WebSecurityConfig {
                 // OAuth setup
                 oauth2Client { }
                 oauth2Login {
-                    // TODO clientRegistrationRepository
+                    // TODO Use an extension point for this
                     userInfoEndpoint {
-                        // TODO Use an extension point for this
                         oidcUserService = OntrackOidcUserService(accountService, securityService, providedGroupsService)
                     }
                 }
@@ -100,6 +104,24 @@ class WebSecurityConfig {
                 }
             }
         }
+    }
+
+    // TODO Use an extension point for this
+    @Bean
+    fun clientRegistrationRepository(): ClientRegistrationRepository {
+        val properties = OAuth2ClientProperties()
+        properties.provider["okta"] = OAuth2ClientProperties.Provider().apply {
+            issuerUri = "https://dev-991108.okta.com/oauth2/default"
+        }
+        properties.registration["okta"] = OAuth2ClientProperties.Registration().apply {
+            provider = "okta"
+            clientName = "Okta"
+            clientId = "0oa3prwngqXvuHmcJ357"
+            clientSecret = "Ym7SFacOkH9ARisax5IpqVXhiW4m9GnFs6pRHV2J"
+        }
+        return InMemoryClientRegistrationRepository(
+                OAuth2ClientPropertiesRegistrationAdapter.getClientRegistrations(properties)
+        )
     }
 
 }
