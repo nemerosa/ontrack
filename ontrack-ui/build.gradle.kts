9e3c9ca97835c9aaee226688ce5cdf0ec8bec594
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-05-02 14:05:50 +0200

    #762 RC1 No need for declaration of developmentOnly configuration

--- ontrack-ui/build.gradle.kts
+++ ontrack-ui/build.gradle.kts
@@ -9,11 +9,6 @@ plugins {
 
 apply(plugin = "org.springframework.boot")
 
-val developmentOnly by configurations.creating
-configurations.runtime {
-    extendsFrom(developmentOnly)
-}
-
 dependencies {
     api("org.springframework.boot:spring-boot-starter-web")
     api("org.springframework.boot:spring-boot-starter-security")
