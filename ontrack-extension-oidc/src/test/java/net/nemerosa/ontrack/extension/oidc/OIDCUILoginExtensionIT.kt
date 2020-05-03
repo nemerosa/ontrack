e15712b066fbb3a9de6d47c6cedffd3864f34f74
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-05-03 17:08:59 +0200

    #756 UI login extension test

---
+++ ontrack-extension-oidc/src/test/java/net/nemerosa/ontrack/extension/oidc/OIDCUILoginExtensionIT.kt
@@ -0,0 +1,55 @@
+package net.nemerosa.ontrack.extension.oidc
+
+import net.nemerosa.ontrack.extension.api.UILogin
+import net.nemerosa.ontrack.extension.oidc.settings.OIDCSettingsService
+import net.nemerosa.ontrack.extension.oidc.settings.OntrackOIDCProvider
+import net.nemerosa.ontrack.it.AbstractDSLTestSupport
+import net.nemerosa.ontrack.test.TestUtils
+import org.junit.Before
+import org.junit.Test
+import org.springframework.beans.factory.annotation.Autowired
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class OIDCUILoginExtensionIT : AbstractDSLTestSupport() {
+
+    @Autowired
+    private lateinit var service: OIDCSettingsService
+
+    @Autowired
+    protected lateinit var uiLoginExtension: OIDCUILoginExtension
+
+    @Before
+    fun cleanup() {
+        asAdmin {
+            service.providers.forEach {
+                service.deleteProvider(it.id)
+            }
+        }
+    }
+
+    @Test
+    fun `UI login extension after creation and after deletion of a provider`() {
+        val id = TestUtils.uid("I")
+        asAdmin {
+            service.createProvider(OntrackOIDCProvider(id, "Test", "", "", "", ""))
+        }
+        asAnonymous {
+            val logins = uiLoginExtension.contributions
+            assertEquals(
+                    listOf(
+                            UILogin(id, "/oauth2/authorization/$id", "Test")
+                    ),
+                    logins
+            )
+        }
+        asAdmin {
+            service.deleteProvider(id)
+        }
+        asAnonymous {
+            val logins = uiLoginExtension.contributions
+            assertTrue(logins.isEmpty())
+        }
+    }
+
+}
