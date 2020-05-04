5ab32a93ab969d6825089f04e8b1dbef5700dc0d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-05-04 10:42:58 +0200

    #756 Correction of tests

--- ontrack-service/src/test/java/net/nemerosa/ontrack/service/security/PasswordAuthenticationSourceProviderTest.kt
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/security/PasswordAuthenticationSourceProviderTest.kt
@@ -2,12 +2,13 @@ package net.nemerosa.ontrack.service.security
 
 import net.nemerosa.ontrack.model.security.BuiltinAuthenticationSourceProvider
 import org.junit.Test
+import kotlin.test.assertTrue
 
 class PasswordAuthenticationSourceProviderTest {
 
     @Test
-    void 'Password change is allowed'() {
-        assert new BuiltinAuthenticationSourceProvider().source.allowingPasswordChange
+    fun `Password change is allowed`() {
+        assertTrue(BuiltinAuthenticationSourceProvider.SOURCE.isAllowingPasswordChange)
     }
 
 }
