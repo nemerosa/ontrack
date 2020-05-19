c14084018170caeece4792d838d775d97569ccb8
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-05-19 16:36:12 +0200

    #773 Computed flag displayed in indicators

--- ontrack-extension-indicators/src/main/resources/static/extension/indicators/module.js
+++ ontrack-extension-indicators/src/main/resources/static/extension/indicators/module.js
@@ -246,6 +246,7 @@ angular.module('ontrack.extension.indicators', [
                             id
                           }
                         }
+                        computed
                       }
                       value
                       compliance
