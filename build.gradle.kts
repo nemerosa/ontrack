da8921c53ba9057b9409f7584adce402143f8309
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-06-09 09:19:55 +0200

    [maven] Increasing timeout to the Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ import org.ajoberstar.gradle.git.publish.GitPublishExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.redline_rpm.header.Os
 import org.springframework.boot.gradle.plugin.SpringBootPlugin
+import java.time.Duration
 
 buildscript {
     repositories {
@@ -222,6 +223,8 @@ configure(javaProjects) p@{
     }
 
     configure<NexusPublishExtension> {
+        clientTimeout.set(Duration.ofMinutes(10))
+        connectTimeout.set(Duration.ofMinutes(10))
         repositories {
             sonatype {
                 username.set(ossrhUsername)
