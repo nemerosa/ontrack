63d66f889a9030c185f82b63279fb3dbe24ab63e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-06-10 17:52:40 +0200

    #796 Granting access to the portfolios to all users - correction of tests

--- ontrack-extension-indicators/src/test/java/net/nemerosa/ontrack/extension/indicators/ui/IndicatorPortfolioUserMenuExtensionTest.kt
+++ ontrack-extension-indicators/src/test/java/net/nemerosa/ontrack/extension/indicators/ui/IndicatorPortfolioUserMenuExtensionTest.kt
@@ -1,7 +1,7 @@
 package net.nemerosa.ontrack.extension.indicators.ui
 
 import net.nemerosa.ontrack.extension.indicators.IndicatorsTestFixtures
-import net.nemerosa.ontrack.extension.indicators.acl.IndicatorPortfolioManagement
+import net.nemerosa.ontrack.extension.indicators.acl.IndicatorPortfolioAccess
 import net.nemerosa.ontrack.model.support.ActionType
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -11,7 +11,7 @@ class IndicatorPortfolioUserMenuExtensionTest {
     @Test
     fun check() {
         val extension = IndicatorPortfolioUserMenuExtension(IndicatorsTestFixtures.indicatorsExtensionFeature())
-        assertEquals(IndicatorPortfolioManagement::class.java, extension.globalFunction)
+        assertEquals(IndicatorPortfolioAccess::class.java, extension.globalFunction)
         assertEquals("portfolios", extension.action.uri)
         assertEquals(ActionType.LINK, extension.action.type)
     }
