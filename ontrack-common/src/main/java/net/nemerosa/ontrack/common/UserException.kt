aade6a646070a025274716e9b084b4d5a6c7d0f1
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-06-13 14:22:50 +0200

    #792 User exception must not rollback transactions

--- ontrack-common/src/main/java/net/nemerosa/ontrack/common/UserException.kt
+++ ontrack-common/src/main/java/net/nemerosa/ontrack/common/UserException.kt
@@ -1,7 +1,6 @@
 package net.nemerosa.ontrack.common
 
 /**
- * User exceptions are business exceptions, which can be tracked and will not
- * rollback a transaction.
+ * User exceptions are business exceptions.
  */
-abstract class UserException(message: String) : Exception(message)
+abstract class UserException(message: String) : BaseException(message)
