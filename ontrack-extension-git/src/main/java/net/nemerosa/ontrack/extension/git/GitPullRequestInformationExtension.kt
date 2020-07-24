f522d0b79f566d26b044a63616036ddefeaf4286
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-07-24 10:25:31 +0200

    #690 PR as extra information for the branch

---
+++ ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/GitPullRequestInformationExtension.kt
@@ -0,0 +1,33 @@
+package net.nemerosa.ontrack.extension.git
+
+import net.nemerosa.ontrack.extension.api.EntityInformationExtension
+import net.nemerosa.ontrack.extension.api.model.EntityInformation
+import net.nemerosa.ontrack.extension.git.model.getBranchAsPullRequest
+import net.nemerosa.ontrack.extension.git.property.GitBranchConfigurationProperty
+import net.nemerosa.ontrack.extension.git.property.GitBranchConfigurationPropertyType
+import net.nemerosa.ontrack.extension.git.service.GitService
+import net.nemerosa.ontrack.extension.support.AbstractExtension
+import net.nemerosa.ontrack.model.structure.Branch
+import net.nemerosa.ontrack.model.structure.ProjectEntity
+import net.nemerosa.ontrack.model.structure.PropertyService
+import org.springframework.stereotype.Component
+
+@Component
+class GitPullRequestInformationExtension(
+        extensionFeature: GitExtensionFeature,
+        private val propertyService: PropertyService,
+        private val gitService: GitService
+) : AbstractExtension(extensionFeature), EntityInformationExtension {
+
+    override fun getInformation(entity: ProjectEntity): EntityInformation? =
+            if (entity is Branch) {
+                val property: GitBranchConfigurationProperty? = propertyService.getProperty(entity, GitBranchConfigurationPropertyType::class.java).value
+                val pr = gitService.getBranchAsPullRequest(entity, property)
+                pr?.let {
+                    EntityInformation(this, pr)
+                }
+            } else {
+                null
+            }
+
+}
---
+++ ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/model/GitConfiguratorExtensions.kt
@@ -0,0 +1,17 @@
+package net.nemerosa.ontrack.extension.git.model
+
+import net.nemerosa.ontrack.extension.git.property.GitBranchConfigurationProperty
+import net.nemerosa.ontrack.extension.git.service.GitService
+import net.nemerosa.ontrack.model.structure.Branch
+
+fun GitService.getBranchAsPullRequest(
+        branch: Branch,
+        gitBranchConfigurationProperty: GitBranchConfigurationProperty?
+) = gitBranchConfigurationProperty?.let {
+    getGitConfiguratorAndConfiguration(branch.project)
+            ?.let { (configurator, configuration) ->
+                configurator.toPullRequestID(gitBranchConfigurationProperty.branch)?.let { prId ->
+                    configurator.getPullRequest(configuration, prId)
+                }
+            }
+}
