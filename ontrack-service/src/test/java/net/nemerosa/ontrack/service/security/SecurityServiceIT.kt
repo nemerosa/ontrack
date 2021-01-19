3ca3545c79446f42f178b7b81e5b7d4cdbd67161
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-01-19 15:39:24 +0100

    #826 Global participant role

--- ontrack-service/src/test/java/net/nemerosa/ontrack/service/security/SecurityServiceIT.kt
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/security/SecurityServiceIT.kt
@@ -3,6 +3,8 @@ package net.nemerosa.ontrack.service.security
 import net.nemerosa.ontrack.it.AbstractServiceTestSupport
 import net.nemerosa.ontrack.model.security.ProjectCreation
 import net.nemerosa.ontrack.model.security.Roles
+import net.nemerosa.ontrack.model.security.ValidationRunStatusChange
+import net.nemerosa.ontrack.model.security.ValidationRunStatusCommentEditOwn
 import org.apache.commons.lang3.StringUtils
 import org.junit.Test
 import org.springframework.security.access.AccessDeniedException
@@ -89,6 +91,36 @@ class SecurityServiceIT : AbstractServiceTestSupport() {
                 assertNotNull(structureService.getProject(id))
                 assertTrue(structureService.findProjectByName(name1).isPresent)
                 assertNotNull(structureService.getProject(id1))
+                true
+            }
+        }
+    }
+
+    @Test
+    fun `Participant in all projects`() {
+        withNoGrantViewToAll {
+
+            // Creates two projects
+            val (id, name) = doCreateProject()
+            val (id1, name1) = doCreateProject()
+            // Creates an account authorised to access all projects
+            val account = doCreateAccountWithGlobalRole(Roles.GLOBAL_PARTICIPANT)
+            asFixedAccount(account).call {
+                // With this account, gets the list of projects
+                val list = structureService.projectList
+                // Checks we only have the two projects (among all others)
+                assertTrue(list.size >= 2)
+                assertTrue(list.stream().anyMatch { (_, name2) -> StringUtils.equals(name, name2) })
+                assertTrue(list.stream().anyMatch { (_, name2) -> StringUtils.equals(name1, name2) })
+                // Access to the projects
+                assertTrue(structureService.findProjectByName(name).isPresent)
+                assertNotNull(structureService.getProject(id))
+                assertTrue(structureService.findProjectByName(name1).isPresent)
+                assertNotNull(structureService.getProject(id1))
+                // Checks the access right for participation
+                securityService.checkProjectFunction(id.value, ValidationRunStatusChange::class.java)
+                securityService.checkProjectFunction(id.value, ValidationRunStatusCommentEditOwn::class.java)
+                //
                 true
             }
         }
