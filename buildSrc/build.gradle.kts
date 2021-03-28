c97699a422f8d8e3dc636f5ba763b710a561be36
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-03-28 21:47:51 +0200

    Correction after merge

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 dependencies {
     implementation(gradleApi())
     implementation(gradleKotlinDsl())
-    implementation("com.github.node-gradle:gradle-node-plugin:5.5.1")
+    implementation("com.github.node-gradle:gradle-node-plugin:2.2.3")
     implementation("org.apache.httpcomponents:httpclient:4.5.3")
     implementation("org.apache.httpcomponents:httpcore:4.4.6")
     implementation("net.nemerosa.ontrack:ontrack-dsl-v4:4.0-beta.20") {
