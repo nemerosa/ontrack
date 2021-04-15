712d7e7ba10e1dd24e121f0a040ed12be5303bda
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-04-15 09:22:32 +0200

    #857 Branch graph model decorated with builds

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/links/BranchLinksNode.kt
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/links/BranchLinksNode.kt
@@ -1,14 +1,17 @@
 package net.nemerosa.ontrack.model.links
 
 import net.nemerosa.ontrack.model.structure.Branch
+import net.nemerosa.ontrack.model.structure.Build
 
 /**
  * Node in the graph of links for a branch.
  *
  * @property branch Branch associated to this node
+ * @property build Actual build for this node
  * @property edges Edges coming out of this node to other nodes
  */
 class BranchLinksNode(
     val branch: Branch,
+    val build: Build?,
     val edges: List<BranchLinksEdge>
 )
--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/links/BranchLinksService.kt
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/links/BranchLinksService.kt
@@ -17,4 +17,17 @@ interface BranchLinksService {
      */
     fun getBranchLinks(branch: Branch, direction: BranchLinksDirection): BranchLinksNode
 
+    /**
+     * Given a starting [build] and a [direction], computes the graph of links starting
+     * from this build.
+     *
+     * The [abstract graph][getBranchLinks] is computed first and is used as a _skeleton_ for the
+     * build graph.
+     *
+     * @param Build Build to start from
+     * @param direction Direction to follow for the build links
+     * @return Node for this build
+     */
+    fun getBuildLinks(branch: Branch, direction: BranchLinksDirection): BranchLinksNode
+
 }
