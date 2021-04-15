d2afa6dded2c89a065f5fcbd31b0b7df35eba3f4
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-04-15 12:59:59 +0200

    #857 History tests

--- ontrack-service/src/test/java/net/nemerosa/ontrack/service/links/BranchLinksServiceIT.kt
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/links/BranchLinksServiceIT.kt
@@ -98,43 +98,105 @@ class BranchLinksServiceIT : AbstractDSLTestSupport() {
 
     @Test
     fun `Depth limit`() {
-        asAdmin {
-            withSettings<BranchLinksSettings> {
-                settingsManagerService.saveSettings(
-                    BranchLinksSettings(
-                        depth = 1,
-                        history = BranchLinksSettings.DEFAULT_HISTORY,
-                        maxLinksPerLevel = BranchLinksSettings.DEFAULT_MAX_LINKS_PER_LEVEL
-                    )
-                )
+        withBranchLinkSettings(depth = 1) {
+            val library = build("library")
+            val component = build("component")
+            val project = build("project")
+            asAdmin {
+                project.linkTo(component)
+                component.linkTo(library)
+            }
 
-                val library = build("library")
-                val component = build("component")
-                val project = build("project")
-                asAdmin {
-                    project.linkTo(component)
-                    component.linkTo(library)
+            val libraryNode = branchLinksService.getBranchLinks(library.branch, BranchLinksDirection.USED_BY)
+            assertEquals(library.branch.id, libraryNode.branch.id)
+            assertNull(libraryNode.build, "Abstract graph nodes don't contain builds")
+            assertEquals(1, libraryNode.edges.size)
+
+            val componentEdge = libraryNode.edges.first()
+            assertEquals(BranchLinksDirection.USED_BY, componentEdge.direction)
+
+            val componentNode = componentEdge.linkedTo
+            assertEquals(component.branch.id, componentNode.branch.id)
+            assertNull(componentNode.build, "Abstract graph nodes don't contain builds")
+            assertEquals(0, componentNode.edges.size) // We don't go further because depth = 1
+        }
+    }
+
+    @Test
+    fun `History must be taken into account`() {
+        val a = build()
+        val b = build()
+        val c = build()
+        project {
+            val branch = branch {
+                build {
+                    linkTo(a)
                 }
+                build {
+                    linkTo(b)
+                }
+                build {
+                    linkTo(c)
+                }
+            }
+            val node = branchLinksService.getBranchLinks(branch, BranchLinksDirection.USING)
+            assertEquals(
+                listOf(a, b, c).map { it.branch.name }.toSet(),
+                node.edges.map { it.linkedTo.branch.name }.toSet()
+            )
+        }
+    }
 
-                val libraryNode = branchLinksService.getBranchLinks(library.branch, BranchLinksDirection.USED_BY)
-                assertEquals(library.branch.id, libraryNode.branch.id)
-                assertNull(libraryNode.build, "Abstract graph nodes don't contain builds")
-                assertEquals(1, libraryNode.edges.size)
-
-                val componentEdge = libraryNode.edges.first()
-                assertEquals(BranchLinksDirection.USED_BY, componentEdge.direction)
-
-                val componentNode = componentEdge.linkedTo
-                assertEquals(component.branch.id, componentNode.branch.id)
-                assertNull(componentNode.build, "Abstract graph nodes don't contain builds")
-                assertEquals(0, componentNode.edges.size) // We don't go further because depth = 1
+    @Test
+    fun `History limit`() {
+        val a = build()
+        val b = build()
+        val c = build()
+        project {
+            val branch = branch {
+                build {
+                    linkTo(a)
+                }
+                build {
+                    linkTo(b)
+                }
+                build {
+                    linkTo(c)
+                }
+            }
+            withBranchLinkSettings(history = 2) {
+                val node = branchLinksService.getBranchLinks(branch, BranchLinksDirection.USING)
+                assertEquals(
+                    listOf(b, c).map { it.branch.name }.toSet(), // History = 2, 3rd build's not taken
+                    node.edges.map { it.linkedTo.branch.name }.toSet()
+                )
             }
         }
     }
 
-    private fun build(name: String) = project<Build> {
+    private fun build(name: String = "build") = project<Build> {
         branch<Build> {
             build(name)
+        }
+    }
+
+    private fun withBranchLinkSettings(
+        depth: Int = BranchLinksSettings.DEFAULT_DEPTH,
+        history: Int = BranchLinksSettings.DEFAULT_HISTORY,
+        maxLinksPerLevel: Int = BranchLinksSettings.DEFAULT_MAX_LINKS_PER_LEVEL,
+        code: () -> Unit
+    ) {
+        asAdmin {
+            withSettings<BranchLinksSettings> {
+                settingsManagerService.saveSettings(
+                    BranchLinksSettings(
+                        depth = depth,
+                        history = history,
+                        maxLinksPerLevel = maxLinksPerLevel
+                    )
+                )
+                code()
+            }
         }
     }
 
