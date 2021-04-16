215d53627ce0a49a7dc5e7fb9e6fc53d6f18ff0e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-04-16 09:21:39 +0200

    #857 GraphQL branch links - 'using' tests

---
+++ ontrack-ui-graphql/src/test/java/net/nemerosa/ontrack/graphql/schema/links/BranchGraphQLIT.kt
@@ -0,0 +1,91 @@
+package net.nemerosa.ontrack.graphql.schema.links
+
+import net.nemerosa.ontrack.graphql.AbstractQLKTITSupport
+import net.nemerosa.ontrack.json.asJson
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class BranchGraphQLIT : AbstractQLKTITSupport() {
+
+    @Test
+    fun `Two layers`() {
+        withLinks {
+            val project = build("project", 1)
+            val component = build("component", 1)
+            val library = build("library", 1)
+            component linkTo library
+            project linkTo component
+
+            run("""query {
+                branches(id: ${project.branch.id}) {
+                    graph(direction: USING) {
+                        ...nodeContent
+                        edges {
+                            linkedTo {
+                                ...nodeContent
+                                edges {
+                                    linkedTo {
+                                        ...nodeContent
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            fragment nodeContent on BranchLinksNode {
+                branch {
+                    name
+                    project {
+                        name
+                    }
+                }
+            }
+            """).let { data ->
+                assertEquals(
+                    mapOf(
+                        "branches" to listOf(
+                            mapOf(
+                                "graph" to mapOf(
+                                    "branch" to mapOf(
+                                        "name" to project.branch.name,
+                                        "project" to mapOf(
+                                            "name" to project.project.name
+                                        )
+                                    ),
+                                    "edges" to listOf(
+                                        mapOf(
+                                            "linkedTo" to mapOf(
+                                                "branch" to mapOf(
+                                                    "name" to component.branch.name,
+                                                    "project" to mapOf(
+                                                        "name" to component.project.name
+                                                    )
+                                                ),
+                                                "edges" to listOf(
+                                                    mapOf(
+                                                        "linkedTo" to mapOf(
+                                                            "branch" to mapOf(
+                                                                "name" to library.branch.name,
+                                                                "project" to mapOf(
+                                                                    "name" to library.project.name
+                                                                )
+                                                            )
+                                                        )
+                                                    )
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    ).asJson(),
+                    data
+                )
+            }
+        }
+    }
+
+}
