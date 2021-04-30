d3edf0398c6e66728218168ff187463ca359883b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-04-30 11:01:05 +0200

    #863 Password of a locked account can be changed by an admin

--- ontrack-service/src/test/java/net/nemerosa/ontrack/service/security/UserServiceIT.kt
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/security/UserServiceIT.kt
@@ -1,6 +1,8 @@
 package net.nemerosa.ontrack.service.security
 
 import net.nemerosa.ontrack.it.AbstractDSLTestSupport
+import net.nemerosa.ontrack.model.security.Account
+import net.nemerosa.ontrack.model.security.AccountInput
 import net.nemerosa.ontrack.model.security.UserService
 import net.nemerosa.ontrack.model.support.PasswordChange
 import org.junit.Test
@@ -15,16 +17,32 @@ class UserServiceIT : AbstractDSLTestSupport() {
 
     @Test
     fun `Locked account cannot change its password`() {
-        val account = asAdmin {
-            val initial = doCreateAccount()
-            accountService.setAccountLocked(initial.id, true)
-            accountService.getAccount(initial.id)
-        }
+        val account = createLockedAccount()
         asFixedAccount(account) {
             assertFailsWith<AccessDeniedException> {
                 userService.changePassword(PasswordChange("old", "new"))
             }
         }
+    }
+
+    @Test
+    fun `Password of a locked account can be changed by an admin`() {
+        val account = createLockedAccount()
+        asAdmin {
+            accountService.updateAccount(account.id, AccountInput(
+                name = account.name,
+                fullName = account.fullName,
+                email = account.email,
+                password = "new-password",
+                groups = emptyList()
+            ))
+        }
+    }
+
+    private fun createLockedAccount(): Account = asAdmin {
+        val initial = doCreateAccount()
+        accountService.setAccountLocked(initial.id, true)
+        accountService.getAccount(initial.id)
     }
 
 }
