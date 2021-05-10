38500187ee65c14b2c0c64a81b6b1a4808836c9b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-05-10 15:41:07 +0200

    #820 Portfolio of portfolios - migration of global indicators to a view

--- ontrack-extension-indicators/src/main/java/net/nemerosa/ontrack/extension/indicators/portfolio/PortfolioGlobalIndicators.kt
+++ ontrack-extension-indicators/src/main/java/net/nemerosa/ontrack/extension/indicators/portfolio/PortfolioGlobalIndicators.kt
@@ -1,5 +1,6 @@
 package net.nemerosa.ontrack.extension.indicators.portfolio
 
+@Deprecated("Use indicator views. This class will be removed in V4.")
 class PortfolioGlobalIndicators(
         val categories: List<String>
 )
