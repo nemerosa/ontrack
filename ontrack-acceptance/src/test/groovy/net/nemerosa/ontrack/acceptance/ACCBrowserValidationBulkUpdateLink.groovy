544774a67cb861f86cbc9eaffa7683fa1f81f591
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-06-07 11:29:20 +0200

    #868 Restored tests

--- ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/ACCBrowserValidationBulkUpdateLink.groovy
+++ ontrack-acceptance/src/test/groovy/net/nemerosa/ontrack/acceptance/ACCBrowserValidationBulkUpdateLink.groovy
@@ -5,7 +5,6 @@ import net.nemerosa.ontrack.acceptance.support.AcceptanceTestSuite
 import net.nemerosa.ontrack.dsl.ValidationStamp
 import org.junit.Before
 import org.junit.Test
-import org.junit.Ignore
 
 import static net.nemerosa.ontrack.acceptance.steps.BasicSteps.login
 import static net.nemerosa.ontrack.acceptance.steps.BasicSteps.loginAsAdmin
@@ -15,7 +14,6 @@ import static net.nemerosa.ontrack.test.TestUtils.uid
  * Regression test for #678
  */
 @AcceptanceTestSuite
-@Ignore
 class ACCBrowserValidationBulkUpdateLink extends AcceptanceTestClient {
 
     private final String groupName = "ValidationManagers"
