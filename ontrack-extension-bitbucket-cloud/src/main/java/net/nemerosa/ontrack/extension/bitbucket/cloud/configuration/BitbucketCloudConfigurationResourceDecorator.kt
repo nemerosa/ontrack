710a0acc18f5156339e8af09f50728a917534125
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-06-11 11:05:11 +0200

    #876 BBC configuration UI

---
+++ ontrack-extension-bitbucket-cloud/src/main/java/net/nemerosa/ontrack/extension/bitbucket/cloud/configuration/BitbucketCloudConfigurationResourceDecorator.kt
@@ -0,0 +1,42 @@
+package net.nemerosa.ontrack.extension.bitbucket.cloud.configuration
+
+import net.nemerosa.ontrack.model.security.GlobalSettings
+import net.nemerosa.ontrack.model.security.SecurityService
+import net.nemerosa.ontrack.ui.resource.AbstractResourceDecorator
+import net.nemerosa.ontrack.ui.resource.Link
+import net.nemerosa.ontrack.ui.resource.ResourceContext
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on
+
+@Component
+class BitbucketCloudConfigurationResourceDecorator(
+    private val securityService: SecurityService
+) : AbstractResourceDecorator<BitbucketCloudConfiguration>(
+    BitbucketCloudConfiguration::class.java
+) {
+
+    /**
+     * Obfuscates the password
+     */
+    override fun decorateBeforeSerialization(bean: BitbucketCloudConfiguration): BitbucketCloudConfiguration =
+        bean.obfuscate()
+
+    override fun links(configuration: BitbucketCloudConfiguration, resourceContext: ResourceContext): List<Link> {
+        val globalSettingsGranted: Boolean = securityService.isGlobalFunctionGranted(GlobalSettings::class.java)
+        return resourceContext.links()
+            .self(on(BitbucketCloudConfigurationController::class.java).getConfiguration(configuration.name))
+            .link(
+                Link.UPDATE,
+                on(BitbucketCloudConfigurationController::class.java).updateConfigurationForm(configuration.name),
+                globalSettingsGranted
+            )
+            .link(
+                Link.DELETE,
+                on(BitbucketCloudConfigurationController::class.java).deleteConfiguration(configuration.name),
+                globalSettingsGranted
+            )
+            // OK
+            .build()
+    }
+
+}
--- ontrack-extension-bitbucket-cloud/src/main/resources/static/extension/bitbucket-cloud/module.js
+++ ontrack-extension-bitbucket-cloud/src/main/resources/static/extension/bitbucket-cloud/module.js
@@ -1,29 +1,28 @@
-angular.module('ontrack.extension.stash', [
+angular.module('ontrack.extension.bitbucket-cloud', [
     'ui.router',
     'ot.service.core',
     'ot.service.configuration',
     'ot.service.form'
 ])
     .config(function ($stateProvider) {
-        // Artifactory configurations
-        $stateProvider.state('stash-configurations', {
-            url: '/extension/stash/configurations',
-            templateUrl: 'extension/stash/stash.configurations.tpl.html',
-            controller: 'StashConfigurationsCtrl'
+        $stateProvider.state('bitbucket-cloud-configurations', {
+            url: '/extension/bitbucket-cloud/configurations',
+            templateUrl: 'extension/bitbucket-cloud/configurations.tpl.html',
+            controller: 'BitbucketCloudConfigurationsCtrl'
         });
     })
-    .controller('StashConfigurationsCtrl', function ($scope, $http, ot, otFormService, otAlertService, otConfigurationService) {
+    .controller('BitbucketCloudConfigurationsCtrl', function ($scope, $http, ot, otFormService, otAlertService, otConfigurationService) {
         var view = ot.view();
-        view.title = 'Bitbucket Server configurations';
-        view.description = 'Management of the Bitbucket Server configurations.';
+        view.title = 'Bitbucket Cloud configurations';
+        view.description = 'Management of the Bitbucket Cloud configurations.';
 
         // Loading the configurations
         function load() {
-            ot.call($http.get('extension/stash/configurations')).then(function (configurations) {
+            ot.call($http.get('extension/bitbucket-cloud/configurations')).then(function (configurations) {
                 $scope.configurations = configurations;
                 view.commands = [
                     {
-                        id: 'stash-configuration-create',
+                        id: 'bitbucket-cloud-configuration-create',
                         name: "Create a configuration",
                         cls: 'ot-command-new',
                         action: $scope.createConfiguration
@@ -39,7 +38,7 @@ angular.module('ontrack.extension.stash', [
         $scope.createConfiguration = function () {
             otFormService.display({
                 uri: $scope.configurations._create,
-                title: "Bitbucket Server configuration",
+                title: "Bitbucket Cloud configuration",
                 buttons: [ otConfigurationService.testButton($scope.configurations._test) ],
                 submit: function (data) {
                     return ot.call($http.post($scope.configurations._create, data));
@@ -50,8 +49,8 @@ angular.module('ontrack.extension.stash', [
         // Deleting a configuration
         $scope.deleteConfiguration = function (configuration) {
             otAlertService.confirm({
-                title: 'Bitbucket Server configuration',
-                message: "Do you really want to delete this Bitbucket Server configuration? Some projects may still refer to it."
+                title: 'Bitbucket Cloud configuration',
+                message: "Do you really want to delete this Bitbucket Cloud configuration? Some projects may still refer to it."
             }).then(
                 function success() {
                     ot.call($http.delete(configuration._delete)).then(load);
@@ -63,7 +62,7 @@ angular.module('ontrack.extension.stash', [
         $scope.updateConfiguration = function (configuration) {
             otFormService.display({
                 uri: configuration._update,
-                title: "Bitbucket Server configuration",
+                title: "Bitbucket Cloud configuration",
                 buttons: [ otConfigurationService.testButton($scope.configurations._test) ],
                 submit: function (data) {
                     return ot.call($http.put(configuration._update, data));
