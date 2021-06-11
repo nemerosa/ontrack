64bf3352366037b50fb4fd6900e4a19233991670
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-06-11 15:08:44 +0200

    #876 BBC decoration for projects

---
+++ ontrack-extension-bitbucket-cloud/src/test/java/net/nemerosa/ontrack/extension/bitbucket/cloud/property/BitbucketCloudProjectDecoratorIT.kt
@@ -0,0 +1,65 @@
+package net.nemerosa.ontrack.extension.bitbucket.cloud.property
+
+import net.nemerosa.ontrack.extension.bitbucket.cloud.AbstractBitbucketCloudTestSupport
+import net.nemerosa.ontrack.extension.bitbucket.cloud.bitbucketCloudTestConfigMock
+import net.nemerosa.ontrack.model.exceptions.PropertyUnsupportedEntityTypeException
+import net.nemerosa.ontrack.model.structure.ProjectEntityType
+import org.junit.Test
+import org.springframework.beans.factory.annotation.Autowired
+import java.util.*
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
+
+class BitbucketCloudProjectDecoratorIT : AbstractBitbucketCloudTestSupport() {
+
+    @Autowired
+    private lateinit var decorator: BitbucketCloudProjectDecorator
+
+    @Test
+    fun `Project scope`() {
+        assertEquals(
+            EnumSet.of(ProjectEntityType.PROJECT),
+            decorator.scope
+        )
+    }
+
+    @Test
+    fun `Not a projet`() {
+        project {
+            branch {
+                assertFailsWith<PropertyUnsupportedEntityTypeException> {
+                    decorator.getDecorations(this)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `No decoration when no Bitbucket Cloud configuration`() {
+        project {
+            val list = decorator.getDecorations(this)
+            assertTrue(list.isEmpty(), "No Bitbucket Cloud decoration on a project which is not configured")
+        }
+    }
+
+    @Test
+    fun `Decoration when Bitbucket Cloud configuration`() {
+        asAdmin {
+            withDisabledConfigurationTest {
+                project {
+                    val config = bitbucketCloudTestConfigMock()
+                    bitbucketCloudConfigurationService.newConfiguration(config)
+                    setBitbucketCloudProperty(config, "my-repository")
+                    val list = decorator.getDecorations(this)
+                    assertEquals(1, list.size)
+                    val decoration = list.first()
+                    assertEquals(
+                        "${config.workspace}/my-repository",
+                        decoration.data
+                    )
+                }
+            }
+        }
+    }
+}
