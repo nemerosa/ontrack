f30cd7389ba2b659901ac6c07d15c65419028d3d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-07-23 07:58:28 +0200

    #884 More tests

--- ontrack-extension-jenkins/src/test/java/net/nemerosa/ontrack/extension/jenkins/indicator/JenkinsPipelineLibraryIndicatorComputerIT.kt
+++ ontrack-extension-jenkins/src/test/java/net/nemerosa/ontrack/extension/jenkins/indicator/JenkinsPipelineLibraryIndicatorComputerIT.kt
@@ -26,6 +26,43 @@ class JenkinsPipelineLibraryIndicatorComputerIT : AbstractGitTestSupport() {
     }
 
     @Test
+    fun `Single library`() {
+        createRepo {
+            file("Jenkinsfile", """
+                @Library('pipeline-general@1.0.1') _
+                
+                pipeline()
+            """.trimIndent())
+            git("commit", "-m", "Commit of the Jenkinsfile")
+        } and { repo, _ ->
+            project {
+                gitProject(repo)
+                val indicators = computer.computeIndicators(project)
+                assertEquals(
+                    listOf(
+                        IndicatorComputedValue(
+                            type = IndicatorComputedType(
+                                category = IndicatorComputedCategory(
+                                    "jenkins-pipeline-library",
+                                    "Jenkins pipeline libraries"
+                                ),
+                                id = "pipeline-general",
+                                name = "pipeline-general",
+                                link = null,
+                                valueType = valueType,
+                                valueConfig = JenkinsPipelineLibraryIndicatorValueTypeConfig(false, null),
+                            ),
+                            value = JenkinsPipelineLibraryVersion("1.0.1"),
+                            comment = null
+                        ),
+                    ),
+                    indicators
+                )
+            }
+        }
+    }
+
+    @Test
     fun `Several libraries detected`() {
         createRepo {
             file("Jenkinsfile", """
