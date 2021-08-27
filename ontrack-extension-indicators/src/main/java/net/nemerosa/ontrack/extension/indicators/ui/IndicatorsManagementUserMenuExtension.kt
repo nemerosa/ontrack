f057ff3e38e9439c748596f12fffb5b809b34b39
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-08-27 10:40:37 +0200

    #891 Direct access to the portfolios when authorized only to this

--- ontrack-extension-indicators/src/main/java/net/nemerosa/ontrack/extension/indicators/ui/IndicatorsManagementUserMenuExtension.kt
+++ ontrack-extension-indicators/src/main/java/net/nemerosa/ontrack/extension/indicators/ui/IndicatorsManagementUserMenuExtension.kt
@@ -4,8 +4,10 @@ import net.nemerosa.ontrack.extension.api.UserMenuExtension
 import net.nemerosa.ontrack.extension.indicators.IndicatorsExtensionFeature
 import net.nemerosa.ontrack.extension.indicators.acl.IndicatorPortfolioAccess
 import net.nemerosa.ontrack.extension.indicators.acl.IndicatorTypeManagement
+import net.nemerosa.ontrack.extension.indicators.acl.IndicatorViewManagement
 import net.nemerosa.ontrack.extension.support.AbstractExtension
 import net.nemerosa.ontrack.model.security.GlobalFunction
+import net.nemerosa.ontrack.model.security.SecurityService
 import net.nemerosa.ontrack.model.support.Action
 import org.springframework.stereotype.Component
 
@@ -14,11 +16,20 @@ import org.springframework.stereotype.Component
  */
 @Component
 class IndicatorsManagementUserMenuExtension(
-    extension: IndicatorsExtensionFeature
+    extension: IndicatorsExtensionFeature,
+    private val securityService: SecurityService,
 ) : AbstractExtension(extension), UserMenuExtension {
 
+    /**
+     * If the user is authorized to more functions than only the access to the portfolios, we
+     * provide an access to the indicators management. If not, to the portfolios only.
+     */
     override fun getAction(): Action =
-        Action.of("indicators-management", "Indicators", "indicators-management")
+        if (securityService.isGlobalFunctionGranted(IndicatorTypeManagement::class.java) || securityService.isGlobalFunctionGranted(IndicatorViewManagement::class.java)) {
+            Action.of("indicators-management", "Indicators", "indicators-management")
+        } else {
+            Action.of("indicators-portfolios", "Indicators portfolios", "portfolios")
+        }
 
     override fun getGlobalFunction(): Class<out GlobalFunction> = IndicatorPortfolioAccess::class.java
 
