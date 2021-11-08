f7ef0acc17a8b08d03b174787f91a66dde44a1de
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-11-08 11:41:34 +0100

    #927 When several job events are received, the validation runs must not be created several times.

--- ontrack-extension-github/src/test/java/net/nemerosa/ontrack/extension/github/ingestion/processing/job/WorkflowJobProcessingServiceIT.kt
+++ ontrack-extension-github/src/test/java/net/nemerosa/ontrack/extension/github/ingestion/processing/job/WorkflowJobProcessingServiceIT.kt
@@ -29,7 +29,7 @@ class WorkflowJobProcessingServiceIT : AbstractDSLTestSupport() {
         project {
             branch {
                 build {
-                    test()
+                    runTest()
                 }
             }
         }
@@ -40,7 +40,7 @@ class WorkflowJobProcessingServiceIT : AbstractDSLTestSupport() {
         project {
             branch {
                 build {
-                    test(
+                    runTest(
                         conclusion = WorkflowJobStepConclusion.failure,
                         expectedStatus = "FAILED",
                     )
@@ -54,50 +54,88 @@ class WorkflowJobProcessingServiceIT : AbstractDSLTestSupport() {
         project {
             branch {
                 build {
-                    test(runId = 1)
+                    runTest(runId = 1)
                 }
                 build {
-                    test(runId = 2)
+                    runTest(runId = 2)
                 }
             }
         }
     }
 
-    private fun Build.test(
+    @Test
+    fun `Job progressing on several payloads`() {
+        project {
+            branch {
+                build {
+                    setupTest(
+                        status = WorkflowJobStepStatus.queued,
+                        conclusion = null,
+                    )
+                    setupTest(
+                        status = WorkflowJobStepStatus.in_progress,
+                        conclusion = null,
+                    )
+                    setupTest(
+                        status = WorkflowJobStepStatus.completed,
+                        conclusion = WorkflowJobStepConclusion.success,
+                    )
+                    setupTest(
+                        status = WorkflowJobStepStatus.completed,
+                        conclusion = WorkflowJobStepConclusion.success,
+                    )
+                    assertNotNull(
+                        structureService.findValidationStampByName(
+                            project.name,
+                            branch.name,
+                            "build-publishing-to-the-repository"
+                        ).getOrNull(),
+                        "Validation stamp has been created"
+                    ) { vs ->
+                        val runs = structureService.getValidationRunsForBuildAndValidationStamp(
+                            id,
+                            vs.id,
+                            offset = 0,
+                            count = 1
+                        )
+                        assertEquals(1, runs.size, "One and only one run created")
+                        val run = runs.first()
+                        assertEquals("PASSED", run.lastStatusId)
+                        assertNotNull(runInfoService.getRunInfo(run), "Run info has been set") { info ->
+                            assertEquals(60, info.runTime, "Run time = 60 seconds")
+                            assertEquals("github-workflow", info.sourceType)
+                            assertEquals("uri:job", info.sourceUri)
+                        }
+                        assertNotNull(getProperty(run, ValidationRunGitHubWorkflowJobPropertyType::class.java)) { p ->
+                            assertEquals("build", p.job)
+                            assertEquals("run-name", p.name)
+                            assertEquals(1, p.runNumber)
+                            assertEquals("uri:job", p.url)
+                            assertEquals(false, p.running)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun Build.runTest(
         runId: Long = 1L,
         job: String = "build",
         step: String? = "Publishing to the repository",
         status: WorkflowJobStepStatus = WorkflowJobStepStatus.completed,
-        conclusion: WorkflowJobStepConclusion = WorkflowJobStepConclusion.success,
+        conclusion: WorkflowJobStepConclusion? = WorkflowJobStepConclusion.success,
         expectedVsName: String = normalizeName("$job-$step"),
         expectedStatus: String = "PASSED",
     ) {
-        val ref = Time.now()
+        setupTest(
+            runId = runId,
+            job = job,
+            step = step,
+            status = status,
+            conclusion = conclusion,
+        )
         asAdmin {
-            setProperty(
-                this,
-                BuildGitHubWorkflowRunPropertyType::class.java,
-                BuildGitHubWorkflowRunProperty(
-                    runId = runId,
-                    url = "",
-                    name = "run-name",
-                    runNumber = 1,
-                    running = true,
-                )
-            )
-            workflowJobProcessingService.setupValidation(
-                owner = "nemerosa",
-                repository = project.name,
-                runId = runId,
-                runAttempt = 1,
-                job = "build",
-                jobUrl = "uri:job",
-                step = "Publishing to the repository",
-                status = status,
-                conclusion = conclusion,
-                startedAt = ref.minusMinutes(1),
-                completedAt = ref,
-            )
             // Checks the validation stamp has been created
             assertNotNull(
                 structureService.findValidationStampByName(project.name, branch.name, expectedVsName).getOrNull(),
@@ -126,6 +164,42 @@ class WorkflowJobProcessingServiceIT : AbstractDSLTestSupport() {
                     }
                 }
             }
+        }
+    }
+
+    private fun Build.setupTest(
+        runId: Long = 1L,
+        job: String = "build",
+        step: String? = "Publishing to the repository",
+        status: WorkflowJobStepStatus = WorkflowJobStepStatus.completed,
+        conclusion: WorkflowJobStepConclusion? = WorkflowJobStepConclusion.success,
+    ) {
+        val ref = Time.now()
+        asAdmin {
+            setProperty(
+                this,
+                BuildGitHubWorkflowRunPropertyType::class.java,
+                BuildGitHubWorkflowRunProperty(
+                    runId = runId,
+                    url = "",
+                    name = "run-name",
+                    runNumber = 1,
+                    running = true,
+                )
+            )
+            workflowJobProcessingService.setupValidation(
+                owner = "nemerosa",
+                repository = project.name,
+                runId = runId,
+                runAttempt = 1,
+                job = job,
+                jobUrl = "uri:job",
+                step = step,
+                status = status,
+                conclusion = conclusion,
+                startedAt = ref.minusMinutes(1),
+                completedAt = ref,
+            )
         }
     }
 
