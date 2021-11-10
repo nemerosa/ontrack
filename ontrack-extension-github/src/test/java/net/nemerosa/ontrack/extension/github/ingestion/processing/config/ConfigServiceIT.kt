6bd6a7401a7e24d08fcf68b97c49ec400fc0d9ee
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-11-10 10:13:50 +0100

    #928 Loading the configuration when needed

--- ontrack-extension-github/src/test/java/net/nemerosa/ontrack/extension/github/ingestion/processing/config/ConfigServiceIT.kt
+++ ontrack-extension-github/src/test/java/net/nemerosa/ontrack/extension/github/ingestion/processing/config/ConfigServiceIT.kt
@@ -5,6 +5,7 @@ import io.mockk.mockk
 import net.nemerosa.ontrack.common.RunProfile
 import net.nemerosa.ontrack.extension.github.ingestion.AbstractIngestionTestSupport
 import net.nemerosa.ontrack.extension.github.ingestion.IngestionHookFixtures
+import net.nemerosa.ontrack.extension.github.ingestion.processing.model.Repository
 import org.junit.Before
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 class ConfigServiceIT : AbstractIngestionTestSupport() {
 
@@ -30,25 +32,96 @@ class ConfigServiceIT : AbstractIngestionTestSupport() {
 
     @Test
     fun `Saving the configuration`() {
-        val config = IngestionHookFixtures.sampleIngestionConfig()
-        every {
-            configLoaderService.loadConfig(
-                any(),
+        withSavedConfiguration { config, repository, branch ->
+            // Loading the configuration
+            assertNotNull(configService.findConfig(repository, branch), "Configuration saved") {
+                assertEquals(config, it)
+            }
+        }
+    }
+
+    @Test
+    fun `Removing the configuration`() {
+        withSavedConfiguration { _, repository, branch ->
+            // Loading the configuration
+            assertNotNull(configService.findConfig(repository, branch), "Configuration saved")
+            // Removing the configuration
+            configService.removeConfig(repository, branch)
+            // Loading the configuration
+            assertNull(configService.findConfig(repository, branch), "Configuration removed")
+        }
+    }
+
+    @Test
+    fun `Get or load with already saved configuration`() {
+        withSavedConfiguration { saved, repository, branch ->
+            val config = configService.getOrLoadConfig(repository, branch, INGESTION_CONFIG_FILE_PATH)
+            assertEquals(saved, config)
+        }
+    }
+
+    @Test
+    fun `Get or load with loading configuration`() {
+        withLoadingConfiguration(existing = true) { config ->
+            val loaded = configService.getOrLoadConfig(
+                IngestionHookFixtures.sampleRepository(),
+                IngestionHookFixtures.sampleBranch,
                 INGESTION_CONFIG_FILE_PATH
             )
-        } returns config
-        val repository = IngestionHookFixtures.sampleRepository()
-        val branch = IngestionHookFixtures.sampleBranch
-        asAdmin {
+            assertEquals(config, loaded)
+        }
+    }
+
+    @Test
+    fun `Get or load with loading non existent configuration`() {
+        withLoadingConfiguration(existing = false) {
+            val loaded = configService.getOrLoadConfig(
+                IngestionHookFixtures.sampleRepository(),
+                IngestionHookFixtures.sampleBranch,
+                INGESTION_CONFIG_FILE_PATH
+            )
+            assertEquals(IngestionConfig(), loaded, "Loaded configuration is the default configuration")
+        }
+    }
+
+    fun withSavedConfiguration(
+        test: (config: IngestionConfig, repository: Repository, branch: String) -> Unit
+    ) {
+        withLoadingConfiguration(existing = true) { config ->
+            val repository = IngestionHookFixtures.sampleRepository()
+            val branch = IngestionHookFixtures.sampleBranch
             configService.saveConfig(
                 repository,
                 branch,
                 INGESTION_CONFIG_FILE_PATH,
             )
-            // Loading the configuration
-            assertNotNull(configService.findConfig(repository, branch), "Configuration saved") {
-                assertEquals(config, it)
-            }
+            // Testing
+            test(config, repository, branch)
+        }
+    }
+
+    fun withLoadingConfiguration(
+        existing: Boolean,
+        test: (config: IngestionConfig) -> Unit
+    ) {
+        val config = IngestionHookFixtures.sampleIngestionConfig()
+        if (existing) {
+            every {
+                configLoaderService.loadConfig(
+                    any(),
+                    INGESTION_CONFIG_FILE_PATH
+                )
+            } returns config
+        } else {
+            every {
+                configLoaderService.loadConfig(
+                    any(),
+                    INGESTION_CONFIG_FILE_PATH
+                )
+            } returns null
+        }
+        asAdmin {
+            test(config)
         }
     }
 
