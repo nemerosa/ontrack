7a24b7838ba9ef1cf7acff1f85c9cfff4c952b3f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-12-04 10:35:16 +0100

    #930 When a PR is `closed`, disables the branch.

---
+++ ontrack-extension-github/src/main/java/net/nemerosa/ontrack/extension/github/ingestion/processing/pr/ClosedPRPayloadListener.kt
@@ -0,0 +1,27 @@
+package net.nemerosa.ontrack.extension.github.ingestion.processing.pr
+
+import net.nemerosa.ontrack.extension.github.ingestion.support.IngestionModelAccessService
+import net.nemerosa.ontrack.extension.github.ingestion.support.getOrCreateBranch
+import net.nemerosa.ontrack.model.structure.StructureService
+import org.springframework.stereotype.Component
+
+/**
+ * When a PR is `closed`, disables the branch.
+ */
+@Component
+class ClosedPRPayloadListener(
+    private val ingestionModelAccessService: IngestionModelAccessService,
+    private val structureService: StructureService,
+) : AbstractPRPayloadListener(
+    action = PRPayloadAction.opened
+) {
+    override fun process(payload: PRPayload, configuration: String?) {
+        val branch = ingestionModelAccessService.getOrCreateBranch(
+            repository = payload.repository,
+            configuration = configuration,
+            headBranch = payload.pullRequest.head.ref,
+            pullRequest = payload.pullRequest,
+        )
+        structureService.disableBranch(branch)
+    }
+}
