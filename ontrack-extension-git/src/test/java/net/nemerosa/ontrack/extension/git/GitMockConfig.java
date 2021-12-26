cff10f190fdd1e71d836f80c9594cccb393e775b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-12-26 13:11:23 +0100

    #934 Fixing compilation issues with new dependencies

--- ontrack-extension-git/src/test/java/net/nemerosa/ontrack/extension/git/GitMockConfig.java
+++ ontrack-extension-git/src/test/java/net/nemerosa/ontrack/extension/git/GitMockConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
