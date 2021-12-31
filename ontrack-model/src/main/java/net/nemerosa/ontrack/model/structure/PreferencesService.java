65c6134fa17f8b1ec38d428b11622b94d7a6694a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-12-31 11:07:07 +0100

    #502 Preferences - removed all classes

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/structure/PreferencesService.java
+++
@@ -1,9 +0,0 @@
-package net.nemerosa.ontrack.model.structure;
-
-public interface PreferencesService {
-
-    <T> T load(PreferencesType<T> preferencesType, T defaultValue);
-
-    <T> void store(PreferencesType<T> preferencesType, T value);
-
-}
