032acf63e9e6d5c96b89e3ea5d3562637da6e6f7
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-01-19 10:11:55 +0100

    #941 Testing the validation data export - for test summaries

--- ontrack-extension-elastic/src/test/java/net/nemerosa/ontrack/extension/elastic/metrics/ElasticMetricsValidationRunMetricsExtensionIT.kt
+++ ontrack-extension-elastic/src/test/java/net/nemerosa/ontrack/extension/elastic/metrics/ElasticMetricsValidationRunMetricsExtensionIT.kt
@@ -1,8 +1,8 @@
 package net.nemerosa.ontrack.extension.elastic.metrics
 
-import net.nemerosa.ontrack.extension.general.validation.MetricsValidationData
-import net.nemerosa.ontrack.extension.general.validation.MetricsValidationDataType
+import net.nemerosa.ontrack.extension.general.validation.*
 import net.nemerosa.ontrack.it.AbstractDSLTestSupport
+import net.nemerosa.ontrack.json.asJson
 import net.nemerosa.ontrack.model.structure.config
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,6 +17,9 @@ class ElasticMetricsValidationRunMetricsExtensionIT : AbstractDSLTestSupport() {
 
     @Autowired
     private lateinit var metricsValidationDataType: MetricsValidationDataType
+
+    @Autowired
+    private lateinit var testSummaryValidationDataType: TestSummaryValidationDataType
 
     @Autowired
     private lateinit var elasticMetricsClient: ElasticMetricsClient
@@ -45,6 +48,47 @@ class ElasticMetricsValidationRunMetricsExtensionIT : AbstractDSLTestSupport() {
                     )
                     // Expecting only one result
                     assertEquals(1, results.items.size, "One metric registered")
+                    val result = results.items.first()
+                    val source = result.source.asJson()
+                    assertEquals(10.0, source["fields"]["position"].asDouble())
+                    assertEquals(5.0, source["fields"]["speed"].asDouble())
+                    assertEquals(0.5, source["fields"]["acceleration"].asDouble())
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `Export of validation data metrics for test metrics`() {
+        val dataConfig = testSummaryValidationDataType.config(
+            TestSummaryValidationConfig(
+                warningIfSkipped = true,
+            )
+        )
+        project {
+            branch {
+                // Typed validation
+                val vs = validationStamp(validationDataTypeConfig = dataConfig)
+                // Build
+                build {
+                    // Validation with data
+                    validateWithData(vs, validationRunData = TestSummaryValidationData(
+                        passed = 13,
+                        skipped = 8,
+                        failed = 5,
+                    ))
+                    // Checks the metric has been exported into ES
+                    val results = elasticMetricsClient.rawSearch(
+                        token = vs.name,
+                        indexName = "ontrack_metric_validation_data",
+                    )
+                    // Expecting only one result
+                    assertEquals(1, results.items.size, "One metric registered")
+                    val result = results.items.first()
+                    val source = result.source.asJson()
+                    assertEquals(13.0, source["fields"]["passed"].asDouble())
+                    assertEquals(8.0, source["fields"]["skipped"].asDouble())
+                    assertEquals(5.0, source["fields"]["failed"].asDouble())
                 }
             }
         }
