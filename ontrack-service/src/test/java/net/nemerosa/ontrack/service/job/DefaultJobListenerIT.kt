03a715dd81e7bae29badb331e61ecb74bdfdb7e6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-03-16 15:45:56 +0100

    #622 Conversion to JUnit 5

--- ontrack-service/src/test/java/net/nemerosa/ontrack/service/job/DefaultJobListenerIT.kt
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/job/DefaultJobListenerIT.kt
@@ -1,21 +1,23 @@
 package net.nemerosa.ontrack.service.job
 
 import io.micrometer.core.instrument.MeterRegistry
-import net.nemerosa.ontrack.it.AbstractServiceTestJUnit4Support
+import net.nemerosa.ontrack.it.AbstractServiceTestSupport
 import net.nemerosa.ontrack.job.JobCategory
 import net.nemerosa.ontrack.job.JobStatus
 import net.nemerosa.ontrack.job.Schedule
 import net.nemerosa.ontrack.model.support.*
-import org.junit.Test
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import kotlin.test.assertEquals
 
-class DefaultJobListenerIT : AbstractServiceTestJUnit4Support() {
+class DefaultJobListenerIT : AbstractServiceTestSupport() {
 
     @Autowired
     private lateinit var logService: ApplicationLogService
+
     @Autowired
     private lateinit var meterRegistry: MeterRegistry
+
     @Autowired
     private lateinit var settingsRepository: SettingsRepository
 
@@ -23,23 +25,23 @@ class DefaultJobListenerIT : AbstractServiceTestJUnit4Support() {
     fun onJobError() {
         val jobListener = DefaultJobListener(logService, meterRegistry, settingsRepository)
         val jobStatus = JobStatus(
-                id = 1,
-                key = JobCategory.of("test").getType("test").getKey("1"),
-                schedule = Schedule.EVERY_MINUTE,
-                actualSchedule = Schedule.EVERY_MINUTE,
-                description = "Test job",
-                isDisabled = false,
-                isPaused = false,
-                isRunning = false,
-                isValid = true,
-                lastError = null,
-                lastErrorCount = 0,
-                lastTimeoutCount = 0,
-                lastRunDate = null,
-                lastRunDurationMs = 0,
-                nextRunDate = null,
-                progress = null,
-                runCount = 0
+            id = 1,
+            key = JobCategory.of("test").getType("test").getKey("1"),
+            schedule = Schedule.EVERY_MINUTE,
+            actualSchedule = Schedule.EVERY_MINUTE,
+            description = "Test job",
+            isDisabled = false,
+            isPaused = false,
+            isRunning = false,
+            isValid = true,
+            lastError = null,
+            lastErrorCount = 0,
+            lastTimeoutCount = 0,
+            lastRunDate = null,
+            lastRunDurationMs = 0,
+            nextRunDate = null,
+            progress = null,
+            runCount = 0
         )
         jobListener.onJobError(jobStatus, RuntimeException("Test exception"))
         // Checks the error has been logged
