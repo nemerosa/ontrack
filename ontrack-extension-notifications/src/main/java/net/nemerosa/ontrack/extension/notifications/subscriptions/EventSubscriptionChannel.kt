99eba9f29ee86997ef50e586278e5c462dca4cc0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-03-23 15:48:56 +0100

    #951 Subscription service - GraphQL mutation

--- ontrack-extension-notifications/src/main/java/net/nemerosa/ontrack/extension/notifications/subscriptions/EventSubscriptionChannel.kt
+++ ontrack-extension-notifications/src/main/java/net/nemerosa/ontrack/extension/notifications/subscriptions/EventSubscriptionChannel.kt
@@ -1,6 +1,7 @@
 package net.nemerosa.ontrack.extension.notifications.subscriptions
 
 import com.fasterxml.jackson.databind.JsonNode
+import net.nemerosa.ontrack.model.annotations.APIDescription
 
 /**
  * Channel for a subscription.
@@ -8,7 +9,10 @@ import com.fasterxml.jackson.databind.JsonNode
  * @property channel Type of channel to send the event to
  * @property channelConfig Specific configuration of the channel
  */
+@APIDescription("Channel for a subscription.")
 data class EventSubscriptionChannel(
+    @APIDescription("Type of channel to send the event to")
     val channel: String,
+    @APIDescription("Specific configuration of the channel")
     val channelConfig: JsonNode,
 )
---
+++ ontrack-extension-notifications/src/main/java/net/nemerosa/ontrack/extension/notifications/subscriptions/EventSubscriptionMutations.kt
@@ -0,0 +1,71 @@
+package net.nemerosa.ontrack.extension.notifications.subscriptions
+
+import net.nemerosa.ontrack.graphql.schema.Mutation
+import net.nemerosa.ontrack.graphql.support.TypedMutationProvider
+import net.nemerosa.ontrack.model.annotations.APIDescription
+import net.nemerosa.ontrack.model.structure.ID
+import net.nemerosa.ontrack.model.structure.ProjectEntityType
+import net.nemerosa.ontrack.model.structure.StructureService
+import org.springframework.stereotype.Component
+
+@Component
+class EventSubscriptionMutations(
+    private val eventSubscriptionService: EventSubscriptionService,
+    private val structureService: StructureService,
+) : TypedMutationProvider() {
+
+    override val mutations: List<Mutation> = listOf(
+        simpleMutation(
+            name = "subscribeToEvents",
+            description = "Creates a subscription to a list of events",
+            input = SubscribeToEventsInput::class,
+            outputName = "subscription",
+            outputDescription = "Saved subscription",
+            outputType = EventSubscriptionPayload::class,
+        ) { input ->
+            val projectEntity = input.projectEntity?.run {
+                type.getEntityFn(structureService).apply(ID.of(id))
+            }
+            val record = eventSubscriptionService.subscribe(
+                EventSubscription(
+                    channels = input.channels.toSet(),
+                    events = input.events.toSet(),
+                    projectEntity = projectEntity,
+                )
+            )
+            EventSubscriptionPayload(
+                id = record.id,
+                channels = record.data.channels.toList(),
+                events = record.data.events.toList(),
+            )
+        }
+    )
+}
+
+@APIDescription("Subscription to events")
+data class SubscribeToEventsInput(
+    @APIDescription("Target project entity (null for global events)")
+    val projectEntity: EventSubscriptionEntityInput?,
+    @APIDescription("Channels to send this event to")
+    val channels: List<EventSubscriptionChannel>,
+    @APIDescription("List of events types to subscribe to")
+    val events: List<String>,
+)
+
+@APIDescription("Target entity for a subscription")
+data class EventSubscriptionEntityInput(
+    @APIDescription("Project entity type")
+    val type: ProjectEntityType,
+    @APIDescription("Project entity ID")
+    val id: Int,
+)
+
+@APIDescription("Event subscription record")
+data class EventSubscriptionPayload(
+    @APIDescription("Unique ID for this subscription")
+    val id: String,
+    @APIDescription("Channels to send this event to")
+    val channels: List<EventSubscriptionChannel>,
+    @APIDescription("List of events types to subscribe to")
+    val events: List<String>,
+)
