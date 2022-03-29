614bda9832988c04b43eb45e620921e968313a96
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-03-29 16:53:24 +0200

    #951 UI / Project entity subscriptions page skeleton

--- ontrack-extension-notifications/src/main/resources/static/extension/notifications/module.js
+++ ontrack-extension-notifications/src/main/resources/static/extension/notifications/module.js
@@ -1,4 +1,21 @@
 angular.module('ontrack.extension.notifications', [
     'ot.service.core'
 ])
+    .config(function ($stateProvider) {
+        $stateProvider.state('entity-subscriptions', {
+            url: '/extension/notifications/entity-subscriptions/{type}/{id}',
+            templateUrl: 'extension/notifications/entity-subscriptions.tpl.html',
+            controller: 'EntitySubscriptionsCtrl'
+        });
+    })
+    .controller('EntitySubscriptionsCtrl', function ($scope, $http, ot) {
+        const view = ot.view();
+        view.title = "Subscriptions";
+        // TODO Breadcrumbs to the entity
+        view.breadcrumbs = ot.homeBreadcrumbs();
+        // TODO Close command to the entity
+        view.commands = [
+            ot.viewCloseCommand('/home')
+        ];
+    })
 ;
