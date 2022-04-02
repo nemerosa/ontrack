31b8100c0e6532486f4dd28dd64f8545314a6e73
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-04-02 20:10:15 +0200

    #951 Webhook testing

--- ontrack-extension-notifications/src/test/java/net/nemerosa/ontrack/extension/notifications/webhooks/DefaultWebhookPayloadRendererTest.kt
+++ ontrack-extension-notifications/src/test/java/net/nemerosa/ontrack/extension/notifications/webhooks/DefaultWebhookPayloadRendererTest.kt
@@ -1,22 +1,16 @@
 package net.nemerosa.ontrack.extension.notifications.webhooks
 
-import io.mockk.mockk
 import net.nemerosa.ontrack.model.events.EventFactoryImpl
 import net.nemerosa.ontrack.model.structure.ID
 import net.nemerosa.ontrack.model.structure.NameDescription
 import net.nemerosa.ontrack.model.structure.Project
-import net.nemerosa.ontrack.model.support.OntrackConfigProperties
 import org.junit.jupiter.api.Test
 
 internal class DefaultWebhookPayloadRendererTest {
 
     @Test
     fun `Rendering a new project event`() {
-        val renderer = DefaultWebhookPayloadRenderer(
-            mockk(),
-            emptyList(),
-            OntrackConfigProperties()
-        )
+        val renderer = DefaultWebhookPayloadRenderer()
         val project = Project.of(NameDescription.nd("prj", "New project")).withId(ID.of(1))
         val event = EventFactoryImpl().newProject(project)
         val payload = WebhookPayload(type = "event", data = event)
