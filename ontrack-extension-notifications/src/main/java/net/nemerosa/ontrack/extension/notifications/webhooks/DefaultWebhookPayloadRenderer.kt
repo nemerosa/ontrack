651b45152bb0b878ac6cb7d33259fe9f02143dac
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-04-02 19:19:47 +0200

    #951 Webhook testing

--- ontrack-extension-notifications/src/main/java/net/nemerosa/ontrack/extension/notifications/webhooks/DefaultWebhookPayloadRenderer.kt
+++ ontrack-extension-notifications/src/main/java/net/nemerosa/ontrack/extension/notifications/webhooks/DefaultWebhookPayloadRenderer.kt
@@ -1,38 +1,12 @@
 package net.nemerosa.ontrack.extension.notifications.webhooks
 
-import net.nemerosa.ontrack.json.ObjectMapperFactory
-import net.nemerosa.ontrack.model.security.SecurityService
-import net.nemerosa.ontrack.model.support.OntrackConfigProperties
-import net.nemerosa.ontrack.ui.controller.AbsoluteURIBuilder
-import net.nemerosa.ontrack.ui.resource.DefaultResourceContext
-import net.nemerosa.ontrack.ui.resource.ResourceModule
-import net.nemerosa.ontrack.ui.resource.ResourceObjectMapper
-import net.nemerosa.ontrack.ui.resource.ResourceObjectMapperFactory
+import net.nemerosa.ontrack.json.asJson
 import org.springframework.stereotype.Component
-import java.io.ByteArrayOutputStream
 
 @Component
-class DefaultWebhookPayloadRenderer(
-    securityService: SecurityService,
-    resourceModules: List<ResourceModule>,
-    ontrackConfigProperties: OntrackConfigProperties,
-) : WebhookPayloadRenderer {
-
-    private val resourceObjectMapper: ResourceObjectMapper
-
-    init {
-        val uriBuilder = AbsoluteURIBuilder(ontrackConfigProperties)
-        val resourceContext = DefaultResourceContext(uriBuilder, securityService)
-        val mapper = ObjectMapperFactory.create()
-        resourceObjectMapper = ResourceObjectMapperFactory(mapper).resourceObjectMapper(
-            resourceModules,
-            resourceContext
-        )
-    }
+class DefaultWebhookPayloadRenderer : WebhookPayloadRenderer {
 
     override fun render(payload: WebhookPayload<*>): ByteArray {
-        val output = ByteArrayOutputStream()
-        resourceObjectMapper.write(output, payload)
-        return output.toByteArray()
+        return payload.asJson().toPrettyString().toByteArray()
     }
 }
