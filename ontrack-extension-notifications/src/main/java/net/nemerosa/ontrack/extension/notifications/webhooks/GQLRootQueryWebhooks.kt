10b6155dd4db136ed1509c9818255a7da32e441d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-04-18 10:48:48 +0200

    #951 Webhook by name

--- ontrack-extension-notifications/src/main/java/net/nemerosa/ontrack/extension/notifications/webhooks/GQLRootQueryWebhooks.kt
+++ ontrack-extension-notifications/src/main/java/net/nemerosa/ontrack/extension/notifications/webhooks/GQLRootQueryWebhooks.kt
@@ -1,5 +1,6 @@
 package net.nemerosa.ontrack.extension.notifications.webhooks
 
+import graphql.Scalars.GraphQLString
 import graphql.schema.GraphQLFieldDefinition
 import net.nemerosa.ontrack.graphql.schema.GQLRootQuery
 import net.nemerosa.ontrack.graphql.support.listType
@@ -14,9 +15,25 @@ class GQLRootQueryWebhooks(
         GraphQLFieldDefinition.newFieldDefinition()
             .name("webhooks")
             .description("List of registred webhooks")
+            .argument {
+                it.name(ARG_NAME)
+                    .description("Name of the webhook")
+                    .type(GraphQLString)
+            }
             .type(listType(gqlTypeWebhook.typeRef))
-            .dataFetcher {
-                webhookAdminService.webhooks
+            .dataFetcher { env ->
+                val name: String? = env.getArgument(ARG_NAME)
+                if (name.isNullOrBlank()) {
+                    webhookAdminService.webhooks
+                } else {
+                    listOfNotNull(
+                        webhookAdminService.findWebhookByName(name)
+                    )
+                }
             }
             .build()
+
+    companion object {
+        private const val ARG_NAME = "name"
+    }
 }
--- ontrack-extension-notifications/src/test/java/net/nemerosa/ontrack/extension/notifications/webhooks/GQLRootQueryWebhooksIT.kt
+++ ontrack-extension-notifications/src/test/java/net/nemerosa/ontrack/extension/notifications/webhooks/GQLRootQueryWebhooksIT.kt
@@ -1,7 +1,6 @@
 package net.nemerosa.ontrack.extension.notifications.webhooks
 
 import net.nemerosa.ontrack.extension.notifications.AbstractNotificationTestSupport
-import net.nemerosa.ontrack.json.asJson
 import net.nemerosa.ontrack.json.getRequiredBooleanField
 import net.nemerosa.ontrack.json.getRequiredIntField
 import net.nemerosa.ontrack.json.getRequiredTextField
@@ -11,11 +10,50 @@ import org.springframework.beans.factory.annotation.Autowired
 import java.time.Duration
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 internal class GQLRootQueryWebhooksIT : AbstractNotificationTestSupport() {
 
     @Autowired
     private lateinit var webhookAdminService: WebhookAdminService
+
+    @Test
+    fun `Get a webhook by name`() {
+        val name1 = TestUtils.uid("wh")
+        val name2 = TestUtils.uid("wh")
+        asAdmin {
+            webhookAdminService.createWebhook(
+                name = name1,
+                enabled = true,
+                url = "uri:test",
+                timeout = Duration.ofMinutes(1),
+                authentication = WebhookFixtures.webhookAuthentication(),
+            )
+            webhookAdminService.createWebhook(
+                name = name2,
+                enabled = true,
+                url = "uri:test",
+                timeout = Duration.ofMinutes(1),
+                authentication = WebhookFixtures.webhookAuthentication(),
+            )
+            run("""{
+                webhooks(name: "$name1") {
+                    name
+                    enabled
+                    url
+                    timeoutSeconds
+                    authenticationType
+                }
+            }""") { data ->
+                assertNotNull(data.path("webhooks").find {
+                    it.getRequiredTextField("name") == name1
+                }, "Found the requested webhook")
+                assertNull(data.path("webhooks").find {
+                    it.getRequiredTextField("name") == name2
+                }, "Did not find the requested webhook")
+            }
+        }
+    }
 
     @Test
     fun `Get the list of webhooks`() {
