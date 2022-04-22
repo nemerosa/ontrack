e2ff94cd32db384ff640e60eba25f3ac711844ea
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-04-22 09:21:13 +0200

    #960 Upgrade to Spring Boot 2.6.7 and Kotlin 1.6.21

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,8 +2,8 @@
  * List of versions to use
  */
 object Versions {
-    const val springBootVersion = "2.6.6"
-    const val kotlinVersion = "1.6.10"
+    const val springBootVersion = "2.6.7"
+    const val kotlinVersion = "1.6.21"
     const val kotlinCoroutinesVersion = "1.5.1"
     const val jjwtVersion = "0.11.2"
     const val mockkVersion = "1.12.0"
