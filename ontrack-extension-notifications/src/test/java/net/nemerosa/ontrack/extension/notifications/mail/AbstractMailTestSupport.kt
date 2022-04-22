0fc18baf213fd812a7c88f58035dbb5aa417c88f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-04-22 11:45:43 +0200

    #951 Mail channel test

--- ontrack-extension-notifications/src/test/java/net/nemerosa/ontrack/extension/notifications/mail/AbstractMailTestSupport.kt
+++ ontrack-extension-notifications/src/test/java/net/nemerosa/ontrack/extension/notifications/mail/AbstractMailTestSupport.kt
@@ -1,16 +1,13 @@
 package net.nemerosa.ontrack.extension.notifications.mail
 
-import com.icegreen.greenmail.spring.GreenMailBean
 import com.icegreen.greenmail.util.GreenMail
-import net.nemerosa.ontrack.common.RunProfile
 import net.nemerosa.ontrack.extension.notifications.AbstractNotificationTestSupport
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
-import org.springframework.mail.javamail.JavaMailSender
-import org.springframework.mail.javamail.JavaMailSenderImpl
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ContextConfiguration
 
+@ContextConfiguration(classes = [AbstractMailTestSupportConfiguration::class])
+@DirtiesContext
 abstract class AbstractMailTestSupport : AbstractNotificationTestSupport() {
 
     @Autowired
@@ -22,24 +19,6 @@ abstract class AbstractMailTestSupport : AbstractNotificationTestSupport() {
 
     companion object {
         const val DEFAULT_ADDRESS = "to@localhost"
-    }
-
-    @Configuration
-    @Profile(RunProfile.UNIT_TEST)
-    class AbstractMailTestSupportConfiguration {
-
-        @Bean
-        fun greenMailBean(): GreenMailBean = GreenMailBean()
-
-        @Bean
-        fun greenMail(greenMailBean: GreenMailBean): GreenMail = greenMailBean.greenMail
-
-        @Bean
-        fun javaMailSender(greenMail: GreenMail): JavaMailSender = JavaMailSenderImpl().apply {
-            host = "localhost"
-            port = greenMail.smtp.port
-        }
-
     }
 
 }
