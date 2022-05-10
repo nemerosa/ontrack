3bbedcdf9060e55d4968149d1f4f7b19e7d5e690
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-05-10 11:45:10 +0200

    #977 Creating the new validations ~> integration tests

--- ontrack-extension-github/src/test/java/net/nemerosa/ontrack/extension/github/ingestion/processing/config/ConfigServiceIT.kt
+++ ontrack-extension-github/src/test/java/net/nemerosa/ontrack/extension/github/ingestion/processing/config/ConfigServiceIT.kt
@@ -1,19 +1,30 @@
 package net.nemerosa.ontrack.extension.github.ingestion.processing.config
 
 import io.mockk.every
-import net.nemerosa.ontrack.extension.github.ingestion.AbstractIngestionTestJUnit4Support
+import net.nemerosa.ontrack.extension.general.AutoPromotionProperty
+import net.nemerosa.ontrack.extension.general.AutoPromotionPropertyType
+import net.nemerosa.ontrack.extension.general.validation.MetricsValidationDataType
+import net.nemerosa.ontrack.extension.general.validation.TestSummaryValidationConfig
+import net.nemerosa.ontrack.extension.general.validation.TestSummaryValidationDataType
+import net.nemerosa.ontrack.extension.github.ingestion.AbstractIngestionTestSupport
 import net.nemerosa.ontrack.extension.github.ingestion.IngestionHookFixtures
 import net.nemerosa.ontrack.model.structure.Branch
-import org.junit.Before
-import org.junit.Test
+import net.nemerosa.ontrack.model.structure.PromotionLevel
+import net.nemerosa.ontrack.model.structure.ValidationDataTypeConfig
+import net.nemerosa.ontrack.model.structure.ValidationStamp
+import net.nemerosa.ontrack.test.TestUtils
+import net.nemerosa.ontrack.test.assertIs
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 @ContextConfiguration(classes = [ConfigLoaderServiceITMockConfig::class])
-class ConfigServiceIT : AbstractIngestionTestJUnit4Support() {
+class ConfigServiceIT : AbstractIngestionTestSupport() {
 
     @Autowired
     private lateinit var configLoaderService: ConfigLoaderService
@@ -21,7 +32,7 @@ class ConfigServiceIT : AbstractIngestionTestJUnit4Support() {
     @Autowired
     private lateinit var configService: ConfigService
 
-    @Before
+    @BeforeEach
     fun before() {
         onlyOneGitHubConfig()
     }
@@ -78,10 +89,194 @@ class ConfigServiceIT : AbstractIngestionTestJUnit4Support() {
         }
     }
 
+    @Test
+    fun `Creation of validation stamps`() {
+        asAdmin {
+            project {
+                branch {
+                    withConfigFile("/ingestion/config-validations-creation.yml") {
+                        checkValidationStampExists(this, "unit-tests") { vs ->
+                            assertDataType(vs) {
+                                assertEquals(
+                                    TestSummaryValidationDataType::class.java.name,
+                                    it.descriptor.id
+                                )
+                                assertIs<TestSummaryValidationConfig>(it.config) { config ->
+                                    assertTrue(config.warningIfSkipped, "Configuration is OK")
+                                }
+                            }
+                        }
+                        checkValidationStampExists(this, "e2e-tests") { vs ->
+                            assertNoDataType(vs)
+                        }
+                        checkValidationStampExists(this, "sonarqube") { vs ->
+                            assertNoDataType(vs)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `Update of validation stamps`() {
+        asAdmin {
+            project {
+                branch {
+                    withConfigFile("/ingestion/config-validations-creation.yml") {
+                        // Validation stamps are not setup
+                        // Updating them
+                        withConfigFile("/ingestion/config-validations-update.yml") {
+                            checkValidationStampExists(this, "unit-tests") { vs ->
+                                assertDataType(vs) {
+                                    assertEquals(
+                                        TestSummaryValidationDataType::class.java.name,
+                                        it.descriptor.id
+                                    )
+                                    assertIs<TestSummaryValidationConfig>(it.config) { config ->
+                                        assertTrue(config.warningIfSkipped, "Configuration is OK")
+                                    }
+                                }
+                            }
+                            checkValidationStampExists(this, "e2e-tests") { vs ->
+                                assertDataType(vs) {
+                                    assertEquals(
+                                        TestSummaryValidationDataType::class.java.name,
+                                        it.descriptor.id
+                                    )
+                                    assertIs<TestSummaryValidationConfig>(it.config) { config ->
+                                        assertTrue(config.warningIfSkipped, "Configuration is OK")
+                                    }
+                                }
+                            }
+                            // Existing validation stamps are preserved
+                            checkValidationStampExists(this, "sonarqube") { vs ->
+                                assertNoDataType(vs)
+                            }
+                            // New validation stamp
+                            checkValidationStampExists(this, "new-metrics") { vs ->
+                                assertDataType(vs) {
+                                    assertEquals(
+                                        MetricsValidationDataType::class.java.name,
+                                        it.descriptor.id
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `Mix of validations and promotions`() {
+        asAdmin {
+            project {
+                branch {
+                    withConfigFile("/ingestion/config-validations-mix.yml") {
+
+                        // Checking the validation stamps
+
+                        checkValidationStampExists(this, "unit-tests") { vs ->
+                            assertDataType(vs) {
+                                assertEquals(
+                                    TestSummaryValidationDataType::class.java.name,
+                                    it.descriptor.id
+                                )
+                                assertIs<TestSummaryValidationConfig>(it.config) { config ->
+                                    assertTrue(config.warningIfSkipped, "Configuration is OK")
+                                }
+                            }
+                        }
+                        checkValidationStampExists(this, "e2e-tests") { vs ->
+                            assertNoDataType(vs)
+                        }
+                        checkValidationStampExists(this, "sonarqube") { vs ->
+                            assertNoDataType(vs)
+                        }
+
+                        // Checking the promotion levels
+
+                        checkPromotionLevelExists(this, "IRON") { iron ->
+                            checkAutoPromotion(iron) {
+                                assertEquals(
+                                    setOf("unit-tests", "e2e-tests"),
+                                    it.validationStamps.map { vs -> vs.name }.toSet()
+                                )
+                                assertTrue(it.promotionLevels.isEmpty(), "No promotion")
+                            }
+                        }
+
+                        checkPromotionLevelExists(this, "SILVER") { silver ->
+                            checkAutoPromotion(silver) {
+                                assertEquals(
+                                    setOf("sonarqube"),
+                                    it.validationStamps.map { vs -> vs.name }.toSet()
+                                )
+                                assertEquals(
+                                    setOf("SILVER"),
+                                    it.promotionLevels.map { pl -> pl.name }.toSet()
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkAutoPromotion(promotionLevel: PromotionLevel, code: (AutoPromotionProperty) -> Unit) {
+        val property: AutoPromotionProperty? = getProperty(promotionLevel, AutoPromotionPropertyType::class.java)
+        assertNotNull(property, "Auto promotion has been set on $promotionLevel") {
+            code(it)
+        }
+    }
+
+    private fun checkPromotionLevelExists(branch: Branch, name: String, code: (PromotionLevel) -> Unit) {
+        val pl = structureService.getPromotionLevelListForBranch(branch.id).firstOrNull { it.name == name }
+        assertNotNull(pl, "Promotion level $name has been created for $branch") {
+            code(it)
+        }
+    }
+
+    private fun assertDataType(vs: ValidationStamp, code: (ValidationDataTypeConfig<*>) -> Unit) {
+        assertNotNull(vs.dataType, "Data type for $vs") {
+            code(it)
+        }
+    }
+
+    private fun assertNoDataType(vs: ValidationStamp) {
+        assertNull(vs.dataType, "No data type for $vs")
+    }
+
+    fun checkValidationStampExists(branch: Branch, name: String, code: (vs: ValidationStamp) -> Unit) {
+        val vs = structureService.getValidationStampListForBranch(branch.id).firstOrNull { it.name == name }
+        assertNotNull(vs, "Validation stamp $name has been created for $branch") {
+            code(it)
+        }
+    }
+
+    fun Branch.withConfigFile(path: String, code: () -> Unit) {
+        val yaml = TestUtils.resourceString(path)
+        val config = ConfigParser.parseYaml(yaml)
+        every {
+            configLoaderService.loadConfig(
+                this@withConfigFile,
+                INGESTION_CONFIG_FILE_PATH
+            )
+        } returns config
+        configService.loadAndSaveConfig(
+            this,
+            INGESTION_CONFIG_FILE_PATH,
+        )
+    }
+
     fun withSavedConfiguration(
-        test: (config: IngestionConfig, branch: Branch) -> Unit
+        config: IngestionConfig = IngestionHookFixtures.sampleIngestionConfig(),
+        test: (config: IngestionConfig, branch: Branch) -> Unit,
     ) {
-        withLoadingConfiguration(existing = true) { config, branch ->
+        withLoadingConfiguration(config, existing = true) { _, branch ->
             configService.loadAndSaveConfig(
                 branch,
                 INGESTION_CONFIG_FILE_PATH,
@@ -92,13 +287,13 @@ class ConfigServiceIT : AbstractIngestionTestJUnit4Support() {
     }
 
     fun withLoadingConfiguration(
+        config: IngestionConfig = IngestionHookFixtures.sampleIngestionConfig(),
         existing: Boolean,
-        test: (config: IngestionConfig, branch: Branch) -> Unit
+        test: (config: IngestionConfig, branch: Branch) -> Unit,
     ) {
         asAdmin {
             project {
                 branch {
-                    val config = IngestionHookFixtures.sampleIngestionConfig()
                     if (existing) {
                         every {
                             configLoaderService.loadConfig(
