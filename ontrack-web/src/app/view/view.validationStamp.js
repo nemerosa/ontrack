e762e79ae45696b324f81b4bb09b30a0118d34d7
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-05-12 09:47:18 +0200

    #979 Second chart example (stability)

--- ontrack-web/src/app/view/view.validationStamp.js
+++ ontrack-web/src/app/view/view.validationStamp.js
@@ -245,97 +245,97 @@ angular.module('ot.view.validationStamp', [
             }
         };
 
-        // Graph data to inject into the options
-        const chartData = {
-            categories: [],
-            dates: [],
-            data: {
-                mean: [],
-                percentile90: [],
-                maximum: []
-            }
-        };
-
-        // Base options
-        const options = {
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'cross',
-                    crossStyle: {
-                        color: '#999'
-                    }
-                }
-            },
-            toolbox: {
-                feature: {
-                    dataView: { show: true, readOnly: true },
-                    // magicType: { show: true, type: ['line', 'bar'] },
-                    // restore: { show: true },
-                    saveAsImage: { show: true }
-                }
-            },
-            legend: {
-                data: chartData.categories
-            },
-            xAxis: [
-                {
-                    type: 'category',
-                    data: chartData.dates,
-                    axisPointer: {
-                        type: 'shadow'
-                    },
-                    axisLabel: {
-                        rotate: 45
-                    }
-                }
-            ],
-            yAxis: [
-                {
-                    type: 'value',
-                    name: 'Duration',
-                    min: 0,
-                    axisLabel: {
-                        formatter: '{value} s'
-                    }
-                }
-            ],
-            series: [
-                {
-                    name: 'Mean',
-                    type: 'bar',
-                    tooltip: {
-                        valueFormatter: function (value) {
-                            return value + ' s';
-                        }
-                    },
-                    data: chartData.data.mean
-                },
-                {
-                    name: '90th Percentile',
-                    type: 'line',
-                    tooltip: {
-                        valueFormatter: function (value) {
-                            return value + ' s';
-                        }
-                    },
-                    data: chartData.data.percentile90
-                },
-                {
-                    name: 'Maximum',
-                    type: 'line',
-                    tooltip: {
-                        valueFormatter: function (value) {
-                            return value + ' s';
-                        }
-                    },
-                    data: chartData.data.maximum
-                }
-            ]
-        };
-
         // Duration graph
         $scope.durationOptions = () => {
+            // Graph data to inject into the options
+            const chartData = {
+                categories: [],
+                dates: [],
+                data: {
+                    mean: [],
+                    percentile90: [],
+                    maximum: []
+                }
+            };
+
+            // Base options
+            const options = {
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'cross',
+                        crossStyle: {
+                            color: '#999'
+                        }
+                    }
+                },
+                toolbox: {
+                    feature: {
+                        dataView: { show: true, readOnly: true },
+                        // magicType: { show: true, type: ['line', 'bar'] },
+                        // restore: { show: true },
+                        saveAsImage: { show: true }
+                    }
+                },
+                legend: {
+                    data: chartData.categories
+                },
+                xAxis: [
+                    {
+                        type: 'category',
+                        data: chartData.dates,
+                        axisPointer: {
+                            type: 'shadow'
+                        },
+                        axisLabel: {
+                            rotate: 45
+                        }
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value',
+                        name: 'Duration',
+                        min: 0,
+                        axisLabel: {
+                            formatter: '{value} s'
+                        }
+                    }
+                ],
+                series: [
+                    {
+                        name: 'Mean',
+                        type: 'bar',
+                        tooltip: {
+                            valueFormatter: function (value) {
+                                return value + ' s';
+                            }
+                        },
+                        data: chartData.data.mean
+                    },
+                    {
+                        name: '90th Percentile',
+                        type: 'line',
+                        tooltip: {
+                            valueFormatter: function (value) {
+                                return value + ' s';
+                            }
+                        },
+                        data: chartData.data.percentile90
+                    },
+                    {
+                        name: 'Maximum',
+                        type: 'line',
+                        tooltip: {
+                            valueFormatter: function (value) {
+                                return value + ' s';
+                            }
+                        },
+                        data: chartData.data.maximum
+                    }
+                ]
+            };
+
             const d = $q.defer();
 
             chartData.categories.length = 0;
@@ -370,6 +370,91 @@ angular.module('ot.view.validationStamp', [
             chartData.data.maximum.length = 0;
             chartData.data.maximum.push(...[
                 3.0, 6.2, 9.3, 34.5, 36.3, 80.2, 220.3, 223.4, 53.0, 36.5, 12.0, 6.2
+            ]);
+
+            d.resolve(options);
+            return d.promise;
+        };
+
+        // Stability graph
+        $scope.stabilityOptions = () => {
+            // Graph data to inject into the options
+            const chartData = {
+                dates: [],
+                data: {
+                    value: []
+                }
+            };
+
+            // Base options
+            const options = {
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'cross',
+                        crossStyle: {
+                            color: '#999'
+                        }
+                    }
+                },
+                toolbox: {
+                    feature: {
+                        dataView: { show: true, readOnly: true },
+                        // magicType: { show: true, type: ['line', 'bar'] },
+                        // restore: { show: true },
+                        saveAsImage: { show: true }
+                    }
+                },
+                xAxis: [
+                    {
+                        type: 'category',
+                        data: chartData.dates,
+                        axisPointer: {
+                            type: 'shadow'
+                        },
+                        axisLabel: {
+                            rotate: 45
+                        }
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value',
+                        name: '% of success',
+                        min: 0,
+                        max: 100
+                    }
+                ],
+                series: [
+                    {
+                        name: 'Value',
+                        type: 'bar',
+                        data: chartData.data.value
+                    }
+                ]
+            };
+
+            const d = $q.defer();
+
+            chartData.dates.length = 0;
+            chartData.dates.push(...[
+                '2022-02-28',
+                '2022-03-07',
+                '2022-03-14',
+                '2022-03-21',
+                '2022-03-28',
+                '2022-04-04',
+                '2022-04-11',
+                '2022-04-18',
+                '2022-04-25',
+                '2022-05-02',
+                '2022-05-09',
+                '2022-05-16'
+            ]);
+
+            chartData.data.value.length = 0;
+            chartData.data.value.push(...[
+                1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 100, 100
             ]);
 
             d.resolve(options);
