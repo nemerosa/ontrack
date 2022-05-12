3219132e4386e0d265b701b5d078d02a5ba16f26
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-05-12 09:26:58 +0200

    #979 Setup of the options for the durations

--- ontrack-web/src/app/view/view.validationStamp.js
+++ ontrack-web/src/app/view/view.validationStamp.js
@@ -249,22 +249,99 @@ angular.module('ot.view.validationStamp', [
         $scope.durationOptions = () => {
             const d = $q.defer();
             d.resolve({
-                legend: {},
-                tooltip: {},
-                dataset: {
-                    dimensions: ['product', '2015', '2016', '2017'],
-                    source: [
-                        { product: 'Matcha Latte', 2015: 43.3, 2016: 85.8, 2017: 93.7 },
-                        { product: 'Milk Tea', 2015: 83.1, 2016: 73.4, 2017: 55.1 },
-                        { product: 'Cheese Cocoa', 2015: 86.4, 2016: 65.2, 2017: 82.5 },
-                        { product: 'Walnut Brownie', 2015: 72.4, 2016: 53.9, 2017: 39.1 }
-                    ]
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'cross',
+                        crossStyle: {
+                            color: '#999'
+                        }
+                    }
                 },
-                xAxis: { type: 'category' },
-                yAxis: {},
-                // Declare several bar series, each will be mapped
-                // to a column of dataset.source by default.
-                series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }]
+                toolbox: {
+                    feature: {
+                        dataView: { show: true, readOnly: true },
+                        // magicType: { show: true, type: ['line', 'bar'] },
+                        // restore: { show: true },
+                        saveAsImage: { show: true }
+                    }
+                },
+                legend: {
+                    data: ['Mean', '90th Percentile', 'Maximum']
+                },
+                xAxis: [
+                    {
+                        type: 'category',
+                        data: [
+                            '2022-02-28',
+                            '2022-03-07',
+                            '2022-03-14',
+                            '2022-03-21',
+                            '2022-03-28',
+                            '2022-04-04',
+                            '2022-04-11',
+                            '2022-04-18',
+                            '2022-04-25',
+                            '2022-05-02',
+                            '2022-05-09',
+                            '2022-05-16'
+                        ],
+                        axisPointer: {
+                            type: 'shadow'
+                        },
+                        axisLabel: {
+                            rotate: 45
+                        }
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value',
+                        name: 'Duration',
+                        min: 0,
+                        max: 250,
+                        interval: 50,
+                        axisLabel: {
+                            formatter: '{value} s'
+                        }
+                    }
+                ],
+                series: [
+                    {
+                        name: 'Mean',
+                        type: 'bar',
+                        tooltip: {
+                            valueFormatter: function (value) {
+                                return value + ' s';
+                            }
+                        },
+                        data: [
+                            2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3
+                        ]
+                    },
+                    {
+                        name: '90th Percentile',
+                        type: 'line',
+                        tooltip: {
+                            valueFormatter: function (value) {
+                                return value + ' s';
+                            }
+                        },
+                        data: [
+                            2.6, 5.9, 9.0, 26.4, 28.7, 78.7, 175.6, 182.2, 48.7, 24.8, 7.0, 5.3
+                        ]
+                    },
+                    {
+                        name: 'Maximum',
+                        type: 'line',
+                        tooltip: {
+                            valueFormatter: function (value) {
+                                return value + ' s';
+                            }
+                        },
+                        data: [3.0, 6.2, 9.3, 34.5, 36.3, 80.2, 220.3, 223.4, 53.0, 36.5, 12.0, 6.2]
+                    }
+                ]
             });
             return d.promise;
         };
