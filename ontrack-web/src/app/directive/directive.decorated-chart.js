21ccf3c21da98fbaec90c7356f6987ac30be679b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-05-21 12:38:25 +0200

    #980 One component only

--- ontrack-web/src/app/directive/directive.decorated-chart.js
+++ ontrack-web/src/app/directive/directive.decorated-chart.js
@@ -8,7 +8,7 @@ angular.module('ot.directive.decorated-chart', [
             scope: {
                 // Title of the chart
                 title: '@',
-                // Reference to the chart
+                // Reference to the chart, as being created by otChartService
                 chart: '=',
                 // Option to edit the chart options
                 chartOptions: '='
@@ -18,6 +18,37 @@ angular.module('ot.directive.decorated-chart', [
                 const domChart = element[0].children[1].children[0];
 
                 const eChart = echarts.init(domChart);
+
+                let loaded = false;
+
+                // Loading the data
+                const loadData = () => {
+                    eChart.showLoading('default', { text: 'Loading data...' });
+                    scope.chart.run().then(options => {
+                        options = options || {};
+                        if (options.forceClear) eChart.clear();
+                        if (options.series && options.series.length) {
+                            eChart.hideLoading();
+                            eChart.setOption(options, options.notMerge);
+                            eChart.resize();
+                        } else {
+                            eChart.showLoading('default', options.errorMsg || { text: 'No data' });
+                        }
+                        loaded = true;
+                    });
+                };
+
+                // Watching the general chart options
+                scope.$watch("chart.chartOptions", (newVal, oldVal) => {
+                    if (scope.chart.chartOptions) {
+                        if (loaded) {
+                            loadData();
+                        }
+                    }
+                }, /* objectEquality */ true);
+
+                // Loads the data on first call
+                loadData();
             }
         };
     })
