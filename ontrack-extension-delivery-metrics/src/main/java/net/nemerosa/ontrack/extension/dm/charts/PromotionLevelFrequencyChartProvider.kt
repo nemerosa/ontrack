e68cff0ea1ec27cd900da6a24c17e976cac58fe5
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-05-22 12:08:16 +0200

    #980 Performance: using the helper

--- ontrack-extension-delivery-metrics/src/main/java/net/nemerosa/ontrack/extension/dm/charts/PromotionLevelFrequencyChartProvider.kt
+++ ontrack-extension-delivery-metrics/src/main/java/net/nemerosa/ontrack/extension/dm/charts/PromotionLevelFrequencyChartProvider.kt
@@ -1,30 +1,45 @@
 package net.nemerosa.ontrack.extension.dm.charts
 
+import com.fasterxml.jackson.databind.JsonNode
+import net.nemerosa.ontrack.extension.chart.ChartProvider
 import net.nemerosa.ontrack.extension.chart.GetChartOptions
 import net.nemerosa.ontrack.extension.chart.support.CountChart
 import net.nemerosa.ontrack.extension.chart.support.CountChartItemData
-import net.nemerosa.ontrack.model.structure.PromotionRun
-import net.nemerosa.ontrack.model.structure.StructureService
+import net.nemerosa.ontrack.extension.dm.data.EndToEndPromotionFilter
+import net.nemerosa.ontrack.extension.dm.data.EndToEndPromotionsHelper
+import net.nemerosa.ontrack.json.parse
 import org.springframework.stereotype.Component
 
 @Component
 class PromotionLevelFrequencyChartProvider(
-    structureService: StructureService,
-) : AbstractPromotionLevelChartProvider<CountChart>(
-    structureService
-) {
+    private val endToEndPromotionsHelper: EndToEndPromotionsHelper,
+) : ChartProvider<PromotionLevelChartParameters, CountChart> {
 
     override val name: String = "promotion-level-frequency"
 
-    override fun getChart(runs: List<PromotionRun>, options: GetChartOptions): CountChart =
-        CountChart.compute(
-            items = runs.map { run ->
-                CountChartItemData(
-                    timestamp = run.signature.time,
+    override fun parseParameters(data: JsonNode): PromotionLevelChartParameters = data.parse()
+
+    override fun getChart(options: GetChartOptions, parameters: PromotionLevelChartParameters): CountChart {
+        val filter = EndToEndPromotionFilter(
+            maxDepth = 1,
+            promotionId = parameters.id,
+            afterTime = options.actualInterval.start,
+            beforeTime = options.actualInterval.end,
+        )
+        val items = mutableListOf<CountChartItemData>()
+        endToEndPromotionsHelper.forEachEndToEndPromotionRecord(filter) { record ->
+            val promotionCreation = record.ref.promotionCreation
+            if (promotionCreation != null) {
+                items += CountChartItemData(
+                    timestamp = promotionCreation
                 )
-            },
+            }
+        }
+        return CountChart.compute(
+            items,
             interval = options.actualInterval,
             period = options.period,
         )
+    }
 
 }
