42be1d3ee95f62b192cc8ea53cff387b67807d7b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-05-22 12:04:31 +0200

    #980 Performance: using the helper

--- ontrack-extension-delivery-metrics/src/main/java/net/nemerosa/ontrack/extension/dm/charts/PromotionLevelLeadTimeChartProvider.kt
+++ ontrack-extension-delivery-metrics/src/main/java/net/nemerosa/ontrack/extension/dm/charts/PromotionLevelLeadTimeChartProvider.kt
@@ -1,35 +1,49 @@
 package net.nemerosa.ontrack.extension.dm.charts
 
+import com.fasterxml.jackson.databind.JsonNode
+import net.nemerosa.ontrack.extension.chart.ChartProvider
 import net.nemerosa.ontrack.extension.chart.GetChartOptions
 import net.nemerosa.ontrack.extension.chart.support.DurationChart
 import net.nemerosa.ontrack.extension.chart.support.DurationChartItemData
-import net.nemerosa.ontrack.model.structure.PromotionRun
-import net.nemerosa.ontrack.model.structure.StructureService
+import net.nemerosa.ontrack.extension.dm.data.EndToEndPromotionFilter
+import net.nemerosa.ontrack.extension.dm.data.EndToEndPromotionsHelper
+import net.nemerosa.ontrack.json.parse
 import org.springframework.stereotype.Component
 import java.time.Duration
 
 @Component
 class PromotionLevelLeadTimeChartProvider(
-    structureService: StructureService,
-) : AbstractPromotionLevelChartProvider<DurationChart>(
-    structureService
-) {
+    private val endToEndPromotionsHelper: EndToEndPromotionsHelper,
+) : ChartProvider<PromotionLevelChartParameters, DurationChart> {
 
     override val name: String = "promotion-level-lead-time"
 
-    override fun getChart(runs: List<PromotionRun>, options: GetChartOptions): DurationChart =
-        DurationChart.compute(
-            items = runs.map { run ->
-                // From the time of the build to the promotion creation
-                val duration = Duration.between(run.build.signature.time, run.signature.time).toSeconds()
-                DurationChartItemData(
-                    // Time of the build
-                    timestamp = run.build.signature.time,
-                    value = duration.toDouble()
+    override fun parseParameters(data: JsonNode): PromotionLevelChartParameters = data.parse()
+
+    override fun getChart(options: GetChartOptions, parameters: PromotionLevelChartParameters): DurationChart {
+        val filter = EndToEndPromotionFilter(
+            maxDepth = 1,
+            promotionId = parameters.id,
+            afterTime = options.actualInterval.start,
+            beforeTime = options.actualInterval.end,
+        )
+        val items = mutableListOf<DurationChartItemData>()
+        endToEndPromotionsHelper.forEachEndToEndPromotionRecord(filter) { record ->
+            val buildCreation = record.ref.buildCreation
+            val promotionCreation = record.ref.promotionCreation
+            if (promotionCreation != null) {
+                val duration = Duration.between(buildCreation, promotionCreation).toSeconds()
+                items += DurationChartItemData(
+                    timestamp = buildCreation,
+                    value = duration.toDouble(),
                 )
-            },
+            }
+        }
+        return DurationChart.compute(
+            items,
             interval = options.actualInterval,
             period = options.period,
         )
+    }
 
 }
