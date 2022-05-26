6443c963b32eb68f2ba0777473da7396b8813553
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-05-26 14:11:18 +0200

    #983 Fixing the schema

--- ontrack-extension-github/src/main/java/net/nemerosa/ontrack/extension/github/ingestion/extensions/links/GitHubIngestionBuildLinksMutations.kt
+++ ontrack-extension-github/src/main/java/net/nemerosa/ontrack/extension/github/ingestion/extensions/links/GitHubIngestionBuildLinksMutations.kt
@@ -1,6 +1,10 @@
 package net.nemerosa.ontrack.extension.github.ingestion.extensions.links
 
+import graphql.schema.GraphQLObjectType
+import net.nemerosa.ontrack.graphql.schema.GQLType
+import net.nemerosa.ontrack.graphql.schema.GQLTypeCache
 import net.nemerosa.ontrack.graphql.schema.Mutation
+import net.nemerosa.ontrack.graphql.support.GraphQLBeanConverter
 import net.nemerosa.ontrack.graphql.support.TypedMutationProvider
 import net.nemerosa.ontrack.model.annotations.APIDescription
 import org.springframework.stereotype.Component
@@ -17,18 +21,18 @@ class GitHubIngestionBuildLinksMutations(
         /**
          * Getting the build by run ID
          */
-//        simpleMutation(
-//            name = "gitHubIngestionBuildLinksByRunId",
-//            description = "Sets some links on a build identified using a GHA workflow run ID",
-//            input = GitHubIngestionLinksByRunIdInput::class,
-//            outputName = "payload",
-//            outputType = GitHubIngestionBuildLinksOutput::class,
-//            outputDescription = "Payload being processed in the background"
-//        ) { input ->
-//            GitHubIngestionBuildLinksOutput(
-//                ingestionLinksService.ingestLinks(input).toString()
-//            )
-//        },
+        simpleMutation(
+            name = "gitHubIngestionBuildLinksByRunId",
+            description = "Sets some links on a build identified using a GHA workflow run ID",
+            input = GitHubIngestionLinksByRunIdInput::class,
+            outputName = "payload",
+            outputType = GitHubIngestionBuildLinksOutput::class,
+            outputDescription = "Payload being processed in the background"
+        ) { input ->
+            GitHubIngestionBuildLinksOutput(
+                ingestionLinksService.ingestLinks(input).toString()
+            )
+        },
         /**
          * TODO Getting the build by build name
          */
@@ -66,3 +70,12 @@ data class GitHubIngestionBuildLinksOutput(
     @APIDescription("UUID of the payload being processed in the background")
     val uuid: String,
 )
+
+@Component
+class GQLTypeGitHubIngestionBuildLinksOutput : GQLType {
+    override fun getTypeName(): String = GitHubIngestionBuildLinksOutput::class.java.simpleName
+
+    override fun createType(cache: GQLTypeCache): GraphQLObjectType =
+        GraphQLBeanConverter.asObjectType(GitHubIngestionBuildLinksOutput::class, cache)
+
+}
