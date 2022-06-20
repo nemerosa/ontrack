171a581c2e62e812a3ea422fe9387c3507cefc5c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-06-20 13:42:44 +0200

    #997 Cleanup of run info metrics

--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/RunInfoServiceImpl.kt
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/RunInfoServiceImpl.kt
@@ -1,8 +1,7 @@
 package net.nemerosa.ontrack.service
 
-import io.micrometer.core.instrument.MeterRegistry
-import io.micrometer.core.instrument.Tag
 import net.nemerosa.ontrack.model.Ack
+import net.nemerosa.ontrack.model.metrics.MetricsExportService
 import net.nemerosa.ontrack.model.security.ApplicationManagement
 import net.nemerosa.ontrack.model.security.ProjectEdit
 import net.nemerosa.ontrack.model.security.ProjectView
@@ -13,16 +12,15 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.util.concurrent.TimeUnit
 
 @Service
 @Transactional
 class RunInfoServiceImpl(
-        private val runInfoRepository: RunInfoRepository,
-        private val structureService: StructureService,
-        private val securityService: SecurityService,
-        private val meterRegistry: MeterRegistry,
-        private val runInfoListeners: List<RunInfoListener>
+    private val runInfoRepository: RunInfoRepository,
+    private val structureService: StructureService,
+    private val securityService: SecurityService,
+    private val metricsExportService: MetricsExportService,
+    private val runInfoListeners: List<RunInfoListener>,
 ) : RunInfoService {
 
     private val logger: Logger = LoggerFactory.getLogger(RunInfoService::class.java)
@@ -34,27 +32,26 @@ class RunInfoServiceImpl(
     override fun getRunInfo(entity: RunnableEntity): RunInfo? {
         securityService.checkProjectFunction(entity, ProjectView::class.java)
         return runInfoRepository.getRunInfo(
-                entity.runnableEntityType,
-                entity.id()
+            entity.runnableEntityType,
+            entity.id()
         )
     }
 
     override fun setRunInfo(entity: RunnableEntity, input: RunInfoInput): RunInfo {
         securityService.checkProjectFunction(entity, entity.runnableEntityType.projectFunction.java)
         val runInfo = runInfoRepository.setRunInfo(
-                entity.runnableEntityType,
-                entity.id(),
-                input,
-                securityService.currentSignature
+            entity.runnableEntityType,
+            entity.id(),
+            input,
+            securityService.currentSignature
         )
         val time = input.runTime
         if (time != null) {
-            meterRegistry.timer(
-                    "ontrack_run_${entity.runnableEntityType.name}_time_seconds",
-                    entity.runMetricTags.map { (name, value) -> Tag.of(name, value) }
-            ).record(
-                    time.toLong(),
-                    TimeUnit.SECONDS
+            metricsExportService.exportMetrics(
+                metric = "ontrack_run_${entity.runnableEntityType.name}_time_seconds",
+                tags = entity.runMetricTags,
+                fields = mapOf("value" to time.toDouble()),
+                timestamp = entity.signature.time,
             )
         }
         // Listeners
@@ -66,8 +63,8 @@ class RunInfoServiceImpl(
     override fun deleteRunInfo(runnableEntity: RunnableEntity): Ack {
         securityService.checkProjectFunction(runnableEntity, ProjectEdit::class.java)
         return runInfoRepository.deleteRunInfo(
-                runnableEntity.runnableEntityType,
-                runnableEntity.id()
+            runnableEntity.runnableEntityType,
+            runnableEntity.id()
         )
     }
 
