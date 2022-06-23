c27d9a48f990b45408c548faf8b3a7517843d7da
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-06-23 15:34:46 +0200

    #1000 E2E Promotion Metrics Exports settings Casc

---
+++ ontrack-extension-delivery-metrics/src/main/java/net/nemerosa/ontrack/extension/dm/export/EndToEndPromotionMetricsExportSettingsCascContext.kt
@@ -0,0 +1,17 @@
+package net.nemerosa.ontrack.extension.dm.export
+
+import net.nemerosa.ontrack.extension.casc.context.settings.AbstractSubSettingsContext
+import net.nemerosa.ontrack.model.settings.CachedSettingsService
+import net.nemerosa.ontrack.model.settings.SettingsManagerService
+import org.springframework.stereotype.Component
+
+@Component
+class EndToEndPromotionMetricsExportSettingsCascContext(
+    settingsManagerService: SettingsManagerService,
+    cachedSettingsService: CachedSettingsService,
+) : AbstractSubSettingsContext<EndToEndPromotionMetricsExportSettings>(
+    "e2e-promotion-metrics",
+    EndToEndPromotionMetricsExportSettings::class,
+    settingsManagerService,
+    cachedSettingsService
+)
---
+++ ontrack-extension-delivery-metrics/src/test/java/net/nemerosa/ontrack/extension/dm/export/EndToEndPromotionMetricsExportSettingsCascContextIT.kt
@@ -0,0 +1,37 @@
+package net.nemerosa.ontrack.extension.dm.export
+
+import net.nemerosa.ontrack.extension.casc.AbstractCascTestSupport
+import net.nemerosa.ontrack.model.support.SettingsRepository
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+internal class EndToEndPromotionMetricsExportSettingsCascContextIT : AbstractCascTestSupport() {
+
+    @Autowired
+    private lateinit var settingsRepository: SettingsRepository
+
+    @Test
+    fun `End to end promotion metrics export settings using CasC`() {
+        withSettings<EndToEndPromotionMetricsExportSettings> {
+            settingsRepository.deleteAll(EndToEndPromotionMetricsExportSettings::class.java)
+            casc(
+                """
+                ontrack:
+                    config:
+                        settings:
+                            e2e-promotion-metrics:
+                                enabled: true
+                                branches: "develop|main|master|release-.*|maintenance-.*"
+                                pastDays: 14
+            """.trimIndent()
+            )
+            val settings = settingsService.getCachedSettings(EndToEndPromotionMetricsExportSettings::class.java)
+            assertTrue(settings.enabled)
+            assertEquals("develop|main|master|release-.*|maintenance-.*", settings.branches)
+            assertEquals(14, settings.pastDays)
+        }
+    }
+
+}
