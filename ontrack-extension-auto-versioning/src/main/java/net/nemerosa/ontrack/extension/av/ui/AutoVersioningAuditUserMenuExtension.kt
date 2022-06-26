c5cdc28acea493b229694638d2c967fc6a1de820
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-06-26 17:59:11 +0200

    #990 Audit UI ~> user menus

---
+++ ontrack-extension-auto-versioning/src/main/java/net/nemerosa/ontrack/extension/av/ui/AutoVersioningAuditUserMenuExtension.kt
@@ -0,0 +1,23 @@
+package net.nemerosa.ontrack.extension.av.ui
+
+import net.nemerosa.ontrack.extension.api.UserMenuExtension
+import net.nemerosa.ontrack.extension.av.AutoVersioningExtensionFeature
+import net.nemerosa.ontrack.extension.support.AbstractExtension
+import net.nemerosa.ontrack.model.security.GlobalFunction
+import net.nemerosa.ontrack.model.support.Action
+import org.springframework.stereotype.Component
+
+@Component
+class AutoVersioningAuditUserMenuExtension(
+    extensionFeature: AutoVersioningExtensionFeature,
+) : AbstractExtension(extensionFeature), UserMenuExtension {
+
+    /**
+     * All authenticated users are allowed
+     */
+    override fun getGlobalFunction(): Class<out GlobalFunction>? = null
+
+    override fun getAction(): Action =
+        Action.of("auto-versioning-audit-global", "Auto versioning audit", "auto-versioning-audit/global")
+
+}
---
+++ ontrack-extension-auto-versioning/src/main/java/net/nemerosa/ontrack/extension/av/ui/ProjectAutoVersioningAuditActionExtension.kt
@@ -0,0 +1,30 @@
+package net.nemerosa.ontrack.extension.av.ui
+
+import net.nemerosa.ontrack.extension.api.ProjectEntityActionExtension
+import net.nemerosa.ontrack.extension.av.AutoVersioningExtensionFeature
+import net.nemerosa.ontrack.extension.support.AbstractExtension
+import net.nemerosa.ontrack.model.structure.Project
+import net.nemerosa.ontrack.model.structure.ProjectEntity
+import net.nemerosa.ontrack.model.support.Action
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class ProjectAutoVersioningAuditActionExtension(
+    extensionFeature: AutoVersioningExtensionFeature,
+) : AbstractExtension(extensionFeature), ProjectEntityActionExtension {
+
+    override fun getAction(entity: ProjectEntity): Optional<Action> {
+        return if (entity is Project) {
+            Optional.of(
+                Action.of(
+                    "auto-versioning-audit-project",
+                    "Auto versioning audit (target)",
+                    "auto-versioning-audit/project/${entity.name}"
+                )
+            )
+        } else {
+            Optional.empty()
+        }
+    }
+}
