477e511a36eb472603dadff8dc28082f53f99410
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-08-25 17:20:04 +0200

    #1016 License API

--- ontrack-extension-license/src/main/java/net/nemerosa/ontrack/extension/license/ui/LicenseController.kt
+++ ontrack-extension-license/src/main/java/net/nemerosa/ontrack/extension/license/ui/LicenseController.kt
@@ -1,26 +1,20 @@
 package net.nemerosa.ontrack.extension.license.ui
 
-import net.nemerosa.ontrack.extension.license.License
 import net.nemerosa.ontrack.extension.license.LicenseService
-import net.nemerosa.ontrack.ui.controller.AbstractResourceController
-import net.nemerosa.ontrack.ui.resource.Resource
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on
 
 /**
  * Gets access to the license.
  */
-@RestController("/extension/license")
+@RestController
+@RequestMapping("/extension/license")
 class LicenseController(
     private val licenseService: LicenseService,
-) : AbstractResourceController() {
+) {
 
     @GetMapping("")
-    fun getLicense(): Resource<License> =
-        Resource.of(
-            licenseService.license,
-            uri(on(LicenseController::class.java).getLicense())
-        )
+    fun getLicense(): LicenseResponse? = LicenseResponse(licenseService.license)
 
 }
---
+++ ontrack-extension-license/src/main/java/net/nemerosa/ontrack/extension/license/ui/LicenseResponse.kt
@@ -0,0 +1,7 @@
+package net.nemerosa.ontrack.extension.license.ui
+
+import net.nemerosa.ontrack.extension.license.License
+
+data class LicenseResponse(
+    val licence: License?,
+)
