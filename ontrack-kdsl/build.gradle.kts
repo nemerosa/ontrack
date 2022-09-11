5b808c6b775c2ee78b3a5a35e8371ba2899f9df1
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-09-11 13:05:57 +0200

    #995 Spring Boot 2.7.3 and Kotlin 1.7.10

--- ontrack-kdsl/build.gradle.kts
+++ ontrack-kdsl/build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
     `java-library`
-    id("com.apollographql.apollo").version("2.5.11")
+    id("com.apollographql.apollo").version("2.5.13")
 }
 
 dependencies {
     api(project(":ontrack-json"))
 
-    api("com.apollographql.apollo:apollo-runtime:2.5.11")
-    api("com.apollographql.apollo:apollo-coroutines-support:2.5.11")
+    api("com.apollographql.apollo:apollo-runtime:2.5.13")
+    api("com.apollographql.apollo:apollo-coroutines-support:2.5.13")
     implementation("org.apache.httpcomponents:httpclient:4.5.13")
     implementation("org.springframework.boot:spring-boot-starter-web")
 }
