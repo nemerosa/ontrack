239d64dc32078ad4e535c902a8abbdcf0232d50d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-09-11 14:07:15 +0200

    #995 Adapting security config for Spring Boot 2.7

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/support/WebSecurityConfig.kt
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/support/WebSecurityConfig.kt
@@ -4,12 +4,14 @@ import net.nemerosa.ontrack.extension.api.UISecurityExtension
 import net.nemerosa.ontrack.model.structure.TokensService
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
+import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.web.servlet.invoke
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 
@@ -20,96 +22,98 @@ class WebSecurityConfig {
     /**
      * Management end points are accessible on a separate port without any authentication needed
      */
-    @Configuration
+    @Bean
     @Order(1)
     @ConditionalOnWebApplication
-    class ActuatorWebSecurityConfigurationAdapter : WebSecurityConfigurerAdapter() {
-        override fun configure(http: HttpSecurity) {
-            http {
-                securityMatcher("/manage/**")
-                // Disables CSRF for the actuator calls
-                csrf {
-                    disable()
-                }
-                authorizeRequests {
-                    authorize(EndpointRequest.toAnyEndpoint(), permitAll)
-                }
+    fun actuatorWebSecurity(http: HttpSecurity): SecurityFilterChain {
+        http {
+            securityMatcher("/manage/**")
+            // Disables CSRF for the actuator calls
+            csrf {
+                disable()
+            }
+            authorizeRequests {
+                authorize(EndpointRequest.toAnyEndpoint(), permitAll)
             }
         }
+        return http.build()
     }
 
     /**
      * API login
      */
-    @Configuration
+    @Bean
     @Order(2)
     @ConditionalOnWebApplication
-    class ApiWebSecurityConfigurationAdapter(
-            private val tokensService: TokensService
-    ) : WebSecurityConfigurerAdapter() {
-        override fun configure(http: HttpSecurity) {
-            http {
-                securityMatcher("/rest/**")
-                securityMatcher("/graphql/**")
-                securityMatcher("/extension/**")
-                securityMatcher("/hook/secured/**")
-                // Disables CSRF for the API calls
-                csrf {
-                    disable()
-                }
-                // Requires authentication
-                authorizeRequests {
-                    authorize("/hook/secured/**", permitAll)
-                    authorize(anyRequest, authenticated)
-                }
-                // Requires BASIC authentication
-                httpBasic { }
-                // Token based authentication (for API only)
-                addFilterAt(TokenHeaderAuthenticationFilter(authenticationManager(), tokensService = tokensService), BasicAuthenticationFilter::class.java)
+    fun apiWebSecurity(
+        http: HttpSecurity,
+        tokensService: TokensService,
+        authenticationManager: AuthenticationManager,
+    ): SecurityFilterChain {
+        http {
+            securityMatcher("/rest/**")
+            securityMatcher("/graphql/**")
+            securityMatcher("/extension/**")
+            securityMatcher("/hook/secured/**")
+            // Disables CSRF for the API calls
+            csrf {
+                disable()
             }
+            // Requires authentication
+            authorizeRequests {
+                authorize("/hook/secured/**", permitAll)
+                authorize(anyRequest, authenticated)
+            }
+            // Requires BASIC authentication
+            httpBasic { }
+            // Token based authentication (for API only)
+            addFilterAt<BasicAuthenticationFilter>(
+                TokenHeaderAuthenticationFilter(authenticationManager, tokensService = tokensService),
+            )
         }
+        return http.build()
     }
 
     /**
      * Default UI login
      */
-    @Configuration
+    @Bean
     @ConditionalOnWebApplication
-    class UIWebSecurityConfigurerAdapter(
-            private val uiSecurityExtensions: List<UISecurityExtension>
-    ) : WebSecurityConfigurerAdapter() {
-        override fun configure(http: HttpSecurity) {
-            http {
-                // Enabling JS Cookies for CSRF protection (for AngularJS)
-                csrf {
-                    csrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse()
-                }
-                // Excludes assets and login page from authentication
-                authorizeRequests {
-                    authorize("/login/**", permitAll)
-                    authorize("/assets/**", permitAll)
-                    authorize("/favicon.ico", permitAll)
-                }
-                // Requires authentication always
-                authorizeRequests {
-                    authorize(anyRequest, authenticated)
-                }
-                // UI extensions
-                uiSecurityExtensions.forEach { extension ->
-                    extension.configure(this, LoginSuccessHandler())
-                }
-                // Using a form login
-                formLogin {
-                    loginPage = "/login"
-                    permitAll()
-                    authenticationSuccessHandler = LoginSuccessHandler()
-                }
-                // Logout setup
-                logout {
-                    logoutUrl = "/logout"
-                }
+    fun webSecurity(
+        http: HttpSecurity,
+        uiSecurityExtensions: List<UISecurityExtension>,
+    ): SecurityFilterChain {
+        http {
+            // Enabling JS Cookies for CSRF protection (for AngularJS)
+            csrf {
+                csrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse()
+            }
+            // Excludes assets and login page from authentication
+            authorizeRequests {
+                authorize("/login/**", permitAll)
+                authorize("/assets/**", permitAll)
+                authorize("/favicon.ico", permitAll)
+            }
+            // Requires authentication always
+            authorizeRequests {
+                authorize(anyRequest, authenticated)
+            }
+            // UI extensions
+            uiSecurityExtensions.forEach { extension ->
+                extension.configure(this, LoginSuccessHandler())
+            }
+            // Using a form login
+            formLogin {
+                loginPage = "/login"
+                permitAll()
+                authenticationSuccessHandler = LoginSuccessHandler()
+            }
+            // Logout setup
+            logout {
+                logoutUrl = "/logout"
             }
         }
+        return http.build()
     }
 
 }
