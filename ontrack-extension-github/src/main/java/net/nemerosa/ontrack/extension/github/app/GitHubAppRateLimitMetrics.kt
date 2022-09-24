16d71506677fee0a9e048e2389e2802aaca7dc2c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-09-24 14:43:30 +0200

    #1004 Scheduling and registration

--- ontrack-extension-github/src/main/java/net/nemerosa/ontrack/extension/github/app/GitHubAppRateLimitMetrics.kt
+++ ontrack-extension-github/src/main/java/net/nemerosa/ontrack/extension/github/app/GitHubAppRateLimitMetrics.kt
@@ -2,13 +2,15 @@ package net.nemerosa.ontrack.extension.github.app
 
 import io.micrometer.core.instrument.Gauge
 import io.micrometer.core.instrument.MeterRegistry
-import io.micrometer.core.instrument.binder.MeterBinder
 import net.nemerosa.ontrack.extension.github.GitHubConfigurationProperties
 import net.nemerosa.ontrack.extension.github.client.GitHubRateLimit
 import net.nemerosa.ontrack.extension.github.client.OntrackGitHubClientFactory
 import net.nemerosa.ontrack.extension.github.model.GitHubEngineConfiguration
 import net.nemerosa.ontrack.extension.github.service.GitHubConfigurationService
 import net.nemerosa.ontrack.model.support.ConfigurationServiceListener
+import net.nemerosa.ontrack.model.support.StartupService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
@@ -21,78 +23,78 @@ class GitHubAppRateLimitMetrics(
     private val gitHubConfigurationProperties: GitHubConfigurationProperties,
     private val gitHubConfigurationService: GitHubConfigurationService,
     private val gitHubClientFactory: OntrackGitHubClientFactory,
-) : MeterBinder, ConfigurationServiceListener<GitHubEngineConfiguration> {
+    private val meterRegistry: MeterRegistry,
+) : ConfigurationServiceListener<GitHubEngineConfiguration>, StartupService {
 
-    private var meterRegistry: MeterRegistry? = null
+    private val logger: Logger = LoggerFactory.getLogger(GitHubAppRateLimitMetrics::class.java)
 
-    override fun bindTo(registry: MeterRegistry) {
+    override fun getName(): String = "GitHub App Rate Limit metrics"
+
+    override fun startupOrder(): Int = StartupService.JOB_REGISTRATION
+
+    override fun start() {
+        gitHubConfigurationService.addConfigurationServiceListener(this)
         if (gitHubConfigurationProperties.metrics.enabled) {
             gitHubConfigurationService.configurations.forEach {
-                registerMetrics(registry, it)
+                registerMetrics(it)
             }
         }
     }
 
     override fun onNewConfiguration(configuration: GitHubEngineConfiguration) {
-        meterRegistry?.let {
-            registerMetrics(it, configuration)
-        }
+        registerMetrics(configuration)
     }
 
     override fun onDeletedConfiguration(configuration: GitHubEngineConfiguration) {
-        meterRegistry?.let {
-            unregisterMetric(it, configuration, RATE_LIMIT_CORE_LIMIT_METRIC)
-            unregisterMetric(it, configuration, RATE_LIMIT_CORE_REMAINING_METRIC)
-            unregisterMetric(it, configuration, RATE_LIMIT_CORE_USED_METRIC)
-            unregisterMetric(it, configuration, RATE_LIMIT_GRAPHQL_LIMIT_METRIC)
-            unregisterMetric(it, configuration, RATE_LIMIT_GRAPHQL_REMAINING_METRIC)
-            unregisterMetric(it, configuration, RATE_LIMIT_GRAPHQL_USED_METRIC)
-        }
+        logger.info("Unregistering metrics for configuration ${configuration.name}")
+        unregisterMetric(configuration, RATE_LIMIT_CORE_LIMIT_METRIC)
+        unregisterMetric(configuration, RATE_LIMIT_CORE_REMAINING_METRIC)
+        unregisterMetric(configuration, RATE_LIMIT_CORE_USED_METRIC)
+        unregisterMetric(configuration, RATE_LIMIT_GRAPHQL_LIMIT_METRIC)
+        unregisterMetric(configuration, RATE_LIMIT_GRAPHQL_REMAINING_METRIC)
+        unregisterMetric(configuration, RATE_LIMIT_GRAPHQL_USED_METRIC)
     }
 
-    private fun registerMetrics(registry: MeterRegistry, configuration: GitHubEngineConfiguration) {
+    private fun registerMetrics(configuration: GitHubEngineConfiguration) {
+
+        logger.info("Registering metrics for configuration ${configuration.name}")
+
         val rateLimitFn = {
             val client = gitHubClientFactory.create(configuration)
             client.getRateLimit()
         }
 
         registerMetric(
-            registry,
             configuration,
             rateLimitFn,
             RATE_LIMIT_CORE_LIMIT_METRIC
         ) { core.limit }
 
         registerMetric(
-            registry,
             configuration,
             rateLimitFn,
             RATE_LIMIT_CORE_REMAINING_METRIC
         ) { core.remaining }
 
         registerMetric(
-            registry,
             configuration,
             rateLimitFn,
             RATE_LIMIT_CORE_USED_METRIC
         ) { core.used }
 
         registerMetric(
-            registry,
             configuration,
             rateLimitFn,
             RATE_LIMIT_GRAPHQL_LIMIT_METRIC
         ) { graphql.limit }
 
         registerMetric(
-            registry,
             configuration,
             rateLimitFn,
             RATE_LIMIT_GRAPHQL_REMAINING_METRIC
         ) { graphql.remaining }
 
         registerMetric(
-            registry,
             configuration,
             rateLimitFn,
             RATE_LIMIT_GRAPHQL_USED_METRIC
@@ -100,7 +102,6 @@ class GitHubAppRateLimitMetrics(
     }
 
     private fun registerMetric(
-        registry: MeterRegistry,
         configuration: GitHubEngineConfiguration,
         rateLimitFn: () -> GitHubRateLimit?,
         metric: String,
@@ -111,12 +112,12 @@ class GitHubAppRateLimitMetrics(
             rateLimitFn
         ) {
             rateLimitFn()?.valueFn()?.toDouble() ?: 0.0
-        }.tag("configuration", configuration.name).register(registry)
+        }.tag("configuration", configuration.name).register(meterRegistry)
     }
 
-    private fun unregisterMetric(registry: MeterRegistry, configuration: GitHubEngineConfiguration, metric: String) {
-        registry.find(metric).tag("configuration", configuration.name).gauge()?.let {
-            registry.remove(it)
+    private fun unregisterMetric(configuration: GitHubEngineConfiguration, metric: String) {
+        meterRegistry.find(metric).tag("configuration", configuration.name).gauges().forEach {
+            meterRegistry.remove(it)
         }
     }
 
