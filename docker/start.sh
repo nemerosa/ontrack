3cf1331c23df851ec10b673adcad012dc152a547
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-10-28 13:19:10 +0200

    #1037 Removing extension support from the Docker image

--- docker/start.sh
+++ docker/start.sh
@@ -7,14 +7,12 @@ rm -rf /opt/ontrack/config \
     && cp -R /var/ontrack/conf /opt/ontrack/config
 
 # Traces
-echo "[START] EXTENSIONS_DIR = ${EXTENSIONS_DIR}"
 echo "[START] JAVA_OPTIONS   = ${JAVA_OPTIONS}"
 echo "[START] ONTRACK_ARGS   = ${ONTRACK_ARGS}"
 echo "[START] PROFILE        = ${PROFILE}"
 
 # Launching the application
 exec java \
-    -Dloader.path=${EXTENSIONS_DIR} \
     ${JAVA_OPTIONS} \
     -jar /opt/ontrack/ontrack.jar \
     "--spring.profiles.active=${PROFILE}" \
