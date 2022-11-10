3416d488e8b1dc8792078d0ee89850bf48b6d647
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-11-10 15:45:57 +0100

    #1048 Distinct suggested group names
    Taking the opportunity to convert a JUnit4 deprecated test

--- ontrack-service/src/test/java/net/nemerosa/ontrack/service/security/ProvidedGroupsServiceIT.kt
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/security/ProvidedGroupsServiceIT.kt
@@ -1,15 +1,15 @@
 package net.nemerosa.ontrack.service.security
 
-import net.nemerosa.ontrack.it.AbstractDSLTestJUnit4Support
+import net.nemerosa.ontrack.it.AbstractDSLTestSupport
 import net.nemerosa.ontrack.model.security.AuthenticationSource
 import net.nemerosa.ontrack.model.security.ProvidedGroupsService
 import net.nemerosa.ontrack.test.TestUtils.uid
-import org.junit.Test
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class ProvidedGroupsServiceIT : AbstractDSLTestJUnit4Support() {
+class ProvidedGroupsServiceIT : AbstractDSLTestSupport() {
 
     @Autowired
     private lateinit var providedGroupsService: ProvidedGroupsService
