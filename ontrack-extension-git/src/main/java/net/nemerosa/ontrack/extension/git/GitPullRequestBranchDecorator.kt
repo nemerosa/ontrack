f28227ee2e5fdf422cf1ea0b2d2b7f74099d4b28
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-12-19 14:28:43 +0100

    #1057 Only getting the PR info, not the full branch config

--- ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/GitPullRequestBranchDecorator.kt
+++ ontrack-extension-git/src/main/java/net/nemerosa/ontrack/extension/git/GitPullRequestBranchDecorator.kt
@@ -13,49 +13,46 @@ import java.util.*
 
 @Component
 class GitPullRequestBranchDecorator(
-        extensionFeature: GitExtensionFeature,
-        private val gitService: GitService
+    extensionFeature: GitExtensionFeature,
+    private val gitService: GitService,
 ) : AbstractExtension(extensionFeature), DecorationExtension<GitPullRequestDecoration> {
 
     override fun getDecorations(entity: ProjectEntity): List<Decoration<GitPullRequestDecoration>> =
-            if (entity is Branch) {
-                gitService.getBranchConfiguration(entity)
-                        ?.let { config ->
-                            config.pullRequest?.let { pr ->
-                                listOf(
-                                        Decoration.of(
-                                                this,
-                                                GitPullRequestDecoration(pr)
-                                        )
-                                )
-                            }
-                        }
-                        ?: emptyList()
-            } else {
-                emptyList()
-            }
+        if (entity is Branch && gitService.isBranchAPullRequest(entity)) {
+            val pr = gitService.getBranchAsPullRequest(entity)
+            pr?.let {
+                listOf(
+                    Decoration.of(
+                        this,
+                        GitPullRequestDecoration(it)
+                    )
+                )
+            } ?: emptyList()
+        } else {
+            emptyList()
+        }
 
     override fun getScope(): EnumSet<ProjectEntityType> = EnumSet.of(ProjectEntityType.BRANCH)
 }
 
 class GitPullRequestDecoration(
-        val isValid: Boolean,
-        val key: String,
-        val source: String,
-        val target: String,
-        val title: String,
-        val status: String,
-        val url: String
+    val isValid: Boolean,
+    val key: String,
+    val source: String,
+    val target: String,
+    val title: String,
+    val status: String,
+    val url: String,
 ) {
 
     constructor(pr: GitPullRequest) : this(
-            isValid = pr.isValid,
-            key = pr.key,
-            source = GitPullRequest.simpleBranchName(pr.source),
-            target = GitPullRequest.simpleBranchName(pr.target),
-            title = pr.title,
-            status = pr.status,
-            url = pr.url
+        isValid = pr.isValid,
+        key = pr.key,
+        source = GitPullRequest.simpleBranchName(pr.source),
+        target = GitPullRequest.simpleBranchName(pr.target),
+        title = pr.title,
+        status = pr.status,
+        url = pr.url
     )
 
 }
