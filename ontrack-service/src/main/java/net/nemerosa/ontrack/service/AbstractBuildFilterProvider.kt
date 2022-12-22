8e066a4a49e84c886c82dc6bb30ab7963dc9bc8b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-12-22 13:42:40 +0100

    #827 Kotlin translation

--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/AbstractBuildFilterProvider.kt
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/AbstractBuildFilterProvider.kt
@@ -1,39 +1,36 @@
-package net.nemerosa.ontrack.service;
+package net.nemerosa.ontrack.service
 
-import net.nemerosa.ontrack.model.buildfilter.BuildFilterForm;
-import net.nemerosa.ontrack.model.buildfilter.BuildFilterProvider;
-import net.nemerosa.ontrack.model.form.Form;
-import net.nemerosa.ontrack.model.structure.ID;
+import net.nemerosa.ontrack.model.buildfilter.BuildFilterForm
+import net.nemerosa.ontrack.model.buildfilter.BuildFilterProvider
+import net.nemerosa.ontrack.model.form.Form
+import net.nemerosa.ontrack.model.form.Form.Companion.create
+import net.nemerosa.ontrack.model.form.Form.Companion.defaultNameField
+import net.nemerosa.ontrack.model.structure.ID
 
-import static net.nemerosa.ontrack.model.form.Form.defaultNameField;
-
-public abstract class AbstractBuildFilterProvider<T> implements BuildFilterProvider<T> {
-
-    @Override
-    public BuildFilterForm newFilterForm(ID branchId) {
-        return new BuildFilterForm(
-                getClass(),
-                getName(),
-                isPredefined(),
-                rootForm().append(blankForm(branchId))
-        );
+abstract class AbstractBuildFilterProvider<T> : BuildFilterProvider<T> {
+    override fun newFilterForm(branchId: ID): BuildFilterForm {
+        return BuildFilterForm(
+            javaClass,
+            name,
+            isPredefined,
+            rootForm().append(blankForm(branchId))
+        )
     }
 
-    @Override
-    public BuildFilterForm getFilterForm(ID branchId, T data) {
-        return new BuildFilterForm(
-                getClass(),
-                getName(),
-                isPredefined(),
-                fill(rootForm().append(blankForm(branchId)), data)
-        );
+    override fun getFilterForm(branchId: ID, data: T): BuildFilterForm {
+        return BuildFilterForm(
+            javaClass,
+            name,
+            isPredefined,
+            fill(rootForm().append(blankForm(branchId)), data)
+        )
     }
 
-    protected abstract Form fill(Form form, T data);
+    protected abstract fun fill(form: Form, data: T): Form
 
-    private Form rootForm() {
-        return Form.create().with(defaultNameField().optional());
+    private fun rootForm(): Form {
+        return create().with(defaultNameField().optional())
     }
 
-    protected abstract Form blankForm(ID branchId);
+    protected abstract fun blankForm(branchId: ID): Form
 }
