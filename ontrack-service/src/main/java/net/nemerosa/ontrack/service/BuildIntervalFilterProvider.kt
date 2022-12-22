18a224d760b62a45f6dd168f6a63d4ec6f22adb0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2022-12-22 15:06:07 +0100

    #827 Deprecated filter

--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/BuildIntervalFilterProvider.kt
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/BuildIntervalFilterProvider.kt
@@ -16,64 +16,61 @@ import java.util.*
 
 @Component
 @Transactional
+@Deprecated("Will be removed in V5")
 class BuildIntervalFilterProvider(
-        private val filterRepository: CoreBuildFilterRepository,
-        private val structureService: StructureService
+    private val filterRepository: CoreBuildFilterRepository,
+    private val structureService: StructureService,
 ) : AbstractBuildFilterProvider<BuildIntervalFilterData>() {
 
-    override fun getType(): String = BuildIntervalFilterProvider::class.java.name
+    override val type: String = BuildIntervalFilterProvider::class.java.name
+
+    override val name: String = "Build interval (deprecated)"
+    override val isPredefined: Boolean = false
 
     override fun fill(form: Form, data: BuildIntervalFilterData): Form {
         return form
-                .fill("from", data.from)
-                .fill("to", data.to)
+            .fill("from", data.from)
+            .fill("to", data.to)
     }
 
     override fun blankForm(branchId: ID): Form = Form.create()
-            .with(
-                    Text.of("from")
-                            .label("From build")
-                            .help("First build")
-            )
-            .with(
-                    Text.of("to")
-                            .label("To build")
-                            .optional()
-                            .help("Last build")
-            )
+        .with(
+            Text.of("from")
+                .label("From build")
+                .help("First build")
+        )
+        .with(
+            Text.of("to")
+                .label("To build")
+                .optional()
+                .help("Last build")
+        )
 
-    override fun getName(): String = "Build interval"
-
-    override fun isPredefined(): Boolean = false
-
-    override fun filterBranchBuilds(branch: Branch, data: BuildIntervalFilterData): List<Build> {
-        return filterRepository.between(branch, data.from, data.to)
+    override fun filterBranchBuilds(branch: Branch, data: BuildIntervalFilterData?): List<Build> {
+        return filterRepository.between(branch, data?.from, data?.to)
     }
 
-    override fun validateData(branch: Branch, data: BuildIntervalFilterData): String? {
-        return validateBuild(branch, data.from) ?: validateBuild(branch, data.to)
+    override fun validateData(branch: Branch, data: BuildIntervalFilterData?): String? {
+        return validateBuild(branch, data?.from) ?: validateBuild(branch, data?.to)
     }
 
     private fun validateBuild(branch: Branch, name: String?): String? =
-            name?.let {
-                val build = structureService.findBuildByName(
-                        branch.project.name,
-                        branch.name,
-                        it
-                ).getOrNull()
-                if (build != null) {
-                    null
-                } else {
-                    """Build "$name" does not exist for "${branch.entityDisplayName}"."""
-                }
+        name?.let {
+            val build = structureService.findBuildByName(
+                branch.project.name,
+                branch.name,
+                it
+            ).getOrNull()
+            if (build != null) {
+                null
+            } else {
+                """Build "$name" does not exist for "${branch.entityDisplayName}"."""
             }
+        }
 
-    override fun parse(data: JsonNode): Optional<BuildIntervalFilterData> {
-        return Optional.of(
-                BuildIntervalFilterData(
-                        JsonUtils.get(data, "from", true, null),
-                        JsonUtils.get(data, "to", true, null)
-                )
+    override fun parse(data: JsonNode): BuildIntervalFilterData =
+        BuildIntervalFilterData(
+            JsonUtils.get(data, "from", true, null),
+            JsonUtils.get(data, "to", true, null)
         )
-    }
 }
