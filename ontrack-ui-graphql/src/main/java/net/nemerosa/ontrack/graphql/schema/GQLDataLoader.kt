e73c99f0adc5ebd935474e515bb5e7f76e75e89c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-01-05 15:58:51 +0100

    #827 GraphQL batching ~> registering the batch loaders

---
+++ ontrack-ui-graphql/src/main/java/net/nemerosa/ontrack/graphql/schema/GQLDataLoader.kt
@@ -0,0 +1,23 @@
+package net.nemerosa.ontrack.graphql.schema
+
+import org.dataloader.BatchLoader
+
+/**
+ * Registration of a data loader.
+ *
+ * @param <K> type parameter indicating the type of the data load keys
+ * @param <V> type parameter indicating the type of the data that is returned
+ */
+interface GQLDataLoader<K, V> {
+
+    /**
+     * Key for the data loader
+     */
+    val key: String
+
+    /**
+     * Associated data loader
+     */
+    val batchLoader: BatchLoader<K, V>
+
+}
--- ontrack-ui-graphql/src/main/java/net/nemerosa/ontrack/graphql/service/GraphQLServiceImpl.kt
+++ ontrack-ui-graphql/src/main/java/net/nemerosa/ontrack/graphql/service/GraphQLServiceImpl.kt
@@ -4,8 +4,11 @@ import graphql.ExecutionInput
 import graphql.ExecutionResult
 import graphql.GraphQL
 import graphql.execution.ExecutionStrategy
+import net.nemerosa.ontrack.graphql.schema.GQLDataLoader
 import net.nemerosa.ontrack.graphql.schema.GraphqlSchemaService
 import net.nemerosa.ontrack.tx.TransactionService
+import org.dataloader.DataLoader
+import org.dataloader.DataLoaderRegistry
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -13,34 +16,45 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 @Transactional
 class GraphQLServiceImpl(
-    private val graphqlSchemaService: GraphqlSchemaService,
-    private val graphQLExceptionHandlers: List<GraphQLExceptionHandler>,
-    @Qualifier("queryExecutionStrategy")
-    private val queryExecutionStrategy: ExecutionStrategy,
-    @Qualifier("queryExecutionStrategy")
-    private val mutationExecutionStrategy: ExecutionStrategy,
-    private val transactionService: TransactionService
+        private val graphqlSchemaService: GraphqlSchemaService,
+        private val graphQLExceptionHandlers: List<GraphQLExceptionHandler>,
+        @Qualifier("queryExecutionStrategy")
+        private val queryExecutionStrategy: ExecutionStrategy,
+        @Qualifier("queryExecutionStrategy")
+        private val mutationExecutionStrategy: ExecutionStrategy,
+        private val gqlDataLoaders: List<GQLDataLoader<*,*>>,
+        private val transactionService: TransactionService
 ) : GraphQLService {
 
     private val graphQL: GraphQL by lazy {
         GraphQL.newGraphQL(graphqlSchemaService.schema)
-            .queryExecutionStrategy(queryExecutionStrategy)
-            .mutationExecutionStrategy(mutationExecutionStrategy)
-            .build()
+                .queryExecutionStrategy(queryExecutionStrategy)
+                .mutationExecutionStrategy(mutationExecutionStrategy)
+                .build()
+    }
+
+    private val dataLoaderRegistry: DataLoaderRegistry by lazy {
+        DataLoaderRegistry().apply {
+            gqlDataLoaders.forEach { gqlDataLoader ->
+                val dataLoader = DataLoader(gqlDataLoader.batchLoader)
+                register(gqlDataLoader.key, dataLoader)
+            }
+        }
     }
 
     override fun execute(
-        query: String,
-        variables: Map<String, Any>,
-        operationName: String?,
+            query: String,
+            variables: Map<String, Any>,
+            operationName: String?,
     ): ExecutionResult {
         val result: ExecutionResult = transactionService.doInTransaction {
             graphQL.execute(
-                ExecutionInput.newExecutionInput()
-                    .query(query)
-                    .operationName(operationName)
-                    .variables(variables)
-                    .build()
+                    ExecutionInput.newExecutionInput()
+                            .query(query)
+                            .dataLoaderRegistry(dataLoaderRegistry)
+                            .operationName(operationName)
+                            .variables(variables)
+                            .build()
             )
         }
         if (result.errors != null && result.errors.isNotEmpty()) {
