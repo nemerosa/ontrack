fb082a428b029633ac80bd17a2df3828b5a52c48
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-01-08 13:56:29 +0100

    #827 GraphQL batching ~> fixing some weird issue with missing converters

--- ontrack-ui-support/src/main/java/net/nemerosa/ontrack/ui/support/ConversionConfig.kt
+++ ontrack-ui-support/src/main/java/net/nemerosa/ontrack/ui/support/ConversionConfig.kt
@@ -1,21 +1,22 @@
-package net.nemerosa.ontrack.ui.support;
+package net.nemerosa.ontrack.ui.support
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.ConverterRegistry;
-
-import javax.annotation.PostConstruct;
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.convert.converter.ConverterRegistry
+import javax.annotation.PostConstruct
 
 @Configuration
-public class ConversionConfig {
+class ConversionConfig(
+        private val converterRegistry: ConverterRegistry,
+) {
 
-    @Autowired
-    private ConverterRegistry converterRegistry;
+    private val logger: Logger = LoggerFactory.getLogger(ConversionConfig::class.java)
 
     @PostConstruct
-    public void registerConverters() {
-        converterRegistry.addConverter(new IDToString());
-        converterRegistry.addConverter(new StringToID());
+    fun registerConverters() {
+        logger.info("Registering ID/String Spring converters")
+        converterRegistry.addConverter(IDToString())
+        converterRegistry.addConverter(StringToID())
     }
-
 }
