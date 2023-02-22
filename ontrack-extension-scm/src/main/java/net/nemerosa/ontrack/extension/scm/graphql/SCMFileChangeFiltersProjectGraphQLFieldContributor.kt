17006556e3fa96bf6075faa292763735eb056c69
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-02-22 10:08:37 +0100

    #1068 File filters ~> GraphQL model fix to include rights

--- ontrack-extension-scm/src/main/java/net/nemerosa/ontrack/extension/scm/graphql/SCMFileChangeFiltersProjectGraphQLFieldContributor.kt
+++ ontrack-extension-scm/src/main/java/net/nemerosa/ontrack/extension/scm/graphql/SCMFileChangeFiltersProjectGraphQLFieldContributor.kt
@@ -4,6 +4,7 @@ import graphql.schema.GraphQLFieldDefinition
 import net.nemerosa.ontrack.extension.scm.service.SCMFileChangeFilterService
 import net.nemerosa.ontrack.graphql.schema.GQLProjectEntityFieldContributor
 import net.nemerosa.ontrack.graphql.support.listType
+import net.nemerosa.ontrack.graphql.support.toNotNull
 import net.nemerosa.ontrack.model.structure.Project
 import net.nemerosa.ontrack.model.structure.ProjectEntity
 import net.nemerosa.ontrack.model.structure.ProjectEntityType
@@ -11,9 +12,9 @@ import org.springframework.stereotype.Component
 
 @Component
 class SCMFileChangeFiltersProjectGraphQLFieldContributor(
-    private val gqlTypeSCMFileChangeFilter: GQLTypeSCMFileChangeFilter,
+    private val gqlTypeSCMFileChangeFilters: GQLTypeSCMFileChangeFilters,
     private val scmFileChangeFilterService: SCMFileChangeFilterService,
-): GQLProjectEntityFieldContributor {
+) : GQLProjectEntityFieldContributor {
     override fun getFields(
         projectEntityClass: Class<out ProjectEntity>,
         projectEntityType: ProjectEntityType
@@ -23,10 +24,13 @@ class SCMFileChangeFiltersProjectGraphQLFieldContributor(
                 GraphQLFieldDefinition.newFieldDefinition()
                     .name("scmFileChangeFilters")
                     .description("List of saved SCM file filters for the project")
-                    .type(listType(gqlTypeSCMFileChangeFilter.typeRef))
+                    .type(gqlTypeSCMFileChangeFilters.typeRef.toNotNull())
                     .dataFetcher { env ->
                         val project: Project = env.getSource()
-                        scmFileChangeFilterService.loadSCMFileChangeFilters(project).filters
+                        GQLTypeSCMFileChangeFilters.SCMFileChangeFiltersWithProject(
+                            project = project,
+                            filters = scmFileChangeFilterService.loadSCMFileChangeFilters(project),
+                        )
                     }
                     .build()
             )
