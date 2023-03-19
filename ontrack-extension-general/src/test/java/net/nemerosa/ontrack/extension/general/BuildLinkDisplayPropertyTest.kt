fd0fe22521b8967798b8fdc569e213b55a0c2573
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-03-19 09:20:39 +0100

    #1075 Fixing some tests

--- ontrack-extension-general/src/test/java/net/nemerosa/ontrack/extension/general/BuildLinkDisplayPropertyTest.kt
+++ ontrack-extension-general/src/test/java/net/nemerosa/ontrack/extension/general/BuildLinkDisplayPropertyTest.kt
@@ -11,27 +11,27 @@ class BuildLinkDisplayPropertyTest {
 
     @Test
     fun `No settings, no label, use build name`() {
-        test(settings = null, label = null, expected = buildName)
+        test(settings = null, label = null, expected = null)
     }
 
     @Test
     fun `No settings, with label, use build name`() {
-        test(settings = null, label = label, expected = buildName)
+        test(settings = null, label = label, expected = null)
     }
 
     @Test
     fun `Settings to false, no label, use build name`() {
-        test(settings = false, label = null, expected = buildName)
+        test(settings = false, label = null, expected = null)
     }
 
     @Test
     fun `Settings to false, with label, use build name`() {
-        test(settings = false, label = label, expected = buildName)
+        test(settings = false, label = label, expected = null)
     }
 
     @Test
     fun `Settings to true, no label, use build name`() {
-        test(settings = true, label = null, expected = buildName)
+        test(settings = true, label = null, expected = null)
     }
 
     @Test
@@ -42,21 +42,13 @@ class BuildLinkDisplayPropertyTest {
     private fun test(
             settings: Boolean?,
             label: String?,
-            expected: String
+            expected: String?
     ) {
         val settingsProperty = settings?.let { BuildLinkDisplayProperty(it) }
         val labelProperty = label?.let { ReleaseProperty(label) }
-        val build = Build.of(
-                Branch.of(
-                        Project.of(NameDescription.nd("P", "")).withId(ID.of(1)),
-                        NameDescription.nd("master", "")
-                ).withId(ID.of(10)),
-                NameDescription.nd(buildName, ""),
-                Signature.of("test")
-        )
         assertEquals(
                 expected,
-                settingsProperty.getLabel(build, labelProperty)
+                settingsProperty.getLabel(labelProperty)
         )
     }
 
