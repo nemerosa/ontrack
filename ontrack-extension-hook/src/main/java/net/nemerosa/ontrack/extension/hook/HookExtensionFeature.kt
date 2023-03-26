b33a3fbe6cce47155598a3ad2551d7b995004b62
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-03-26 16:08:45 +0200

    #1079 hook: feature

---
+++ ontrack-extension-hook/src/main/java/net/nemerosa/ontrack/extension/hook/HookExtensionFeature.kt
@@ -0,0 +1,13 @@
+package net.nemerosa.ontrack.extension.hook
+
+import net.nemerosa.ontrack.extension.support.AbstractExtensionFeature
+import net.nemerosa.ontrack.model.extension.ExtensionFeatureOptions
+import org.springframework.stereotype.Component
+
+@Component
+class HookExtensionFeature : AbstractExtensionFeature(
+    "hook",
+    "Hooks",
+    "Framework to manage hooks in other extensions",
+    ExtensionFeatureOptions.DEFAULT
+)
--- ontrack-extension-tfc/src/main/java/net/nemerosa/ontrack/extension/tfc/TFCExtensionFeature.kt
+++ ontrack-extension-tfc/src/main/java/net/nemerosa/ontrack/extension/tfc/TFCExtensionFeature.kt
@@ -2,6 +2,7 @@ package net.nemerosa.ontrack.extension.tfc
 
 import net.nemerosa.ontrack.extension.casc.CascExtensionFeature
 import net.nemerosa.ontrack.extension.general.GeneralExtensionFeature
+import net.nemerosa.ontrack.extension.hook.HookExtensionFeature
 import net.nemerosa.ontrack.extension.support.AbstractExtensionFeature
 import net.nemerosa.ontrack.model.extension.ExtensionFeatureOptions
 import org.springframework.stereotype.Component
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component
 class TFCExtensionFeature(
     cascExtensionFeature: CascExtensionFeature,
     generalExtensionFeature: GeneralExtensionFeature,
+    hookExtensionFeature: HookExtensionFeature,
 ) : AbstractExtensionFeature(
     "tfc",
     "Terraform Cloud",
@@ -17,5 +19,6 @@ class TFCExtensionFeature(
     ExtensionFeatureOptions.DEFAULT
         .withDependency(cascExtensionFeature)
         .withDependency(generalExtensionFeature)
+        .withDependency(hookExtensionFeature)
         .withGui(true)
 )
