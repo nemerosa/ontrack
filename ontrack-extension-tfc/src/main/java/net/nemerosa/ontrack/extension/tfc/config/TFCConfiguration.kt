c73d91ce33414e5950cd4da3971a91477e54a4b4
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-03-26 15:43:14 +0200

    #1079 UI mgt of configurations

--- ontrack-extension-tfc/src/main/java/net/nemerosa/ontrack/extension/tfc/config/TFCConfiguration.kt
+++ ontrack-extension-tfc/src/main/java/net/nemerosa/ontrack/extension/tfc/config/TFCConfiguration.kt
@@ -1,11 +1,20 @@
 package net.nemerosa.ontrack.extension.tfc.config
 
+import net.nemerosa.ontrack.model.annotations.APIDescription
+import net.nemerosa.ontrack.model.annotations.APILabel
 import net.nemerosa.ontrack.model.support.ConfigurationDescriptor
 import net.nemerosa.ontrack.model.support.CredentialsConfiguration
 
-class TFCConfiguration(
+// TODO #532 Using `open` as a workaround
+open class TFCConfiguration(
+    @APIDescription("Unique name for the configuration")
+    @APILabel("Name")
     override val name: String,
+    @APIDescription("API root URL")
+    @APILabel("URL")
     val url: String,
+    @APIDescription("User token to access the API")
+    @APILabel("Token")
     val token: String,
 ) : CredentialsConfiguration<TFCConfiguration> {
 
