0648e50ca97a8dd093ac284c173c3c6730f77e40
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-04-05 11:40:20 +0200

    #1079 Fix after merge from 4.5

--- ontrack-extension-queue/src/main/java/net/nemerosa/ontrack/extension/queue/ui/QueueUserMenuExtension.kt
+++ ontrack-extension-queue/src/main/java/net/nemerosa/ontrack/extension/queue/ui/QueueUserMenuExtension.kt
@@ -1,6 +1,7 @@
 package net.nemerosa.ontrack.extension.queue.ui
 
 import net.nemerosa.ontrack.extension.api.UserMenuExtension
+import net.nemerosa.ontrack.extension.api.UserMenuExtensionGroups
 import net.nemerosa.ontrack.extension.queue.QueueExtensionFeature
 import net.nemerosa.ontrack.extension.support.AbstractExtension
 import net.nemerosa.ontrack.model.security.GlobalFunction
@@ -12,9 +13,10 @@ class QueueUserMenuExtension(
     extension: QueueExtensionFeature,
 ) : AbstractExtension(extension), UserMenuExtension {
 
-    override fun getGlobalFunction(): Class<out GlobalFunction>? = null
+    override val globalFunction: Class<out GlobalFunction>? = null
 
-    override fun getAction(): Action =
+    override val action: Action =
         Action.of("queue-records", "Queuing records", "records")
+                .withGroup(UserMenuExtensionGroups.information)
 
 }
