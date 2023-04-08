06c11e9cfed1b9f773cf044f45c5577306695a72
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-04-08 12:42:43 +0200

    #1079 [recordings] Cleanup

--- ontrack-extension-queue/src/main/java/net/nemerosa/ontrack/extension/queue/ui/GQLRootQueryQueueRecordFilter.kt
+++ ontrack-extension-queue/src/main/java/net/nemerosa/ontrack/extension/queue/ui/GQLRootQueryQueueRecordFilter.kt
@@ -1,18 +1,10 @@
 package net.nemerosa.ontrack.extension.queue.ui
 
-import graphql.schema.DataFetchingEnvironment
 import graphql.schema.GraphQLFieldDefinition
 import net.nemerosa.ontrack.extension.queue.QueueProcessor
-import net.nemerosa.ontrack.extension.queue.record.QueueRecord
-import net.nemerosa.ontrack.extension.queue.record.QueueRecordQueryFilter
-import net.nemerosa.ontrack.extension.queue.record.QueueRecordQueryService
 import net.nemerosa.ontrack.extension.queue.record.QueueRecordState
 import net.nemerosa.ontrack.graphql.schema.GQLRootQuery
-import net.nemerosa.ontrack.graphql.schema.GQLTypeCache
-import net.nemerosa.ontrack.graphql.support.pagination.GQLPaginatedListFactory
-import net.nemerosa.ontrack.graphql.support.stringArgument
 import net.nemerosa.ontrack.graphql.support.toNotNull
-import net.nemerosa.ontrack.model.pagination.PaginatedList
 import org.springframework.stereotype.Component
 
 /**
@@ -20,21 +12,21 @@ import org.springframework.stereotype.Component
  */
 @Component
 class GQLRootQueryQueueRecordFilter(
-    private val gqlTypeQueueRecordFilterInfo: GQLTypeQueueRecordFilterInfo,
-    private val queueProcessors: List<QueueProcessor<*>>,
+        private val gqlTypeQueueRecordFilterInfo: GQLTypeQueueRecordFilterInfo,
+        private val queueProcessors: List<QueueProcessor<*>>,
 ) : GQLRootQuery {
 
     override fun getFieldDefinition(): GraphQLFieldDefinition =
-        GraphQLFieldDefinition.newFieldDefinition()
-            .name("queueRecordFilterInfo")
-            .description("Information for the queue record query filter")
-            .type(gqlTypeQueueRecordFilterInfo.typeRef.toNotNull())
-            .dataFetcher {
-                QueueRecordFilterInfo(
-                    processors = queueProcessors.map { it.id }.sorted(),
-                    states = QueueRecordState.values().map { it.name },
-                )
-            }
-            .build()
+            GraphQLFieldDefinition.newFieldDefinition()
+                    .name("queueRecordFilterInfo")
+                    .description("Information for the queue record query filter")
+                    .type(gqlTypeQueueRecordFilterInfo.typeRef.toNotNull())
+                    .dataFetcher {
+                        QueueRecordFilterInfo(
+                                processors = queueProcessors.map { it.id }.sorted(),
+                                states = QueueRecordState.values().map { it.name },
+                        )
+                    }
+                    .build()
 
 }
