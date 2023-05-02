db9cb0b67d403e6dd594bfe205d517d08016da21
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-05-02 10:56:42 +0200

    next-ui: nav bar ~> user menu

--- ontrack-web-core/components/layouts/UserMenu.js
+++ ontrack-web-core/components/layouts/UserMenu.js
@@ -1,5 +1,8 @@
-import {Drawer} from "antd";
+import {Drawer, Menu, Space, Typography} from "antd";
 import {useState} from "react";
+import {LogoutOutlined, SettingOutlined, UserOutlined} from "@ant-design/icons";
+
+const {Text} = Typography;
 
 export function useUserMenu() {
     const [open, setOpen] = useState(false);
@@ -16,6 +19,39 @@ export default function UserMenu({userMenu}) {
         userMenu.setOpen(false)
     }
 
+    const items = [
+        {
+            key: 'user-profile',
+            label: "User profile",
+            icon: <UserOutlined/>,
+        },
+        {
+            key: 'system',
+            label: "System",
+            icon: <SettingOutlined/>,
+            children: [
+                {
+                    key: 'casc',
+                    label: "Configuration as code",
+                },
+                {
+                    key: 'global-subscriptions',
+                    label: "Global subscriptions",
+                },
+            ],
+        },
+        {
+            key: 'logout',
+            label: "Sign out",
+            icon: <LogoutOutlined/>,
+        },
+    ];
+
+    const onClick = (e) => {
+        console.log("User menu: ", e.key)
+        onClose()
+    };
+
     return (
         <>
             <Drawer placement="right"
@@ -23,7 +59,12 @@ export default function UserMenu({userMenu}) {
                     closable={false}
                     onClose={onClose}
             >
-                <p>Sample</p>
+                <Menu mode="inline"
+                      selectable={false}
+                      items={items}
+                      onClick={onClick}
+                >
+                </Menu>
             </Drawer>
         </>
     )
