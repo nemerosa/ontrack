1d30e20264770e8588c2f34e483f2ad1f80648b9
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-05-02 10:31:02 +0200

    next-ui: nav bar ~> using flex directly

--- ontrack-web-core/components/layouts/NavBar.js
+++ ontrack-web-core/components/layouts/NavBar.js
@@ -1,36 +1,41 @@
 import {Avatar, Image, Space, Typography} from "antd";
-import Link from "next/link";
 import {UserOutlined} from "@ant-design/icons";
+import Link from "next/link";
 
 const {Text} = Typography;
 
+function NavBarText({text}) {
+    return (
+        <Text style={{color: 'white'}}>{text}</Text>
+    )
+}
+
 export default function NavBar() {
 
-    const items = [
-        {
-            key: 'logout',
-            label: 'Logout',
-        }
-    ];
-
-    // noinspection JSValidateTypes
     return (
         <>
-            <Space direction="horizontal" size={16}>
-                {/* Logo */}
-                <Link href="/">
-                    <Image width={32} src="/ontrack-128.png" preview={false}/>
-                </Link>
-                {/* Title + link to home page */}
-                <Link href="/">
-                    <Text style={{color: "white", fontSize: '175%', verticalAlign: 'middle'}}>Ontrack</Text>
-                </Link>
-                {/* TODO Search component */}
-                {/* TODO Application messages */}
-                {/* TODO User name */}
-                {/* User menu (drawer) */}
-                <Avatar icon={<UserOutlined/>} style={{backgroundColor: 'white', color: 'black'}}/>
-            </Space>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+            }}>
+                <Space direction="horizontal" size={16}>
+                    <Link href="/">
+                        <Image width={32} src="/ontrack-128.png" preview={false}/>
+                    </Link>
+                    <Link href="/">
+                        <Text style={{color: "white", fontSize: '175%', verticalAlign: 'middle'}}>Ontrack</Text>
+                    </Link>
+                </Space>
+                <Space direction="horizontal" size={8}>
+                    <NavBarText text="Search component"/>
+                    <NavBarText text="App messages"/>
+                    <NavBarText text="User name"/>
+                    <Avatar icon={<UserOutlined/>} style={{
+                        backgroundColor: 'white',
+                        color: 'black',
+                    }}/>
+                </Space>
+            </div>
         </>
     )
 }
