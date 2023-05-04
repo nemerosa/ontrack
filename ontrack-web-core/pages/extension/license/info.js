06b1fbc0c050250630305ef390d31385adcb8bd3
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-05-04 09:55:30 +0200

    next-ui: license info page ~> page skeleton

---
+++ ontrack-web-core/pages/extension/license/info.js
@@ -0,0 +1,14 @@
+import MainLayout from "@components/layouts/MainLayout";
+
+export default function ExtensionLicenseInfoPage() {
+    return (
+        <>
+            <main>
+                <MainLayout>
+                    License info page
+                </MainLayout>
+            </main>
+
+        </>
+    )
+}
