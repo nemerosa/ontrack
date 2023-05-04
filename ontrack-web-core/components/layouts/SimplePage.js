d2bb678cd71710ac793a8d2ec00b3369a626ab35
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-05-04 10:14:49 +0200

    next-ui: license info page ~> simple page

---
+++ ontrack-web-core/components/layouts/SimplePage.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import {title} from "@components/common/Titles";
+import MainLayout from "@components/layouts/MainLayout";
+import MainPage from "@components/layouts/MainPage";
+import {homeBreadcrumbs} from "@components/common/Breadcrumbs";
+import {CloseToHomeCommand} from "@components/common/Commands";
+
+export default function SimplePage({
+                                       pageTitle,
+                                       breadcrumbs = homeBreadcrumbs(),
+                                       commands = [
+                                           <CloseToHomeCommand/>,
+                                       ],
+                                       children,
+                                   }) {
+    return (
+        <>
+            <main>
+                <Head>
+                    {title(pageTitle)}
+                </Head>
+                <MainLayout>
+                    <MainPage
+                        title={pageTitle}
+                        breadcrumbs={breadcrumbs}
+                        commands={commands}
+                    >
+                        {children}
+                    </MainPage>
+                </MainLayout>
+            </main>
+
+        </>
+    )
+}
--- ontrack-web-core/pages/extension/license/info.js
+++ ontrack-web-core/pages/extension/license/info.js
@@ -1,12 +1,7 @@
-import MainLayout from "@components/layouts/MainLayout";
 import {useEffect, useState} from "react";
 import restCall from "@client/restCall";
-import {Alert, Space} from "antd";
-import Head from "next/head";
-import {projectTitle, title} from "@components/common/Titles";
-import MainPage from "@components/layouts/MainPage";
-import {homeBreadcrumbs, projectBreadcrumbs} from "@components/common/Breadcrumbs";
-import {CloseToHomeCommand} from "@components/common/Commands";
+import {Alert} from "antd";
+import SimplePage from "@components/layouts/SimplePage";
 
 export default function ExtensionLicenseInfoPage() {
 
@@ -19,46 +14,31 @@ export default function ExtensionLicenseInfoPage() {
     }, [])
 
     return (
-        <>
-            <main>
-                <Head>
-                    {title('License info')}
-                </Head>
-                <MainLayout>
-                    <MainPage
-                        title="License information"
-                        breadcrumbs={homeBreadcrumbs()}
-                        commands={[
-                            <CloseToHomeCommand/>,
-                        ]}
-                    >
-                        {
-                            !license &&
-                            <Alert message="No license is associated with this instance." type="info"/>
-                        }
-                        {
-                            license &&
-                            <dl>
-                                <dt>License name</dt>
-                                <dd>{license.name}</dd>
-                                <dt>License assignee</dt>
-                                <dd>{license.assignee}</dd>
-                                <dt>Valid until</dt>
-                                <dd>
-                                    <span ng-if="license.validUntil">{license.validUntil}</span>
-                                    <span ng-if="!license.validUntil">Unlimited</span>
-                                </dd>
-                                <dt>Max projects</dt>
-                                <dd>
-                                    <span ng-if="license.maxProjects > 0">{license.maxProjects}</span>
-                                    <span ng-if="license.maxProjects <= 0">Unlimited</span>
-                                </dd>
-                            </dl>
-                        }
-                    </MainPage>
-                </MainLayout>
-            </main>
-
-        </>
+        <SimplePage
+            pageTitle="License info">
+            {
+                !license &&
+                <Alert message="No license is associated with this instance." type="info"/>
+            }
+            {
+                license &&
+                <dl>
+                    <dt>License name</dt>
+                    <dd>{license.name}</dd>
+                    <dt>License assignee</dt>
+                    <dd>{license.assignee}</dd>
+                    <dt>Valid until</dt>
+                    <dd>
+                        <span ng-if="license.validUntil">{license.validUntil}</span>
+                        <span ng-if="!license.validUntil">Unlimited</span>
+                    </dd>
+                    <dt>Max projects</dt>
+                    <dd>
+                        <span ng-if="license.maxProjects > 0">{license.maxProjects}</span>
+                        <span ng-if="license.maxProjects <= 0">Unlimited</span>
+                    </dd>
+                </dl>
+            }
+        </SimplePage>
     )
 }
