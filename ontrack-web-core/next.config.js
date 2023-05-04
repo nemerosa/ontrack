ddfe85141cee2bf7288f447cbd9c08e6a6c79b2f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-05-04 08:43:38 +0200

    next-ui: conditional base path

--- ontrack-web-core/next.config.js
+++ ontrack-web-core/next.config.js
@@ -1,11 +1,18 @@
-/** @type {import('next').NextConfig} */
+const {PHASE_DEVELOPMENT_SERVER} = require('next/constants')
+
 const nextConfig = {
     reactStrictMode: false,
     output: "export",
-    basePath: "/ui",
+    basePath: '/ui',
     images: {
         unoptimized: true,
     },
 }
 
-module.exports = nextConfig
+module.exports = (phase, {defaultConfig}) => {
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        nextConfig.basePath = ''
+    }
+
+    return nextConfig
+}
