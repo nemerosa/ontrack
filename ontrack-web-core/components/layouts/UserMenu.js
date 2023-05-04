d7bef6d9a909d94604fc91d1d130ba764d84350d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-05-04 09:35:33 +0200

    next-ui: menu ~> ungrouped actions

--- ontrack-web-core/components/layouts/UserMenu.js
+++ ontrack-web-core/components/layouts/UserMenu.js
@@ -4,6 +4,7 @@ import {LogoutOutlined, PoweroffOutlined} from "@ant-design/icons";
 import {UserContext} from "@components/providers/UserProvider";
 import {logout} from "@client/login";
 import {legacyUri} from "@components/common/Links";
+import {useRouter} from "next/router";
 
 export function useUserMenu() {
     const [open, setOpen] = useState(false);
@@ -16,42 +17,69 @@ export function useUserMenu() {
 
 export default function UserMenu({userMenu}) {
 
+    const router = useRouter()
+
     const user = useContext(UserContext)
     const [items, setItems] = useState([])
 
     useEffect(() => {
-        // Menu
-        const menu = []
-        // TODO Building the menu from the user actions
-        // Separator
-        menu.push({
-            type: 'divider',
-        })
-        // Adding predefined "Legacy UI"
-        menu.push({
-            key: 'legacy',
-            label: "Legacy UI",
-            icon: <PoweroffOutlined/>,
-            onClick: () => {
-                location.href = legacyUri()
-            }
-        })
-        // Separator
-        menu.push({
-            type: 'divider',
-        })
-        // Adding predefined "Sign out"
-        // Not working in local development mode
-        menu.push({
-            key: 'logout',
-            label: "Sign out",
-            icon: <LogoutOutlined/>,
-            onClick: () => {
-                logout()
-            },
-        })
-        // Registers the menu
-        setItems(menu)
+        if (user?.actions) {
+            // Menu
+            const menu = []
+            const groups = {}
+            // Building the menu from the user actions
+            user.actions.forEach(action => {
+                const groupName = action.group
+                if (groupName) {
+
+                } else {
+                    menu.push({
+                        key: action.id,
+                        label: action.name,
+                        onClick: () => {
+                            if (action.type === 'LINK' && action.uri) {
+                                // TODO Legacy vs. Next UI
+                                // noinspection JSIgnoredPromiseFromCall
+                                router.push(action.uri)
+                            } else {
+                                console.log(`Unsupported action type ${action.type} for action ${action.id} (${action.name}).`)
+                            }
+                        }
+                    })
+                }
+            })
+            // TODO Sorting root menu items
+            // TODO Sorting items in groups
+            // Separator
+            menu.push({
+                type: 'divider',
+            })
+            // Adding predefined "Legacy UI"
+            menu.push({
+                key: 'legacy',
+                label: "Legacy UI",
+                icon: <PoweroffOutlined/>,
+                onClick: () => {
+                    location.href = legacyUri()
+                }
+            })
+            // Separator
+            menu.push({
+                type: 'divider',
+            })
+            // Adding predefined "Sign out"
+            // Not working in local development mode
+            menu.push({
+                key: 'logout',
+                label: "Sign out",
+                icon: <LogoutOutlined/>,
+                onClick: () => {
+                    logout()
+                },
+            })
+            // Registers the menu
+            setItems(menu)
+        }
     }, [user])
 
     const onClose = () => {
