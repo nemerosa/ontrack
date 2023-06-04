b34cdc295c670b400d069a93c5c908aa0b2bd75d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-06-04 14:31:16 +0200

    ci: notifications integration tests

---
+++ ontrack-extension-notifications/src/test/java/net/nemerosa/ontrack/extension/notifications/subscriptions/PromotionRunSubscriptionIT.kt
@@ -0,0 +1,50 @@
+package net.nemerosa.ontrack.extension.notifications.subscriptions
+
+import net.nemerosa.ontrack.extension.notifications.AbstractNotificationTestSupport
+import net.nemerosa.ontrack.extension.notifications.mock.MockNotificationChannelConfig
+import net.nemerosa.ontrack.model.events.EventFactory
+import net.nemerosa.ontrack.test.TestUtils.uid
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+
+class PromotionRunSubscriptionIT : AbstractNotificationTestSupport() {
+
+    @Test
+    fun `Subscribing to a promotion run using the exact promotion level name`() {
+        asAdmin {
+            project {
+                branch {
+                    val bronze = promotionLevel()
+                    val silver = promotionLevel()
+                    // Subscribing at branch level for the bronze promotion
+                    val target = uid("t_")
+                    eventSubscriptionService.subscribe(
+                            channel = mockNotificationChannel,
+                            channelConfig = MockNotificationChannelConfig(target),
+                            projectEntity = this, // The branch
+                            keywords = silver.name, // Promotion level name
+                            origin = "test",
+                            EventFactory.NEW_PROMOTION_RUN
+                    )
+                    build {
+                        // Bronze promotion --> no message
+                        promote(bronze)
+                        assertNull(mockNotificationChannel.messages[target], "No message")
+                        // Silver promotion --> we get a message
+                        promote(silver)
+                        assertNotNull(mockNotificationChannel.messages[target], "Message received") { messages ->
+                            assertEquals(1, messages.size, "One message received")
+                            assertEquals(
+                                    "Build $name has been promoted to ${silver.name} for branch ${branch.name} in ${project.name}.",
+                                    messages.first()
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ ontrack-extension-notifications/src/test/java/net/nemerosa/ontrack/extension/notifications/subscriptions/ValidationRunSubscriptionIT.kt
@@ -0,0 +1,57 @@
+package net.nemerosa.ontrack.extension.notifications.subscriptions
+
+import net.nemerosa.ontrack.extension.notifications.AbstractNotificationTestSupport
+import net.nemerosa.ontrack.extension.notifications.mock.MockNotificationChannelConfig
+import net.nemerosa.ontrack.model.events.EventFactory
+import net.nemerosa.ontrack.model.structure.ValidationRunStatusID
+import net.nemerosa.ontrack.test.TestUtils.uid
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+
+class ValidationRunSubscriptionIT : AbstractNotificationTestSupport() {
+
+    @Test
+    fun `Subscribing to a failed validation run using the exact validation stamp name`() {
+        asAdmin {
+            project {
+                branch {
+                    val vsa = validationStamp()
+                    val vsb = validationStamp()
+                    // Subscribing at validation stamp level for the B validation
+                    val target = uid("t_")
+                    eventSubscriptionService.subscribe(
+                            channel = mockNotificationChannel,
+                            channelConfig = MockNotificationChannelConfig(target),
+                            projectEntity = vsb,
+                            keywords = "failed", // Status
+                            origin = "test",
+                            EventFactory.NEW_VALIDATION_RUN
+                    )
+                    build {
+                        // VSA passed validation --> no message
+                        validate(vsa, validationRunStatusID = ValidationRunStatusID.STATUS_PASSED)
+                        assertNull(mockNotificationChannel.messages[target], "No message")
+                        // VSA failed validation --> no message
+                        validate(vsa, validationRunStatusID = ValidationRunStatusID.STATUS_FAILED)
+                        assertNull(mockNotificationChannel.messages[target], "No message")
+                        // VSB passed validation --> no message
+                        validate(vsb, validationRunStatusID = ValidationRunStatusID.STATUS_PASSED)
+                        assertNull(mockNotificationChannel.messages[target], "No message")
+                        // VSB failed validation --> we get a message
+                        validate(vsb, validationRunStatusID = ValidationRunStatusID.STATUS_FAILED)
+                        assertNotNull(mockNotificationChannel.messages[target], "Message received") { messages ->
+                            assertEquals(1, messages.size, "One message received")
+                            assertEquals(
+                                    "Build $name has run for ${vsb.name} with status Failed in branch ${branch.name} in ${project.name}.",
+                                    messages.first()
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
