1f53f3801c26fc4a18b62383896b83982d90f756
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-06-18 13:23:20 +0200

    #1096 Conversion

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/support/VersionInfoConfig.kt
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/support/VersionInfoConfig.kt
@@ -1,92 +1,96 @@
-package net.nemerosa.ontrack.model.support;
+package net.nemerosa.ontrack.model.support
 
-import lombok.Data;
-import net.nemerosa.ontrack.common.Time;
-import net.nemerosa.ontrack.model.structure.VersionInfo;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import net.nemerosa.ontrack.common.Time.now
+import net.nemerosa.ontrack.model.structure.VersionInfo
+import org.apache.commons.lang3.StringUtils
+import org.slf4j.LoggerFactory
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-
-@Data
 @Component
 @ConfigurationProperties(prefix = "ontrack.version")
-public class VersionInfoConfig {
-
-    /**
-     * Logger
-     */
-    private static final Logger logger = LoggerFactory.getLogger(VersionInfoConfig.class);
+class VersionInfoConfig {
 
     /**
      * Creation date
      */
-    private String date;
+    var date: String? = null
+
     /**
      * Display version. Example: 2.3 or master or feature/158-my-feature
      */
-    private String display = "n/a";
+    var display = "n/a"
+
     /**
      * Full version string, including the build number. Example: release-2.0-10a1bb7
      */
-    private String full = "n/a";
+    var full = "n/a"
+
     /**
      * Branch for the version. Example: release-2.0 or master
      */
-    private String branch = "n/a";
+    var branch = "n/a"
+
     /**
      * Build number. Example: 10a1bb7
      */
-    private String build = "0";
+    var build = "0"
+
     /**
      * Associated commit (hash). Example: 10a1bb77276321bef16abd7dcf19a5533ab8bd97
      */
-    private String commit = "NA";
+    var commit = "NA"
+
     /**
      * Source of the version.
      * Example: release/2.0
      */
-    private String source = "source";
+    var source = "source"
+
     /**
      * Type of source for the version.
      * Example: release
      */
-    private String sourceType = "sourceType";
+    var sourceType = "sourceType"
 
     /**
      * Gets the representation of the version
      */
-    public VersionInfo toInfo() {
-        return new VersionInfo(
-                parseDate(date),
-                display,
-                full,
-                branch,
-                build,
-                commit,
-                source,
-                sourceType
-        );
-    }
+    fun toInfo(): VersionInfo = VersionInfo(
+        parseDate(date),
+        display,
+        full,
+        branch,
+        build,
+        commit,
+        source,
+        sourceType
+    )
 
-    /**
-     * Parses the date defined in the application configuration properties as <code>ontrack.version.date</code>.
-     */
-    public static LocalDateTime parseDate(String value) {
-        if (StringUtils.isBlank(value)) {
-            logger.info("[version-info] No date defined, using current date.");
-            return Time.now();
-        } else {
-            try {
-                return LocalDateTime.parse(value, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
-            } catch (DateTimeParseException ex) {
-                logger.warn("[version-info] Wrong date format, using current date: " + value);
-                return Time.now();
+    companion object {
+
+        /**
+         * Logger
+         */
+        private val logger = LoggerFactory.getLogger(VersionInfoConfig::class.java)
+
+        /**
+         * Parses the date defined in the application configuration properties as `ontrack.version.date`.
+         */
+        fun parseDate(value: String?): LocalDateTime {
+            return if (value.isNullOrBlank()) {
+                logger.info("[version-info] No date defined, using current date.")
+                now()
+            } else {
+                try {
+                    LocalDateTime.parse(value, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"))
+                } catch (ex: DateTimeParseException) {
+                    logger.warn("[version-info] Wrong date format, using current date: $value")
+                    now()
+                }
             }
         }
     }
--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/EnvServiceImpl.kt
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/EnvServiceImpl.kt
@@ -1,81 +1,70 @@
-package net.nemerosa.ontrack.service;
+package net.nemerosa.ontrack.service
 
-import net.nemerosa.ontrack.model.exceptions.CannotCreateWorkingDirException;
-import net.nemerosa.ontrack.model.structure.VersionInfo;
-import net.nemerosa.ontrack.model.support.EnvService;
-import net.nemerosa.ontrack.model.support.OntrackConfigProperties;
-import net.nemerosa.ontrack.model.support.VersionInfoConfig;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import net.nemerosa.ontrack.model.exceptions.CannotCreateWorkingDirException
+import net.nemerosa.ontrack.model.structure.VersionInfo
+import net.nemerosa.ontrack.model.support.EnvService
+import net.nemerosa.ontrack.model.support.OntrackConfigProperties
+import net.nemerosa.ontrack.model.support.VersionInfoConfig
+import org.apache.commons.io.FileUtils
+import org.slf4j.LoggerFactory
+import org.springframework.context.ApplicationContext
+import org.springframework.stereotype.Service
+import java.io.File
+import java.io.IOException
+import javax.annotation.PostConstruct
 
 @Service
-public class EnvServiceImpl implements EnvService {
+class EnvServiceImpl(
+    version: VersionInfoConfig,
+    configProperties: OntrackConfigProperties,
+    ctx: ApplicationContext
+) : EnvService {
 
-    private final Logger logger = LoggerFactory.getLogger(EnvService.class);
-    private final String defaultProfiles;
-    private final String profiles;
-    private final VersionInfo version;
-    private final String jdbcUrl;
-    private final File home;
+    private val logger = LoggerFactory.getLogger(EnvService::class.java)
 
-    @Autowired
-    public EnvServiceImpl(VersionInfoConfig version,
-                          OntrackConfigProperties configProperties,
-                          ApplicationContext ctx) {
-        this.defaultProfiles = StringUtils.join(ctx.getEnvironment().getDefaultProfiles(), ",");
-        this.profiles = StringUtils.join(ctx.getEnvironment().getActiveProfiles(), ",");
-        // Version information from the configuration
-        this.version = version.toInfo();
-        // JDBC URL
-        this.jdbcUrl = ctx.getEnvironment().getProperty("spring.datasource.url");
-        // Home directory
-        this.home = new File(configProperties.getApplicationWorkingDir());
+    override val version: VersionInfo by lazy {
+        version.toInfo()
     }
 
-    @Override
-    public String getProfiles() {
-        return profiles;
+    private val defaultProfiles: String by lazy {
+        ctx.environment.defaultProfiles.joinToString(",")
     }
 
-    @Override
-    public VersionInfo getVersion() {
-        return version;
+    override val profiles: String by lazy {
+        ctx.environment.activeProfiles.joinToString(",")
     }
 
-    @Override
-    public File getWorkingDir(String context, String name) {
-        File cxd = new File(home, context);
-        File wd = new File(cxd, name);
+    private val jdbcUrl: String by lazy {
+        ctx.environment.getProperty("spring.datasource.url") ?: error("No JDBC URL available")
+    }
+
+    private val home: File by lazy {
+        File(configProperties.applicationWorkingDir)
+    }
+
+    override fun getWorkingDir(context: String, name: String): File {
+        val cxd = File(home, context)
+        val wd = File(cxd, name)
         try {
-            FileUtils.forceMkdir(wd);
-        } catch (IOException e) {
-            throw new CannotCreateWorkingDirException(wd, e);
+            FileUtils.forceMkdir(wd)
+        } catch (e: IOException) {
+            throw CannotCreateWorkingDirException(wd, e)
         }
-        return wd;
+        return wd
     }
 
     @PostConstruct
-    public void init() throws FileNotFoundException {
-        logger.info("[env] With JDK:              {}", System.getProperty("java.version"));
-        logger.info("[env] With default profiles: {}", defaultProfiles);
-        logger.info("[env] With active profiles:  {}", profiles);
-        logger.info("[datasource] URL:            {}", jdbcUrl);
-        logger.info("[version] Display:           {}", version.getDisplay());
-        logger.info("[version] Full:              {}", version.getFull());
-        logger.info("[version] Branch:            {}", version.getBranch());
-        logger.info("[version] Build:             {}", version.getBuild());
-        logger.info("[version] Commit:            {}", version.getCommit());
-        logger.info("[version] Source:            {}", version.getSource());
-        logger.info("[version] Source type:       {}", version.getSourceType());
+    fun init() {
+        logger.info("[env] With JDK:              {}", System.getProperty("java.version"))
+        logger.info("[env] With default profiles: {}", defaultProfiles)
+        logger.info("[env] With active profiles:  {}", profiles)
+        logger.info("[datasource] URL:            {}", jdbcUrl)
+        logger.info("[version] Display:           {}", version.display)
+        logger.info("[version] Full:              {}", version.full)
+        logger.info("[version] Branch:            {}", version.branch)
+        logger.info("[version] Build:             {}", version.build)
+        logger.info("[version] Commit:            {}", version.commit)
+        logger.info("[version] Source:            {}", version.source)
+        logger.info("[version] Source type:       {}", version.sourceType)
     }
 }
