c93dbf90cb30c7a045a74495779c0d37428f7e5a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-06-21 11:01:46 +0200

    next-ui: cleanup

---
+++ ontrack-web-core/README.md
@@ -0,0 +1,24 @@
+Ontrack Web Core (Next UI)
+==========================
+
+## Local development
+
+Run the main application, this starts the API on http://localhost:8080
+and the legacy UI remains available.
+
+> The middleware must first be made available by running
+> `./gradlew devStart`
+
+Run the UI locally:
+
+```bash
+cd ontrack-web-core
+npm run dev
+```
+
+This script runs NextJS on http://localhost:3000.
+
+> Behind the scene, the UI connects to the http://localhost:8080 API.
+> `admin/admin` credentials are always used.
+>
+> This behaviour is driven by the values in the `.env.development` file.
--- ontrack-web-core/components/layouts/UserMenu.js
+++ ontrack-web-core/components/layouts/UserMenu.js
@@ -59,94 +59,95 @@ export default function UserMenu({userMenu}) {
     }
 
     useEffect(() => {
-        if (user?.actions) {
-            // Menu
-            const menu = []
-            const topLevelActions = []
-            const groupIndex = {}
-            // Building the menu from the user actions
-            user.actions.forEach(action => {
-                const groupName = action.group
-                if (groupName) {
-                    let group = groupIndex[groupName]
-                    if (!group) {
-                        group = {
-                            key: groupName,
-                            label: groupName,
-                            icon: groupIcons[groupName],
-                            children: [],
-                        }
-                        groupIndex[groupName] = group
-                    }
-                    group.children.push(createMenuItem(action))
-                } else {
-                    topLevelActions.push(createMenuItem(action))
-                }
-            })
-            // Top level actions at the start
-            menu.push(...topLevelActions)
-            // Sorting the groups
-            const groups = [];
-            for (const [_, group] of Object.entries(groupIndex)) {
-                groups.push(group);
+        const menu = []
+        // TODO Loading the user menu from the server?
+        // if (user?.actions) {
+        //     // Menu
+        //     const topLevelActions = []
+        //     const groupIndex = {}
+        //     // Building the menu from the user actions
+        //     user.actions.forEach(action => {
+        //         const groupName = action.group
+        //         if (groupName) {
+        //             let group = groupIndex[groupName]
+        //             if (!group) {
+        //                 group = {
+        //                     key: groupName,
+        //                     label: groupName,
+        //                     icon: groupIcons[groupName],
+        //                     children: [],
+        //                 }
+        //                 groupIndex[groupName] = group
+        //             }
+        //             group.children.push(createMenuItem(action))
+        //         } else {
+        //             topLevelActions.push(createMenuItem(action))
+        //         }
+        //     })
+        //     // Top level actions at the start
+        //     // TODO menu.push(...topLevelActions)
+        //     // Sorting the groups
+        //     const groups = [];
+        //     for (const [_, group] of Object.entries(groupIndex)) {
+        //         groups.push(group);
+        //     }
+        //     groups.sort((a, b) => {
+        //         const na = a.key;
+        //         const nb = b.key;
+        //         if (na < nb) {
+        //             return -1;
+        //         } else if (na > nb) {
+        //             return 1;
+        //         } else {
+        //             return 0;
+        //         }
+        //     });
+        //     // Sorting items in groups
+        //     groups.forEach(group => {
+        //         group.children.sort((a, b) => {
+        //             const na = a.label;
+        //             const nb = b.label;
+        //             if (na < nb) {
+        //                 return -1;
+        //             } else if (na > nb) {
+        //                 return 1;
+        //             } else {
+        //                 return 0;
+        //             }
+        //         })
+        //     })
+        //     // Groups in the menu
+        //     // TODO menu.push(...groups)
+        //     // TODO Separator
+        //     // menu.push({
+        //     //     type: 'divider',
+        //     // })
+        // }
+        // Adding predefined "Legacy UI"
+        menu.push({
+            key: 'legacy',
+            label: "Legacy UI",
+            icon: <PoweroffOutlined/>,
+            onClick: () => {
+                location.href = legacyUri()
             }
-            groups.sort((a, b) => {
-                const na = a.key;
-                const nb = b.key;
-                if (na < nb) {
-                    return -1;
-                } else if (na > nb) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            });
-            // Sorting items in groups
-            groups.forEach(group => {
-                group.children.sort((a, b) => {
-                    const na = a.label;
-                    const nb = b.label;
-                    if (na < nb) {
-                        return -1;
-                    } else if (na > nb) {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                })
-            })
-            // Groups in the menu
-            menu.push(...groups)
-            // Separator
-            menu.push({
-                type: 'divider',
-            })
-            // Adding predefined "Legacy UI"
-            menu.push({
-                key: 'legacy',
-                label: "Legacy UI",
-                icon: <PoweroffOutlined/>,
-                onClick: () => {
-                    location.href = legacyUri()
-                }
-            })
-            // Separator
-            menu.push({
-                type: 'divider',
-            })
-            // Adding predefined "Sign out"
-            // Not working in local development mode
-            menu.push({
-                key: 'logout',
-                label: "Sign out",
-                icon: <LogoutOutlined/>,
-                onClick: () => {
-                    logout()
-                },
-            })
-            // Registers the menu
-            setItems(menu)
-        }
+        })
+        // Separator
+        menu.push({
+            type: 'divider',
+        })
+        // Adding predefined "Sign out"
+        // Not working in local development mode
+        menu.push({
+            key: 'logout',
+            label: "Sign out",
+            icon: <LogoutOutlined/>,
+            onClick: () => {
+                logout()
+            },
+        })
+        // Registers the menu
+        setItems(menu)
     }, [user])
 
     const onClose = () => {
