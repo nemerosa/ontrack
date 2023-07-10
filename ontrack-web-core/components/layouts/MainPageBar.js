89f0e9f3f12a8a260987d10bd5fcd030a093659c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-07-10 21:28:57 +0200

    next-ui: fixing breadcrumbs deprecation

--- ontrack-web-core/components/layouts/MainPageBar.js
+++ ontrack-web-core/components/layouts/MainPageBar.js
@@ -1,30 +1,34 @@
 import {Breadcrumb, Typography} from "antd";
 import MainPageCommands from "@components/layouts/MainPageCommands";
+import {useEffect, useState} from "react";
 
 const {Text} = Typography;
 
 export default function MainPageBar({breadcrumbs, title, commands}) {
+
+    const [actualBreadcrumbs, setActualBreadcrumbs] = useState([])
+    useEffect(() => {
+        const list = breadcrumbs.map(b => ({
+            title: b,
+        }))
+        list.push({
+            title: <Text strong>{title}</Text>
+        })
+        setActualBreadcrumbs(list)
+    }, [breadcrumbs, title])
+
     return (
         <>
             <div style={{
                 display: 'flex',
                 justifyContent: 'space-between',
             }}>
-                {/* Breadcrumbs on the left */}
                 <Breadcrumb
                     style={{
                         margin: '16px 0',
                     }}
-                >
-                    {
-                        breadcrumbs ?
-                            breadcrumbs.map((item, index) => {
-                                return <Breadcrumb.Item key={index}>{item}</Breadcrumb.Item>
-                            }) :
-                            undefined
-                    }
-                    <Breadcrumb.Item><Text strong>{title}</Text></Breadcrumb.Item>
-                </Breadcrumb>
+                    items={actualBreadcrumbs}
+                />
 
                 {/* Commands on the right */}
                 <MainPageCommands commands={commands}/>
