44be471a829c81ec2f2e2c56429a59080648216a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-07-17 18:57:33 +0200

    next-ui: missing `npm install`

--- ontrack-web-core/build.gradle.kts
+++ ontrack-web-core/build.gradle.kts
@@ -1,3 +1,4 @@
+import com.github.gradle.node.NodeExtension
 import com.github.gradle.node.npm.task.NpmTask
 
 plugins {
@@ -5,12 +6,16 @@ plugins {
     id("com.github.node-gradle.node")
 }
 
-val webInstall by tasks.registering(NpmTask::class) {
-    args.set(listOf("install"))
+// Node environment
+
+configure<NodeExtension> {
+    version.set("20.2.0")
+    npmVersion.set("9.6.6")
+    download.set(true)
 }
 
 val webBuild by tasks.registering(NpmTask::class) {
-    dependsOn(webInstall)
+    dependsOn("npmInstall")
     args.set(listOf("run", "build"))
 }
 
