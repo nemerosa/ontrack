90cdc80424257fc052f27dfa35507c864014d5d8
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-09-03 12:40:18 +0200

    #1104 Fixing empty validation run data

--- ontrack-web-core/components/framework/validation-run-data/ValidationRunData.js
+++ ontrack-web-core/components/framework/validation-run-data/ValidationRunData.js
@@ -21,6 +21,8 @@ export default function ValidationRunData({data}) {
             }
             loadValidationRunData().then(() => {
             })
+        } else {
+            setLoadedValidationRunData('')
         }
     }, [data])
 
