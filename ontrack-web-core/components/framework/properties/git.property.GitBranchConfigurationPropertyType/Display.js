92f8dfc943379782ebe5af2c9c0818e044d79153
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-09-05 18:21:31 +0200

    #1104 Info drawer for branch properties

--- ontrack-web-core/components/framework/properties/git.property.GitBranchConfigurationPropertyType/Display.js
+++ ontrack-web-core/components/framework/properties/git.property.GitBranchConfigurationPropertyType/Display.js
@@ -1,25 +1,10 @@
+import {Typography} from "antd";
+
 export default function Display({property}) {
+
     return (
         <>
+            <Typography.Text code>{property.value.branch}</Typography.Text>
         </>
-        // <ul className="fa-ul">
-        //     <li>
-        //         <span className="fa fa-li fa-code-fork"></span>
-        //         <b>{{property.value.branch}}</b>
-        //     </li>
-        //     <li>
-        //         <span className="fa fa-li fa-gear"></span>
-        //         <span ng-include="'extension/git/buildCommitLink/' + property.value.buildCommitLink.id + '.tpl.html'"></span>
-        //     </li>
-        //     <li>
-        //         <span className="fa fa-li fa-clock-o"></span>
-        //         <span ng-if="!property.value.buildTagInterval" className="text-muted">No automatic synchronisation.</span>
-        //         <span ng-if="property.value.buildTagInterval">Synchronisation every {{property.value.buildTagInterval}} minute(s).</span>
-        //     </li>
-        //     <li ng-if="property.value.override" className="text-danger">
-        //         <span className="fa fa-li fa-warning"></span>
-        //         <span><b>Builds are overridden by the synchronisation.</b></span>
-        //     </li>
-        // </ul>
     )
 }
