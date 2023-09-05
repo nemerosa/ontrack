479e58e7647f602d62323df53b6d02d934618944
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-09-05 18:31:39 +0200

    #1104 Fixed warning on title

--- ontrack-web-core/components/common/Titles.js
+++ ontrack-web-core/components/common/Titles.js
@@ -1,15 +1,15 @@
 export function projectTitle(project) {
-    return title(project.name)
+    return title(project ? project.name : undefined)
 }
 
 export function branchTitle(branch) {
-    return title(`${branch.name} @ ${branch.project.name}`)
+    return title(branch ? `${branch.name} @ ${branch.project.name}` : undefined)
 }
 
 export function buildTitle(build) {
-    return title(`${build.branch.name}#${build.name} @ ${build.branch.project.name}`)
+    return title(build ? `${build.branch.name}#${build.name} @ ${build.branch.project.name}` : undefined)
 }
 
 export function title(text) {
-    return <title>Ontrack | {text}</title>
+    return <title>{`Ontrack | ${text ? text : "loading..."}`}</title>
 }
