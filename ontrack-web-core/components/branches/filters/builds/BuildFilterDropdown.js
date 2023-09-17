3138b7fc54c0c67fbe80cb9561ca0fc7637ee03e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-09-17 14:31:57 +0200

    #1104 Build filters

--- ontrack-web-core/components/branches/filters/builds/BuildFilterDropdown.js
+++ ontrack-web-core/components/branches/filters/builds/BuildFilterDropdown.js
@@ -1,6 +1,28 @@
+import {useState} from "react";
+import {Button, Dropdown, Space, Typography} from "antd";
+import {FaCaretDown, FaFilter} from "react-icons/fa";
+
 export default function BuildFilterDropdown() {
+
+    const [items, setItems] = useState([])
+
     return (
         <>
+            <Dropdown
+                menu={{
+                    items,
+                }}
+                trigger={['click']}
+                className="ot-action"
+            >
+                <Button>
+                    <Space>
+                        <FaFilter/>
+                        <Typography.Text>Build filter</Typography.Text>
+                        <FaCaretDown/>
+                    </Space>
+                </Button>
+            </Dropdown>
         </>
     )
 }
