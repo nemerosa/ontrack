ed8e048331f78a5f4e2fb7653b05c7f52fdd7566
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-11-23 20:54:38 +0100

    #1129 NextUI authenticated by Ontrack
    - Setting the cookie & redirection

--- ontrack-web-core/middleware.js
+++ ontrack-web-core/middleware.js
@@ -1,18 +1,29 @@
 import {NextResponse} from "next/server";
 
 export default function middleware(request) {
-    if (request.nextUrl.pathname.startsWith('/auth')) {
+
+    const cookieName = 'ontrack'
+    const cookieMs = 30 * 60 * 1000 // 30 minutes
+    const cookie = request.cookies.get(cookieName)
+
+    if (cookie) {
+        // Already authenticated, going forward
+    } else if (request.nextUrl.pathname.startsWith('/auth')) {
         const token = request.nextUrl.searchParams.get('token')
         const href = request.nextUrl.searchParams.get('href')
-        return Response.json(
-            {success: false, message: 'Authentication failed'},
-            {status: 403}
-        )
+        // Preparing the response
+        const response = NextResponse.redirect(href ?? 'http://localhost:3000')
+        // Setting the cookie
+        response.cookies.set(cookieName, token)
+        response.cookies.set({
+            name: cookieName,
+            value: token,
+            path: '/',
+            expires: Date.now() + cookieMs,
+        })
+        // OK
+        return response
     } else {
-        const cookie = request.cookies.get('ontrack')
-        console.log({cookie})
-        if (!cookie) {
-            return NextResponse.redirect(new URL(`http://localhost:8080/login?targetUrl=test&token=true&tokenCallback=http://localhost:3000/auth&tokenCallbackHref=${request.url}`))
-        }
+        return NextResponse.redirect(new URL(`http://localhost:8080/login?targetUrl=test&token=true&tokenCallback=http://localhost:3000/auth&tokenCallbackHref=${request.url}`))
     }
 }
