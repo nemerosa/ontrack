e25076c12e0a91bb88370e8353706f44db962f51
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-11-27 22:56:02 +0100

    #1129 `/ui` base path for the production build

--- ontrack-web-core/next.config.js
+++ ontrack-web-core/next.config.js
@@ -1,7 +1,16 @@
-module.exports = {
-    reactStrictMode: false,
-    output: 'standalone',
-    images: {
-        unoptimized: true,
-    },
+const {PHASE_PRODUCTION_BUILD} = require('next/constants')
+
+module.exports = (phase) => {
+    const config = {
+        reactStrictMode: false,
+        output: 'standalone',
+        images: {
+            unoptimized: true,
+        },
+    }
+    if (phase === PHASE_PRODUCTION_BUILD) {
+        config.basePath = '/ui'
+    }
+
+    return config
 }
