2863c07ad107278b6dd809923db0eb820390c685
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-12-01 13:48:22 +0100

    #1129 Favicon fix

--- ontrack-web-core/pages/_app.js
+++ ontrack-web-core/pages/_app.js
@@ -6,10 +6,18 @@ import EventsContextProvider from "@components/common/EventsContext";
 import PreferencesContextProvider from "@components/providers/PreferencesProvider";
 import RefDataContextProvider from "@components/providers/RefDataProvider";
 import ConnectionContextProvider from "@components/providers/ConnectionContextProvider";
+import Head from "next/head";
+import {useRouter} from "next/router";
 
 export default function App({Component, pageProps}) {
+
+    const router = useRouter()
+
     return (
         <>
+            <Head>
+                <link rel="shortcut icon" href={`${router.basePath}/favicon.ico`} />
+            </Head>
             <ConnectionContextProvider>
                 <UserContextProvider>
                     <PreferencesContextProvider>
