d4f912b5eb15708bf1b5ff228459f014b3a336bc
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-12-01 07:35:21 +0100

    #1129 Not setting the base path into the app

--- ontrack-web-core/next.config.js
+++ ontrack-web-core/next.config.js
@@ -1,16 +1,7 @@
-const {PHASE_PRODUCTION_BUILD} = require('next/constants')
-
-module.exports = (phase) => {
-    const config = {
-        reactStrictMode: false,
-        output: 'standalone',
-        images: {
-            unoptimized: true,
-        },
-    }
-    if (phase === PHASE_PRODUCTION_BUILD) {
-        config.basePath = '/ui'
-    }
-
-    return config
-}
+module.exports = (phase) => ({
+    reactStrictMode: false,
+    output: 'standalone',
+    images: {
+        unoptimized: true,
+    },
+})
