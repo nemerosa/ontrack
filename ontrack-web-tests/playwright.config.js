df283f38ed58abde36c00a9a6f35bfc52078b218
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-12-03 15:20:17 +0100

    #1134 Limiting in projects

--- ontrack-web-tests/playwright.config.js
+++ ontrack-web-tests/playwright.config.js
@@ -33,19 +33,21 @@ module.exports = defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+      /*
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
+    },
+       */
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
     },
 
     /* Test against mobile viewports. */
