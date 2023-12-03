325b8fe9695ca48941649746a5adbaa835435db3
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-12-03 09:20:48 +0100

    #1134 Connection parameters

---
+++ ontrack-web-tests/tests/connection.js
@@ -0,0 +1,14 @@
+export const ui = () => {
+    return process.env.ONTRACK_UI_URL ?? "http://localhost:3000/ui"
+}
+
+export const backend = () => {
+    return process.env.ONTRACK_BACKEND_URL ?? "http://localhost:8080"
+}
+
+export const credentials = () => {
+    return {
+        username: process.env.ONTRACK_CREDENTIALS_USERNAME ?? "admin",
+        password: process.env.ONTRACK_CREDENTIALS_PASSWORD ?? "admin",
+    }
+}
--- ontrack-web-tests/tests/login.spec.js
+++ ontrack-web-tests/tests/login.spec.js
@@ -1,15 +1,17 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
+const {ui, credentials} = require("./connection");
 
 test('login', async ({page}) => {
-    await page.goto('http://localhost:3000')
+    const {username, password} = credentials()
+    await page.goto(ui())
     // We expect to land on the Legacy UI login page
     await expect(page).toHaveTitle(/Ontrack - Sign in/)
     // Filling the user name & password
-    await page.getByPlaceholder("User name").fill("admin")
-    await page.getByPlaceholder("Password").fill("admin")
+    await page.getByPlaceholder("User name").fill(username)
+    await page.getByPlaceholder("Password").fill(password)
     // Launching the login
-    await page.getByText("Sign in", { exact: true }).click()
+    await page.getByText("Sign in", {exact: true}).click()
     // We expect to be on the Next UI home page now
-    await expect(page.getByText("Dashboard", { exact: true })).toBeVisible()
+    await expect(page.getByText("Dashboard", {exact: true})).toBeVisible()
 })
