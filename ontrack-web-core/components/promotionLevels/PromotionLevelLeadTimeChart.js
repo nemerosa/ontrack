86c38668fab28728923b2235e470d6832fbbb01a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-12-13 08:28:34 +0100

    #1144 Promotion level lead time chart - query

--- ontrack-web-core/components/promotionLevels/PromotionLevelLeadTimeChart.js
+++ ontrack-web-core/components/promotionLevels/PromotionLevelLeadTimeChart.js
@@ -1,4 +1,35 @@
+import {useGraphQLClient} from "@components/providers/ConnectionContextProvider";
+import {useEffect} from "react";
+import {gql} from "graphql-request";
+
 export default function PromotionLevelLeadTimeChart({promotionLevel}) {
+
+    const client = useGraphQLClient()
+
+    useEffect(() => {
+        if (client && promotionLevel) {
+            client.request(
+                gql`
+                    query PromotionLevelLeadTimeChart($parameters: JSON!) {
+                        getChart(input: {
+                            name: "promotion-level-lead-time",
+                            options: {
+                                interval: "1y",
+                                period: "1w",
+                            },
+                            parameters: $parameters,
+                        })
+                    }
+                `,
+                {
+                    parameters: {
+                        id: promotionLevel.id,
+                    }
+                }
+            )
+        }
+    }, [client, promotionLevel]);
+
     return (
         <>
             TODO
