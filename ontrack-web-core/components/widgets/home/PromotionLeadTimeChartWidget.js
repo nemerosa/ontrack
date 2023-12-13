d529015ad15189f4bfc88ebd5be8944d4db7dcd5
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-12-13 14:22:08 +0100

    #1144 Promotion level lead time chart - title

--- ontrack-web-core/components/widgets/home/PromotionLeadTimeChartWidget.js
+++ ontrack-web-core/components/widgets/home/PromotionLeadTimeChartWidget.js
@@ -1,4 +1,15 @@
+import {useContext, useEffect} from "react";
+import {DashboardWidgetCellContext} from "@components/dashboards/DashboardWidgetCellContextProvider";
+
 export default function PromotionLeadTimeChartWidget({project, branch, promotionLevel}) {
+
+    const {setTitle, setExtra} = useContext(DashboardWidgetCellContext)
+
+    useEffect(() => {
+        if (project && branch && promotionLevel) {
+            setTitle(`Lead time to promotion ${promotionLevel} on ${branch}@${project}`)
+        }
+    }, [project, branch, promotionLevel]);
 
     return (
         <>
