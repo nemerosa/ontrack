9a4ace528169236e06d9372333e8add395e71d44
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-12-13 14:42:44 +0100

    #1144 Promotion level lead time chart - widget

--- ontrack-web-core/components/widgets/home/PromotionLeadTimeChartWidget.js
+++ ontrack-web-core/components/widgets/home/PromotionLeadTimeChartWidget.js
@@ -1,7 +1,12 @@
-import {useContext, useEffect} from "react";
+import {useContext, useEffect, useState} from "react";
 import {DashboardWidgetCellContext} from "@components/dashboards/DashboardWidgetCellContextProvider";
+import {useGraphQLClient} from "@components/providers/ConnectionContextProvider";
+import {gql} from "graphql-request";
+import PromotionLevelLeadTimeChart from "@components/promotionLevels/PromotionLevelLeadTimeChart";
 
 export default function PromotionLeadTimeChartWidget({project, branch, promotionLevel}) {
+
+    const client = useGraphQLClient()
 
     const {setTitle, setExtra} = useContext(DashboardWidgetCellContext)
 
@@ -9,11 +14,44 @@ export default function PromotionLeadTimeChartWidget({project, branch, promotion
         if (project && branch && promotionLevel) {
             setTitle(`Lead time to promotion ${promotionLevel} on ${branch}@${project}`)
         }
-    }, [project, branch, promotionLevel]);
+    }, [project, branch, promotionLevel])
+
+    const [promotionLevelObject, setPromotionLevelObject] = useState(undefined)
+
+    useEffect(() => {
+        if (client && project && branch && promotionLevel) {
+            client.request(
+                gql`
+                    query GetPromotionLevelByName(
+                        $project: String!,
+                        $branch: String!,
+                        $promotionLevel: String!,
+                    ) {
+                        promotionLevelByName(project: $project, branch: $branch, name: $promotionLevel) {
+                            id
+                            name
+                            description
+                            image
+                        }
+                    }
+                `,
+                {
+                    project, branch, promotionLevel
+                }
+            ).then(data => {
+                setPromotionLevelObject(data.promotionLevelByName)
+            })
+        }
+    }, [client, project, branch, promotionLevel]);
 
     return (
         <>
-            W: {JSON.stringify({project, branch, promotionLevel})}
+            {
+                promotionLevelObject &&
+                <PromotionLevelLeadTimeChart
+                    promotionLevel={promotionLevelObject}
+                />
+            }
         </>
     )
 }
