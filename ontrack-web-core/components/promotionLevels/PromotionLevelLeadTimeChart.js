b0e13f42bed4bd7629ab40742be9151c23a7c373
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-12-15 18:09:05 +0100

    #1144 Promotion level lead time chart - interval/period in chart props

--- ontrack-web-core/components/promotionLevels/PromotionLevelLeadTimeChart.js
+++ ontrack-web-core/components/promotionLevels/PromotionLevelLeadTimeChart.js
@@ -1,19 +1,19 @@
 import {gql} from "graphql-request";
 import DurationChart from "@components/charts/DurationChart";
 
-export default function PromotionLevelLeadTimeChart({promotionLevel}) {
+export default function PromotionLevelLeadTimeChart({promotionLevel, interval = "3m", period = "1w"}) {
 
     return (
         <>
             <DurationChart
                 query={
                     gql`
-                        query PromotionLevelLeadTimeChart($parameters: JSON!) {
+                        query PromotionLevelLeadTimeChart($parameters: JSON!, $interval: String!, $period: String!) {
                             getChart(input: {
                                 name: "promotion-level-lead-time",
                                 options: {
-                                    interval: "3m",
-                                    period: "1w",
+                                    interval: $interval,
+                                    period: $period,
                                 },
                                 parameters: $parameters,
                             })
@@ -23,7 +23,9 @@ export default function PromotionLevelLeadTimeChart({promotionLevel}) {
                 variables={{
                     parameters: {
                         id: promotionLevel.id
-                    }
+                    },
+                    interval,
+                    period,
                 }}
             />
         </>
