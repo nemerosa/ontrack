33a332c6a3987d94aa8815ce82239d3c3dc9722b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-12-15 19:31:41 +0100

    #1144 Reusing some code

--- ontrack-web-core/components/widgets/home/PromotionLeadTimeChartWidget.js
+++ ontrack-web-core/components/widgets/home/PromotionLeadTimeChartWidget.js
@@ -1,57 +1,32 @@
-import {useContext, useEffect, useState} from "react";
+import {useContext, useEffect} from "react";
 import {DashboardWidgetCellContext} from "@components/dashboards/DashboardWidgetCellContextProvider";
-import {useGraphQLClient} from "@components/providers/ConnectionContextProvider";
-import {gql} from "graphql-request";
 import PromotionLevelLeadTimeChart from "@components/promotionLevels/PromotionLevelLeadTimeChart";
 import {PromotionLevelImage} from "@components/promotionLevels/PromotionLevelImage";
 import {Space, Typography} from "antd";
 import ChartOptions from "@components/charts/ChartOptions";
+import {usePromotionLevel} from "@components/widgets/home/promotionChartUtils";
 
 export default function PromotionLeadTimeChartWidget({project, branch, promotionLevel, interval, period}) {
 
-    const client = useGraphQLClient()
+    const {setTitle} = useContext(DashboardWidgetCellContext)
 
-    const {setTitle, setExtra} = useContext(DashboardWidgetCellContext)
-
-    const [promotionLevelObject, setPromotionLevelObject] = useState(undefined)
+    const promotionLevelObject = usePromotionLevel(project, branch, promotionLevel)
 
     useEffect(() => {
-        if (client && project && branch && promotionLevel) {
-            client.request(
-                gql`
-                    query GetPromotionLevelByName(
-                        $project: String!,
-                        $branch: String!,
-                        $promotionLevel: String!,
-                    ) {
-                        promotionLevelByName(project: $project, branch: $branch, name: $promotionLevel) {
-                            id
-                            name
-                            description
-                            image
-                        }
-                    }
-                `,
-                {
-                    project, branch, promotionLevel
-                }
-            ).then(data => {
-                const pl = data.promotionLevelByName;
-                setPromotionLevelObject(pl)
-                setTitle(
-                    <>
-                        <Space size={4}>
-                            Lead time to
-                            <PromotionLevelImage promotionLevel={pl} />
-                            <Typography.Text strong>{promotionLevel}</Typography.Text>
-                            on {branch}@${project}
-                            &nbsp;<ChartOptions interval={interval} period={period}/>
-                        </Space>
-                    </>
-                )
-            })
+        if (promotionLevelObject) {
+            setTitle(
+                <>
+                    <Space size={4}>
+                        Lead time to
+                        <PromotionLevelImage promotionLevel={promotionLevelObject}/>
+                        <Typography.Text strong>{promotionLevel}</Typography.Text>
+                        on {branch}@${project}
+                        &nbsp;<ChartOptions interval={interval} period={period}/>
+                    </Space>
+                </>
+            )
         }
-    }, [client, project, branch, promotionLevel]);
+    }, [promotionLevelObject]);
 
     return (
         <>
---
+++ ontrack-web-core/components/widgets/home/promotionChartUtils.js
@@ -0,0 +1,37 @@
+import {useGraphQLClient} from "@components/providers/ConnectionContextProvider";
+import {useEffect, useState} from "react";
+import {gql} from "graphql-request";
+
+export const usePromotionLevel = (project, branch, promotionLevel) => {
+    const client = useGraphQLClient()
+
+    const [promotionLevelObject, setPromotionLevelObject] = useState({})
+
+    useEffect(() => {
+        if (client && project && branch && promotionLevel) {
+            client.request(
+                gql`
+                    query GetPromotionLevelByName(
+                        $project: String!,
+                        $branch: String!,
+                        $promotionLevel: String!,
+                    ) {
+                        promotionLevelByName(project: $project, branch: $branch, name: $promotionLevel) {
+                            id
+                            name
+                            description
+                            image
+                        }
+                    }
+                `,
+                {
+                    project, branch, promotionLevel
+                }
+            ).then(data => {
+                setPromotionLevelObject(data.promotionLevelByName)
+            })
+        }
+    }, [client, project, branch, promotionLevel]);
+
+    return promotionLevelObject
+}
