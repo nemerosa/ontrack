0afe1f66d74e6389d33dd7ae7ad47f6219bfe0c5
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2023-12-15 19:10:10 +0100

    #1144 Common code for the promotion charts

--- ontrack-web-core/components/widgets/home/PromotionLeadTimeChartWidgetForm.js
+++ ontrack-web-core/components/widgets/home/PromotionLeadTimeChartWidgetForm.js
@@ -1,41 +1,5 @@
-import {useContext, useEffect, useMemo} from "react";
-import {DashboardWidgetCellContext} from "@components/dashboards/DashboardWidgetCellContextProvider";
-import {Form} from "antd";
-import SelectProjectBranchPromotionLevel from "@components/promotionLevels/SelectProjectBranchPromotionLevel";
+import PromotionChartWidgetForm from "@components/widgets/home/PromotionChartWidgetForm";
 
 export default function PromotionLeadTimeChartWidgetForm({project, branch, promotionLevel}) {
-
-    const {widgetEditionForm} = useContext(DashboardWidgetCellContext)
-
-    const {onReceivingValuesHandler} = useContext(DashboardWidgetCellContext)
-
-    const onFormValues = (values) => {
-        // console.log("PromotionLeadTimeChartWidgetForm", values)
-        // Flattening
-        return {
-            ...values,
-            project: values.promotionLevel.project,
-            branch: values.promotionLevel.branch,
-            promotionLevel: values.promotionLevel.promotionLevel,
-        }
-    }
-
-    onReceivingValuesHandler(onFormValues)
-
-    return (
-        <>
-            <Form
-                layout="vertical"
-                form={widgetEditionForm}
-            >
-                <Form.Item
-                    name="promotionLevel"
-                    label="Promotion level"
-                    initialValue={{project, branch, promotionLevel}}
-                >
-                    <SelectProjectBranchPromotionLevel/>
-                </Form.Item>
-            </Form>
-        </>
-    )
+    return <PromotionChartWidgetForm project={project} branch={branch} promotionLevel={promotionLevel}/>
 }
--- ontrack-web-core/components/widgets/home/PromotionTTRChartWidgetForm.js
+++ ontrack-web-core/components/widgets/home/PromotionTTRChartWidgetForm.js
@@ -1,40 +1,5 @@
-import {useContext, useEffect, useMemo} from "react";
-import {DashboardWidgetCellContext} from "@components/dashboards/DashboardWidgetCellContextProvider";
-import {Form} from "antd";
-import SelectProjectBranchPromotionLevel from "@components/promotionLevels/SelectProjectBranchPromotionLevel";
+import PromotionChartWidgetForm from "@components/widgets/home/PromotionChartWidgetForm";
 
 export default function PromotionTTRChartWidgetForm({project, branch, promotionLevel}) {
-
-    const {widgetEditionForm} = useContext(DashboardWidgetCellContext)
-
-    const {onReceivingValuesHandler} = useContext(DashboardWidgetCellContext)
-
-    const onFormValues = (values) => {
-        // Flattening
-        return {
-            ...values,
-            project: values.promotionLevel.project,
-            branch: values.promotionLevel.branch,
-            promotionLevel: values.promotionLevel.promotionLevel,
-        }
-    }
-
-    onReceivingValuesHandler(onFormValues)
-
-    return (
-        <>
-            <Form
-                layout="vertical"
-                form={widgetEditionForm}
-            >
-                <Form.Item
-                    name="promotionLevel"
-                    label="Promotion level"
-                    initialValue={{project, branch, promotionLevel}}
-                >
-                    <SelectProjectBranchPromotionLevel/>
-                </Form.Item>
-            </Form>
-        </>
-    )
+    return <PromotionChartWidgetForm project={project} branch={branch} promotionLevel={promotionLevel}/>
 }
