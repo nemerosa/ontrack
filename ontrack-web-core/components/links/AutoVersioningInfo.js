d45e37c03d09de41b947952306701ba561e52c81
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-01-22 10:44:41 +0100

    #1161 Help on the AV status

--- ontrack-web-core/components/links/AutoVersioningInfo.js
+++ ontrack-web-core/components/links/AutoVersioningInfo.js
@@ -1,13 +1,14 @@
-import {Descriptions, Popover, Space, Spin, Tooltip, Typography} from "antd";
-import {FaCalendar, FaCheck, FaCodeBranch, FaMedal, FaTag, FaThumbsUp, FaTimes} from "react-icons/fa";
+import {Space, Spin, Tooltip, Typography} from "antd";
+import {FaCalendar, FaCodeBranch, FaMedal, FaTag, FaThumbsUp} from "react-icons/fa";
 import BuildRef from "@components/links/BuildRef";
-import CheckIcon from "@components/common/CheckIcon";
 import Rows from "@components/common/Rows";
 import Columns from "@components/common/Columns";
 import Timestamp from "@components/common/Timestamp";
 import Link from "next/link";
+import CheckStatus from "@components/common/CheckStatus";
 
 export default function AutoVersioningInfo({autoVersioning, branchLink}) {
+    const commonStatusText = "Indicates if the auto versioning has caught up with last eligible build or not."
     return (
         <>
             <Space direction="vertical" className="ot-line">
@@ -17,7 +18,13 @@ export default function AutoVersioningInfo({autoVersioning, branchLink}) {
                     <Space>
                         <FaMedal title="Last eligible build"/>
                         <BuildRef build={autoVersioning.lastEligibleBuild}/>
-                        <CheckIcon value={branchLink.targetBuild.id === autoVersioning.lastEligibleBuild.id}/>
+                        <CheckStatus
+                            value={branchLink.targetBuild.id === autoVersioning.lastEligibleBuild.id}
+                            tooltip={(status) => status ?
+                                `Using latest. ${commonStatusText}` :
+                                `Not using latest. ${commonStatusText}`
+                            }
+                        />
                     </Space>
                 }
                 {/* AV status */}
