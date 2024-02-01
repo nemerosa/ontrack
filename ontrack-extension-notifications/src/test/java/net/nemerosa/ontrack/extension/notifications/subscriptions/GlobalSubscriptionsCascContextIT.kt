0cfd4c64ebd7bb508b2643557b9941a58ab07328
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-02-01 19:49:36 +0100

    #1168 Testing the Casc interface

--- ontrack-extension-notifications/src/test/java/net/nemerosa/ontrack/extension/notifications/subscriptions/GlobalSubscriptionsCascContextIT.kt
+++ ontrack-extension-notifications/src/test/java/net/nemerosa/ontrack/extension/notifications/subscriptions/GlobalSubscriptionsCascContextIT.kt
@@ -60,4 +60,64 @@ class GlobalSubscriptionsCascContextIT : AbstractCascTestSupport() {
         }
     }
 
+    @Test
+    fun `Creates a global subscription as code with a content template`() {
+        val target = uid("t")
+        casc("""
+            ontrack:
+                extensions:
+                    notifications:
+                        global-subscriptions:
+                            - events:
+                                - new_promotion_run
+                              keywords: "GOLD main"
+                              channel: mock
+                              channel-config:
+                                target: "#$target"
+                              contentTemplate: |
+                                This is a fairly simple template
+                                for a ${'$'}{branch} name and
+                                a ${'$'}{changelog?format=html}.
+        """.trimIndent())
+        // Check we can find this global subscriptions
+        asAdmin {
+            val subscriptions = eventSubscriptionService.filterSubscriptions(
+                EventSubscriptionFilter(
+                    channel = "mock",
+                    channelConfig = "#$target"
+                )
+            )
+            assertEquals(1, subscriptions.pageItems.size)
+            val subscription = subscriptions.pageItems.first()
+            assertEquals(
+                setOf("new_promotion_run"),
+                subscription.data.events
+            )
+            assertEquals(
+                "GOLD main",
+                subscription.data.keywords
+            )
+            assertEquals(
+                "mock",
+                subscription.data.channel
+            )
+            assertEquals(
+                mapOf("target" to "#$target").asJson(),
+                subscription.data.channelConfig
+            )
+            assertEquals(
+                false,
+                subscription.data.disabled
+            )
+            assertEquals(
+                """
+                    This is a fairly simple template
+                    for a ${'$'}{branch} name and
+                    a ${'$'}{changelog?format=html}.
+                """.trimIndent(),
+                subscription.data.contentTemplate
+            )
+        }
+    }
+
 }
