f54406f192eacdf34559c42de0413928ee499e59
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-02-12 07:44:42 +0100

    #1168 Fix after merge

--- ontrack-extension-notifications/src/test/java/net/nemerosa/ontrack/extension/notifications/mail/AbstractMailTestSupportConfiguration.kt
+++ ontrack-extension-notifications/src/test/java/net/nemerosa/ontrack/extension/notifications/mail/AbstractMailTestSupportConfiguration.kt
@@ -3,16 +3,12 @@ package net.nemerosa.ontrack.extension.notifications.mail
 import com.icegreen.greenmail.spring.GreenMailBean
 import com.icegreen.greenmail.util.GreenMail
 import net.nemerosa.ontrack.extension.notifications.NotificationsConfigProperties
-import net.nemerosa.ontrack.model.events.EventTemplatingService
-import net.nemerosa.ontrack.model.events.HtmlNotificationEventRenderer
 import net.nemerosa.ontrack.model.support.ApplicationLogService
 import org.springframework.context.annotation.Bean
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.JavaMailSenderImpl
 
-class AbstractMailTestSupportConfiguration(
-    private val eventTemplatingService: EventTemplatingService,
-) {
+class AbstractMailTestSupportConfiguration {
 
     @Bean
     fun greenMailBean(): GreenMailBean = GreenMailBean()
@@ -34,18 +30,6 @@ class AbstractMailTestSupportConfiguration(
         javaMailSender,
         applicationLogService,
         NotificationsConfigProperties(),
-    )
-
-    @Bean
-    fun mailNotificationChannel(
-        javaMailSender: JavaMailSender,
-        mailService: MailService,
-        htmlNotificationEventRenderer: HtmlNotificationEventRenderer,
-    ) = MailNotificationChannel(
-        javaMailSender,
-        mailService,
-        htmlNotificationEventRenderer,
-        eventTemplatingService,
     )
 
 }
