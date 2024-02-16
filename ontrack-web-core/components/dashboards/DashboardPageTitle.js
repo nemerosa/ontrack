2603b5a17d6565846d825a6f1770a396879c984e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-02-16 12:15:51 +0100

    #1184 Dashboard name in the window title

--- ontrack-web-core/components/dashboards/DashboardPageTitle.js
+++ ontrack-web-core/components/dashboards/DashboardPageTitle.js
@@ -1,17 +1,24 @@
 import {Space, Tag} from "antd";
 import {useContext} from "react";
 import {DashboardContext} from "@components/dashboards/DashboardContextProvider";
+import Head from "next/head";
+import {pageTitle} from "@components/common/Titles";
 
 export default function DashboardPageTitle({title}) {
 
     const {dashboard} = useContext(DashboardContext)
 
     return (
-        <Space>
-            {title}
-            <Tag>
-                {dashboard?.name}
-            </Tag>
-        </Space>
+        <>
+            <Head>
+                {pageTitle(`${title} - ${dashboard?.name}`)}
+            </Head>
+            <Space>
+                {title}
+                <Tag>
+                    {dashboard?.name}
+                </Tag>
+            </Space>
+        </>
     )
 }
