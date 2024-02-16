703651e6c8bb4ef6148cfa9618c42dd9e47ffdf1
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-02-16 18:15:07 +0100

    #1183 Copy of the dashboard link

--- ontrack-web-core/components/dashboards/DashboardPageTitle.js
+++ ontrack-web-core/components/dashboards/DashboardPageTitle.js
@@ -1,12 +1,29 @@
-import {Space, Tag} from "antd";
-import {useContext} from "react";
+import {Button, Space, Tag} from "antd";
+import {useContext, useEffect, useState} from "react";
 import {DashboardContext} from "@components/dashboards/DashboardContextProvider";
 import Head from "next/head";
 import {pageTitle} from "@components/common/Titles";
+import {FaCheck, FaRegCopy} from "react-icons/fa";
+import copy from "copy-to-clipboard";
+import {useRouter} from "next/router";
+import {useConnection} from "@components/providers/ConnectionContextProvider";
 
 export default function DashboardPageTitle({title}) {
 
+    const router = useRouter()
+    const {environment} = useConnection()
     const {dashboard} = useContext(DashboardContext)
+
+    const [copied, setCopied] = useState(false)
+
+    useEffect(() => {
+        setCopied(false)
+    }, [dashboard?.uuid])
+
+    const copyDashboardLink = () => {
+        const link = `${environment.ontrack.ui.url}${router.pathname}?dashboard=${dashboard.uuid}`
+        setCopied(copy(link))
+    }
 
     return (
         <>
@@ -18,6 +35,21 @@ export default function DashboardPageTitle({title}) {
                 <Tag>
                     {dashboard?.name}
                 </Tag>
+                <FaRegCopy
+                    className="ot-action"
+                    title="Copy a link to this dashboard."
+                    onClick={copyDashboardLink}
+                />
+                {
+                    copied &&
+                    <Button
+                        type="text"
+                        icon={<FaCheck/>}
+                        disabled={true}
+                    >
+                        Copied
+                    </Button>
+                }
             </Space>
         </>
     )
