63121a83daceaf65c184965acb1be927a79ee57d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-02-16 14:33:35 +0100

    #1190 Unit test

---
+++ ontrack-service/src/test/java/net/nemerosa/ontrack/service/EntityDisplayNameServiceImplTest.kt
@@ -0,0 +1,62 @@
+package net.nemerosa.ontrack.service
+
+import io.mockk.every
+import io.mockk.mockk
+import net.nemerosa.ontrack.model.structure.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class EntityDisplayNameServiceImplTest {
+
+    private lateinit var branchDisplayNameService: BranchDisplayNameService
+    private lateinit var buildDisplayNameService: BuildDisplayNameService
+    private lateinit var entityDisplayNameService: EntityDisplayNameService
+
+    @BeforeEach
+    fun init() {
+
+        branchDisplayNameService = mockk()
+        buildDisplayNameService = mockk()
+
+        entityDisplayNameService = EntityDisplayNameServiceImpl(
+            branchDisplayNameService = branchDisplayNameService,
+            buildDisplayNameService = buildDisplayNameService,
+        )
+    }
+
+    @Test
+    fun `Project display name`() {
+        val project = ProjectFixtures.testProject()
+        assertEquals(project.name, entityDisplayNameService.getEntityDisplayName(project))
+    }
+
+    @Test
+    fun `Branch display name without custom display name`() {
+        val branch = BranchFixtures.testBranch()
+        every { branchDisplayNameService.getBranchDisplayName(branch) } returns branch.name
+        assertEquals(branch.name, entityDisplayNameService.getEntityDisplayName(branch))
+    }
+
+    @Test
+    fun `Branch display name with custom display name`() {
+        val branch = BranchFixtures.testBranch()
+        every { branchDisplayNameService.getBranchDisplayName(branch) } returns "release/1.0"
+        assertEquals("release/1.0", entityDisplayNameService.getEntityDisplayName(branch))
+    }
+
+    @Test
+    fun `Build display name without custom display name`() {
+        val build = BuildFixtures.testBuild()
+        every { buildDisplayNameService.getBuildDisplayName(build) } returns build.name
+        assertEquals(build.name, entityDisplayNameService.getEntityDisplayName(build))
+    }
+
+    @Test
+    fun `Build display name with custom display name`() {
+        val build = BuildFixtures.testBuild()
+        every { buildDisplayNameService.getBuildDisplayName(build) } returns "1.0.0"
+        assertEquals("1.0.0", entityDisplayNameService.getEntityDisplayName(build))
+    }
+
+}
