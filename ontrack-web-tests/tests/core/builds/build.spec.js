c37401d7ac78a8be44872ea2e53a0449bef04f55
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-03-05 18:08:59 +0100

    #1212 Accessing the build page with some links

--- ontrack-web-tests/tests/core/builds/build.spec.js
+++ ontrack-web-tests/tests/core/builds/build.spec.js
@@ -14,3 +14,22 @@ test('build page', async ({page}) => {
     const buildPage = new BuildPage(page, build)
     await buildPage.goTo()
 })
+
+test('build page with links', async ({page}) => {
+    // Provisioning
+    const project = await ontrack().createProject()
+    const branch = await project.createBranch()
+    const build = await branch.createBuild()
+
+    // Link
+    const targetProject = await ontrack().createProject()
+    const targetBranch = await targetProject.createBranch()
+    const target = await targetBranch.createBuild()
+    await build.linkTo(target)
+
+    // Login
+    await login(page)
+    // Navigating to the build
+    const buildPage = new BuildPage(page, build)
+    await buildPage.goTo()
+})
