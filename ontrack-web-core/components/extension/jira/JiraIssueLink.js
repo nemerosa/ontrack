54e8d66341fc21ce8eaac8f190ba01135ca1a654
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-03-06 15:33:03 +0100

    #1213 JIRA issue links in a separate pop over

--- ontrack-web-core/components/extension/jira/JiraIssueLink.js
+++ ontrack-web-core/components/extension/jira/JiraIssueLink.js
@@ -4,9 +4,6 @@ import Link from "next/link";
 
 export default function JiraIssueLink({issue}) {
     return (
-        <Space className="ot-extension-jira-issue-link">
-            <JiraIssueStatus status={issue.status}/>
-            <Link href={issue.url}>{issue.key}</Link>
-        </Space>
+        <Link href={issue.url}>{issue.key}</Link>
     )
 }
--- ontrack-web-core/components/extension/jira/JiraIssueLinks.js
+++ ontrack-web-core/components/extension/jira/JiraIssueLinks.js
@@ -1,19 +1,41 @@
-import {Space} from "antd";
+import {Popover, Space, Tag} from "antd";
 import JiraIssueLink from "@components/extension/jira/JiraIssueLink";
+import {FaLink} from "react-icons/fa";
 
 export default function JiraIssueLinks({links = []}) {
     return (
         <>
-            <Space size={2}>
-                {
-                    links.map((link, index) => (
-                        <Space size={2} key={`link-${index}`}>
-                            {link.link}
-                            <JiraIssueLink issue={link}/>
+            {
+                links && links.length > 0 &&
+                <Popover
+                    title="Linked issues"
+                    content={
+                        <Space direction="vertical">
+                            {
+                                links.map((link, index) => (
+                                    <Space key={`link-${index}`}>
+                                        <Tag>{link.link}</Tag>
+                                        <JiraIssueLink issue={link}/>
+                                    </Space>
+                                ))
+                            }
                         </Space>
-                    ))
-                }
-            </Space>
+                    }
+                >
+                    <FaLink className="ot-action"/>
+                </Popover>
+            }
+
+            {/*<Space size={2}>*/}
+            {/*    {*/}
+            {/*        links.map((link, index) => (*/}
+            {/*            <Space size={2} key={`link-${index}`}>*/}
+            {/*                {link.link}*/}
+            {/*                <JiraIssueLink issue={link}/>*/}
+            {/*            </Space>*/}
+            {/*        ))*/}
+            {/*    }*/}
+            {/*</Space>*/}
         </>
     )
 }
