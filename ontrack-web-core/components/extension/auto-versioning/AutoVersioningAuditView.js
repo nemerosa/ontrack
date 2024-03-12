3ba2d2d263740c62b1f20ca78f96f12147901a25
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-03-12 19:52:10 +0100

    #1230 Fixing link to branch

--- ontrack-web-core/components/extension/auto-versioning/AutoVersioningAuditView.js
+++ ontrack-web-core/components/extension/auto-versioning/AutoVersioningAuditView.js
@@ -136,6 +136,7 @@ export default function AutoVersioningAuditView() {
                                     uuid
                                     sourceProject
                                     branch {
+                                        id
                                         name
                                         project {
                                             id
