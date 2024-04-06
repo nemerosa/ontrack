2ff7e324398d62c152e08ff3afcb6392b2dec431
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-04-06 09:28:07 +0200

    #1245 Always displays timestamps using the local timezone

--- ontrack-web-core/components/common/TimestampText.js
+++ ontrack-web-core/components/common/TimestampText.js
@@ -1,5 +1,11 @@
 import dayjs from "dayjs";
 
+import * as utc from 'dayjs/plugin/utc';
+import * as timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 export const weekDayFormat = "ddd, MMM DD, YYYY, HH:mm:ss"
 
 export default function TimestampText({
@@ -8,11 +14,14 @@ export default function TimestampText({
                                           suffix = '',
                                           format = "YYYY MMM DD, HH:mm",
                                       }) {
+
+    const localDateTime = dayjs.utc(value).local()
+
     return (
         <>
 
             {prefix && `${prefix} `}
-            {dayjs(value).format(format)}
+            {localDateTime.format(format)}
             {suffix && ` ${suffix}`}
         </>
     )
