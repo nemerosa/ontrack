0d38f8fc7d16b150d4065eb119f360b98b5cd8c8
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-05-01 17:32:41 +0200

    #1180 Acceptance test for parallel workflow

--- ontrack-kdsl-acceptance/src/test/java/net/nemerosa/ontrack/kdsl/acceptance/tests/workflows/ACCDSLWorkflows.kt
+++ ontrack-kdsl-acceptance/src/test/java/net/nemerosa/ontrack/kdsl/acceptance/tests/workflows/ACCDSLWorkflows.kt
@@ -42,4 +42,40 @@ class ACCDSLWorkflows : AbstractACCDSLTestSupport() {
         // TODO Checks the outcome of the workflow run
     }
 
+    @Test
+    fun `Parallel with join workflow`() {
+        // Defining a workflow
+        val name = uid("w-")
+        val workflow = """
+            name: $name
+            data: ""
+            nodes:
+                - id: start-a
+                  data: "Start node A"
+                - id: start-b
+                  data: "Start node B"
+                - id: end
+                  data: "End node"
+                  parents:
+                    - id: start-a
+                    - id: start-b
+        """.trimIndent()
+        // Saving the workflow
+        val workflowId = ontrack.workflows.saveYamlWorkflow(
+            workflow = workflow,
+            executor = "mock",
+        ) ?: fail("Error while saving workflow")
+        // Running the workflow
+        val instanceId = ontrack.workflows.launchWorkflow(
+            workflowId = workflowId,
+            // TODO Context
+        ) ?: fail("Error while launching workflow")
+        // Waiting for the workflow result
+        waitUntil {
+            val instance = ontrack.workflows.workflowInstance(instanceId)
+            instance != null && instance.finished
+        }
+        // TODO Checks the outcome of the workflow run
+    }
+
 }
