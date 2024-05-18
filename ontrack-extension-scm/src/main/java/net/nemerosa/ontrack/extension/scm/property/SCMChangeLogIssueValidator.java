ac7cd5fe4c129b2ac1a6395683b9b46beeb8eab6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-05-18 11:36:07 +0200

    #1275 Cleanup of obsolete types

--- ontrack-extension-scm/src/main/java/net/nemerosa/ontrack/extension/scm/property/SCMChangeLogIssueValidator.java
+++ ontrack-extension-scm/src/main/java/net/nemerosa/ontrack/extension/scm/property/SCMChangeLogIssueValidator.java
@@ -9,7 +9,10 @@ import net.nemerosa.ontrack.model.structure.PropertyType;
  * @param <S> Type of SCM data associated with the branch
  * @param <B> Type of SCM data associated with a build
  * @param <I> Type of issue associated with this change log
+ *
+ * @deprecated Will be removed in V5.
  */
+@Deprecated
 public interface SCMChangeLogIssueValidator<T, S, B, I extends SCMChangeLogIssue> extends PropertyType<T> {
 
     void validate(SCMChangeLog<B> changeLog, I issue, T validatorConfig);
