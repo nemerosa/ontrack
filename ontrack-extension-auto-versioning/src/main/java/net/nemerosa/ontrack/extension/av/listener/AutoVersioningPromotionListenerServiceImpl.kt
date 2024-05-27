cca5af6a2854e5567558a9b2465cf58a96c1ffe8
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-05-27 15:33:47 +0200

    chore: caching for the AV configured branches

--- ontrack-extension-auto-versioning/src/main/java/net/nemerosa/ontrack/extension/av/listener/AutoVersioningPromotionListenerServiceImpl.kt
+++ ontrack-extension-auto-versioning/src/main/java/net/nemerosa/ontrack/extension/av/listener/AutoVersioningPromotionListenerServiceImpl.kt
@@ -1,11 +1,13 @@
 package net.nemerosa.ontrack.extension.av.listener
 
+import net.nemerosa.ontrack.common.logTime
 import net.nemerosa.ontrack.extension.av.config.AutoVersioningConfigurationService
 import net.nemerosa.ontrack.extension.av.config.AutoVersioningConfiguredBranch
 import net.nemerosa.ontrack.extension.av.config.AutoVersioningSourceConfig
 import net.nemerosa.ontrack.extension.av.model.AutoVersioningConfiguredBranches
 import net.nemerosa.ontrack.extension.scm.service.SCMDetector
 import net.nemerosa.ontrack.model.structure.Branch
+import net.nemerosa.ontrack.model.structure.Project
 import net.nemerosa.ontrack.model.structure.PromotionRun
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -22,24 +24,19 @@ class AutoVersioningPromotionListenerServiceImpl(
     private val logger: Logger = LoggerFactory.getLogger(AutoVersioningPromotionListenerServiceImpl::class.java)
 
     override fun getConfiguredBranches(promotionRun: PromotionRun): AutoVersioningConfiguredBranches? {
-        logger.debug("Looking for configured branches: ${promotionRun.build.entityDisplayName} @ ${promotionRun.promotionLevel.name}...")
         // Gets all the trigger configurations about this event, based on project & promotion only
-        val branches = autoVersioningConfigurationService.getBranchesConfiguredFor(promotionRun.build.project.name, promotionRun.promotionLevel.name)
-        logger.debug("Raw configured branches for ${promotionRun.build.entityDisplayName} @ ${promotionRun.promotionLevel.name}: ${branches.size}")
+        val branches = autoVersioningConfigurationService.getBranchesConfiguredFor(
+            promotionRun.build.project.name,
+            promotionRun.promotionLevel.name
+        )
         // Filters the branches based on their configuration and the triggering event
+        val cache = mutableMapOf<LatestSourceBranchCacheKey, LatestSourceBranchCacheValue>()
         val configuredBranches = branches
             .filter { !it.isDisabled && !it.project.isDisabled }
             .mapNotNull {
-                filterBranch(it, promotionRun)
+                filterBranch(it, promotionRun, cache)
             }
-        // Logging
-        if (logger.isDebugEnabled) {
-            configuredBranches.forEach { configuredBranch ->
-                configuredBranch.configurations.forEach { config ->
-                    logger.debug("Configured branch: ${configuredBranch.branch.entityDisplayName} -> $config")
-                }
-            }
-        }
+        logger.debug("Cache: {}", cache.keys)
         // OK
         return AutoVersioningConfiguredBranches(
             configuredBranches,
@@ -50,18 +47,17 @@ class AutoVersioningPromotionListenerServiceImpl(
     private fun filterBranch(
         branch: Branch,
         promotionRun: PromotionRun,
+        cache: MutableMap<LatestSourceBranchCacheKey, LatestSourceBranchCacheValue>,
     ): AutoVersioningConfiguredBranch? {
-        logger.debug("Filtering branch {} for event {}...", branch.entityDisplayName, promotionRun)
         // The project must be configured for a SCM
         if (scmDetector.getSCM(branch.project) != null) {
             // Gets the auto versioning configuration for the branch
             val config = autoVersioningConfigurationService.getAutoVersioning(branch)
             if (config != null) {
-                logger.debug("Auto versioning config present. Filtering the configurations...")
                 // If present, gets its configurations
                 val autoVersioningConfigurations = config.configurations
                     // Filters the configurations based on the event
-                    .filter { configuration -> promotionRun.match(branch, configuration) }
+                    .filter { configuration -> promotionRun.match(branch, configuration, cache) }
                     // Removes any empty setup
                     .takeIf { configurations -> configurations.isNotEmpty() }
                 // Accepting the branch based on having at least one matching configuration
@@ -71,13 +67,13 @@ class AutoVersioningPromotionListenerServiceImpl(
             }
             // No auto versioning property for this branch
             else {
-                logger.debug("No auto versioning configuration at branch level")
+                // No auto versioning configuration at branch level
                 return null
             }
         }
         // No SCM configuration
         else {
-            logger.debug("No supported SCM configured at project level")
+            // No supported SCM configured at project level
             return null
         }
     }
@@ -85,35 +81,51 @@ class AutoVersioningPromotionListenerServiceImpl(
     private fun PromotionRun.match(
         eligibleTargetBranch: Branch,
         config: AutoVersioningSourceConfig,
+        cache: MutableMap<LatestSourceBranchCacheKey, LatestSourceBranchCacheValue>,
     ): Boolean {
-        if (logger.isDebugEnabled) {
-            logger.debug(
-                """
-                        | Promotion event matching:
-                        |    event=$this
-                        |    config=$config
-                        |    """.trimMargin()
-            )
-        }
         val match = config.sourceProject == build.project.name &&
                 config.sourcePromotion == promotionLevel.name &&
-                sourceBranchMatch(eligibleTargetBranch, config)
-        if (logger.isDebugEnabled) {
-            logger.debug("Promotion event matching: $match")
-        }
+                logger.logTime("AV Source Branch match") {
+                    sourceBranchMatch(eligibleTargetBranch, config, cache)
+                }
         return match
     }
 
     private fun PromotionRun.sourceBranchMatch(
         eligibleTargetBranch: Branch,
         config: AutoVersioningSourceConfig,
+        cache: MutableMap<LatestSourceBranchCacheKey, LatestSourceBranchCacheValue>,
     ): Boolean {
-        logger.debug("""Promotion event matching based on branch latest promotion""")
-        val latestSourceBranch =
-            autoVersioningConfigurationService.getLatestBranch(eligibleTargetBranch, build.project, config)
-        logger.debug("Latest branch: ${latestSourceBranch?.name}")
+        val cacheKey = LatestSourceBranchCacheKey(eligibleTargetBranch, build.project, config)
+        val latestSourceBranch = cache.getOrPut(cacheKey) {
+            LatestSourceBranchCacheValue(
+                autoVersioningConfigurationService.getLatestBranch(eligibleTargetBranch, build.project, config)
+            )
+        }.branch
         // We want the promoted build to be on the latest source branch
         return latestSourceBranch != null && latestSourceBranch.id() == build.branch.id()
     }
+
+    private data class LatestSourceBranchCacheKey(
+        private val targetBranchId: Int,
+        private val sourceProjectId: Int,
+        private val configSourceBranch: String,
+        private val configSourcePromotion: String,
+    ) {
+        constructor(
+            eligibleTargetBranch: Branch,
+            sourceProject: Project,
+            config: AutoVersioningSourceConfig,
+        ) : this(
+            targetBranchId = eligibleTargetBranch.id(),
+            sourceProjectId = sourceProject.id(),
+            configSourceBranch = config.sourceBranch,
+            configSourcePromotion = config.sourcePromotion,
+        )
+    }
+
+    private data class LatestSourceBranchCacheValue(
+        val branch: Branch?,
+    )
 
 }
--- ontrack-service/src/main/java/net/nemerosa/ontrack/service/events/EventListenerServiceImpl.kt
+++ ontrack-service/src/main/java/net/nemerosa/ontrack/service/events/EventListenerServiceImpl.kt
@@ -1,11 +1,13 @@
 package net.nemerosa.ontrack.service.events
 
 import io.micrometer.core.instrument.MeterRegistry
+import net.nemerosa.ontrack.common.logTime
 import net.nemerosa.ontrack.model.events.Event
 import net.nemerosa.ontrack.model.events.EventListener
 import net.nemerosa.ontrack.model.events.EventListenerService
 import net.nemerosa.ontrack.model.metrics.measure
 import net.nemerosa.ontrack.model.metrics.time
+import org.slf4j.LoggerFactory
 import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Service
 
@@ -14,6 +16,8 @@ class EventListenerServiceImpl(
     private val context: ApplicationContext,
     private val meterRegistry: MeterRegistry,
 ) : EventListenerService {
+
+    private val logger = LoggerFactory.getLogger(EventListenerServiceImpl::class.java)
 
     private val listeners: Collection<EventListener> by lazy {
         context.getBeansOfType(
@@ -32,7 +36,9 @@ class EventListenerServiceImpl(
             name = EventListenerMetrics.METRIC_ONTRACK_EVENT_LISTENER_TIME,
             EventListenerMetrics.TAG_EVENT_LISTENER to eventListener::class.java.name,
         ) {
-            eventListener.onEvent(event)
+            logger.logTime("Event listener ${eventListener.javaClass.simpleName}") {
+                eventListener.onEvent(event)
+            }
         }
     }
 }
