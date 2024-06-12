759f17484c9b507204e5a89524e07df871697e74
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-06-12 14:25:11 +0200

    #1292 Notification to validate builds

---
+++ ontrack-extension-notifications/src/main/java/net/nemerosa/ontrack/extension/notifications/core/OntrackValidationNotificationChannel.kt
@@ -0,0 +1,130 @@
+package net.nemerosa.ontrack.extension.notifications.core
+
+import com.fasterxml.jackson.databind.JsonNode
+import net.nemerosa.ontrack.extension.notifications.channels.AbstractNotificationChannel
+import net.nemerosa.ontrack.extension.notifications.channels.NotificationResult
+import net.nemerosa.ontrack.json.asJson
+import net.nemerosa.ontrack.model.annotations.APIDescription
+import net.nemerosa.ontrack.model.docs.Documentation
+import net.nemerosa.ontrack.model.events.Event
+import net.nemerosa.ontrack.model.events.EventTemplatingService
+import net.nemerosa.ontrack.model.exceptions.BranchNotFoundException
+import net.nemerosa.ontrack.model.exceptions.BuildNotFoundException
+import net.nemerosa.ontrack.model.exceptions.ProjectNotFoundException
+import net.nemerosa.ontrack.model.form.Form
+import net.nemerosa.ontrack.model.form.textField
+import net.nemerosa.ontrack.model.security.SecurityService
+import net.nemerosa.ontrack.model.structure.*
+import org.springframework.stereotype.Component
+import kotlin.jvm.optionals.getOrNull
+
+@Component
+@APIDescription("Validates a build in Ontrack")
+@Documentation(OntrackValidationNotificationChannelConfig::class)
+@Documentation(OntrackValidationNotificationChannelOutput::class, section = "output")
+class OntrackValidationNotificationChannel(
+    private val eventTemplatingService: EventTemplatingService,
+    private val structureService: StructureService,
+    private val securityService: SecurityService,
+) : AbstractNotificationChannel<OntrackValidationNotificationChannelConfig, OntrackValidationNotificationChannelOutput>(
+    OntrackValidationNotificationChannelConfig::class
+) {
+
+    override fun publish(
+        config: OntrackValidationNotificationChannelConfig,
+        event: Event,
+        context: Map<String, Any>,
+        template: String?,
+        outputProgressCallback: (current: OntrackValidationNotificationChannelOutput) -> OntrackValidationNotificationChannelOutput
+    ): NotificationResult<OntrackValidationNotificationChannelOutput> {
+
+        val build = securityService.asAdmin { getTargetBuild(config, event, context) }
+        val description = eventTemplatingService.renderEvent(event, context, template)
+
+        val run = securityService.asAdmin {
+            structureService.newValidationRun(
+                build = build,
+                validationRunRequest = ValidationRunRequest(
+                    validationStampName = config.validation,
+                    validationRunStatusId = ValidationRunStatusID.STATUS_PASSED,
+                    description = description,
+                )
+            )
+        }
+
+        return NotificationResult.ok(
+            OntrackValidationNotificationChannelOutput(
+                runId = run.id(),
+            )
+        )
+    }
+
+    internal fun getTargetBuild(
+        config: OntrackValidationNotificationChannelConfig,
+        event: Event,
+        context: Map<String, Any>
+    ): Build =
+        if (config.build.isNullOrBlank()) {
+            event.getEntity(ProjectEntityType.BUILD)
+        } else {
+            val buildName = eventTemplatingService.renderEvent(event, context, config.build)
+            val branch = getTargetBranch(config, event, context)
+            structureService.findBuildByName(
+                branch.project.name,
+                branch.name,
+                buildName
+            ).getOrNull()
+                ?: throw BuildNotFoundException(
+                    branch.project.name,
+                    branch.name,
+                    buildName
+                )
+        }
+
+    internal fun getTargetBranch(
+        config: OntrackValidationNotificationChannelConfig,
+        event: Event,
+        context: Map<String, Any>
+    ): Branch =
+        if (config.branch.isNullOrBlank()) {
+            event.getEntity(ProjectEntityType.BRANCH)
+        } else {
+            val branchName = eventTemplatingService.renderEvent(event, context, config.branch)
+            val project = getTargetProject(config, event, context)
+            structureService.findBranchByName(project.name, branchName).getOrNull()
+                ?: throw BranchNotFoundException(project.name, branchName)
+        }
+
+    internal fun getTargetProject(
+        config: OntrackValidationNotificationChannelConfig,
+        event: Event,
+        context: Map<String, Any>
+    ): Project =
+        if (config.project.isNullOrBlank()) {
+            event.getEntity(ProjectEntityType.PROJECT)
+        } else {
+            val projectName = eventTemplatingService.renderEvent(event, context, config.project)
+            structureService.findProjectByName(projectName).getOrNull()
+                ?: throw ProjectNotFoundException(projectName)
+        }
+
+    override fun toSearchCriteria(text: String): JsonNode =
+        mapOf(
+            OntrackValidationNotificationChannelConfig::validation.name to text
+        ).asJson()
+
+    @Deprecated("Will be removed in V5. Only Next UI is used.")
+    override fun toText(config: OntrackValidationNotificationChannelConfig): String = config.validation
+
+    @Deprecated("Will be removed in V5. Only Next UI is used.")
+    override fun getForm(c: OntrackValidationNotificationChannelConfig?): Form =
+        Form.create()
+            .textField(OntrackValidationNotificationChannelConfig::project, c?.project)
+            .textField(OntrackValidationNotificationChannelConfig::branch, c?.branch)
+            .textField(OntrackValidationNotificationChannelConfig::build, c?.build)
+            .textField(OntrackValidationNotificationChannelConfig::validation, c?.validation)
+
+    override val type: String = "ontrack-validation"
+    override val displayName: String = "Ontrack validation"
+    override val enabled: Boolean = true
+}
---
+++ ontrack-web-core/components/framework/notification-channel/ontrack-validation/Output.js
@@ -0,0 +1,7 @@
+export default function OntrackValidationNotificationChannelOutput() {
+    return (
+        <>
+            Validation created.
+        </>
+    )
+}
