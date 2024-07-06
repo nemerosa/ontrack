963de2aa7056e32a5e4e9da9b2f1d33404a42811
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-07-06 10:48:50 +0200

    #1272 Fixing notification source data

--- ontrack-extension-notifications/src/main/java/net/nemerosa/ontrack/extension/notifications/sources/EntitySubscriptionNotificationSourceDataType.kt
+++ ontrack-extension-notifications/src/main/java/net/nemerosa/ontrack/extension/notifications/sources/EntitySubscriptionNotificationSourceDataType.kt
@@ -3,7 +3,7 @@ package net.nemerosa.ontrack.extension.notifications.sources
 import net.nemerosa.ontrack.model.structure.ProjectEntityType
 
 data class EntitySubscriptionNotificationSourceDataType(
-    private val entityType: ProjectEntityType,
-    private val entityId: Int,
-    private val subscriptionName: String,
+    val entityType: ProjectEntityType,
+    val entityId: Int,
+    val subscriptionName: String,
 )
--- ontrack-extension-notifications/src/main/java/net/nemerosa/ontrack/extension/notifications/sources/GlobalSubscriptionNotificationSourceDataType.kt
+++ ontrack-extension-notifications/src/main/java/net/nemerosa/ontrack/extension/notifications/sources/GlobalSubscriptionNotificationSourceDataType.kt
@@ -1,5 +1,5 @@
 package net.nemerosa.ontrack.extension.notifications.sources
 
 data class GlobalSubscriptionNotificationSourceDataType(
-    private val subscriptionName: String,
+    val subscriptionName: String,
 )
