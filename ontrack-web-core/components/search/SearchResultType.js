93028d94f4a0def38fe54dff73ed26e17e9e4b8a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-07-08 17:07:54 +0200

    #1320 Filtering on type

--- ontrack-web-core/components/search/SearchResultType.js
+++ ontrack-web-core/components/search/SearchResultType.js
@@ -1,17 +1,27 @@
-import {Popover, Typography} from "antd";
+import {Popover, Space, Typography} from "antd";
 import SearchResultTypeIcon from "@components/search/SearchResultTypeIcon";
 
-export default function SearchResultType({type}) {
+export default function SearchResultType({type, displayName = false, popover = true}) {
     return (
         <>
-            <Popover
-                title={type.name}
-                content={<Typography.Text type="secondary">{type.description}</Typography.Text>}
-            >
-                <div>
+            {
+                popover &&
+                <Popover
+                    title={type.name}
+                    content={<Typography.Text type="secondary">{type.description}</Typography.Text>}
+                >
+                    <Space>
+                        <SearchResultTypeIcon type={type}/>
+                        {displayName && type.name}
+                    </Space>
+                </Popover>
+            }
+            {
+                !popover && <Space>
                     <SearchResultTypeIcon type={type}/>
-                </div>
-            </Popover>
+                    {displayName && type.name}
+                </Space>
+            }
         </>
     )
 }
--- ontrack-web-core/components/search/SearchView.js
+++ ontrack-web-core/components/search/SearchView.js
@@ -6,24 +6,29 @@ import {homeUri} from "@components/common/Links";
 import {CloseCommand} from "@components/common/Commands";
 import {homeBreadcrumbs} from "@components/common/Breadcrumbs";
 import MainPage from "@components/layouts/MainPage";
-import {Skeleton} from "antd";
+import {Button, Popover, Skeleton, Space} from "antd";
 import {useEffect, useState} from "react";
 import {gql} from "graphql-request";
 import SearchResultList from "@components/search/SearchResultList";
+import SearchResultType from "@components/search/SearchResultType";
+import {FaBan} from "react-icons/fa";
 
 export default function SearchView({q}) {
 
     const client = useGraphQLClient()
     const [searching, setSearching] = useState(true)
+    const [selectedType, setSelectedType] = useState('')
     const [results, setResults] = useState([])
+
+    const {searchResultTypes} = useRefData()
 
     useEffect(() => {
         if (client && q) {
             setSearching(true)
             client.request(
                 gql`
-                    query SearchWithType($q: String!) {
-                        search(offset: 0, size: 10, token: $q) {
+                    query SearchWithType($type: String, $q: String!) {
+                        search(offset: 0, size: 10, type: $type, token: $q) {
                             pageInfo {
                                 totalSize
                                 nextPage {
@@ -47,6 +52,7 @@ export default function SearchView({q}) {
                 `,
                 {
                     q: q,
+                    type: selectedType,
                 }
             ).then(data => {
                 setResults(data.search.pageItems)
@@ -54,7 +60,7 @@ export default function SearchView({q}) {
                 setSearching(false)
             })
         }
-    }, [client, q])
+    }, [client, q, selectedType])
 
     return (
         <>
@@ -69,7 +75,37 @@ export default function SearchView({q}) {
                 ]}
             >
                 <Skeleton active loading={searching}>
-                    <SearchResultList results={results}/>
+                    <Space direction="vertical" className="ot-line">
+                        <Space direction="horizontal" wrap className="ot-line">
+                            <Button
+                                style={{height: 48}}
+                                onClick={() => setSelectedType('')}
+                                icon={<FaBan/>}
+                                type={selectedType === '' ? "primary" : "default"}
+                            >
+                                All types
+                            </Button>
+                            {
+                                searchResultTypes.map(type => (
+                                    <>
+                                        <Popover
+                                            key={type.id}
+                                            title={`Filters results on ${type.name}`}
+                                        >
+                                            <Button
+                                                style={{height: 48}}
+                                                onClick={() => setSelectedType(type.id)}
+                                                type={selectedType === type.id ? "primary" : "default"}
+                                            >
+                                                <SearchResultType type={type} displayName={true} popover={false}/>
+                                            </Button>
+                                        </Popover>
+                                    </>
+                                ))
+                            }
+                        </Space>
+                        <SearchResultList results={results}/>
+                    </Space>
                 </Skeleton>
             </MainPage>
         </>
