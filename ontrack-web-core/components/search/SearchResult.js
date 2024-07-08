b7c062fd249a9d91a3b4c8d07944f6f37edae712
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-07-08 17:00:20 +0200

    #1320 Displaying the accuracy

--- ontrack-web-core/components/search/SearchResult.js
+++ ontrack-web-core/components/search/SearchResult.js
@@ -1,12 +1,16 @@
 import {Dynamic} from "@components/common/Dynamic";
 import {Space} from "antd";
 import SearchResultType from "@components/search/SearchResultType";
+import SearchResultAccuracy from "@components/search/SearchResultAccuracy";
 
 export default function SearchResult({result}) {
     return (
         <>
             <Space>
-                <SearchResultType type={result.type}/>
+                <Space direction="vertical" size={8}>
+                    <SearchResultType type={result.type}/>
+                    <SearchResultAccuracy accuracy={result.accuracy}/>
+                </Space>
                 <Dynamic path={`framework/search/${result.type.id}/Result`} props={result}/>
             </Space>
         </>
