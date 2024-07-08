3d49b9adea4ebabb3cfddc8d6c2ba408138227c9
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-07-08 19:48:44 +0200

    #1320 Search result for a Git issue

--- ontrack-web-core/components/framework/search/git-issue/Result.js
+++ ontrack-web-core/components/framework/search/git-issue/Result.js
@@ -1,8 +1,11 @@
 import SearchResultComponent from "@components/framework/search/SearchResultComponent";
+import LegacyLink from "@components/common/LegacyLink";
 
-export default function Result({data}) {
+export default function Result({data, page}) {
     return <SearchResultComponent
-        title="TODO"
-        description={JSON.stringify(data)}
+        title={
+            <LegacyLink href={page}>{data.item.displayKey}</LegacyLink>
+        }
+        description=""
     />
 }
