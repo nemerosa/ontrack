61ddee341023c5ad5e6baf64de9cc10f7ec74588
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-07-09 15:58:56 +0200

    #1320 No result found

--- ontrack-web-core/components/search/SearchResultList.js
+++ ontrack-web-core/components/search/SearchResultList.js
@@ -1,17 +1,24 @@
-import {Space} from "antd";
+import {Empty, Space} from "antd";
 import SearchResult from "@components/search/SearchResult";
 
 export default function SearchResultList({results}) {
     return (
         <>
-            <Space direction="vertical">
-                {
-                    results.map((result, index) => <SearchResult
-                        key={index}
-                        result={result}
-                    />)
-                }
-            </Space>
+            {
+                results.length > 0 &&
+                <Space direction="vertical">
+                    {
+                        results.map((result, index) => <SearchResult
+                            key={index}
+                            result={result}
+                        />)
+                    }
+                </Space>
+            }
+            {
+                results.length === 0 &&
+                <Empty description={"No results found."}/>
+            }
         </>
     )
 }
