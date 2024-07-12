8efc37c61c673f46c7ab938a29dd032ccadd3885
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-07-12 18:17:57 +0200

    dagger: build environment

--- dagger/src/index.ts
+++ dagger/src/index.ts
@@ -13,29 +13,21 @@
  * rest is a long description with more detail on the module's purpose or usage,
  * if appropriate. All modules should have a short description.
  */
-import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
+import {dag, Container, Directory, object, func} from "@dagger.io/dagger"
 
 @object()
 class Dagger {
-  /**
-   * Returns a container that echoes whatever string argument is provided
-   */
-  @func()
-  containerEcho(stringArg: string): Container {
-    return dag.container().from("alpine:latest").withExec(["echo", stringArg])
-  }
 
-  /**
-   * Returns lines that match a pattern in the files of the provided Directory
-   */
-  @func()
-  async grepDir(directoryArg: Directory, pattern: string): Promise<string> {
-    return dag
-      .container()
-      .from("alpine:latest")
-      .withMountedDirectory("/mnt", directoryArg)
-      .withWorkdir("/mnt")
-      .withExec(["grep", "-R", pattern, "."])
-      .stdout()
-  }
+    /**
+     * Build environment
+     */
+    @func()
+    buildEnv(source: Directory): Container {
+        return dag
+            .container()
+            .from("azul/zulu-openjdk:17.0.6")
+            .withDirectory("/src", source)
+            .withWorkdir("/src")
+    }
+
 }
