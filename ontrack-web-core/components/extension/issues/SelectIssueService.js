e2240d5569d8089a4f9f9988428ae4b9dec9df27
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-07-29 10:40:49 +0200

    #1275 Issue service configurations select component

--- ontrack-web-core/components/extension/issues/SelectIssueService.js
+++ ontrack-web-core/components/extension/issues/SelectIssueService.js
@@ -1,28 +1,56 @@
 import {Select, Space, Typography} from "antd";
 import {useEffect, useState} from "react";
+import {useGraphQLClient} from "@components/providers/ConnectionContextProvider";
+import {gql} from "graphql-request";
 
 export default function SelectIssueService({value, onChange, self}) {
 
+    const client = useGraphQLClient()
+
+    const [loading, setLoading] = useState(false)
     const [options, setOptions] = useState([])
-    const [selfAdded, setSelfAdded] = useState(false)
 
     useEffect(() => {
-        if (self && !selfAdded) {
-            setOptions(items => [...items, {
-                value: "self",
-                label: <Space>
-                    <Typography.Text>{self}</Typography.Text>
-                    <Typography.Text type="secondary">[self]</Typography.Text>
-                </Space>,
-            }])
-            setSelfAdded(true)
+        if (client) {
+            setLoading(true)
+            client.request(
+                gql`
+                    query GetIssueServicesConfigurations {
+                        issueServiceConfigurations {
+                            name
+                            id
+                        }
+                    }
+                `
+            ).then(data => {
+                const options = data.issueServiceConfigurations.map(({name, id}) => ({
+                    value: id,
+                    label: <Space>
+                        <Typography.Text>{name}</Typography.Text>
+                        <Typography.Text type="secondary">[{id}]</Typography.Text>
+                    </Space>,
+                }))
+                if (self) {
+                    options.push({
+                        value: "self",
+                        label: <Space>
+                            <Typography.Text>{self}</Typography.Text>
+                            <Typography.Text type="secondary">[self]</Typography.Text>
+                        </Space>,
+                    })
+                }
+                setOptions(options)
+            }).finally(() => {
+                setLoading(false)
+            })
         }
-    }, [self, selfAdded])
+    }, [client, self])
 
     return (
         <>
             <Select
                 options={options}
+                loading={loading}
                 value={value}
                 onChange={onChange}
                 allowClear={true}
