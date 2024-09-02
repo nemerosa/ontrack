97b7bf7279c7a1e2d33beaa7ba6feadc6cbb2f05
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-09-02 17:43:38 +0200

    ci: disabling Playwright parallel tests

--- ontrack-web-tests/playwright.config.js
+++ ontrack-web-tests/playwright.config.js
@@ -13,13 +13,14 @@ const {defineConfig, devices} = require('@playwright/test');
 module.exports = defineConfig({
     testDir: './tests',
     /* Run tests in files in parallel */
-    fullyParallel: true,
+    fullyParallel: false,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    workers: 1,
+    // workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
         ['junit', {outputFile: 'reports/junit/report.xml'}],
