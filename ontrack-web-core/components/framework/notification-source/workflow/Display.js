06a7727f36727be63c8be4f43568c912af468565
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-09-16 14:46:13 +0200

    #1350 Link to workflow from the notifications is not correct

--- ontrack-web-core/components/framework/notification-source/workflow/Display.js
+++ ontrack-web-core/components/framework/notification-source/workflow/Display.js
@@ -1,17 +1,20 @@
 import {Button, Space, Typography} from "antd";
 import {FaProjectDiagram} from "react-icons/fa";
 import ShowWorkflowInstanceStatus from "@components/extension/workflows/ShowWorkflowInstanceStatus";
+import Link from "next/link";
 
 export default function WorkflowNotificationSource({workflowInstanceId}) {
     return (
         <>
-            <Button href={`/extension/workflows/instances/${workflowInstanceId}`}>
-                <Space>
-                    <FaProjectDiagram/>
-                    <Typography.Text>Workflow</Typography.Text>
-                    <ShowWorkflowInstanceStatus instanceId={workflowInstanceId}/>
-                </Space>
-            </Button>
+            <Link href={`/extension/workflows/instances/${workflowInstanceId}`} passHref>
+                <Button>
+                    <Space>
+                        <FaProjectDiagram/>
+                        <Typography.Text>Workflow</Typography.Text>
+                        <ShowWorkflowInstanceStatus instanceId={workflowInstanceId}/>
+                    </Space>
+                </Button>
+            </Link>
         </>
 
     )
