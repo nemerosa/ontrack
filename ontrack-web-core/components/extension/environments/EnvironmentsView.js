2e82a76acad13bed41c47280d4d566299bfebfeb
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-10-12 19:19:21 +0200

    #1235 Environments basic list

--- ontrack-web-core/components/extension/environments/EnvironmentsView.js
+++ ontrack-web-core/components/extension/environments/EnvironmentsView.js
@@ -4,8 +4,40 @@ import {homeBreadcrumbs} from "@components/common/Breadcrumbs";
 import {CloseCommand} from "@components/common/Commands";
 import {homeUri} from "@components/common/Links";
 import MainPage from "@components/layouts/MainPage";
+import {useGraphQLClient} from "@components/providers/ConnectionContextProvider";
+import {useEffect, useState} from "react";
+import {gql} from "graphql-request";
 
 export default function EnvironmentsView() {
+
+    const client = useGraphQLClient()
+
+    const [loading, setLoading] = useState(false)
+    const [environments, setEnvironments] = useState([])
+
+    useEffect(() => {
+        if (client) {
+            setLoading(true)
+            client.request(
+                gql`
+                    query EnvironmentList {
+                        environments {
+                            id
+                            name
+                            description
+                            order
+                            tags
+                        }
+                    }
+                `
+            ).then(data => {
+                setEnvironments(data.environments)
+            }).finally(() => {
+                setLoading(false)
+            })
+        }
+    }, [client])
+
     return (
         <>
             <Head>
@@ -18,6 +50,7 @@ export default function EnvironmentsView() {
                     <CloseCommand key="close" href={homeUri()}/>
                 ]}
             >
+                {JSON.stringify(environments)}
             </MainPage>
         </>
     )
