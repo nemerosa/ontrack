ca5d7240d6c885d365254cdde79fa64e557c86d7
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-11-30 07:56:58 +0100

    #1363 Logging including the workflow name

--- ontrack-extension-workflows/src/main/java/net/nemerosa/ontrack/extension/workflows/engine/WorkflowEngineImpl.kt
+++ ontrack-extension-workflows/src/main/java/net/nemerosa/ontrack/extension/workflows/engine/WorkflowEngineImpl.kt
@@ -78,39 +78,52 @@ class WorkflowEngineImpl(
         return instance
     }
 
+    private fun debug(message: String, instance: WorkflowInstance, nodeId: String) {
+        logger.debug("WORKFLOW {} INSTANCE {} NODE {}: {}", instance.workflow.name, instance.id, nodeId, message)
+    }
+
+    private fun error(message: String, instance: WorkflowInstance, nodeId: String, error: Throwable? = null) {
+        val logMessage = "WORKFLOW ${instance.workflow.name} INSTANCE ${instance.id} NODE $nodeId: $message"
+        if (error != null) {
+            logger.error(logMessage, error)
+        } else {
+            logger.error(logMessage)
+        }
+    }
+
     override fun processNode(workflowInstanceId: String, workflowNodeId: String) {
         // Getting the instance
         val instance = getWorkflowInstanceTx(workflowInstanceId)
         val node = instance.workflow.getNode(workflowNodeId)
         val nodeExecution = instance.getNode(workflowNodeId)
-        logger.debug("{} INSTANCE {} NODE RECEIVED", workflowInstanceId, workflowNodeId)
+        debug("NODE RECEIVED", instance, workflowNodeId)
         // Checks of the instance has been interrupted or not
         if (instance.status.finished) {
-            logger.debug("{} INSTANCE {} NODE INSTANCE FINISHED", workflowInstanceId, workflowNodeId)
+            debug("NODE INSTANCE FINISHED", instance, workflowNodeId)
             return
         }
         // Checks the instance node status
         if (nodeExecution.status.finished) {
-            logger.debug("{} INSTANCE {} NODE FINISHED", workflowInstanceId, workflowNodeId)
+            debug("NODE FINISHED", instance, workflowNodeId)
             return
         } else if (nodeExecution.status != WorkflowInstanceNodeStatus.CREATED) {
-            logger.error("{} INSTANCE {} NODE NOT IN CREATED STATE", workflowInstanceId, workflowNodeId)
+            error("NODE NOT IN CREATED STATE", instance, workflowNodeId)
             return
         }
         // Starting the job for the node
         CoroutineScope(Dispatchers.Default).launch {
-            logger.debug("{} INSTANCE {} NODE PROCESSING", workflowInstanceId, workflowNodeId)
+            debug("NODE PROCESSING", instance, workflowNodeId)
             // Changes the node's status to WAITING
-            logger.debug("{} INSTANCE {} NODE WAITING", workflowInstanceId, workflowNodeId)
+            debug("NODE WAITING", instance, workflowNodeId)
             nodeWaiting(workflowInstanceId, workflowNodeId)
             // Waiting for the parent nodes to be OK
             val okToStart = if (node.parents.isNotEmpty()) {
                 val parentJobs = node.parents.map {
                     createParentJob(this, instance, it)
                 }
-                logger.debug("{} INSTANCE {} NODE WAITING FOR PARENTS", workflowInstanceId, workflowNodeId)
+                debug("NODE WAITING FOR PARENTS", instance, workflowNodeId)
                 val parentStatuses = parentJobs.awaitAll()
-                logger.debug("{} INSTANCE {} NODE WAITED FOR PARENTS", workflowInstanceId, workflowNodeId)
+                debug("NODE WAITED FOR PARENTS", instance, workflowNodeId)
                 // TODO OK to start depending on the conditions
                 val parentsOK = parentStatuses.all { it.status == WorkflowInstanceNodeStatus.SUCCESS }
                 // Checking the instance state again
@@ -125,18 +138,18 @@ class WorkflowEngineImpl(
             }
             // Starting the node execution
             if (okToStart) {
-                logger.debug("{} INSTANCE {} NODE STARTED", workflowInstanceId, workflowNodeId)
+                debug("NODE STARTED", instance, workflowNodeId)
                 nodeStarted(workflowInstanceId, workflowNodeId)
                 // Loading a fresh instance before starting
                 val freshInstance = getWorkflowInstanceTx(workflowInstanceId)
                 // Starts the node execution
                 nodeExecution(freshInstance, workflowNodeId)
             } else {
-                logger.debug("{} INSTANCE {} NODE PARENT NOT OK", workflowInstanceId, workflowNodeId)
+                debug("NODE PARENT NOT OK", instance, workflowNodeId)
                 nodeCancelled(workflowInstanceId, workflowNodeId, "Parents conditions were not met.")
             }
         }
-        logger.debug("{} INSTANCE {} NODE LAUNCHED", workflowInstanceId, workflowNodeId)
+        debug("NODE LAUNCHED", instance, workflowNodeId)
     }
 
     private fun getWorkflowInstanceTx(workflowInstanceId: String) = transactionHelper.inNewTransaction {
@@ -190,29 +203,29 @@ class WorkflowEngineImpl(
             // Running the executor
             val result = withTimeoutOrNull(timeout.toMillis()) {
                 val deferred = async {
-                    logger.debug("{} INSTANCE {} NODE EXECUTING", instance.id, nodeId)
+                    debug("NODE EXECUTING", instance, nodeId)
                     executor.execute(instance, node.id, nodeFeedback)
                 }
                 val outcome = deferred.await()
-                logger.debug("{} INSTANCE {} NODE EXECUTED (type = {})", instance.id, nodeId, outcome.type)
+                debug("NODE EXECUTED (type = ${outcome.type})", instance, nodeId)
                 outcome
             }
 
             // Timeout?
             if (result == null) {
-                logger.debug("{} INSTANCE {} NODE TIMEOUT", instance.id, nodeId)
+                debug("NODE TIMEOUT", instance, nodeId)
                 nodeError(instance.id, nodeId, "Timeout", null)
             }
             // Progressing the instance or stopping it in case of error
             else {
                 when (result.type) {
                     WorkflowNodeExecutorResultType.ERROR -> {
-                        logger.debug("{} INSTANCE {} NODE ERROR", instance.id, nodeId)
+                        debug("NODE ERROR (message = ${result.message})", instance, nodeId)
                         nodeError(instance.id, nodeId, result.message, result.output)
                     }
 
                     WorkflowNodeExecutorResultType.SUCCESS -> {
-                        logger.debug("{} INSTANCE {} NODE SUCCESS", instance.id, nodeId)
+                        debug("NODE SUCCESS", instance, nodeId)
                         // Stores the output back into the instance and progresses the node's status
                         nodeSuccess(instance.id, nodeId, result.output, result.event)
                     }
@@ -220,7 +233,7 @@ class WorkflowEngineImpl(
             }
         } catch (any: Throwable) {
             // Stores the node error status
-            logger.error("${instance.id} INSTANCE $nodeId NODE UNCAUGHT ERROR", any)
+            error("NODE UNCAUGHT ERROR", instance, nodeId, any)
             nodeError(instance.id, nodeId, any.message, null)
         }
     }
