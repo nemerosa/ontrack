65290c78a434ae08430b936affb664a9747e3fa0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2024-12-15 14:13:54 +0100

    ci: increased the timeout for the legacy page

--- ontrack-web-tests/tests/core/legacy/legacyHome.js
+++ ontrack-web-tests/tests/core/legacy/legacyHome.js
@@ -7,11 +7,11 @@ export class LegacyHomePage {
     }
 
     async checkCreateProject() {
-        await expect(this.page.getByRole('button', {name: 'Create Project'})).toBeVisible()
+        await expect(this.page.getByRole('button', {name: 'Create Project'})).toBeVisible({timeout: 15_000})
     }
 
     async checkNextUI() {
-        await expect(this.page.getByRole('button', {name: 'Next UI'})).toBeVisible()
+        await expect(this.page.getByRole('button', {name: 'Next UI'})).toBeVisible({timeout: 15_000})
     }
 
 }
