61f4509ada5eeec002895079134e2685bb34a6b2
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-01-04 10:32:14 +0100

    ci: improving stability of tests which need isolation

--- ontrack-it-utils/src/main/java/net/nemerosa/ontrack/it/NewTxRollbackedExtension.kt
+++ ontrack-it-utils/src/main/java/net/nemerosa/ontrack/it/NewTxRollbackedExtension.kt
@@ -3,26 +3,33 @@ package net.nemerosa.ontrack.it
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback
 import org.junit.jupiter.api.extension.ExtensionContext
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.TransactionDefinition
 import org.springframework.transaction.TransactionStatus
-import org.springframework.transaction.support.TransactionTemplate
+import org.springframework.transaction.support.DefaultTransactionDefinition
+
 
 class NewTxRollbackedExtension : BeforeTestExecutionCallback, AfterTestExecutionCallback {
 
+    private val logger: Logger = LoggerFactory.getLogger(NewTxRollbackedExtension::class.java)
+
     override fun beforeTestExecution(exc: ExtensionContext) {
         val transactionManager = getTransactionManager(exc)
-        val transactionTemplate = TransactionTemplate(transactionManager)
-        transactionTemplate.propagationBehavior = TransactionDefinition.PROPAGATION_REQUIRES_NEW
-        val status = transactionManager.getTransaction(transactionTemplate)
+        val definition = DefaultTransactionDefinition()
+        definition.propagationBehavior = TransactionDefinition.PROPAGATION_REQUIRES_NEW
+        val status = transactionManager.getTransaction(definition)
         exc.getStore(namespace).put(TX_STATUS, status)
+        logger.info("[${exc.displayName}] TX NEW")
     }
 
     override fun afterTestExecution(exc: ExtensionContext) {
         val transactionManager = getTransactionManager(exc)
         val status = exc.getStore(namespace).get(TX_STATUS, TransactionStatus::class.java)
         transactionManager.rollback(status)
+        logger.info("[${exc.displayName}] TX ROLLBACK")
     }
 
     companion object {
