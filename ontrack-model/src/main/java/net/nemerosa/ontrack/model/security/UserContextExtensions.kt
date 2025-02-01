260c495a005283dae72330d0bdd44290b6f52a94
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-02-01 08:43:25 +0100

    #1395 Checking global functions

--- ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/UserContextExtensions.kt
+++ ontrack-model/src/main/java/net/nemerosa/ontrack/model/security/UserContextExtensions.kt
@@ -1,12 +1,30 @@
 package net.nemerosa.ontrack.model.security
 
 import net.nemerosa.ontrack.model.structure.ProjectEntity
+import org.springframework.security.access.AccessDeniedException
+import kotlin.reflect.KClass
 
 /**
  * Inline variant of [UserContext.isGlobalFunctionGranted]
  */
 inline fun <reified T : GlobalFunction> UserContext.isGlobalFunctionGranted() =
     isGlobalFunctionGranted(T::class)
+
+/**
+ * Checking the access to a global function
+ */
+fun UserContext.checkGlobalFunction(fn: KClass<out GlobalFunction>) {
+    if (!isGlobalFunctionGranted(fn)) {
+        throw AccessDeniedException("Global function '${fn.simpleName}' is not granted.")
+    }
+}
+
+/**
+ * Checking the access to a global function (inline variant)
+ */
+inline fun <reified T : GlobalFunction> UserContext.checkGlobalFunction() {
+    checkGlobalFunction(T::class)
+}
 
 /**
  * Inline variant of [UserContext.isProjectFunctionGranted]
