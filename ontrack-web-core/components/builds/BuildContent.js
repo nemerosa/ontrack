26f269f7e248828cbf52088611439eedb6489344
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-03-10 09:13:40 +0100

    #1399 Environments panel

--- ontrack-web-core/components/builds/BuildContent.js
+++ ontrack-web-core/components/builds/BuildContent.js
@@ -4,21 +4,47 @@ import StoredGridLayout from "@components/grid/StoredGridLayout";
 import BuildContentLinksUsedBy from "@components/builds/BuildContentLinksUsedBy";
 import BuildContentNotifications from "@components/builds/BuildContentNotifications";
 import BuildContentPromotions from "@components/builds/BuildContentPromotions";
+import {UserContext} from "@components/providers/UserProvider";
+import {useContext} from "react";
+import BuildContentEnvironments from "@components/builds/BuildContentEnvironments";
 
 export default function BuildContent({build}) {
 
-    const defaultLayout = [
-        {i: "promotions", x: 0, y: 0, w: 4, h: 9},
-        {i: "validations", x: 6, y: 0, w: 8, h: 9},
-        {i: "using", x: 0, y: 9, w: 6, h: 9},
-        {i: "usedBy", x: 6, y: 9, w: 6, h: 9},
-        {i: "notifications", x: 0, y: 18, w: 12, h: 9},
-    ]
+    const user = useContext(UserContext)
+
+    let defaultLayout = []
+    let layoutName = ''
+    if (user.authorizations.environment?.view) {
+        defaultLayout = [
+            {i: "promotions", x: 0, y: 0, w: 6, h: 9},
+            {i: "environments", x: 6, y: 0, w: 6, h: 9},
+            {i: "validations", x: 0, y: 9, w: 12, h: 9},
+            {i: "using", x: 0, y: 18, w: 6, h: 9},
+            {i: "usedBy", x: 6, y: 18, w: 6, h: 9},
+            {i: "notifications", x: 0, y: 27, w: 12, h: 9},
+        ]
+        layoutName = "page-build-layout-v5-env"
+    } else {
+        defaultLayout = [
+            {i: "promotions", x: 0, y: 0, w: 4, h: 9},
+            {i: "validations", x: 6, y: 0, w: 8, h: 9},
+            {i: "using", x: 0, y: 9, w: 6, h: 9},
+            {i: "usedBy", x: 6, y: 9, w: 6, h: 9},
+            {i: "notifications", x: 0, y: 18, w: 12, h: 9},
+        ]
+        layoutName = "page-build-layout-v5-noenv"
+    }
 
     const items = [
         {
             id: "promotions",
             content: <BuildContentPromotions
+                build={build}
+            />,
+        },
+        {
+            id: "environments",
+            content: <BuildContentEnvironments
                 build={build}
             />,
         },
@@ -51,7 +77,7 @@ export default function BuildContent({build}) {
     return (
         <>
             <StoredGridLayout
-                id="page-build-layout-v4"
+                id={layoutName}
                 defaultLayout={defaultLayout}
                 items={items}
                 rowHeight={30}
---
+++ ontrack-web-core/components/builds/BuildContentEnvironments.js
@@ -0,0 +1,11 @@
+import GridCell from "@components/grid/GridCell";
+
+export default function BuildContentEnvironments({build}) {
+    return (
+        <>
+            <GridCell id="environments" title="Environments" loading={false} padding={true}>
+                TODO
+            </GridCell>
+        </>
+    )
+}
