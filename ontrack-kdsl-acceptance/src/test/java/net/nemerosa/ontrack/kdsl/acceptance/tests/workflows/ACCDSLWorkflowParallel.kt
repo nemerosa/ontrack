780806d204b2def1d2a64e52c0c0abeb6b574aaf
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-03-24 13:48:42 +0100

    Revert "#1422 Deadlock detection when stopping a workflow"
    This reverts commit 50a2befeec6cfaed39eab4427c3a8bebc6e31e36.

--- ontrack-kdsl-acceptance/src/test/java/net/nemerosa/ontrack/kdsl/acceptance/tests/workflows/ACCDSLWorkflowParallel.kt
+++ ontrack-kdsl-acceptance/src/test/java/net/nemerosa/ontrack/kdsl/acceptance/tests/workflows/ACCDSLWorkflowParallel.kt
@@ -2,142 +2,14 @@ package net.nemerosa.ontrack.kdsl.acceptance.tests.workflows
 
 import net.nemerosa.ontrack.kdsl.acceptance.tests.support.uid
 import net.nemerosa.ontrack.kdsl.acceptance.tests.support.waitUntil
-import net.nemerosa.ontrack.kdsl.spec.extension.workflows.WorkflowInstanceNodeStatus
 import net.nemerosa.ontrack.kdsl.spec.extension.workflows.WorkflowInstanceStatus
 import net.nemerosa.ontrack.kdsl.spec.extension.workflows.mock.mock
 import net.nemerosa.ontrack.kdsl.spec.extension.workflows.workflows
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertNotNull
 import kotlin.test.fail
 
 class ACCDSLWorkflowParallel : AbstractACCDSLWorkflowsTestSupport() {
-
-
-    /**
-     * Regression test for
-     *
-     * #1422 Postgres deadlock when dealing with parallel nodes failing at the same time
-     */
-    @Test
-    fun `Parallel nodes running immediately in parallel and one failing`() {
-        project {
-            branch {
-                val subName = uid("pl-")
-                val pl = promotion().apply {
-                    subscribe(
-                        name = subName,
-                        channel = "workflow",
-                        channelConfig = mapOf(
-                            "workflow" to WorkflowTestSupport.yamlWorkflowToJson(
-                                """
-                                    name: Parallel workflow
-                                    nodes:
-                                      - id: csv
-                                        executorId: mock
-                                        data:
-                                            text: CSV
-                                      - id: promotion
-                                        executorId: mock
-                                        data:
-                                            text: Promotion
-                                      - id: diff
-                                        executorId: mock
-                                        data:
-                                            text: Diff
-                                            error: true
-                                      - id: test1
-                                        executorId: mock
-                                        data:
-                                            text: Test1
-                                      - id: test2
-                                        executorId: mock
-                                        data:
-                                            text: Test2
-                                      - id: test3
-                                        executorId: mock
-                                        data:
-                                            text: Test3
-                                      - id: test4
-                                        executorId: mock
-                                        data:
-                                            text: Test4
-                                      - id: test5
-                                        executorId: mock
-                                        data:
-                                            text: Test5
-                                      - id: audit
-                                        parents:
-                                          - id: promotion
-                                        executorId: mock
-                                        data:
-                                            text: Audit
-                                      - id: validation
-                                        parents:
-                                          - id: csv
-                                          - id: diff
-                                          - id: test1
-                                          - id: test2
-                                          - id: test3
-                                          - id: test4
-                                          - id: test5
-                                          - id: audit
-                                        executorId: mock
-                                        data:
-                                            text: Validation
-                                """.trimIndent()
-                            )
-                        ),
-                        keywords = null,
-                        contentTemplate = null,
-                        events = listOf("new_promotion_run"),
-                    )
-                }
-
-                build {
-                    // This launches the workflow
-                    promote(pl.name)
-
-                    // Gets the workflow instance ID from the output of the notification
-                    waitUntil(
-                        task = "Getting the workflow instance id",
-                        timeout = 10_000L,
-                        interval = 500L,
-                    ) {
-                        val instanceId = getWorkflowInstanceId(subName)
-                        instanceId.isNullOrBlank().not()
-                    }
-
-                    // Getting the instance ID
-                    val instanceId = getWorkflowInstanceId(subName) ?: fail("Cannot get the workflow instance ID")
-
-                    // Waits until the workflow is finished
-                    val instance = waitUntilWorkflowFinished(
-                        instanceId = instanceId,
-                        returnInstanceOnError = true,
-                        timeout = 10_000L,
-                    )
-
-                    // Checks the instance is marked as error because of one node
-                    assertEquals(WorkflowInstanceStatus.ERROR, instance.status)
-
-                    // Getting the nodes in error
-                    val errors = instance.nodesExecutions.filter { it.status == WorkflowInstanceNodeStatus.ERROR }
-                    assertEquals(1, errors.size, "Only one node to be failed")
-                    val error = errors.first()
-                    val message = error.error
-                    assertNotNull(message, "Message set") {
-                        assertFalse(
-                            it.contains("deadlock detected", ignoreCase = true),
-                            "No deadlock detected"
-                        )
-                    }
-
-                }
-            }
-        }
-    }
 
     @Test
     fun `Running a parallel workflow in parallel`() {
