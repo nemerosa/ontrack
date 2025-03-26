e31ac019a4bdff8a0045ca726dee1114257323ca
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-03-26 16:00:18 +0100

    #1442 Upgrade of JJWT & Mockk

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,6 +5,6 @@ object Versions {
     const val springBootVersion = "2.7.18"
     const val kotlinVersion = "1.9.25"
     const val kotlinCoroutinesVersion = "1.9.0"
-    const val jjwtVersion = "0.11.2"
-    const val mockkVersion = "1.12.0"
+    const val jjwtVersion = "0.12.6"
+    const val mockkVersion = "1.13.17"
 }
