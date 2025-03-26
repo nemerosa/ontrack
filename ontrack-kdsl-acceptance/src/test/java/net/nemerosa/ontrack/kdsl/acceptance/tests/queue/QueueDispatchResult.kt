ddd40f375b7926cfda521610ac98cfe9317e404c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-03-26 12:06:52 +0100

    #1440 Auto-versioning queue framework - fixing routing key reporting

--- ontrack-kdsl-acceptance/src/test/java/net/nemerosa/ontrack/kdsl/acceptance/tests/queue/QueueDispatchResult.kt
+++ ontrack-kdsl-acceptance/src/test/java/net/nemerosa/ontrack/kdsl/acceptance/tests/queue/QueueDispatchResult.kt
@@ -6,9 +6,11 @@ package net.nemerosa.ontrack.kdsl.acceptance.tests.queue
  * @property type Result of the dispatching
  * @property id Queue message ID is applicable
  * @property message Additional information
+ * @property routingKey Routing key which was used
  */
 data class QueueDispatchResult(
         val type: QueueDispatchResultType,
         val id: String?,
         val message: String? = null,
+        val routingKey: String? = null,
 )
