7c537cb991d3b902c2814a27cbbaea5cf3b9f9b3
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-03-29 17:08:52 +0100

    chore(v5): migration of ontrack-extension-api

--- ontrack-extension-api/build.gradle.kts
+++ ontrack-extension-api/build.gradle.kts
@@ -11,21 +11,21 @@ dependencies {
     implementation("org.apache.commons:commons-lang3")
 }
 
-val testJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("tests")
-    from(sourceSets["test"].output)
-}
-
-configure<PublishingExtension> {
-    publications {
-        maybeCreate<MavenPublication>("mavenCustom").artifact(tasks["testJar"])
-    }
-}
-
-tasks["assemble"].dependsOn("testJar")
-
-val tests by configurations.creating
-
-artifacts {
-    add("tests", testJar)
-}
+//val testJar by tasks.registering(Jar::class) {
+//    archiveClassifier.set("tests")
+//    from(sourceSets["test"].output)
+//}
+//
+//configure<PublishingExtension> {
+//    publications {
+//        maybeCreate<MavenPublication>("mavenCustom").artifact(tasks["testJar"])
+//    }
+//}
+//
+//tasks["assemble"].dependsOn("testJar")
+//
+//val tests by configurations.creating
+//
+//artifacts {
+//    add("tests", testJar)
+//}
