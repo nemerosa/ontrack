3a0e66ed031ff6e287bb25a4cb06bdea8f5297f4
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-03-30 17:58:11 +0200

    chore(v5): using Postgres 17 & Flyway adapted

--- ontrack-database/build.gradle.kts
+++ ontrack-database/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     implementation("org.springframework:spring-jdbc")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
     implementation(project(":ontrack-model"))
     implementation("org.slf4j:slf4j-api")
 }
