36b59ec9c48e76fa9c61fa5aecee66f28835363c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-04-06 21:02:01 +0200

    chore(v5): Gradle buildSrc cleanup

--- ontrack-web-core/build.gradle.kts
+++ ontrack-web-core/build.gradle.kts
@@ -4,7 +4,7 @@ import com.github.gradle.node.npm.task.NpmTask
 
 plugins {
     base
-    id("com.github.node-gradle.node")
+    id("com.github.node-gradle.node") version "7.1.0"
     id("com.bmuschko.docker-remote-api")
 }
 
