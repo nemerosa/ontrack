7fef012e0bf70c7d3639822c80ce12e1cc7f9f21
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-04-09 10:27:13 +0200

    chore(v5): restoring the RefDataProvider layer

--- ontrack-web-core/components/providers/RefDataProvider.js
+++ ontrack-web-core/components/providers/RefDataProvider.js
@@ -1,4 +1,8 @@
-import {createContext, useContext} from "react";
+import {createContext, useContext, useEffect, useState} from "react";
+import LoadingContainer from "@components/common/LoadingContainer";
+import {useQuery} from "@components/services/GraphQL";
+import {gql} from "graphql-request";
+import {useJobStates} from "@components/core/admin/jobs/JobState";
 
 const refDataSignature = {
     /**
@@ -48,83 +52,81 @@ export const useRefData = () => useContext(RefDataContext)
 
 export default function RefDataContextProvider({children}) {
 
-    // const {data, loading, error, finished} = useQuery(
-    //     gql`
-    //         query RefData {
-    //             validationRunStatusIDList {
-    //                 id
-    //                 name
-    //                 root
-    //                 passed
-    //                 followingStatuses
-    //             }
-    //             info {
-    //                 version {
-    //                     display
-    //                 }
-    //             }
-    //             eventTypes {
-    //                 id
-    //                 description
-    //             }
-    //             searchResultTypes {
-    //                 feature {
-    //                     id
-    //                 }
-    //                 id
-    //                 name
-    //                 description
-    //             }
-    //         }
-    //     `
-    // )
-    //
-    //
-    // const jobStates = useJobStates()
-    // const [refData, setRefData] = useState(refDataSignature)
+    const {data, loading, error, finished} = useQuery(
+        gql`
+            query RefData {
+                validationRunStatusIDList {
+                    id
+                    name
+                    root
+                    passed
+                    followingStatuses
+                }
+                info {
+                    version {
+                        display
+                    }
+                }
+                eventTypes {
+                    id
+                    description
+                }
+                searchResultTypes {
+                    feature {
+                        id
+                    }
+                    id
+                    name
+                    description
+                }
+            }
+        `
+    )
 
-    // const validationRunStatuses = (list) => {
-    //     // Indexing all validation run statuses
-    //     const index = {}
-    //     list.forEach(vrs => {
-    //         index[vrs.id] = vrs
-    //     })
-    //
-    //     //
-    //
-    //     const getAccessibleStatuses = (id) => {
-    //         const vrs = index[id]
-    //         if (vrs) {
-    //             return vrs.followingStatuses.map(fid => index[fid])
-    //         } else {
-    //             return []
-    //         }
-    //     }
-    //
-    //     // OK
-    //     return {
-    //         getAccessibleStatuses,
-    //         roots: list.filter(vrs => vrs.root),
-    //         list,
-    //     }
-    // }
-    //
-    // useEffect(() => {
-    //     if (data && finished) {
-    //         setRefData({
-    //             validationRunStatuses: validationRunStatuses(data.validationRunStatusIDList),
-    //             eventTypes: data.eventTypes,
-    //             jobStates: jobStates,
-    //             version: data.info.version.display,
-    //             searchResultTypes: data.searchResultTypes,
-    //         })
-    //     }
-    // }, [data, finished, jobStates])
 
-    // return <LoadingContainer loading={loading} error={error}>
-    //     <RefDataContext.Provider value={refData}>{children}</RefDataContext.Provider>
-    // </LoadingContainer>
+    const jobStates = useJobStates()
+    const [refData, setRefData] = useState(refDataSignature)
 
-    return children
+    const validationRunStatuses = (list) => {
+        // Indexing all validation run statuses
+        const index = {}
+        list.forEach(vrs => {
+            index[vrs.id] = vrs
+        })
+
+        //
+
+        const getAccessibleStatuses = (id) => {
+            const vrs = index[id]
+            if (vrs) {
+                return vrs.followingStatuses.map(fid => index[fid])
+            } else {
+                return []
+            }
+        }
+
+        // OK
+        return {
+            getAccessibleStatuses,
+            roots: list.filter(vrs => vrs.root),
+            list,
+        }
+    }
+
+    useEffect(() => {
+        if (data && finished) {
+            setRefData({
+                validationRunStatuses: validationRunStatuses(data.validationRunStatusIDList),
+                eventTypes: data.eventTypes,
+                jobStates: jobStates,
+                version: data.info.version.display,
+                searchResultTypes: data.searchResultTypes,
+            })
+        }
+    }, [data, finished, jobStates])
+
+    return <LoadingContainer loading={loading} error={error}>
+        <RefDataContext.Provider value={refData}>{children}</RefDataContext.Provider>
+    </LoadingContainer>
 
 }
