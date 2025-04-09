be1344eb28925e68e2eead687c94f94bbc389761
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-04-09 15:50:31 +0200

    chore(v5): tokens UI

--- ontrack-web-core/components/core/admin/userProfile/UserProfileTokens.js
+++ ontrack-web-core/components/core/admin/userProfile/UserProfileTokens.js
@@ -1,126 +1,33 @@
-import {Button, Form, Input, Popconfirm, Space, Spin, Table} from "antd";
-import {useEffect, useState} from "react";
-import {FaCog, FaCopy, FaTrash} from "react-icons/fa";
-import {useGraphQLClient} from "@components/providers/ConnectionContextProvider";
-import {gql} from "graphql-request";
+import {Button, Form, Input, Space, Table, Typography} from "antd";
+import {FaCog} from "react-icons/fa";
 import CheckStatus from "@components/common/CheckStatus";
 import TimestampText, {weekDayFormat} from "@components/common/TimestampText";
-import {getUserErrors} from "@components/services/graphql-utils";
 import FormErrors from "@components/form/FormErrors";
-import copy from 'copy-to-clipboard';
 import InlineConfirmCommand from "@components/common/InlineConfirmCommand";
+import {useRefresh} from "@components/common/RefreshUtils";
+import {useGenerateToken, useRevokeToken, useTokens} from "@components/core/admin/userProfile/TokensService";
 
 const {Column} = Table
 
 export default function UserProfileTokens() {
 
-    const client = useGraphQLClient()
+    const [refreshState, refresh] = useRefresh()
+
+    const {tokens, loading} = useTokens({refreshState})
 
     const [generateTokenForm] = Form.useForm()
-    const [generatingToken, setGeneratingToken] = useState(false)
-    const [errors, setErrors] = useState([])
 
-    const [tokensReloadCount, setTokensReloadCount] = useState(0)
-    const reloadTokens = () => {
-        setTokensReloadCount(tokensReloadCount + 1)
-    }
-    const [tokens, setTokens] = useState([])
-
-    const gqlToken = gql`
-        fragment TokenData on Token {
-            name
-            value
-            scope
-            creation
-            lastUsed
-            validUntil
-            valid
-            transient
-        }
-    `
-
-    useEffect(() => {
-        if (client) {
-            client.request(
-                gql`
-                    query UserTokens {
-                        user {
-                            account {
-                                id
-                                tokens {
-                                    ...TokenData
-                                }
-                            }
-                        }
-                    }
-                    ${gqlToken}
-                `
-            ).then(data => {
-                setTokens(data.user.account.tokens)
-            })
-        }
-    }, [client, tokensReloadCount]);
-
-    const [generatedToken, setGeneratedToken] = useState('')
-    const [generatedTokenCopied, setGeneratedTokenCopied] = useState(false)
-
-    const copyGeneratedToken = () => {
-        if (generatedToken) {
-            setGeneratedTokenCopied(copy(generatedToken))
-        }
-    }
-
-    const onGenerateToken = ({name}) => {
-        if (client && name) {
+    const {generateToken, data, loading: generatingToken, error} = useGenerateToken({refresh})
+    const onGenerateToken = async ({name}) => {
+        if (name) {
             generateTokenForm.resetFields()
-            setGeneratedToken('')
-            setGeneratedTokenCopied(false)
-            setGeneratingToken(true)
-            setErrors([])
-            client.request(
-                gql`
-                    mutation GenerateToken($name: String!) {
-                        generateToken(input: {name: $name}) {
-                            errors {
-                                message
-                            }
-                            token {
-                                ...TokenData
-                            }
-                        }
-                    }
-                    ${gqlToken}
-                `,
-                {name}
-            ).then(data => {
-                const errors = getUserErrors(data.generateToken)
-                if (errors) {
-                    setErrors(errors)
-                } else {
-                    reloadTokens()
-                    setGeneratedToken(data.generateToken.token.value)
-                }
-            }).finally(() => {
-                setGeneratingToken(false)
-            })
+            await generateToken({name})
         }
     }
 
-    const onRevokeToken = (token) => {
-        client.request(
-            gql`
-                mutation RevokeToken($name: String!) {
-                    revokeToken(input: {name: $name}) {
-                        errors {
-                            message
-                        }
-                    }
-                }
-            `,
-            {name: token.name}
-        ).then(() => {
-            reloadTokens()
-        })
+    const {revokeToken} = useRevokeToken({refresh})
+    const onRevokeToken = async (token) => {
+        await revokeToken({name: token.name})
     }
 
     return (
@@ -138,42 +45,28 @@ export default function UserProfileTokens() {
                             type="primary"
                             htmlType="submit"
                             disabled={generatingToken}
+                            loading={generatingToken}
                             icon={<FaCog/>}
                         >
-                            {
-                                generatingToken && <Spin/>
-                            }
                             Generate token
                         </Button>
                     </Form.Item>
                     {
-                        generatedToken &&
+                        data?.token?.value &&
                         <Form.Item>
                             <Space>
-                                <Input
-                                    style={{width: "32em"}}
-                                    disabled={true}
-                                    value={generatedToken}
-                                    data-testid="generatedToken"
-                                />
-                                <Button
-                                    type="default"
-                                    icon={<FaCopy/>}
-                                    title="Copies the generated token into the clipboard."
-                                    onClick={copyGeneratedToken}
-                                />
-                                {
-                                    generatedTokenCopied &&
-                                    <CheckStatus value={true} text="Copied!"/>
-                                }
+                                <Typography.Text code={true} copyable={true}>
+                                    {data.token.value}
+                                </Typography.Text>
                             </Space>
                         </Form.Item>
                     }
                 </Form>
-                <FormErrors errors={errors}/>
+                <FormErrors errors={error ? [error] : []}/>
                 <Table
                     dataSource={tokens}
                     pagination={false}
+                    loading={loading}
                 >
                     <Column
                         key="name"
