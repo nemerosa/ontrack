4f5e58e4e456c0cb5dd39d1b1bbc86e3ac4b58fc
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-05-30 08:32:28 +0200

    chore(v5): no longer using `ontrack.config.license.provider`

--- ontrack-extension-license/src/test/java/net/nemerosa/ontrack/extension/license/AbstractLicenseTestSupport.kt
+++ ontrack-extension-license/src/test/java/net/nemerosa/ontrack/extension/license/AbstractLicenseTestSupport.kt
@@ -2,13 +2,7 @@ package net.nemerosa.ontrack.extension.license
 
 import net.nemerosa.ontrack.it.AbstractDSLTestSupport
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.context.TestPropertySource
 
-@TestPropertySource(
-    properties = [
-        "ontrack.config.license.provider=fixed",
-    ]
-)
 abstract class AbstractLicenseTestSupport : AbstractDSLTestSupport() {
 
     @Autowired
