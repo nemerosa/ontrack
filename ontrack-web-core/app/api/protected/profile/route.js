7b8ac5f103ed48b4711ba0ebc952461f8b8457b6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-06-14 19:39:49 +0200

    chore(V5): dynamic route for the profile - fixing the export

--- ontrack-web-core/app/api/protected/profile/route.js
+++ ontrack-web-core/app/api/protected/profile/route.js
@@ -1,10 +1,10 @@
 import {NextResponse} from "next/server";
 
-export async function GET() {
+// Forcing the route to be dynamic
+// See https://nextjs.org/docs/13/app/building-your-application/data-fetching/fetching-caching-and-revalidating#multiple-fetch-requests
+export const dynamic = 'force-dynamic'
 
-    // Forcing the route to be dynamic
-    // See https://nextjs.org/docs/13/app/building-your-application/data-fetching/fetching-caching-and-revalidating#multiple-fetch-requests
-    export const dynamic = 'force-dynamic'
+export async function GET() {
 
     return NextResponse.json({
         auth: {
