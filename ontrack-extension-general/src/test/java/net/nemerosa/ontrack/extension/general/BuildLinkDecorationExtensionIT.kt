dd9d5479a7155f2b40fefd8205ce05c7228b7d17
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-06-14 09:25:45 +0200

    chore(V5): removal of legacy code

--- ontrack-extension-general/src/test/java/net/nemerosa/ontrack/extension/general/BuildLinkDecorationExtensionIT.kt
+++ ontrack-extension-general/src/test/java/net/nemerosa/ontrack/extension/general/BuildLinkDecorationExtensionIT.kt
@@ -1,12 +1,9 @@
 package net.nemerosa.ontrack.extension.general
 
 import net.nemerosa.ontrack.model.structure.Build
-import net.nemerosa.ontrack.test.TestUtils.uid
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import kotlin.test.assertEquals
-import kotlin.test.assertNull
-import kotlin.test.assertTrue
 
 class BuildLinkDecorationExtensionIT : AbstractGeneralExtensionTestSupport() {
 
@@ -19,89 +16,37 @@ class BuildLinkDecorationExtensionIT : AbstractGeneralExtensionTestSupport() {
             branch {
                 build {
                     val decorations = extension.getDecorations(this)
-                    assertTrue(decorations.isEmpty(), "No decoration")
+                    assertEquals(1, decorations.size)
+                    val decoration = decorations[0]
+                    val list = decoration.data
+                    assertEquals(0, list.linksCount)
                 }
             }
         }
     }
 
     @Test
-    fun `Only main links when main links are not configured`() {
-        val labelCategory = uid("c")
-        val labelName = uid("n")
-        withMainBuildLinksSettings {
-            setMainBuildLinksSettings()
-            val ref1 = project<Build> {
-                labels = listOf(label(labelCategory, labelName), label("something", "else"))
-                branch<Build> {
-                    build()
-                }
-            }
-            val ref2 = project<Build> {
-                labels = listOf(label(labelCategory, labelName))
-                branch<Build> {
-                    build()
-                }
-            }
-            project {
-                branch {
-                    build {
-                        linkTo(ref1)
-                        linkTo(ref2)
-                        val decorations = extension.getDecorations(this)
-                        assertEquals(1, decorations.size)
-                        val decoration = decorations[0]
-                        val list = decoration.data
-                        assertNull(list.extraLink, "No extra link")
-                        assertEquals(
-                                setOf(
-                                        ref1.name,
-                                        ref2.name
-                                ),
-                                list.decorations.map { it.build }.toSet()
-                        )
-                    }
-                }
+    fun `Recording the number of links`() {
+        val ref1 = project<Build> {
+            branch<Build> {
+                build()
             }
         }
-    }
-
-    @Test
-    fun `Only main links`() {
-        val labelCategory = uid("c")
-        val labelName = uid("n")
-        withMainBuildLinksSettings {
-            setMainBuildLinksSettings("$labelCategory:$labelName")
-            val ref1 = project<Build> {
-                labels = listOf(label(labelCategory, labelName), label("something", "else"))
-                branch<Build> {
-                    build()
-                }
+        val ref2 = project<Build> {
+            branch<Build> {
+                build()
             }
-            val ref2 = project<Build> {
-                labels = listOf(label(labelCategory, labelName))
-                branch<Build> {
-                    build()
-                }
-            }
-            project {
-                branch {
-                    build {
-                        linkTo(ref1)
-                        linkTo(ref2)
-                        val decorations = extension.getDecorations(this)
-                        assertEquals(1, decorations.size)
-                        val decoration = decorations[0]
-                        val list = decoration.data
-                        assertNull(list.extraLink, "No extra link")
-                        assertEquals(
-                                setOf(
-                                        ref1.name,
-                                        ref2.name
-                                ),
-                                list.decorations.map { it.build }.toSet()
-                        )
-                    }
+        }
+        project {
+            branch {
+                build {
+                    linkTo(ref1)
+                    linkTo(ref2)
+                    val decorations = extension.getDecorations(this)
+                    assertEquals(1, decorations.size)
+                    val decoration = decorations[0]
+                    val list = decoration.data
+                    assertEquals(2, list.linksCount)
                 }
             }
         }
