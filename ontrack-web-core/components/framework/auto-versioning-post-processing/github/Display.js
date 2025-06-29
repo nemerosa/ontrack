cfe97ea13ef2bebf1cf25bac13547d354ce460a9
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-06-29 15:36:47 +0200

    #1456 Missing GitHub display for AV post-processing config
    (cherry picked from commit 841ab13095a44447705aefa4ac075c4bc664932e)

---
+++ ontrack-web-core/components/framework/auto-versioning-post-processing/github/Display.js
@@ -0,0 +1,54 @@
+import {Descriptions, Space, Typography} from "antd";
+
+export default function Display({
+                                    dockerImage,
+                                    dockerCommand,
+                                    commitMessage,
+                                    config,
+                                    workflow,
+                                }) {
+    const items = [
+        {
+            key: 'dockerImage',
+            label: "Docker image",
+            children: <Typography.Text code>{dockerImage}</Typography.Text>,
+            span: 12,
+        },
+        {
+            key: 'dockerCommand',
+            label: "Docker command",
+            children: <Typography.Text code>{dockerCommand}</Typography.Text>,
+            span: 12,
+        },
+        {
+            key: 'commitMessage',
+            label: "Commit message",
+            children: <Typography.Text code>{commitMessage}</Typography.Text>,
+            span: 12,
+        },
+        {
+            key: 'config',
+            label: "Specific GitHub config",
+            children: <Typography.Text code>{config}</Typography.Text>,
+            span: 12,
+        },
+        {
+            key: 'workflow',
+            label: "Specific GitHub workflow job",
+            children: <Typography.Text code>{workflow}</Typography.Text>,
+            span: 12,
+        },
+    ]
+
+    return (
+        <>
+            <Space direction="vertical">
+                <Typography.Text code>github</Typography.Text>
+                <Descriptions
+                    items={items}
+                    span={12}
+                />
+            </Space>
+        </>
+    )
+}
