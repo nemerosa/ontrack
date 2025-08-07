61db39163c248f1787405044c4012d63bd63592f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-08-07 20:04:01 +0200

    chore(V5): big deprecation cleanup™ - IssueExportMoreThanOneGroupException

--- ontrack-extension-issues/src/main/java/net/nemerosa/ontrack/extension/issues/model/IssueServiceNotConfiguredException.java
+++
@@ -1,9 +0,0 @@
-package net.nemerosa.ontrack.extension.issues.model;
-
-import net.nemerosa.ontrack.common.BaseException;
-
-public class IssueServiceNotConfiguredException extends BaseException {
-    public IssueServiceNotConfiguredException() {
-        super("No issue service has been configured.");
-    }
-}
