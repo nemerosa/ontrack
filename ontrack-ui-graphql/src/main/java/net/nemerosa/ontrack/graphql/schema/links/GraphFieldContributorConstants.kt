9cfc4824ec143697fabc95141ac3a0ec67dff0e0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-08-22 16:19:17 +0200

    chore(V5): big deprecation cleanup™ - Graph UI

--- ontrack-ui-graphql/src/main/java/net/nemerosa/ontrack/graphql/schema/links/GraphFieldContributorConstants.kt
+++
@@ -1,7 +0,0 @@
-package net.nemerosa.ontrack.graphql.schema.links
-
-object GraphFieldContributorConstants {
-
-    const val DEPRECATION = "Graph UI will be dropped in 4.6 and `graph` fields in 5.0."
-
-}
