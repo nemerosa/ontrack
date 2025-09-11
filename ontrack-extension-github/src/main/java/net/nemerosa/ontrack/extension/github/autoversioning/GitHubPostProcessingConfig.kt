d4729be31e960887bdb8b310c29b9634542f71e6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-09-11 20:23:08 +0200

    #1476 GH AV post-processing: configuration should be able to override the repo, branch, retries & interval
    (cherry picked from commit 50b36707e81ec90dc0b3f14948750fcdb654600b)

--- ontrack-extension-github/src/main/java/net/nemerosa/ontrack/extension/github/autoversioning/GitHubPostProcessingConfig.kt
+++ ontrack-extension-github/src/main/java/net/nemerosa/ontrack/extension/github/autoversioning/GitHubPostProcessingConfig.kt
@@ -1,20 +1,27 @@
 package net.nemerosa.ontrack.extension.github.autoversioning
 
+import net.nemerosa.ontrack.graphql.support.ListRef
+import net.nemerosa.ontrack.model.annotations.APIDescription
+
 /**
  * Configuration of the post-processing for GitHub.
- *
- * @property dockerImage This image defines the environment for the upgrade command to run in
- * @property dockerCommand Command to run in the Docker container
- * @property commitMessage Commit message to use to commit and push the result of the post-processing
- * @property config GitHub configuration to use for the connection (optional, using defaults if not specified)
- * @property workflow If defined, name of the workflow in _this_ repository containing the post-processing (like `post-processing.yml`)
- * @property parameters List of extra parameters to pass to the workflow (optional, none by default)
  */
 data class GitHubPostProcessingConfig(
+    @APIDescription("This image defines the environment for the upgrade command to run in")
     val dockerImage: String,
+    @APIDescription("Command to run in the Docker container")
     val dockerCommand: String,
+    @APIDescription("Commit message to use to commit and push the result of the post-processing")
     val commitMessage: String,
+    @APIDescription("GitHub configuration to use for the connection (optional, using defaults if not specified)")
     val config: String?,
+    @APIDescription("GitHub repository (`owner/repo`). To be set to override the default settings.")
+    val repository: String?,
+    @APIDescription("If defined, name of the workflow in _this_ repository containing the post-processing (like `post-processing.yml`)")
     val workflow: String?,
+    @APIDescription("If defined, overrides the default settings for the branch to use when launching the workflow")
+    val branch: String?,
+    @APIDescription("List of extra parameters to pass to the workflow (optional, none by default)")
+    @ListRef
     val parameters: List<GitHubPostProcessingConfigParam> = emptyList(),
 )
--- ontrack-extension-github/src/main/java/net/nemerosa/ontrack/extension/github/autoversioning/GitHubPostProcessingConfigParam.kt
+++ ontrack-extension-github/src/main/java/net/nemerosa/ontrack/extension/github/autoversioning/GitHubPostProcessingConfigParam.kt
@@ -5,6 +5,6 @@ import net.nemerosa.ontrack.model.annotations.APIDescription
 data class GitHubPostProcessingConfigParam(
     @APIDescription("Name of the parameter")
     val name: String,
-    @APIDescription("Value of the parameter")
+    @APIDescription("Value of the parameter (can contain template expressions)")
     val value: String,
 )
