d68203b52973354c464a05e76bbedcfac0ae936d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-10-01 20:10:58 +0200

    #1489 Using the build display name in the favorite branches widget

--- ontrack-web-core/components/builds/LatestBuildBox.js
+++ ontrack-web-core/components/builds/LatestBuildBox.js
@@ -1,13 +1,14 @@
 import {Typography} from "antd";
 import TransitionBox from "@components/common/TransitionBox";
 import {buildLink} from "@components/common/Links";
+import BuildLink from "@components/builds/BuildLink";
 
 export default function LatestBuildBox({build}) {
     return (
         <>
             <TransitionBox
                 before={<Typography.Text type="secondary" italic>Latest</Typography.Text>}
-                after={buildLink(build)}
+                after={<BuildLink build={build}/>}
             />
         </>
     )
