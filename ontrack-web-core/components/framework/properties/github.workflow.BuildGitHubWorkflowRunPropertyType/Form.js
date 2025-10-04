e3e4e5efefc99c04eccefd2a604052027e46ba9a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-10-04 10:05:58 +0200

    fix(v5) - properties - GitHub workflow run

---
+++ ontrack-web-core/components/framework/properties/github.workflow.BuildGitHubWorkflowRunPropertyType/Form.js
@@ -0,0 +1,11 @@
+import {Form, Typography} from "antd";
+
+export default function PropertyForm() {
+    return (
+        <>
+            <Form.Item>
+                <Typography.Text>Not editable</Typography.Text>
+            </Form.Item>
+        </>
+    )
+}
