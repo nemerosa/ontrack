c428adde0e88ee0056cda920f0997cd65b6a9558
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-10-14 18:42:08 +0200

    #1493 Adapt the Bitbucket Server configuration to not use the UI but the API only
    (cherry picked from commit f909e8b9149a1305030658543bff09150bdd4234)
    
    # Conflicts:
    #	ontrack-extension-stash/src/main/java/net/nemerosa/ontrack/extension/stash/service/StashConfigurationServiceImpl.kt

--- ontrack-extension-stash/src/main/java/net/nemerosa/ontrack/extension/stash/service/StashConfigurationServiceImpl.kt
+++ ontrack-extension-stash/src/main/java/net/nemerosa/ontrack/extension/stash/service/StashConfigurationServiceImpl.kt
@@ -1,6 +1,6 @@
 package net.nemerosa.ontrack.extension.stash.service
 
-import com.fasterxml.jackson.databind.JsonNode
+import net.nemerosa.ontrack.extension.stash.client.BitbucketClientFactory
 import net.nemerosa.ontrack.extension.stash.model.StashConfiguration
 import net.nemerosa.ontrack.extension.support.AbstractConfigurationService
 import net.nemerosa.ontrack.model.events.EventFactory
@@ -10,47 +10,39 @@ import net.nemerosa.ontrack.model.security.SecurityService
 import net.nemerosa.ontrack.model.support.ConfigurationRepository
 import net.nemerosa.ontrack.model.support.ConnectionResult
 import net.nemerosa.ontrack.model.support.OntrackConfigProperties
-import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.client.RestTemplate
-import org.springframework.web.client.getForObject
 
 @Service
 @Transactional
 class StashConfigurationServiceImpl(
-    configurationRepository: ConfigurationRepository,
-    securityService: SecurityService,
-    encryptionService: EncryptionService,
-    eventPostService: EventPostService,
-    eventFactory: EventFactory,
-    ontrackConfigProperties: OntrackConfigProperties
+        configurationRepository: ConfigurationRepository,
+        securityService: SecurityService,
+        encryptionService: EncryptionService,
+        eventPostService: EventPostService,
+        eventFactory: EventFactory,
+        private val bitbucketClientFactory: BitbucketClientFactory,
+        ontrackConfigProperties: OntrackConfigProperties
 ) : AbstractConfigurationService<StashConfiguration>(
-    StashConfiguration::class.java,
-    configurationRepository,
-    securityService,
-    encryptionService,
-    eventPostService,
-    eventFactory,
-    ontrackConfigProperties
+    configurationClass = StashConfiguration::class.java,
+    configurationRepository = configurationRepository,
+    securityService = securityService,
+    encryptionService = encryptionService,
+    eventPostService = eventPostService,
+    eventFactory = eventFactory,
+    ontrackConfigProperties = ontrackConfigProperties
 ), StashConfigurationService {
 
     override val type: String = "bitbucket-server"
 
     override fun validate(configuration: StashConfiguration): ConnectionResult {
         return try {
-            val client = getHttpClient(configuration)
-            client.getForObject<JsonNode>("/projects")
+            val client = bitbucketClientFactory.getBitbucketClient(configuration)
+            client.projects
             ConnectionResult.ok()
         } catch (ex: Exception) {
             ConnectionResult.error(ex)
         }
     }
-
-    private fun getHttpClient(configuration: StashConfiguration): RestTemplate =
-        RestTemplateBuilder()
-            .rootUri(configuration.url)
-            .basicAuthentication(configuration.user, configuration.password)
-            .build()
 
 }
