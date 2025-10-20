afbc1a77825f22047e9702e7b260fd9f8d9e837e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-10-20 09:52:45 +0200

    chore(config) - fixing tests

--- ontrack-extension-github/src/test/java/net/nemerosa/ontrack/extension/github/config/GitHubSCMEngineTest.kt
+++ ontrack-extension-github/src/test/java/net/nemerosa/ontrack/extension/github/config/GitHubSCMEngineTest.kt
@@ -1,6 +1,10 @@
 package net.nemerosa.ontrack.extension.github.config
 
+import io.mockk.every
 import io.mockk.mockk
+import net.nemerosa.ontrack.extension.github.model.GitHubEngineConfiguration
+import net.nemerosa.ontrack.extension.github.service.GitHubConfigurationService
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
@@ -9,24 +13,55 @@ import kotlin.test.assertTrue
 
 class GitHubSCMEngineTest {
 
-    private val engine = GitHubSCMEngine(
-        propertyService = mockk(),
-        gitHubConfigurationService = mockk(),
-        gitSCMEngineHelper = mockk(),
-    )
+    private lateinit var gitHubConfigurationService: GitHubConfigurationService
+    private lateinit var engine: GitHubSCMEngine
+
+    @BeforeEach
+    fun before() {
+        gitHubConfigurationService = mockk()
+
+        engine = GitHubSCMEngine(
+            propertyService = mockk(),
+            gitHubConfigurationService = gitHubConfigurationService,
+            gitSCMEngineHelper = mockk(),
+        )
+    }
 
     @Test
-    fun `Detection of GitHub SCM engine`() {
+    fun `Detection of GitHub SCM engine when no matching config is found`() {
+        every { gitHubConfigurationService.configurations } returns emptyList()
         assertFalse(engine.matchesUrl("https://bitbucket.dev.yontrack.com/project/NEMEROSA/repository/YONTRACK"))
-        assertTrue(engine.matchesUrl("https://github.com/nemerosa/yontrack.git"))
+    }
+
+    @Test
+    fun `Detection of GitHub SCM engine when a matching config is found`() {
+        every { gitHubConfigurationService.configurations } returns listOf(
+            GitHubEngineConfiguration(
+                name = "Test",
+                url = "https://github.com",
+                oauth2Token = "xxx",
+            ),
+        )
         assertTrue(engine.matchesUrl("git@github.com:nemerosa/ontrack.git"))
     }
 
     @Test
-    fun `Detection of the GitHub repository`() {
+    fun `Detection of the GitHub repository when no matching config`() {
+        every { gitHubConfigurationService.configurations } returns emptyList()
         assertFailsWith<GitHubSCMRepositoryNotDetectedException> {
             engine.getGitHubRepository("https://bitbucket.dev.yontrack.com/project/NEMEROSA/repository/YONTRACK")
         }
+    }
+
+    @Test
+    fun `Detection of the GitHub repository when matching config`() {
+        every { gitHubConfigurationService.configurations } returns listOf(
+            GitHubEngineConfiguration(
+                name = "Test",
+                url = "https://github.com",
+                oauth2Token = "xxx",
+            ),
+        )
         assertEquals(
             "nemerosa/yontrack",
             engine.getGitHubRepository("https://github.com/nemerosa/yontrack.git")
