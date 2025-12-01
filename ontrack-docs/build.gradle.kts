0ccf1fd8214f17aad03e398ca4e540dc5e6f99dc
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-12-01 18:38:58 +0100

    doc: cleanup

--- ontrack-docs/build.gradle.kts
+++ ontrack-docs/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     `java-library`
-//    id("org.asciidoctor.jvm.convert") version "4.0.5"
-//    id("org.asciidoctor.jvm.pdf") version "4.0.5"
 }
 
 description = "Generation of the Yontrack documentation."
@@ -108,62 +106,3 @@ tasks.named("clean") {
 tasks.named("build") {
     dependsOn("buildDocs")
 }
-
-// ===== Asciidoc
-
-//asciidoctorj {
-//    setVersion("2.5.13")
-//    modules {
-//        diagram.use()
-//    }
-//}
-
-// HTML specific settings
-//tasks.named<AsciidoctorTask>("asciidoctor") {
-//    description = "Generates HTML documentation."
-//    attributes = mapOf(
-//        "ontrack-version" to version,
-//        "spring-boot-version" to (
-//                org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
-//                    .split(":")
-//                    .getOrNull(2)
-//                    ?.removeSuffix(")")
-//                    ?: "unknown"
-//                ),
-//        "icons" to "font"
-//    )
-//    logDocuments = true
-//    baseDirFollowsSourceDir()
-//    sources {
-//        include("index.adoc")
-//    }
-//}
-
-// PDF specific settings
-
-//tasks.named<AsciidoctorPdfTask>("asciidoctorPdf") {
-//    dependsOn("asciidoctor")
-//    description = "Generates PDF documentation."
-//    attributes = mapOf(
-//        "ontrack-version" to version,
-//        "spring-boot-version" to (
-//                org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
-//                    .split(":")
-//                    .getOrNull(2)
-//                    ?.removeSuffix(")")
-//                    ?: "unknown"
-//                ),
-//        "icons" to "font",
-//        "imagesdir" to file("build/docs/asciidoc")
-//    )
-//    logDocuments = true
-//    baseDirFollowsSourceDir()
-//    sources {
-//        include("index.adoc")
-//    }
-//}
-
-//tasks.named<Task>("build") {
-//    dependsOn("asciidoctor")
-//    dependsOn("asciidoctorPdf")
-//}
