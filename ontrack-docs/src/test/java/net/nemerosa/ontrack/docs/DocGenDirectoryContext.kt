cb42b1827221a7e30fa37c4728225185e75d51c7
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-12-02 17:51:12 +0100

    doc: cleanup

--- ontrack-docs/src/test/java/net/nemerosa/ontrack/docs/DocGenDirectoryContext.kt
+++ ontrack-docs/src/test/java/net/nemerosa/ontrack/docs/DocGenDirectoryContext.kt
@@ -41,26 +41,6 @@ class DocGenDirectoryContext(
         file.writeText(s.toString())
     }
 
-    @Deprecated("Use Markdown")
-    fun writeFile(
-        fileId: String,
-        fileName: String = fileId,
-        level: Int,
-        title: String,
-        code: (s: StringBuilder) -> Unit,
-    ) {
-        writeFile(
-            fileName = fileName,
-        ) { s ->
-            val fileTitle = "${(1..level).joinToString("") { "=" }} $title"
-
-            s.append("[[").append(fileId).append("]]\n")
-            s.append(fileTitle).append("\n").append("\n")
-
-            code(s)
-        }
-    }
-
     fun writeFile(
         fileId: String,
         fileName: String = fileId,
@@ -72,79 +52,6 @@ class DocGenDirectoryContext(
         ) { s ->
             s.title(title)
             code(s)
-        }
-    }
-
-    @Deprecated("Use inline generation of links for Markdown")
-    fun writeIndex(
-        fileId: String,
-        level: Int,
-        title: String,
-        items: Map<String, String>,
-    ) {
-        writeFile(
-            fileId = fileId,
-            fileName = "index",
-            level = level,
-            title = title,
-        ) { s ->
-            val sortedItems = items.toSortedMap()
-            sortedItems.forEach { (id, title) ->
-                s.append("* ").append("<<$id,$title>>\n")
-            }
-            s.append("\n")
-            sortedItems.forEach { (id, _) ->
-                s.append("include::$id.adoc[]\n")
-            }
-        }
-    }
-
-    @Deprecated("Use Markdown")
-    fun writeFile(
-        fileId: String,
-        level: Int,
-        title: String,
-        header: String?,
-        fields: List<FieldDocumentation>,
-        example: String?,
-        links: List<DocumentationLink> = emptyList(),
-        extendedHeader: (s: StringBuilder) -> Unit = {},
-        extendedConfig: (s: StringBuilder) -> Unit = {},
-    ) {
-        writeFile(
-            fileId = fileId,
-            level = level,
-            title = title,
-        ) { s ->
-
-            if (!header.isNullOrBlank()) {
-                s.append(header.trimIndent()).append("\n").append("\n")
-            }
-
-            extendedHeader(s)
-
-            if (links.isNotEmpty()) {
-                s.append("Links:\n\n")
-                links.forEach { link ->
-                    s.append("* <<").append(link.value).append(",").append(link.name).append(">>\n")
-                }
-                s.append("\n")
-            }
-
-            if (fields.isNotEmpty()) {
-                s.append("Configuration:\n\n")
-                writeFields(s, fields, 1)
-            }
-
-            extendedConfig(s)
-
-            if (!example.isNullOrBlank()) {
-                s.append("Example:").append("\n").append("\n")
-                s.append("[source]\n----\n")
-                s.append(example)
-                s.append("\n----\n")
-            }
-
         }
     }
 
