9c59181bbace0605dfa9304ab3eab194381451a0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2026-01-17 11:52:52 +0100

    #1510 Branch dependencies table - extracting GraphQL fragments

--- ontrack-web-core/components/links/BranchLinksGraph.js
+++ ontrack-web-core/components/links/BranchLinksGraph.js
@@ -8,7 +8,6 @@ import {
     useReactFlow
 } from "reactflow";
 import {useCallback, useContext, useEffect, useMemo, useState} from "react";
-import {gql} from "graphql-request";
 import BranchNode from "@components/links/BranchNode";
 import BranchLinkNode from "@components/links/BranchLinkNode";
 import {autoLayout} from "@components/links/GraphUtils";
@@ -16,145 +15,11 @@ import {useGraphQLClient} from "@components/providers/ConnectionContextProvider"
 import {Skeleton} from "antd";
 import {AutoRefreshContext} from "@components/common/AutoRefresh";
 import {edgeStyle} from "@components/links/LinksGraphConstants";
+import {branchQuery} from "@components/links/BranchDependenciesFragments";
 
 function BranchLinksFlow({branch}) {
 
     const client = useGraphQLClient()
-
-    const maxDownstreamDepth = 5
-    const maxUpstreamDepth = 5
-
-    const gqlBuildInfo = gql`
-        fragment BuildInfo on Build {
-            id
-            name
-            branch {
-                id
-                name
-                project {
-                    id
-                    name
-                }
-            }
-            releaseProperty {
-                value
-            }
-            creation {
-                time
-            }
-            promotionRuns(lastPerLevel: true) {
-                id
-                creation {
-                    time
-                }
-                promotionLevel {
-                    id
-                    description
-                    name
-                    image
-                }
-            }
-        }
-    `
-
-    const gqlBranchInfo = gql`
-        fragment BranchInfo on Branch {
-            id
-            name
-            displayName
-            project {
-                id
-                name
-            }
-        }
-    `
-
-    const gqlBranchNodeInfo = gql`
-        fragment BranchNodeInfo on Branch {
-            ...BranchInfo
-            latestBuilds: builds(count: 1) {
-                ...BuildInfo
-            }
-        }
-    `
-
-    const gqlDownstreamDependencies = (depth) => {
-        if (depth <= 0) {
-            return ''
-        } else {
-            return `
-                downstreamLinks(builds: 5) {
-                    qualifier
-                    sourceBuild {
-                        ...BuildInfo
-                    }
-                    targetBuild {
-                        ...BuildInfo
-                    }
-                    autoVersioning {
-                        lastEligibleBuild {
-                            ...BuildInfo
-                        }
-                        status {
-                            order {
-                                uuid
-                                targetVersion
-                            }
-                            running
-                            mostRecentState {
-                              state
-                              running
-                              processing
-                              creation {
-                                  time
-                              }
-                              data
-                            }
-                        }
-                    }
-                    branch {
-                        ...BranchNodeInfo
-                        ${gqlDownstreamDependencies(depth - 1)}
-                    }
-                }
-            `
-        }
-    }
-
-    const gqlUpstreamDependencies = (depth) => {
-        if (depth <= 0) {
-            return ''
-        } else {
-            return `
-                upstreamLinks(builds: 5) {
-                    qualifier
-                    sourceBuild {
-                        ...BuildInfo
-                    }
-                    targetBuild {
-                        ...BuildInfo
-                    }
-                    branch {
-                        ...BranchNodeInfo
-                        ${gqlUpstreamDependencies(depth - 1)}
-                    }
-                }
-            `
-        }
-    }
-
-    const branchQuery = `
-        query RootBranch($branchId: Int!) {
-            branch(id: $branchId) {
-                ...BranchNodeInfo
-                ${gqlDownstreamDependencies(maxDownstreamDepth)}
-                ${gqlUpstreamDependencies(maxUpstreamDepth)}
-            }
-        }
-        ${gqlBuildInfo}
-        ${gqlBranchInfo}
-        ${gqlBranchNodeInfo}
-    `
 
     const [nodes, setNodes] = useState([])
     const [edges, setEdges] = useState([])
