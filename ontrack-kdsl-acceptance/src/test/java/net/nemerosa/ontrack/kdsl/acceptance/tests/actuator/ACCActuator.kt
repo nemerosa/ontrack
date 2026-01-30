a2afa9d521ea1539cc2e3651c232576bd89f4ed1
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2026-01-30 12:30:58 +0100

    chore: metrics tags for application & application_instance

--- ontrack-kdsl-acceptance/src/test/java/net/nemerosa/ontrack/kdsl/acceptance/tests/actuator/ACCActuator.kt
+++ ontrack-kdsl-acceptance/src/test/java/net/nemerosa/ontrack/kdsl/acceptance/tests/actuator/ACCActuator.kt
@@ -2,6 +2,7 @@ package net.nemerosa.ontrack.kdsl.acceptance.tests.actuator
 
 import net.nemerosa.ontrack.kdsl.acceptance.tests.AbstractACCTestSupport
 import org.junit.jupiter.api.Test
+import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 class ACCActuator : AbstractACCTestSupport() {
@@ -10,6 +11,17 @@ class ACCActuator : AbstractACCTestSupport() {
     fun `Access to Prometheus metrics`() {
         val metrics = getMetrics()
         assertTrue(metrics.list.isNotEmpty(), "Metrics are available")
+        val record = metrics.list.first()
+        val value = record.values.first()
+        val tags = value.tags
+        assertNotNull(
+            tags.find { it.name == "application" && it.value == "ontrack" },
+            "Tag application is present and is equal to `ontrack`"
+        )
+        assertNotNull(
+            tags.find { it.name == "application_instance" && it.value == "ontrack" },
+            "Tag application_instance is present and is equal to `ontrack`"
+        )
     }
 
 }
